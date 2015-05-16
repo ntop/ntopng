5075ed4f2cba8a57f2496fd24ca9d688beb7b149
Arianna Avanzini <avanzini@ntop.org>
2015-05-16 21:42:00 +0200

    ntop_includes.h: fix include paths for pro version
    This commit adapts the include file with the correct include
    path according to latest changes in the pro version.

--- include/ntop_includes.h
+++ include/ntop_includes.h
@@ -162,8 +162,9 @@ extern "C" {
 #ifdef NTOPNG_PRO
 #include "NtopPro.h"
 #include "PacketBridge.h"
-#include "TokenBucket.h"
-#include "TokenBucketRing.h"
+#include "attic/TokenBucket.h"
+#include "attic/TokenBucketRing.h"
+#include "TrafficShaper.h"
 #include "L7Policer.h"
 #include "LuaHandler.h"
 #include "NagiosManager.h"
