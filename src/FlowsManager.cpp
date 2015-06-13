02c5904dc9601076400b6e585b9b06a8c0558fbe
Luca Deri <deri@ntop.org>
2015-06-13 17:44:02 +0200

    Fixed compativbility issue with scripts that do not add any select restriction

--- src/FlowsManager.cpp
+++ src/FlowsManager.cpp
@@ -22,37 +22,46 @@
 #include "ntop_includes.h"
 #include "FlowsManager.h"
 
-FlowsManager::FlowsManager(NetworkInterface *intf)
-{
+/* **************************************** */
+
+FlowsManager::FlowsManager(NetworkInterface *intf) {
   this->intf = intf;
 }
 
-static bool flows_select_walker(GenericHashEntry *h, void *user_data)
-{
+/* **************************************** */
+
+static bool flows_select_walker(GenericHashEntry *h, void *user_data) {
   struct flow_details_info *info = (struct flow_details_info*)user_data;
   Flow *flow = (Flow*)h;
 
   switch(info->field) {
-    case FF_NONE: break;
-    case FF_HOST: if((info->host != flow->get_cli_host())
-                     && (info->host != flow->get_srv_host()))
-                    return false;
-                  break;
-    case FF_CLIHOST: if (info->host != flow->get_cli_host())
-                       return false;
-                     break;
-    case FF_SRVHOST: if (info->host != flow->get_srv_host())
-                       return false;
-                     break;
-    case FF_PROTOCOL: if (info->protocol != flow->get_protocol())
-                        return false;
-                      break;
-    case FF_NDPIPROTOCOL: if (info->ndpi_protocol != flow->get_detected_protocol())
-                            return false;
-                          break;
-    default: ntop->getTrace()->traceEvent(TRACE_WARNING,
-               "Wrong value to flow selection (walker)");
-             return true;
+  case FF_NONE:
+    break;
+  case FF_HOST:
+    if((info->host != flow->get_cli_host())
+       && (info->host != flow->get_srv_host()))
+      return false;
+    break;
+  case FF_CLIHOST: 
+    if (info->host != flow->get_cli_host())
+      return false;
+    break;
+  case FF_SRVHOST: 
+    if (info->host != flow->get_srv_host())
+      return false;
+    break;
+  case FF_PROTOCOL: 
+    if (info->protocol != flow->get_protocol())
+      return false;
+    break;
+  case FF_NDPIPROTOCOL: 
+    if (info->ndpi_protocol != flow->get_detected_protocol())
+      return false;
+    break;
+  default: 
+    ntop->getTrace()->traceEvent(TRACE_WARNING,
+				 "Wrong value to flow selection (walker)");
+    return true;
   }
 
   flow->lua(info->vm, info->allowed_hosts, false /* Minimum details */);
@@ -60,37 +69,48 @@ static bool flows_select_walker(GenericHashEntry *h, void *user_data)
   info->count++;
   if (info->limit > 0 && info->count > info->limit)
     return true;
+
   return false; /* false = keep on walking */
 }
+
+/* **************************************** */
 
 void FlowsManager::select(lua_State* vm,
                           patricia_tree_t *allowed_hosts,
                           enum flowsField field,
                           void *value,
                           void *auxiliary_value,
-                          unsigned long limit)
-{
+                          unsigned long limit) {
   struct flow_details_info info;
-  info.vm = vm, info.allowed_hosts = allowed_hosts;
   char * host_ip;
   u_int vlan_id;
 
-  info.field = field;
+  memset(&info, 0, sizeof(info));
+  info.vm = vm, info.allowed_hosts = allowed_hosts, info.field = field;
 
   switch(field) {
-    case FF_HOST:
-    case FF_CLIHOST:
-    case FF_SRVHOST: host_ip = (char *)value;
-                     vlan_id = *((u_int *)auxiliary_value);
-                     info.host = intf->getHost(host_ip, vlan_id);
-                     break;
-    case FF_PROTOCOL: info.protocol = *((u_int8_t *)value);
-                      break;
-    case FF_NDPIPROTOCOL: info.ndpi_protocol = *((u_int16_t *)value);
-                          break;
-    default: ntop->getTrace()->traceEvent(TRACE_WARNING,
-               "Wrong value to flow selection");
-             return;
+  case FF_NONE:
+    break;
+
+  case FF_HOST:
+  case FF_CLIHOST:
+  case FF_SRVHOST:
+    host_ip = (char *)value;
+    vlan_id = *((u_int *)auxiliary_value);
+    info.host = intf->getHost(host_ip, vlan_id);
+    break;
+
+  case FF_PROTOCOL:
+    info.protocol = *((u_int8_t *)value);
+    break;
+
+  case FF_NDPIPROTOCOL: 
+    info.ndpi_protocol = *((u_int16_t *)value);
+    break;
+
+  default:
+    ntop->getTrace()->traceEvent(TRACE_WARNING,	"Wrong value to flow selection");
+    return;
   }
 
   if((field != FF_HOST && field != FF_CLIHOST && field != FF_SRVHOST) ||
