6aee51be257992ef336e2fa0c07450beecbcde40
Arianna Avanzini <avanzini@ntop.org>
2015-07-01 22:39:09 +0200

    flows_stats.lua: use per-network protocols in network historical
    This commit adds protocol visualization in the Historical tab
    of the network flows view, therefore enabling for better
    analysis of per-network traffic.

--- scripts/lua/flows_stats.lua
+++ scripts/lua/flows_stats.lua
@@ -219,8 +219,13 @@ end
 
 if (page == "historical" and _GET["network_name"] ~= nil) then
   local netname_format = string.gsub(_GET["network_name"], "_", "/")
-  local rrd_file = getPathFromKey(netname_format).."/bytes.rrd"
-  drawRRD(ifstats.id, nil, rrd_file, "1d", url.."&page=historical", 0, os.time() , "", nil)
+  local rrd_file = _GET["rrd_file"]
+  if (rrd_file == nil or rrd_file == "all") then
+    rrd_file = "all"
+  else
+    rrd_file = getPathFromKey(netname_format).."/"..rrd_file
+  end
+  drawRRD(ifstats.id, nil, rrd_file, "1d", url.."&page=historical", 1, os.time() , "", nil)
 end
 
 dofile(dirs.installdir .. "/scripts/lua/inc/footer.lua")
