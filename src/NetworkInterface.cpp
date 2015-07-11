ntop/ntopng

commit 6d6387e3be5e62d4f20c5eb68a3cfc977cc119e5

src/Lua.cpp: add possibility to get hosts info just for local hosts
This commit adds iterators to the Host class to the purpose of
allowing to get information just for local hosts. This also adds
Lua bindings to the Lua class so that scripts can access the
newly-added feature. This will help with issue #26.

src/NetworkInterface.cpp
@@ -1435,10 +1435,38 @@ static bool hosts_get_list_details(GenericHashEntry *h, void *user_data) {
 
 /* **************************************************** */
 
+static bool hosts_get_local_list(GenericHashEntry *h, void *user_data) {
+  struct vm_ptree *vp = (struct vm_ptree*)user_data;
+  IpAddress *ip = ((Host*)h)->get_ip();
+
+  if (ip && ((Host*)h)->isLocalHost())
+    ((Host*)h)->lua(vp->vm, vp->ptree, false, false, false);
+
+  return(false); /* false = keep on walking */
+}
+
+/* **************************************************** */
+
+static bool hosts_get_local_list_details(GenericHashEntry *h, void *user_data) {
+  struct vm_ptree *vp = (struct vm_ptree*)user_data;
+  IpAddress *ip = ((Host*)h)->get_ip();
+
+  if (ip && ((Host*)h)->isLocalHost())
+    ((Host*)h)->lua(vp->vm, vp->ptree, true, false, false);
+
+  return(false); /* false = keep on walking */
+}
+
+/* **************************************************** */
+
 void NetworkInterface::getActiveHostsList(lua_State* vm,
 					  vm_ptree *vp,
-					  bool host_details) {
-  hosts_hash->walk(host_details ? hosts_get_list_details : hosts_get_list, (void*)vp);
+					  bool host_details,
+					  bool local_only) {
+  if (local_only)
+    hosts_hash->walk(host_details ? hosts_get_local_list_details : hosts_get_local_list, (void*)vp);
+  else
+    hosts_hash->walk(host_details ? hosts_get_list_details : hosts_get_list, (void*)vp);
 }
 
 /* **************************************************** */

include/NetworkInterface.h
@@ -250,7 +250,7 @@ class NetworkInterface {
   void updateHostStats();
   virtual void lua(lua_State* vm);
   void getnDPIProtocols(lua_State *vm);
-  void getActiveHostsList(lua_State* vm, vm_ptree *vp, bool host_details);
+  void getActiveHostsList(lua_State* vm, vm_ptree *vp, bool host_details, bool local_only);
   void getFlowsStats(lua_State* vm);
   int  retrieve(lua_State* vm, patricia_tree_t *allowed_hosts, char *SQL);
   void getFlowPeersList(lua_State* vm, patricia_tree_t *allowed_hosts, char *numIP, u_int16_t vlanId);

include/NetworkInterfaceView.h
@@ -58,7 +58,7 @@ class NetworkInterfaceView {
 
   void flushHostContacts();
   void getnDPIStats(NdpiStats *stats);
-  void getActiveHostsList(lua_State* vm, patricia_tree_t *allowed_hosts, bool host_details);
+  void getActiveHostsList(lua_State* vm, patricia_tree_t *allowed_hosts, bool host_details, bool local_only);
   void getFlowsStats(lua_State* vm);
   void getActiveAggregatedHostsList(lua_State* vm, patricia_tree_t *allowed_hosts, u_int16_t proto_family, char *host);
   u_int getNumAggregatedHosts(void);

src/Lua.cpp
@@ -417,7 +417,24 @@ static int ntop_get_interface_hosts(lua_State* vm) {
 
   ntop->getTrace()->traceEvent(TRACE_INFO, "%s() called", __FUNCTION__);
 
-  if(ntop_interface) ntop_interface->getActiveHostsList(vm, get_allowed_nets(vm), false);
+  if(ntop_interface) ntop_interface->getActiveHostsList(vm, get_allowed_nets(vm), false, false);
+
+  return(CONST_LUA_OK);
+}
+
+/**
+ * @brief Get all local hosts of network interface.
+ * @details Get the ntop interface global variable of lua and return into lua stack a new hash table of local host information (Host name and number of bytes sent and received).
+ *
+ * @param vm The lua state.
+ * @return CONST_LUA_ERROR if ntop_interface is null, CONST_LUA_OK otherwise.
+ */
+static int ntop_get_interface_local_hosts(lua_State* vm) {
+  NetworkInterfaceView *ntop_interface = get_ntop_interface(vm);
+
+  ntop->getTrace()->traceEvent(TRACE_INFO, "%s() called", __FUNCTION__);
+
+  if(ntop_interface) ntop_interface->getActiveHostsList(vm, get_allowed_nets(vm), false, true);
 
   return(CONST_LUA_OK);
 }
@@ -443,7 +460,31 @@ static int ntop_get_interface_hosts_info(lua_State* vm) {
   else
     show_details = lua_toboolean(vm, 1) ? true : false;
 
-  if(ntop_interface) ntop_interface->getActiveHostsList(vm, get_allowed_nets(vm), show_details);
+  if(ntop_interface) ntop_interface->getActiveHostsList(vm, get_allowed_nets(vm), show_details, false);
+
+  return(CONST_LUA_OK);
+}
+
+/**
+ * @brief Get local hosts information of network interface.
+ * @details Get the ntop interface global variable of lua and return into lua stack a new hash table of hash tables containing the local host information.
+ *
+ * @param vm The lua state.
+ * @return CONST_LUA_ERROR if ntop_interface is null, CONST_LUA_OK otherwise.
+ */
+static int ntop_get_interface_local_hosts_info(lua_State* vm) {
+  NetworkInterfaceView *ntop_interface = get_ntop_interface(vm);
+  bool show_details;
+
+  ntop->getTrace()->traceEvent(TRACE_INFO, "%s() called", __FUNCTION__);
+
+  /* Optional */
+  if(lua_type(vm, 1) != LUA_TBOOLEAN)
+    show_details = true;
+  else
+    show_details = lua_toboolean(vm, 1) ? true : false;
+
+  if(ntop_interface) ntop_interface->getActiveHostsList(vm, get_allowed_nets(vm), show_details, true);
 
   return(CONST_LUA_OK);
 }
@@ -4197,7 +4238,9 @@ static const luaL_Reg ntop_interface_reg[] = {
   { "getNdpiFlowsCount",      ntop_get_ndpi_interface_flows_count },
   { "getNdpiProtoBreed",      ntop_get_ndpi_protocol_breed },
   { "getHosts",               ntop_get_interface_hosts },
+  { "getLocalHosts",          ntop_get_interface_local_hosts },
   { "getHostsInfo",           ntop_get_interface_hosts_info },
+  { "getLocalHostsInfo",      ntop_get_interface_local_hosts_info },
   { "getAggregatedHostsInfo", ntop_get_interface_aggregated_hosts_info },
   { "getAggregationFamilies", ntop_get_interface_aggregation_families },
   { "getNumAggregatedHosts",  ntop_get_interface_num_aggregated_hosts },

src/NetworkInterfaceView.cpp
@@ -173,7 +173,8 @@ void NetworkInterfaceView::getnDPIStats(NdpiStats *stats) {
 
 void NetworkInterfaceView::getActiveHostsList(lua_State* vm,
 					      patricia_tree_t *allowed_hosts,
-					      bool host_details) {
+					      bool host_details,
+					      bool local_only) {
   struct vm_ptree vp;
   list<NetworkInterface *>::iterator p;
 
@@ -181,7 +182,7 @@ void NetworkInterfaceView::getActiveHostsList(lua_State* vm,
 
   lua_newtable(vm);
   for(p = physIntf.begin() ; p != physIntf.end() ; p++)
-    (*p)->getActiveHostsList(vm, &vp, host_details);
+    (*p)->getActiveHostsList(vm, &vp, host_details, local_only);
 }
 
 /* **************************************************** */

commit e0e8fc225b48610edaacfaeea7e41fe095a62f83

src/NetworkInterface.cpp: remove unused local variable
This commit just removes the definition and initialization of
an unused variable.

Fixes: 6d6387e ("src/Lua.cpp: add possibility to get hosts info just for local hosts")

src/NetworkInterface.cpp
@@ -1437,9 +1437,8 @@ static bool hosts_get_list_details(GenericHashEntry *h, void *user_data) {
 
 static bool hosts_get_local_list(GenericHashEntry *h, void *user_data) {
   struct vm_ptree *vp = (struct vm_ptree*)user_data;
-  IpAddress *ip = ((Host*)h)->get_ip();
 
-  if (ip && ((Host*)h)->isLocalHost())
+  if (((Host*)h)->isLocalHost())
     ((Host*)h)->lua(vp->vm, vp->ptree, false, false, false);
 
   return(false); /* false = keep on walking */
@@ -1449,9 +1448,8 @@ static bool hosts_get_local_list(GenericHashEntry *h, void *user_data) {
 
 static bool hosts_get_local_list_details(GenericHashEntry *h, void *user_data) {
   struct vm_ptree *vp = (struct vm_ptree*)user_data;
-  IpAddress *ip = ((Host*)h)->get_ip();
 
-  if (ip && ((Host*)h)->isLocalHost())
+  if (((Host*)h)->isLocalHost())
     ((Host*)h)->lua(vp->vm, vp->ptree, true, false, false);
 
   return(false); /* false = keep on walking */