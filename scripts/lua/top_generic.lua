1ef7b312aa699037661868d8a0487431a9b4bc75
Luca Deri <deri@ntop.org>
2015-08-15 00:51:57 +0200

    Fix for returning the correct top talker with respect to the specified epoch

--- scripts/lua/top_generic.lua
+++ scripts/lua/top_generic.lua
@@ -10,7 +10,7 @@ require "top_talkers"
 sendHTTPHeader('text/html; charset=iso-8859-1')
 
 ifid = getInterfaceId(ifname)
-epoch = _GET["epoch"]
+epoch = _GET["epoch"]+60 -- we return the minute before the event as epochs are stored in the DB 'past' the time period
 module = _GET["m"]
 param = _GET["param"]
 mode = _GET["mode"]
