197283c2a88fe41d9086e2957962785dcfe0366c
Simone Mainardi <simonemainardi@gmail.com>
2016-01-09 18:05:30 +0100

    Returns direct pcap download link

--- scripts/lua/get_nbox_data.lua
+++ scripts/lua/get_nbox_data.lua
@@ -7,6 +7,7 @@ package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 
 require "lua_utils"
 
+local json = require ("dkjson")
 
 interface.select(ifname)
 
@@ -16,6 +17,15 @@ local nbox_password = ntop.getCache("ntopng.prefs.nbox_password")
 if((nbox_host == nil) or (nbox_host == "")) then nbox_host = "localhost" end
 if((nbox_user == nil) or (nbox_user == "")) then nbox_user = "nbox" end
 if((nbox_password == nil) or (nbox_password == "")) then nbox_password = "nbox" end
+
+local base_url = "https://"..nbox_host
+
+local status_url = base_url.."/ntop-bin/check_status_tasks_external.cgi"
+local schedule_url = base_url.."/ntop-bin/sudowrapper_external.cgi?script=npcapextract_external.cgi"
+local activity_scheduler_url = base_url.."/ntop-bin/config_scheduler.cgi"
+local download_url = base_url.."/ntop-bin/sudowrapper.cgi"
+download_url = download_url.."?script=n2disk_filemanager.cgi&opt=download_pcap&dir=/storage/n2disk/&pcap_name=/storage/n2disk/"
+
 
 -- Table parameters
 action     = _GET["action"]
@@ -37,8 +47,6 @@ end
 if action == nil then
 	return "{}"
 elseif action == "schedule" then
-	local schedule_url = "https://"..nbox_host
-	schedule_url = schedule_url.."/ntop-bin/sudowrapper_external.cgi?script=npcapextract_external.cgi"
 	schedule_url = schedule_url.."&ifname="..ifname.."&begin="..epoch_begin.."&end="..epoch_end
 	schedule_url = schedule_url..createBPF()
 	--io.write(schedule_url..'\n')
@@ -51,8 +59,6 @@ elseif action == "schedule" then
 		print("{}")
 	end
 elseif action == "status" then
-	local status_url = "https://"..nbox_host
-	status_url = status_url.."/ntop-bin/check_status_tasks_external.cgi"
 	local resp = ntop.httpGet(status_url, nbox_user, nbox_password, 10)
 	--tprint(resp)
 	sendHTTPHeader('text/html; charset=iso-8859-1')
@@ -64,25 +70,27 @@ elseif action == "status" then
 		content = string.gsub(content, "%s*","")
 		content = string.gsub(content, "{%s*{","[{")
 		content = string.gsub(content, "}%s*}","}]")
-		--tprint(content)
-		print(content)
+		content = json.decode(content, 1, nil)
+		if content == nil or content["tasks"] == nil then
+			print('{"tasks":[]}')
+		else
+			for task_id, task in pairs(content["tasks"]) do
+				task["actions"] = ""
+				if task["status"] ~= "scheduled" then
+					task["actions"] = 
+					'<a href="'..download_url..task["task_id"]..'.pcap"><i class="fa fa-download fa-lg"></i></a> '
+				end
+				task["actions"] = task["actions"]..'<a href="'..activity_scheduler_url..'" target="_blank"><i class="fa fa-external-link fa-lg"></i></a> '
+			end
+			--tprint(content)
+			print(json.encode(content, nil))
+		end
+
 	else
 		print('{"tasks":[]}')
 	end
-elseif action == "download" then
-	local download_url = "https://"..nbox_host
-	download_url = download_url.."/ntop-bin/sudowrapper.cgi"
-	download_url = download_url.."?script=n2disk_filemanager.cgi&opt=download_pcap&dir=/storage/n2disk/&pcap_name=/storage/n2disk/"
-	download_url = download_url..task_id..".pcap"
-	local resp = ntop.httpGet(download_url, nbox_user, nbox_password, 10)
-	-- tprint(resp)
-	if resp ~= nil and resp["CONTENT"] ~= nil then
-		sendHTTPHeader(resp["CONTENT_TYPE"])
-		print(resp["CONTENT"])
-	else
-		sendHTTPHeader('text/html; charset=iso-8859-1')
-		print("{}")
-	end
+else
+	print("{}")
 end
 
 
