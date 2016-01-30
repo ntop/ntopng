21329270e89eeaeb0673fe9c0ae6b09f8ae99f73
Simone Mainardi <simonemainardi@gmail.com>
2016-01-30 17:24:04 +0100

    Fixes compilation errors on FreeBSD 8.1
    The following errors have been fixed
    In file included from src/Redis.cpp:26:
    /usr/src/etc/ntopng/third-party/hiredis/net.c: In function 'int redisKeepAlive(redisContext*, int)':
    /usr/src/etc/ntopng/third-party/hiredis/net.c:136: error: 'TCP_KEEPIDLE' was not declared in this scope
    /usr/src/etc/ntopng/third-party/hiredis/net.c:143: error: 'TCP_KEEPINTVL' was not declared in this scope
    /usr/src/etc/ntopng/third-party/hiredis/net.c:149: error: 'TCP_KEEPCNT' was not declared in this scope
    gmake: *** [src/Redis.o] Error 1

--- third-party/hiredis/net.c
+++ third-party/hiredis/net.c
@@ -132,24 +132,32 @@ int redisKeepAlive(redisContext *c, int interval) {
 #elif defined (__OpenBSD__)
     /* DO NOTHING */
 #else
+
+#ifdef TCP_KEEPIDLE
     val = interval;
     if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPIDLE, &val, sizeof(val)) < 0) {
-        __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
-        return REDIS_ERR;
+      __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
+      return REDIS_ERR;
     }
+#endif
 
+#ifdef TCP_KEEPINTVL
     val = interval/3;
     if (val == 0) val = 1;
     if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPINTVL, &val, sizeof(val)) < 0) {
-        __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
-        return REDIS_ERR;
+      __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
+      return REDIS_ERR;
     }
+#endif
 
+#ifdef TCP_KEEPCNT
     val = 3;
     if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPCNT, &val, sizeof(val)) < 0) {
-        __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
-        return REDIS_ERR;
+      __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
+      return REDIS_ERR;
     }
+#endif
+
 #endif
 
     return REDIS_OK;
