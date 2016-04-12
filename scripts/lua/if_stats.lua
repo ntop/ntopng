834c551b20d46f0a0ee99bb4ce89ac8ca29ceb8f
Simone Mainardi <simonemainardi@gmail.com>
2016-04-12 13:02:46 +0200

    Handles user-overridden interface speeds in if_stats
    Fixes #473

--- scripts/lua/if_stats.lua
+++ scripts/lua/if_stats.lua
@@ -277,7 +277,12 @@ if((page == "overview") or (page == nil)) then
    end
 
    if(ifstats["remote.name"] == nil) then
-     print("<tr><th width=250>Speed</th><td colspan=2>" .. maxRateToString(ifstats.speed*1000) .. "</td><th>MTU</th><td colspan=3>"..ifstats.mtu.." bytes</td></tr>\n")
+      local speed_key = 'ntopng.prefs.'..ifname..'.speed'
+      local speed = ntop.getCache(speed_key)
+      if speed == nil or speed == "" or tonumber(speed) == nil then
+	 speed = ifstats.speed
+      end
+      print("<tr><th width=250>Speed</th><td colspan=2>" .. maxRateToString(speed*1000) .. "</td><th>MTU</th><td colspan=3>"..ifstats.mtu.." bytes</td></tr>\n")
    end
 
    if(ifstats.ip_addresses ~= "") then
