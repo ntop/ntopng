b833496be4cf20eff4cf8e7cf2f5ca943cccc328
Luca Deri <deri@ntop.org>
2016-08-24 23:58:17 +0200

    Workaround until activities are fixes

--- src/NetworkInterface.cpp
+++ src/NetworkInterface.cpp
@@ -3985,6 +3985,8 @@ int NetworkInterface::luaEvalFlow(Flow *f, const LuaCallback cb) {
   lua_State *L;
   const char *luaFunction;
 
+  return(0); /* FIX */
+
   if(reloadLuaInterpreter) {
     if(L_flow_create || L_flow_delete || L_flow_update) termLuaInterpreter();
     L_flow_create = initLuaInterpreter(CONST_FLOWACTIVITY_SCRIPT);
