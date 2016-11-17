01762e99ec260d8ca22ad1d09c033daf06423e66
Luca Deri <deri@ntop.org>
2016-11-17 09:00:19 +0100

    Blackist management code

---
+++ scripts/lua/modules/blacklist_utils.lua
@@ -0,0 +1,45 @@
+--
+-- (C) 2016 - ntop.org
+--
+
+local blacklistURLs = {
+   "https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt"
+}
+
+-- ##################################################################
+
+local function loadBlackListFromURL(url)
+   local resp = ntop.httpGet(url)
+
+   if(resp ~= nil) then
+      local content = resp["CONTENT"]
+      local line
+      local lines = string.split(content, "\n")
+
+      for _,line in pairs(lines) do
+	 line = trimSpace(line)
+	 if((string.len(line) > 0) and not(string.starts(line, "#"))) then
+	    -- print("Loading "..line.."\n")
+	    ntop.addToHostBlacklist(line)
+	 end
+      end
+   end
+end
+
+-- ##################################################################
+
+function loadHostBlackList()
+   local bl = ntop.getCache("ntopng.prefs.host_blacklist")
+
+   if(bl == "enabled") then
+      ntop.allocHostBlacklist()
+      
+      for _,url in pairs(blacklistURLs) do
+	 loadBlackListFromURL(url)
+      end
+      
+      ntop.swapHostBlacklist()
+   end
+end
+
+
