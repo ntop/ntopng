0f448fffc7d5d02c347e7f2f4f1fd6811584d5a1
emanuele-f <black.silver@hotmail.it>
2016-12-10 19:00:31 +0100

    Flow alerts explorer integration and LUA alerts query unification

--- scripts/lua/get_num_alerts.lua
+++ scripts/lua/get_num_alerts.lua
@@ -13,11 +13,6 @@ interface.select(ifname)
 sendHTTPHeader('text/html; charset=iso-8859-1')
 
 if(_GET["status"] ~= nil) then
-   local res = performAlertsQuery("SELECT COUNT(*) AS count", _GET["status"], UrlToalertsQueryParameters(_GET))
-
-   if((res ~= nil) and (#res == 1) and (res[1].count ~= nil)) then
-      print(res[1].count)
-   end
-
-   return
+   local num = getNumAlerts(_GET["status"], UrlToalertsQueryParameters(_GET))
+   print(tostring(num))
 end
