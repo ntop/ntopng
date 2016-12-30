363fa6da44fc67f9463e2a7219083b686b319e15
Simone Mainardi <mainardi@ntop.org>
2016-12-30 18:11:23 +0100

    Makes the search host glyphicon a submit button

--- scripts/lua/inc/search_host_box.lua
+++ scripts/lua/inc/search_host_box.lua
@@ -3,7 +3,7 @@
 --
 
 print [[
-	 <li><form action="/lua/host_details.lua">
+	 <li><form action="/lua/host_details.lua" id="search-host-form" data-toggle="validator">
 ]]
 
 -- FIX: show notifications to the user
@@ -11,15 +11,19 @@ print [[
 
 print [[
 
-
 	 <div class="control-group" style="width:15em;">
-          <div class="input-group"><span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
-   <input id="search_host_ip" type="hidden" name="host" />
-	 <input id="search_typeahead" type="text" class="form-control search-query span2" placeholder="Search Host" data-provide="typeahead" autocomplete="off"></input>
-	 </div>
+           <div class="input-group">
+             <span class="input-group-btn">
+               <button class="btn btn-default" type="submit" id="search-submit">
+                 <span class="glyphicon glyphicon-search"></span>
+               </button>
+             </span>
+             <input id="search_host_ip" type="hidden" name="host"/>
+	     <input id="search_typeahead" type="text" data-minlength="1" class="form-control search-query span2" placeholder="Search Host" data-provide="typeahead" autocomplete="off"></input>
+	   </div>
          </div>
-
 	 </form>
+
 	 </li>
 
 	 <script type='text/javascript'>
@@ -34,6 +38,21 @@ print [[/lua/find_host.lua', { query: query }, function (data) {
       $("#search_host_ip").val(item.ip);
 	 }
 	});
+
+
+  $('#search-submit').click(function(e) {
+    if ($('#search_host_ip').val() === '') {
+      /* No typeahead result has been selected by the user */
+      if($('#search_typeahead').val() === '') {
+        /* Do not submit if also the typeahead content is empty */
+        e.preventDefault();
+      } else {
+        /* Populate the search host ip with the user submitted values */
+        $("#search_host_ip").val($('#search_typeahead').val())
+      }
+    }
+});
+
 	</script>
 
    ]]
