4543ab092c9bc3aaf6ad539a5c5d1ae417af6399
Bogdan Perian <bperian@siscale.com>
2017-04-12 00:24:40 +0300

    Code revision for issue #1144 (#1147)
    * Logstash code cleanup
    
    * removed unnecesary trace warnings from LS code
    
    * sync
    
    * Logstash readme updated + code cleanup
    
    * changed back to default bulk mechanism
    
    * Logstash extension code improvements
    
    * PR #1143 revision
    
    * logstash event timestamp addition to json
    
    * avoid control char 0
    
    * Testing json output without memset
    
    * null byte
    
    * change init for vars
    
    * changed length of data sent on socket
    
    * fix logstash timestamp field conflict
    
    * revert debug trace
    
    * fix for undefined val in case of LS export
    
    * fix code logic
    
    * revert+cleanup
    
    * save original timestamp for flow
    
    * Use ntopng provided timestamp for timeline (Logstash)
    
    * Clear redundant timestamp value

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -1723,16 +1723,28 @@ json_object* Flow::flow2json() {
 
   if((my_object = json_object_new_object()) == NULL) return(NULL);
 
-  if(ntop->getPrefs()->do_dump_flows_on_es()) {
+  if(ntop->getPrefs()->do_dump_flows_on_es()
+    || ntop->getPrefs()->do_dump_flows_on_ls()
+    ) {
     struct tm* tm_info;
 
     t = last_seen;
     tm_info = gmtime(&t);
 
     strftime(buf, sizeof(buf), "%FT%T.0Z", tm_info);
-    json_object_object_add(my_object, "@timestamp", json_object_new_string(buf));
+
+    if(ntop->getPrefs()->do_dump_flows_on_ls()){
+      /*  Add current timestamp differently for Logstash, in case of delay 
+       *  Note: Logstash generates it's own @timestamp field on input
+       */
+      json_object_object_add(my_object,"ntop_timestamp",json_object_new_string(buf));
+    }
+
+    if(ntop->getPrefs()->do_dump_flows_on_es()){
+      json_object_object_add(my_object, "@timestamp", json_object_new_string(buf));
+      json_object_object_add(my_object, "type", json_object_new_string(ntop->getPrefs()->get_es_type()));
+    }
     /* json_object_object_add(my_object, "@version", json_object_new_int(1)); */
-    json_object_object_add(my_object, "type", json_object_new_string(ntop->getPrefs()->get_es_type()));
 
     // MAC addresses are set only when dumping to ES to optimize space consumption
     json_object_object_add(my_object, Utils::jsonLabel(IN_SRC_MAC, "IN_SRC_MAC", jsonbuf, sizeof(jsonbuf)),
