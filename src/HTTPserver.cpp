ae0556b874999b85d5a648b4e965449f00e071fb
Luca Deri <deri@ntop.org>
2017-05-28 22:13:39 +0200

    Minor optimization to avoid scanning a list twice

--- src/HTTPserver.cpp
+++ src/HTTPserver.cpp
@@ -325,11 +325,14 @@ static int is_authorized(const struct mg_connection *conn,
 /* ****************************************** */
 
 static int isCaptiveConnection(struct mg_connection *conn) {
+  char *host = (char*)mg_get_header(conn, "Host");
+  
   return(ntop->getPrefs()->isCaptivePortalEnabled()
 	 && (ntohs(conn->client.lsa.sin.sin_port) == 80)
-	 && (mg_get_header(conn, "Host"))
-	 && (strcasestr((char*)mg_get_header(conn, "Host"), CONST_HELLO_HOST) == NULL)
-	 && (ntop->getPrefs()->get_alt_http_port() != 0));
+	 && (ntop->getPrefs()->get_alt_http_port() != 0)
+	 && host
+	 && (strcasestr(host, CONST_HELLO_HOST) == NULL)
+	 );
 }
 
 /* ****************************************** */
