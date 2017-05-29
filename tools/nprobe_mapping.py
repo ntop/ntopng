74ad513a1164cbe111f92a445259912a16925d1f
emanuele-f <black.silver@hotmail.it>
2017-05-29 16:51:14 +0200

    Update nProbe field mappings and provide a script to automate the process

---
+++ tools/nprobe_mapping.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# A script to map nProbe fields to the ParserInterface.cpp and to the flow_utils.lua
+#
+
+import sys
+import os
+import re
+
+def usage():
+  print("""Usage: %s nprobe_help_filtered_file [indentation_level] [--c-output]
+
+  Example invocations:
+    - For ParserInterface.cpp:
+      nprobe -h | python3 ./nprobe_mapping.py - 4
+
+    - flow_utils.lua:
+      nprobe -h | python3 ./nprobe_mapping.py - 2 --c-output""" % (os.path.basename(sys.argv[0])))
+  exit(1)
+
+if len(sys.argv) < 2:
+  usage()
+
+if len(sys.argv) >= 3:
+  indentation = " " * int(sys.argv[2])
+else:
+  indentation = "    "
+
+if len(sys.argv) >= 4 and sys.argv[3] == "--c-output":
+  c_output = True
+else:
+  c_output = False
+
+# ------------------------------------------------------------------------------
+
+if sys.argv[1] == "-":
+  fin = sys.stdin
+else:
+  fin = open(sys.argv[1])
+
+start_ok = False
+
+
+for line in fin:
+  lstripped = line.strip()
+
+  if lstripped == "------------------------------------------------------------------------------":
+    # Start parsing output
+    start_ok = True
+  elif start_ok:
+    if lstripped.startswith("Major protocol (%L7_PROTO) symbolic mapping"):
+      break
+
+    pattern = re.search('^([^:]+):$', lstripped)
+    if pattern:
+      # This is a section delimiter
+      label = pattern.groups()[0]
+      label = label.lstrip("Plugin ").rstrip("templates").strip()
+      if not c_output:
+        print("")
+        print(indentation + "-- " + label)
+    else:
+      # This is a line into a section
+      pattern = re.search('^\[([^\]]+)\](\[([^\]]+)\]){0,1}\s+%(\w+)\s+(%(\w+)\s+){0,1}(.*)$', lstripped)
+      if pattern:
+        (netflow_id, _, ipfix_id, netflow_label, _, ipfix_label, description) = pattern.groups()
+        if not c_output:
+          print('%s["%s"] = "%s",' % (indentation, netflow_label, description))
+        else:
+          parts = netflow_id.split(" ")
+          idx = parts[len(parts) - 1]
+          print('%saddMapping("%s", %s);' % (indentation, netflow_label, idx))
+
+fin.close()
