c2762fb2eef2ccda2ad3ae4d041934a6ba2d6661
emanuele-f <black.silver@hotmail.it>
2017-06-07 16:29:11 +0200

    Add a tool to check for global variables in lua scripts

---
+++ tools/find_globals.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# A script to scan lua files for global variables.
+# Global variables should be avoided as they are a potentially source of issues
+# and reduce the overall performance.
+#
+
+# Go into the scripts dir
+if [[ -d "tools" ]]; then
+  cd tools
+fi
+
+if [[ ! -f globals.lua ]]; then
+  echo "Cannot find globals.lua script" >&2
+  exit 1
+fi
+
+luac_cmd=
+luac_ver=
+
+function check_ver {
+  luac_cmd="`which $1`"
+
+  if [[ $? -eq 0 ]]; then
+    luac_ver="`$luac_cmd -v 2>&1 | cut -f2 -d' '`"
+
+    if [[ "${luac_ver#5.2}" == "$luac_ver" ]]; then
+      # Bad version
+      luac_ver=
+    fi
+  else
+    luac_ver=
+  fi
+}
+
+# Locate the lua executable
+check_ver "luac"
+if [[ "$luac_ver" == "" ]]; then check_ver "luac5.2"; fi
+if [[ "$luac_ver" == "" ]]; then check_ver "luac5"; fi
+
+if [[ "$luac_ver" == "" ]]; then
+  echo "Required luac version 5.2 not found" >&2
+  exit 1
+fi
+
+lua_cmd="`which lua`"
+
+if [[ $? -ne 0 ]]; then
+  echo "Cannot find LUA interpreter" >&2
+  exit 1
+fi
+
+# Locate lua sources, following symbolic links
+for f in `find -L ../scripts -name "*.lua"`; do
+  has_errors=0
+
+  while read line; do
+    # a little formatting here
+    lineno=`echo $line | cut -f1 -d" "`
+    field=`echo $line | cut -f2 -d" "`
+    sym="${line##* }"
+
+    if [[ $field == "GETTABUP" ]]; then
+      echo -e "\tline${lineno} GETGLOBAL ${sym}"
+    elif [[ $field == "SETTABUP" ]]; then
+      echo -e "\tline${lineno} SETGLOBAL ${sym}"
+    else
+      # exclude first line
+      if [[ $has_errors -ne 0 ]]; then echo; fi
+      echo $line
+    fi
+
+    has_errors=1
+  done < <($luac_cmd -l -p "$f" | $lua_cmd globals.lua "$f")
+
+  # todo exit on error, the below does not work
+  # if [[ $? -ne 0 ]]; then exit 1; fi
+
+  if [[ $has_errors -ne 0 ]]; then
+    exit 1
+  fi
+done
+
+echo "No errors detected"
---
+++ tools/globals.lua
@@ -0,0 +1,178 @@
+
+--[[
+
+globals.lua (FindGlobals), a useful script to find global variable access in 
+.lua files, placed in the public domain by Mikk in 2009. 
+
+HOW TO INVOKE:
+  luac -l MyFile.lua | lua globals.lua MyFile.lua
+or:
+  c:\path\to\luac.exe -l MyFile.lua | c:\path\to\lua.exe c:\path\to\globals.lua MyFile.lua
+
+Directives in the file:
+
+-- GLOBALS: SomeGlobal, SomeOtherGlobal
+  The script will never complain about these. There may be multiple lines of these anywhere in the file, taking effect globally (for now). There is no way to un-GLOBAL an already declared global.
+
+-- SETGLOBALFILE [ON/OFF]
+  Enable/disable SETGLOBAL checks in the global scope
+  Default: ON
+  
+-- SETGLOBALFUNC [ON/OFF]
+  Enable/disable SETGLOBAL checks in functions. This setting affects the whole file (for now)
+  Default: ON
+  
+-- GETGLOBALFILE [ON/OFF]
+  Default: OFF
+
+-- GETGLOBALFUNC [ON/OFF]
+  Default: ON
+  
+--]]
+
+local strmatch=string.match
+local strgmatch=string.gmatch
+local print=print
+local gsub = string.gsub
+local tonumber=tonumber
+local stdin=io.input()
+
+local source=assert(io.open(arg[1]))
+
+
+-- First we parse the source file
+
+local funcNames={}
+local SETGLOBALfile = true
+local SETGLOBALfunc = true
+local GETGLOBALfile = true
+local GETGLOBALfunc = true
+
+local GLOBALS = {
+	ntop=1,
+	dirs=1,
+	interface=1,
+	interface=1,
+	i18n=1,
+	_GET=1,
+	_POST=1,
+	["require"]=1,
+	["package"]=1,
+	["tostring"]=1,
+	["tonumber"]=1,
+	["string"]=1,
+	["print"]=1,
+}
+
+local n=0
+
+while true do
+	local lin = source:read()
+	if not lin then break end
+	n=n+1
+
+	-- Lamely try to find all function headers and remember the line they were on. Yes, you can fool this. You can also shoot yourself in the foot. Either way it doesn't matter hugely, it's just to prettify the output.
+
+	local func = strmatch(lin, "%f[%a_][%a0-9_.:]+%s*=%s*function%s*%([^)]*") or  -- blah=function(...)
+		strmatch(lin, "%f[%a_]function%s*%([^)]*") or -- function(...)
+		strmatch(lin, "%f[%a_]function%s+[%a0-9_.:]+%s*%([^)]*")  -- function blah(...)
+	
+	if func then
+		func=func..")"
+		funcNames[n]=func
+	end
+	
+	if strmatch(lin, "^%s*%-%-") then
+		local args = strmatch(lin, "^%s*%-%-%s*GLOBALS:%s*(.*)")
+		if args then 
+			for name in strgmatch(args, "[%a0-9_]+") do
+				GLOBALS[name]=true
+			end
+		end
+
+		local args = strmatch(lin, "^%s*%-%-%s*SETGLOBALFILE%s+(%u+)")
+		if args=="ON" then 
+			SETGLOBALfile=true
+		elseif args=="OFF" then
+			SETGLOBALfile=false
+		end
+
+		local args = strmatch(lin, "^%s*%-%-%s*GETGLOBALFILE%s+(%u+)")
+		if args=="ON" then 
+			GETGLOBALfile=true
+		elseif args=="OFF" then
+			GETGLOBALfile=false
+		end
+		
+		local args = strmatch(lin, "^%s*%-%-%s*SETGLOBALFUNC%s+(%u+)")
+		if args=="ON" then 
+			SETGLOBALfunc=true
+		elseif args=="OFF" then
+			SETGLOBALfunc=false
+		end
+
+		local args = strmatch(lin, "^%s*%-%-%s*GETGLOBALFUNC%s+(%u+)")
+		if args=="ON" then 
+			GETGLOBALfunc=true
+		elseif args=="OFF" then
+			GETGLOBALfunc=false
+		end
+	end
+end
+
+-- Helper function that prints a line along with which function it is in. 
+
+local curfunc
+local lastfuncprinted
+
+local function printone(lin)
+	local globalName = strmatch(lin, "\t; _ENV \"(.+)\"%s*")
+
+	if globalName and GLOBALS[globalName] then
+		return
+	end
+	
+	if curfunc~=lastfuncprinted then
+		local from,to = strmatch(curfunc, "function <[^:]*:(%d+),(%d+)")
+		from=tonumber(from)
+		if from and funcNames[from] then
+			print(funcNames[from],strmatch(curfunc, "<.*"))
+		else
+			print(curfunc)
+		end
+		lastfuncprinted = curfunc
+	end
+	lin=gsub(lin, "%d+\t(%[%d+%])", "%1")	-- "23 [234]"  -> "[234]"   (strip the byte offset, we're not interested in it)
+	print(lin)
+end
+
+
+-- Loop the compiled output, looking for GETGLOBAL, SETGLOBAL, etc..
+
+local nSource=0
+local funcScope = false
+
+while true do
+	local lin = stdin:read()
+	if not lin then break end
+
+	if strmatch(lin,"^main <") then
+		curfunc=lin
+		funcScope = false
+	elseif strmatch(lin,"^function <") then
+		curfunc=lin
+		funcScope = true
+	elseif strmatch(lin,"SETTABUP \t") then
+		if funcScope and SETGLOBALfunc then
+			printone(lin)
+		elseif not funcScope and SETGLOBALfile then
+			printone(lin)
+		end
+	elseif strmatch(lin,"GETTABUP \t") then
+		if funcScope and GETGLOBALfunc then
+			printone(lin)
+		elseif not funcScope and GETGLOBALfile then
+			printone(lin)
+		end
+	end
+end
