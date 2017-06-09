1c579b56431168cba4b5af08fe5d3004a431ba85
Simone Mainardi <mainardi@ntop.org>
2017-06-09 13:11:33 +0200

    Adds hosts support for grafana

--- scripts/lua/modules/grafana/query.lua
+++ scripts/lua/modules/grafana/query.lua
@@ -28,13 +28,20 @@ else
    local res = {}
 
    for _, t in pairs(_GRAFANA["payload"]["targets"]) do
-      local is_interface = string.starts(t["target"], "interface_")
+      local is_host = string.starts(t["target"] or '', "host_")
+
+      local addr
+      if is_host then
+	 addr = string.match(t["target"], "_(.-)_") -- host address is between the first two underscores
+	 t["target"] = string.gsub(t["target"], "host_(.-)_", "")
+      end
+
       local is_bytes     = string.ends(t["target"], "_bytes") or string.ends(t["target"], "_bytestotal")
       local is_packets   = string.ends(t["target"], "_packets") or string.ends(t["target"], "_packetstotal")
       local is_allprotos = string.ends(t["target"], "_allprotocols")
 
-      local target = string.gsub(t["target"], "^(.-)_", "") -- lazy match to remove up to the first underscore
-      target = string.gsub(target, "_(.-)$", "") -- lazy match to remove up to the last underscore
+      local ifname = string.gsub(t["target"], "^(.-)_", "") -- lazy match to remove up to the first underscore
+      ifname = string.gsub(ifname, "_(.-)$", "") -- lazy match to remove up to the last underscore
 
       local rrdfile = ""
       if is_bytes then rrdfile = "bytes.rrd"
@@ -43,21 +50,19 @@ else
 
       local datapoints = {}
 
-      if is_interface then
-	 local rr = rrd2json(getInterfaceId(target), nil, rrdfile, epoch_begin, epoch_end)
+      local rr = rrd2json(getInterfaceId(ifname), addr, rrdfile, epoch_begin, epoch_end)
 
-	 local totalval = rr["totalval"]
-	 rr = json.decode(rr["json"])
+      local totalval = rr["totalval"]
+      rr = json.decode(rr["json"])
 
-	 if is_allprotos then
-	    res = toSeries(rr)
-	 elseif string.ends(t["target"], "total") then
-	    tprint(totalval)
-	    res[#res + 1] = {target="Total", datapoints={{totalval, 0 --[[ it's an integral, an instant is not meaningful here --]]}}}
-	 else
-	    res = toSeries(rr)
-	 end
+      if is_allprotos then
+	 res = toSeries(rr)
+      elseif string.ends(t["target"], "total") then
+	 res[#res + 1] = {target="Total", datapoints={{totalval, 0 --[[ it's an integral, an instant is not meaningful here --]]}}}
+      else
+	 res = toSeries(rr)
       end
+
 
 
       -- tprint({target=target, is_bytes=is_bytes, is_packets=is_packets, entity_name=entity_name})
--- scripts/lua/modules/grafana/search.lua
+++ scripts/lua/modules/grafana/search.lua
@@ -24,29 +24,86 @@ else
    -- tprint(_GRAFANA)
 
    local target = _GRAFANA["payload"]["target"]
+   --[[
+      example targets:
+      interface_eth0
+      host_192.168.2.0
+      host_192.168.2.0_interface_eth0
+   --]]
    if target == nil then
       target = ""
    end
 
-   local res = {}
+   local host_info
+   local target_interfaces = {}
 
+   -- PARSE the host part of the target
+   -- the host part is preceeded by prefix host_
+   if isEmptyString(target) or string.starts("host_", target) or string.starts(target, "host_") then
+      local addr = string.match(target or '', "_(.-)_") -- assumes address is between the first two underscores
+      if isEmptyString(addr) then
+	 addr = string.match(target or '', "_(.-)$") -- assumes address is between the first underscore and the end of string
+      end
+      host_info = hostkey2hostinfo(addr)
+      -- tprint(host_info)
+
+      local a, b = string.find(target, addr)
+      target = string.sub(target, b + 2 --[[ +2 removes the optional uderscore preceding interface_ --]])
+      -- tprint({target=target})
+   end
+
+   -- PARSE the interface part of the target
+   -- the interface part is preceeded by prefix interface_
    if isEmptyString(target) or string.starts("interface_", target) or string.starts(target, "interface_") then
       local ifnames = interface.getIfNames()
       for _, n in pairs(ifnames) do
-	 local tb  = "interface_"..n.."_bytes"
-	 local tp  = "interface_"..n.."_packets"
-	 local tbt = "interface_"..n.."_bytestotal"
-	 local tpt = "interface_"..n.."_packetstotal"
-	 local tap = "interface_"..n.."_allprotocols"
+	 local t  = "interface_"..n
 
-	 for _, t in pairs({tb, tp, tbt, tpt, tap}) do
-	    if isEmptyString(target) or string.starts(t, target) or string.starts(target, t) then
-	       res[#res +1] = t
-	    end
+	 if isEmptyString(target) or string.starts(t, target) or string.starts(target, t) then
+	    target_interfaces[n] = 1
 	 end
 
       end
    end
 
-   print(json.encode(res, nil))
+   local matches = {}
+
+   for n,_ in pairs(target_interfaces) do
+      interface.select(n)
+      matches[n] = {}
+      if host_info ~= nil then
+	 local matching_hosts = interface.findHost(hostinfo2hostkey(host_info))
+	 for addr, label in pairs(matching_hosts) do
+	    matches[n][addr] = label
+	 end
+      end
+   end
+
+   local rsp_targets = {}
+   local metrics = {"bytes", "bytestotal", "allprotocols"}
+   if host_info == nil then -- packets only for interfaces
+      metrics[#metrics + 1] = "packets"
+      metrics[#metrics + 1] = "packetstotal"
+   end
+
+   for _, metric in pairs(metrics) do
+
+      for n, hosts in pairs(matches) do
+	 local interface_series = "interface_"..n.."_"..metric
+
+	 for addr, label in pairs(hosts) do
+	    local host_series = "host_"..addr
+	    rsp_targets[#rsp_targets + 1] = host_series.."_"..interface_series
+	 end
+
+	 if host_info == nil then
+	    rsp_targets[#rsp_targets + 1] = interface_series
+	 end
+
+      end
+   end
+
+   -- tprint({matches=matches, rsp_targets=rsp_targets})
+
+   print(json.encode(rsp_targets, nil))
 end
