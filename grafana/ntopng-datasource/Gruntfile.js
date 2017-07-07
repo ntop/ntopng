09ce6ca3dabd2754f1657f93356315e8895194b4
Simone Mainardi <mainardi@ntop.org>
2017-07-07 12:50:16 +0200

    Imports Grafana Labs SimpleJSON datasource plugin
    Imported version is:
    https://github.com/grafana/simple-json-datasource/commit/60bb8df9b784fc4013bc6ecf4da614d1465e8a24

---
+++ grafana/ntopng-datasource/Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.initConfig({
+
+    clean: ["dist"],
+
+    copy: {
+      src_to_dist: {
+        cwd: 'src',
+        expand: true,
+        src: ['**/*', '!**/*.js', '!**/*.scss'],
+        dest: 'dist'
+      },
+      pluginDef: {
+        expand: true,
+        src: ['README.md'],
+        dest: 'dist'
+      }
+    },
+
+    watch: {
+      rebuild_all: {
+        files: ['src/**/*'],
+        tasks: ['default'],
+        options: {spawn: false}
+      }
+    },
+
+    babel: {
+      options: {
+        sourceMap: true,
+        presets:  ['es2015']
+      },
+      dist: {
+        options: {
+          plugins: ['transform-es2015-modules-systemjs', 'transform-es2015-for-of']
+        },
+        files: [{
+          cwd: 'src',
+          expand: true,
+          src: ['**/*.js'],
+          dest: 'dist',
+          ext:'.js'
+        }]
+      },
+      distTestNoSystemJs: {
+        files: [{
+          cwd: 'src',
+          expand: true,
+          src: ['**/*.js'],
+          dest: 'dist/test',
+          ext:'.js'
+        }]
+      },
+      distTestsSpecsNoSystemJs: {
+        files: [{
+          expand: true,
+          cwd: 'spec',
+          src: ['**/*.js'],
+          dest: 'dist/test/spec',
+          ext:'.js'
+        }]
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['dist/test/spec/test-main.js', 'dist/test/spec/*_spec.js']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'babel', 'mochaTest']);
+};
---
+++ grafana/ntopng-datasource/src/query_ctrl.js
@@ -0,0 +1,31 @@
+import {QueryCtrl} from 'app/plugins/sdk';
+import './css/query-editor.css!'
+
+export class GenericDatasourceQueryCtrl extends QueryCtrl {
+
+  constructor($scope, $injector, uiSegmentSrv)  {
+    super($scope, $injector);
+
+    this.scope = $scope;
+    this.uiSegmentSrv = uiSegmentSrv;
+    this.target.target = this.target.target || 'select metric';
+    this.target.type = this.target.type || 'timeserie';
+  }
+
+  getOptions() {
+    return this.datasource.metricFindQuery(this.target)
+      .then(this.uiSegmentSrv.transformToSegments(false));
+      // Options have to be transformed by uiSegmentSrv to be usable by metric-segment-model directive
+  }
+
+  toggleEditorMode() {
+    this.target.rawQuery = !this.target.rawQuery;
+  }
+
+  onChangeInternal() {
+    this.panelCtrl.refresh(); // Asks the panel to refresh data.
+  }
+}
+
+GenericDatasourceQueryCtrl.templateUrl = 'partials/query.editor.html';
+
