9098b8057241c750e0b95d23077f166810a8b191
Luca Deri <deri@ntop.org>
2017-08-31 00:43:38 +0200

    Added fix for transparently setting a filter on VLAN tagged interfaces for focusing only on tagged packeets whose id is on the interface name (format: <ifname>.<VLAN ID>)

--- src/main.cpp
+++ src/main.cpp
@@ -204,6 +204,10 @@ int main(int argc, char *argv[])
     }
 
     if(iface) {
+      /* Handle tagged interfaces whose name is enp65s0.3 */
+      char *dot = strchr(ifName, '.');
+      u_int vlan_id = dot ? atoi(&dot[1]) : 0;
+
       if(affinity != NULL) {
 	char *tmp;
 	
@@ -221,7 +225,15 @@ int main(int argc, char *argv[])
 	iface->setCPUAffinity(core_id);
       }
 
-      if(prefs->get_packet_filter() != NULL)
+      if(vlan_id != 0) {
+	char filter[4096];
+
+	snprintf(filter, sizeof(filter), "%s%s(vlan and ( ether[14:2] & 0xfff == %u))",
+		 prefs->get_packet_filter() ? prefs->get_packet_filter()  : "",
+		 prefs->get_packet_filter() ? " and " : "", vlan_id);
+
+	iface->set_packet_filter(filter);	  
+      } else if(prefs->get_packet_filter())
 	iface->set_packet_filter(prefs->get_packet_filter());
 
       ntop->registerInterface(iface);
