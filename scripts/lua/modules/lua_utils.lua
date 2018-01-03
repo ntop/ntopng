ef2f3f881c415535aa63fabb76ac391c476b2a73
emanuele-f <faranda@ntop.org>
2018-01-03 17:52:30 +0100

    Add table comparison function

--- scripts/lua/modules/lua_utils.lua
+++ scripts/lua/modules/lua_utils.lua
@@ -3070,6 +3070,30 @@ function table.clone(orig)
    return copy
 end
 
+-- From http://lua-users.org/lists/lua-l/2014-09/msg00421.html
+-- Returns true if tables are equal
+function table.compare(t1, t2, ignore_mt)
+  local ty1 = type(t1)
+  local ty2 = type(t2)
+
+  if ty1 ~= ty2 then return false end
+  if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
+  local mt = getmetatable(t1)
+  if not ignore_mt and mt and mt.__eq then return t1 == t2 end
+
+  for k1,v1 in pairs(t1) do
+      local v2 = t2[k1]
+      if v2 == nil or not table.compare(v1, v2) then return false end
+  end
+
+  for k2,v2 in pairs(t2) do
+      local v1 = t1[k2]
+      if v1 == nil or not table.compare(v1, v2) then return false end
+  end
+
+  return true
+end
+
 function toboolean(s)
   if s == "true" then
     return true
