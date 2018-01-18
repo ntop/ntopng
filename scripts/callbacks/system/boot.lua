fdb42287b56419c1bb96ab9e623aebe4a5910ae9
Simone Mainardi <mainardi@ntop.org>
2018-01-18 20:09:56 +0100

    Fixes boot.lua actions ordering

--- scripts/callbacks/system/boot.lua
+++ scripts/callbacks/system/boot.lua
@@ -13,13 +13,10 @@
 local dirs = ntop.getDirs()
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 
+local prefs_dump_utils = require "prefs_dump_utils"
+prefs_dump_utils.readPrefsFromDisk()
+
 if(ntop.isPro()) then
    package.path = dirs.installdir .. "/pro/scripts/callbacks/system/?.lua;" .. package.path
    require("boot")
 end
-
-local prefs_dump_utils = require "prefs_dump_utils"
-
-prefs_dump_utils.readPrefsFromDisk()
-
-
