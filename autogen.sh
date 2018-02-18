bd4da895481b0e491840dcdce8da582c1c5f4ead
Bjørn Forsman <bjorn.forsman@gmail.com>
2018-02-18 18:26:57 +0100

    autogen.sh: /bin/rm -> rm
    For portability, don't assume that /bin/rm exists, rely on $PATH lookup
    instead.
    
    This fixes building in sandboxed Nix(pkgs) builds.

--- autogen.sh
+++ autogen.sh
@@ -29,7 +29,7 @@ fi
 
 cat configure.seed | sed "s/@VERSION@/$VERSION/g" | sed "s/@SHORT_VERSION@/$SHORT_VERSION/g" | sed "s/@GIT_TAG@/$GIT_TAG/g" | sed "s/@GIT_DATE@/$GIT_DATE/g" | sed "s/@GIT_RELEASE@/$GIT_RELEASE/g" | sed "s/@GIT_BRANCH@/$GIT_BRANCH/g"  | sed "s/@PRO_GIT_RELEASE@/$PRO_GIT_RELEASE/g" | sed "s/@PRO_GIT_DATE@/$PRO_GIT_DATE/g" > configure.ac
 
-/bin/rm -f config.h config.h.in *~ #*
+rm -f config.h config.h.in *~ #*
 
 echo "Wait please..."
 autoreconf -if
