d5bc83d85f0455990ec31d3c7c663692a4f344e2
Luca Deri <deri@ntop.org>
2018-03-17 19:35:51 +0100

    Improved query form

--- scripts/lua/nindex.lua
+++ scripts/lua/nindex.lua
@@ -12,9 +12,11 @@ sendHTTPContentTypeHeader('text/html')
 ntop.dumpFile(dirs.installdir .. "/httpdocs/inc/header.inc")
 dofile(dirs.installdir .. "/scripts/lua/inc/menu.lua")
 
-select  = _GET["select_clause"]
-where   = _GET["where_clause"]
-maxhits = _GET["maxhits_clause"]
+select     = _GET["select_clause"]
+where      = _GET["where_clause"]
+maxhits    = _GET["maxhits_clause"]
+begin_time = _GET["begin_time_clause"]
+end_time   = _GET["end_time_clause"]
 
 if(_GET["flow_clause"] == "flows") then
    aggregated_flows = false
@@ -34,59 +36,87 @@ if((maxhits == nil) or (maxhits == "")) then
    maxhits = "10"
 end
 
+if((begin_time == nil) or (begin_time == "")) then
+   begin_time = "now-1h"
+end
+
+if((end_time == nil) or (end_time == "")) then
+   end_time = "now"
+end
+
 -- Zap spaces
 select = select:gsub("%s+", "")
-where = where:gsub("%s+", "")
+-- where = where:gsub("%s+", "")
 
 print [[
   <form method=get action=nindex.lua>
 
-  <div class="form-group row">
+  <fieldset class="form-group"> 
+  <div class="form-group form-row">
     <label for="inputSelect3" class="col-sm-2 col-form-label">Select</label>
     <div class="col-sm-10">
       <input name="select_clause" type="text" class="form-control" id="exampleInputSelect1" aria-describedby="selectHelp" placeholder="Enter select term" value="]] print(select) print [[">
       <small id="selectHelp" class="form-text text-muted">Use * for all fields or field1,field2....</small>
     </div>
   </div>
+ </fieldset>
 
-  <div class="form-group row">
+  <fieldset class="form-group">
+  <div class="form-group form-row">
     <label for="inputWhere3" class="col-sm-2 col-form-label">Where</label>
-    <div class="col-sm-10">
+    <div class="col-sm-10  col-xs-2">
       <input name="where_clause" type="text" class="form-control" id="exampleInputWhere1" aria-describedby="whereHelp" placeholder="Enter where term" value="]] print(where) print [[">
       <small id="whereHelp" class="form-text text-muted">Use field &lt;operatore&gt; where operator could be &lt;, &gt;, =, !=</small>
     </div>
   </div>
-
-  <div class="form-group row">
-    <label for="inputMaxhits3" class="col-sm-2 col-form-label">Max Results</label>
-    <div class="col-sm-10">
-      <select name="maxhits_clause" class="form-control" id="exampleFormControlSelect1" id="exampleInputMaxhits1" aria-describedby="maxhitsHelp" placeholder="Enter maxhits condition">
-        <option>10</option>
-        <option>100</option>
-        <option>1000</option>
-      </select>
-    </div>
-  </div>
+  </fieldset>
 
   <fieldset class="form-group">
-    <div class="row">
-<label for="inputMaxhits3" class="col-sm-2 col-form-label">Max Results</label> 
+    <div class="form-group form-row">
+      <label for="inputFlowDB" class="col-sm-2 col-form-label">Time Range</label> 
       <div class="col-sm-10">
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flow_clause" id="gridRadios1" value="flows" checked>
-          <label class="form-check-label" for="gridRadios1">
-            Flows
-          </label>
+        <div class="form-check col-xs-2">
+          <label> Begin Time </label>
+          <input class="form-control  col-xs-2" type="text" name="begin_time_clause" value="]] print(begin_time) print [[">
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flow_clause" id="gridRadios2" value="aggregated_flows">
-          <label class="form-check-label" for="gridRadios2">
-            Aggregated Flows
-          </label>
+        <div class="form-check  col-xs-2">
+          <label> End Time </label>
+          <input class="form-control" type="text" name="end_time_clause" value="]] print(end_time) print [[">
         </div>
       </div>
     </div>
   </fieldset>
+
+
+  <fieldset class="form-group">
+    <div class="form-group form-row">
+      <label for="inputFlowDB" class="col-sm-2 col-form-label">Database</label> 
+      <div class="col-sm-10">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flow_clause" id="gridRadios1" value="flows" ]] if(aggregated_flows == false) then print("checked") end print [[>
+          <label class="form-check-label" for="gridRadios1">Flows</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flow_clause" id="gridRadios2" value="aggregated_flows" ]] if(aggregated_flows == true) then print("checked") end print [[>
+          <label class="form-check-label" for="gridRadios2">Aggregated Flows</label>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+
+  <fieldset class="form-group">
+  <div class="form-group">
+    <label for="inputMaxhits3" class="col-sm-2 col-form-label">Max Results</label>
+    <div class="form-check col-xs-2">
+      <select name="maxhits_clause" class="form-control" id="exampleFormControlSelect1" id="exampleInputMaxhits1" aria-describedby="maxhitsHelp" placeholder="Enter maxhits condition">
+        <option]] if(maxhits == "10") then print(" selected") end print [[>10</option>
+        <option]] if(maxhits == "100") then print(" selected") end print [[>100</option>
+        <option]] if(maxhits == "1000") then print(" selected") end print [[>1000</option>
+      </select>
+    </div>
+  </div>
+  </fieldset>
+
 
 
 
@@ -95,14 +125,22 @@ print [[
 ]]
 
 
-res = interface.nIndexSelect(aggregated_flows, "now-2h", "now", select, where, 0, tonumber(maxhits))
+res = interface.nIndexSelect(aggregated_flows, begin_time, end_time, select, where, 0, tonumber(maxhits))
 
---tprint(res)
+-- tprint(res)
 
 if(res) then
-   print("<p><table class=\"table table-bordered table-striped\">\n")
+   print("<p><small>Query perfomed in "..res.info.duration.." msec</small><br>")
    
-   for k, row in pairs(res) do
+   print("<p><table class=\"table table-bordered table-striped\">\n")
+
+   print("<tr>")
+   for k, row in pairs(res.columns) do      
+      print("<th>"..row.."</th>")     
+   end
+   print("</tr>\n")
+   
+   for k, row in pairs(res.results) do
       print("<tr>")
       -- print("<tr><th>"..k.."</th>")
       
