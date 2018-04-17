c6faed3615ba79ef1f9da9cbd5618ddb8b47647b
Luca <deri@ntop.org>
2018-04-17 17:30:36 +0200

    Removed unnecessary statement from second.lua Minor code cleanup

--- src/Lua.cpp
+++ src/Lua.cpp
@@ -3724,66 +3724,6 @@ static int8_t ntop_ts_step_to_series_id(u_int16_t step) {
 static int ntop_ts_set(lua_State* vm) {
   NetworkInterface *ntop_interface = getCurrentInterface(vm);
 
-#if 0
-#if defined(HAVE_NINDEX) && defined(NTOPNG_PRO)
-  if(ntop->getPro()->is_nindex_in_use()) {
-    const char *label = NULL, *metric = NULL, *key = "";
-    u_int8_t ifaceId, id = 1;
-    int8_t series_id;
-    u_int16_t step;
-    u_int32_t ts;
-    u_int64_t sent = 0, rcvd = 0;
-
-    if(!ntop_interface)
-      return(CONST_LUA_ERROR);
-
-    if(ntop_lua_check(vm, __FUNCTION__, id, LUA_TNUMBER) != CONST_LUA_OK)
-      return(CONST_LUA_PARAM_ERROR);
-    ts = (u_int32_t)lua_tonumber(vm, id++);
-
-    if(ntop_lua_check(vm, __FUNCTION__, id, LUA_TNUMBER) != CONST_LUA_OK)
-      return(CONST_LUA_PARAM_ERROR);
-    ifaceId = (u_int8_t)lua_tonumber(vm, id++);
-
-    if(ntop_lua_check(vm, __FUNCTION__, id, LUA_TNUMBER) != CONST_LUA_OK)
-      return(CONST_LUA_PARAM_ERROR);
-    step = (u_int32_t)lua_tonumber(vm, id++);
-
-    if((series_id = ntop_ts_step_to_series_id(step)) == -1)
-      return(CONST_LUA_ERROR);
-
-    if(ntop_lua_check(vm, __FUNCTION__, id, LUA_TSTRING) != CONST_LUA_OK)
-      return(CONST_LUA_PARAM_ERROR);
-    if((label = (const char*)lua_tostring(vm, id++)) == NULL)
-      return(CONST_LUA_PARAM_ERROR);
-
-    if(lua_type(vm, id) != LUA_TNIL) {
-      if(lua_type(vm, id) == LUA_TSTRING)
-	key = (const char*)lua_tostring(vm, id++);
-    } else
-      id++;
-
-    if(ntop_lua_check(vm, __FUNCTION__, id, LUA_TSTRING) != CONST_LUA_OK)
-      return(CONST_LUA_PARAM_ERROR);
-    if((metric = (const char*)lua_tostring(vm, id++)) == NULL)
-      return(CONST_LUA_PARAM_ERROR);
-
-    if(lua_type(vm, id) == LUA_TNUMBER)
-      sent = (u_int64_t)lua_tonumber(vm, id++);
-    else if(lua_type(vm, id) == LUA_TSTRING)
-      sent = (u_int64_t)atoll((const char*)lua_tostring(vm, id++));
-
-    if(lua_type(vm, id) == LUA_TNUMBER)
-      rcvd = (u_int64_t)lua_tonumber(vm, id++);
-    else if(lua_type(vm, id) == LUA_TSTRING)
-      rcvd = (u_int64_t)atoll((const char*)lua_tostring(vm, id++));
-
-    ntop->tsSet(series_id, ts, true /* counter */, ifaceId,
-		step, label, key, metric, sent, rcvd);
-  }
-#endif
-#endif
-
   if(ntop_interface && ntop_interface->getTSExporter()) {
     const char *label = NULL, *metric = NULL, *key = "";
     /* u_int8_t ifaceId; */
