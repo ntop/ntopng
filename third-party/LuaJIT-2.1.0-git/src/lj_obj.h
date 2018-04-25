6a6e680d35d4124888a932d8c1380e64be288246
Luca Deri <deri@ntop.org>
2018-04-25 14:48:13 +0200

    ntop extensions

--- third-party/LuaJIT-2.1.0-git/src/lj_obj.h
+++ third-party/LuaJIT-2.1.0-git/src/lj_obj.h
@@ -621,6 +621,9 @@ typedef struct global_State {
   MRef jit_base;	/* Current JIT code L->base or NULL. */
   MRef ctype_state;	/* Pointer to C type state. */
   GCRef gcroot[GCROOT_MAX];  /* GC roots. */
+  /* NTOP BEGIN */
+  void *userdata;
+  /* NTOP END */
 } global_State;
 
 #define mainthread(g)	(&gcref(g->mainthref)->th)
