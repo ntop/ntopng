bc4f7094974b1ccda0e49ecd4142fc4b2b93e0a0
Luca <deri@ntop.org>
2018-06-27 09:04:57 -0700

    Integers that can be represented with 32 bits (or less) are now pushed to Lua as integer and not as numbers (that are represented as float) as this causes side effects in some functions

--- src/LuaEngine.cpp
+++ src/LuaEngine.cpp
@@ -4928,7 +4928,12 @@ void lua_push_int_table_entry(lua_State *L, const char *key, u_int64_t value) {
     lua_pushstring(L, key);
     /* using LUA_NUMBER (double: 64 bit) in place of LUA_INTEGER (ptrdiff_t: 32 or 64 bit
      * according to the platform, as defined in luaconf.h) to handle big counters */
-    lua_pushnumber(L, (lua_Number)value);
+
+    if(value > 0xFFFFFFFF)
+      lua_pushnumber(L, (lua_Number)value);
+    else
+      lua_pushinteger(L, (lua_Integer)value);
+    
     lua_settable(L, -3);
   }
 }
