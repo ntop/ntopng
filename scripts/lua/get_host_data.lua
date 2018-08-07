6114b4167109dd7bd2086e7c8add225bc724ac46
Luca Deri <deri@ntop.org>
2018-08-07 12:01:09 +0200

    Added new prefs entry for nIndex Added extra check in get_host_data.lua

--- scripts/lua/get_host_data.lua
+++ scripts/lua/get_host_data.lua
@@ -127,6 +127,7 @@ else
    end
    
    sent2rcvd = round((host["bytes.sent"] * 100) / (host["bytes.sent"]+host["bytes.rcvd"]), 0)
+   if(sent2rcvd == nil) then sent2rcvd = 0 end
    print ("\"column_breakdown\" : \"<div class='progress'><div class='progress-bar progress-bar-warning' style='width: "
 	  .. sent2rcvd .."%;'>Sent</div><div class='progress-bar progress-bar-info' style='width: " .. (100-sent2rcvd) .. "%;'>Rcvd</div></div>")
 
