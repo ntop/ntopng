e8dedd16b6039a6b52579df3832695f197960e18
Simone Mainardi <mainardi@ntop.org>
2018-08-10 13:11:39 +0200

    Uses the manage script also for the config backup

--- scripts/lua/get_config.lua
+++ scripts/lua/get_config.lua
@@ -10,7 +10,6 @@ package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 local os_utils = require "os_utils"
 
 require "lua_utils"
-
 local json = require("dkjson")
 
 local function send_error(error_type)
@@ -43,83 +42,74 @@ else
     print(runtimeprefs:read "*a") 
 
   else -- Unix
-
-    -- -- Old way (single json)
-    -- sendHTTPContentTypeHeader('application/json', 'attachment; filename="runtimeprefs.json"')
-    -- local config_backup = {}
-    -- if not isEmptyString(prefs.config_file) then
-    --   local ntopng_conf = io.open(prefs.config_file, "r")  
-    --   if ntopng_conf ~= nil then
-    --     config_backup[prefs.config_file] = (ntopng_conf:read "*a")
-    --   end
-    -- end
-    -- print(json.encode(config_backup or {}))
-
-    local bin_tar = "/bin/tar"
-    if not ntop.exists(bin_tar) then
-      bin_tar = "/usr/bin/tar"
+    local manage_config = "/usr/bin/ntopng-utils-manage-config"
+    if not ntop.exists(manage_config) then
+      manage_config = os_utils.fixPath(dirs.installdir.."/packages/usr/bin/ntopng-utils-manage-config")
     end
-    if not ntop.exists(bin_tar) then
+
+    if not ntop.exists(manage_config) then
       send_error("tar_not_found")
     else
 
-      local config_files = ""
+    local config_files = ""
 
-      local ntopng_conf_dir = "/etc/ntopng"
-      if ntop.exists(ntopng_conf_dir) then
-        config_files = config_files .. " " .. ntopng_conf_dir
-      end
+    local ntopng_conf_dir = "/etc/ntopng"
+    if ntop.exists(ntopng_conf_dir) then
+      config_files = config_files .. " " .. ntopng_conf_dir
+    end
 
-      local license_path = "/etc/ntopng.license"
-      if ntop.isnEdge() then
-        license_path = "/etc/nedge.license"
-      end
-      if ntop.exists(license_path) then
-        config_files = config_files .. " " .. license_path
-      end
+    local license_path = "/etc/ntopng.license"
+    if ntop.isnEdge() then
+       license_path = "/etc/nedge.license"
+    end
+    if ntop.exists(license_path) then
+       config_files = config_files .. " " .. license_path
+    end
 
-      if not isEmptyString(prefs.config_file) then
-        if ntop.exists(prefs.config_file) and not starts_with(prefs.config_file, ntopng_conf_dir) then
+    if not isEmptyString(prefs.config_file) then
+       if ntop.exists(prefs.config_file) and not starts_with(prefs.config_file, ntopng_conf_dir) then
           config_files = config_files .. " " .. prefs.config_file
-        end
-      end
+       end
+    end
 
-      local runtimeprefs_path = os_utils.fixPath(dirs.workingdir.."/runtimeprefs.json")
-      if ntop.exists(runtimeprefs_path) and not starts_with(runtimeprefs_path, ntopng_conf_dir) then
-        config_files = config_files .. " " .. runtimeprefs_path
-      end
+    local runtimeprefs_path = os_utils.fixPath(dirs.workingdir.."/runtimeprefs.json")
+    if ntop.exists(runtimeprefs_path) and not starts_with(runtimeprefs_path, ntopng_conf_dir) then
+       config_files = config_files .. " " .. runtimeprefs_path
+    end
 
-      if ntop.isnEdge() then
-        local system_config_path = os_utils.fixPath(dirs.workingdir.."/system.config")
-        if ntop.exists(system_config_path) and not starts_with(system_config_path, ntopng_conf_dir) then
+    if ntop.isnEdge() then
+       local system_config_path = os_utils.fixPath(dirs.workingdir.."/system.config")
+       if ntop.exists(system_config_path) and not starts_with(system_config_path, ntopng_conf_dir) then
           config_files = config_files .. " " .. system_config_path
-        end
-      end
+       end
+    end
 
-      if not isEmptyString(prefs.ndpi_proto_file) then
-        if ntop.exists(prefs.ndpi_proto_file) and not starts_with(prefs.ndpi_proto_file, ntopng_conf_dir) then
+    if not isEmptyString(prefs.ndpi_proto_file) then
+       if ntop.exists(prefs.ndpi_proto_file) and not starts_with(prefs.ndpi_proto_file, ntopng_conf_dir) then
           config_files = config_files .. " " .. prefs.ndpi_proto_file
-        end
-      end
+       end
+    end
 
-      local tar_file = "ntopng_conf_backup.tar.gz"
-      if ntop.isnEdge() then
-        tar_file = "nedge_conf_backup.tar.gz"
-      end
+    local tar_file = "ntopng_conf_backup.tar.gz"
+    if ntop.isnEdge() then
+       tar_file = "nedge_conf_backup.tar.gz"
+    end
 
-      sendHTTPContentTypeHeader('application/x-tar-gz', 'attachment; filename="' .. tar_file .. '"')
+    local output_tar = os_utils.fixPath("/tmp/"..tar_file)
+    local cmd = string.format("%s -a backup -c %s %s > /dev/null", manage_config, output_tar, config_files)
 
-      local cmd = bin_tar .. " czP " .. config_files 
+    -- Note: we are using os.execute / ntop.dumpBinaryFile as io.popen / print 
+    -- cannot be used for dumping binary files directly to the connection
 
-      -- Note: we are using os.execute / ntop.dumpBinaryFile as io.popen / print 
-      -- cannot be used for dumping binary files directly to the connection
+    os.execute(cmd)
 
-      local tmp = os.tmpname()
-      os.execute(cmd .. " > " .. tmp)
-      ntop.dumpBinaryFile(tmp)
-      os.remove(tmp)
-
+    if ntop.exists(output_tar) then
+       sendHTTPContentTypeHeader('application/x-tar-gz', 'attachment; filename="' .. tar_file .. '"')
+       ntop.dumpBinaryFile(output_tar)
+       os.remove(output_tar)
+    else
+       send_error("tar_not_found")
     end
   end
+ end
 end
-
