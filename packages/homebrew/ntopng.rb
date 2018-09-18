58423951b627752d8e64c9e87805d7edbcfe7df9
Alfredo Cardigliano <cardigliano@ntop.org>
2018-09-18 23:41:03 +0200

    brew formula update

--- packages/homebrew/ntopng.rb
+++ packages/homebrew/ntopng.rb
@@ -1,40 +1,59 @@
-require "formula"
-
 class Ntopng < Formula
   desc "Next generation version of the original ntop"
-  homepage "http://www.ntop.org/products/ntop/"
-  url "https://downloads.sourceforge.net/project/ntop/ntopng/ntopng-2.0.tar.gz"
-  sha1 "ef8ec9a31637445ab3797930d27b448071239348"
+  homepage "https://www.ntop.org/products/traffic-analysis/ntop/"
+
+  stable do
+    url "https://github.com/ntop/ntopng.git", :branch => "3.6-stable"
+    version "3.6"
+
+    resource "nDPI" do
+      url "https://github.com/ntop/nDPI.git", :branch => "2.4-stable"
+      version "2.4"
+    end
+  end
 
   bottle do
-    sha256 "c3a9c8be354c9fd3cbed7f84a6b9585d2b2d9bd1111a44e68c37694ea85707ba" => :yosemite
-    sha256 "9fbf8cad7c28f36c59bb1db44464d2bece35566721b3f4c54c91deb473f76641" => :mavericks
-    sha256 "355c68400b80698448e8197316a015cb88563600cb8a619df20d0bef2e79b0ca" => :mountain_lion
+    sha256 "e7315a06207aebca9826d516e390142e039885f9630dd53daa0032ea5cecaf65" => :high_sierra
+    sha256 "9c0f54169acb2a4ddd67b201b83a946009d14d1e77590c648be4bb28fd26d099" => :sierra
+    sha256 "ec24a5e1ae49b79a747c5fe61e8e1bcd8f5d427ac1ca69b564cc8cce441859e7" => :el_capitan
+  end
+
+  head do
+    url "https://github.com/ntop/ntopng.git", :branch => "dev"
+
+    resource "nDPI" do
+      url "https://github.com/ntop/nDPI.git", :branch => "dev"
+    end
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "pkg-config" => :build
-  depends_on "libtool" => :build
-  depends_on "json-glib" => :build
-  depends_on "wget" => :build
-  depends_on "zeromq" => :build
   depends_on "gnutls" => :build
+  depends_on "json-glib" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "zeromq" => :build
+  depends_on "lua" => :build
 
-  depends_on "json-c"
-  depends_on "rrdtool"
-  depends_on "luajit"
   depends_on "geoip"
+  depends_on "json-c"
+  depends_on "mysql-client"
   depends_on "redis"
+  depends_on "rrdtool"
 
   def install
+    resource("nDPI").stage do
+      system "./autogen.sh"
+      system "make"
+      (buildpath/"nDPI").install Dir["*"]
+    end
     system "./autogen.sh"
-    system "./configure","--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
   end
 
   test do
-    system "#{bin}/ntopng", "-h"
+    system "#{bin}/ntopng", "-V"
   end
 end
