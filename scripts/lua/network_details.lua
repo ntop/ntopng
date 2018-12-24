590215ac35fd3016bc69447074d6a806881d1a9c
Simone Mainardi <mainardi@ntop.org>
2018-12-24 10:25:05 +0100

    Adds host/interface/network reports to nEdge

--- scripts/lua/network_details.lua
+++ scripts/lua/network_details.lua
@@ -72,17 +72,17 @@ if areAlertsEnabled() and not ifstats.isView then
     end
 end
 
-   if ts_utils.getDriverName() == "rrd" then
-   if(ntop.isEnterprise()) then
+if ts_utils.getDriverName() == "rrd" then
+   if ntop.isEnterprise() or ntop.isnEdgeEnterprise() then
       if(page == "traffic_report") then
          print("\n<li class=\"active\"><a href=\"#\"><i class='fa fa-file-text report-icon'></i></a></li>\n")
       else
          print("\n<li><a href=\""..nav_url.."&page=traffic_report\"><i class='fa fa-file-text report-icon'></i></a></li>")
       end
-   elseif not have_nedge then
+   else
       print("\n<li><a href=\"#\" title=\""..i18n('enterpriseOnly').."\"><i class='fa fa-file-text report-icon'></i></A></li>\n")
    end
-   end
+end
    
 if((network ~= nil) and (areAlertsEnabled())) then
     if(page == "config") then
