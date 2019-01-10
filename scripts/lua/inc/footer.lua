1c23ff2650bb6618d6457eeb7eebe08451f90d16
emanuele-f <faranda@ntop.org>
2019-01-10 12:29:06 +0100

    Fix missing interface selection in footer

--- scripts/lua/inc/footer.lua
+++ scripts/lua/inc/footer.lua
@@ -198,7 +198,7 @@ var footerRefresh = function() {
 	  url: ']]
 print (ntop.getHttpPrefix())
 print [[/lua/network_load.lua',
-	  data: { },
+	  data: { ifid: ]] print(tostring(getInterfaceId(ifname))) print[[ },
 	  /* error: function(content) { alert("JSON Error (session expired?): logging out"); window.location.replace("]]
 print (ntop.getHttpPrefix())
 print [[/lua/logout.lua");  }, */
