9acb73937b0132299e979b9fc079af443276cd6a
Simone Mainardi <mainardi@ntop.org>
2019-01-18 18:39:15 +0100

    Disables login and adds nIndex to tests

--- tests/do.sh
+++ tests/do.sh
@@ -3,7 +3,7 @@
 SCRIPTS_DIR="${HOME}/ntopng/scripts"
 HTTPDOCS_DIR="${HOME}/ntopng/httpdocs"
 CALLBACKS_DIR="${HOME}/ntopng/scripts/callbacks"
-EXECUTABLE="./ntopng --scripts-dir ${SCRIPTS_DIR} --httpdocs-dir ${HTTPDOCS_DIR} --callbacks-dir ${CALLBACKS_DIR} --shutdown-when-done"
+EXECUTABLE="./ntopng --scripts-dir ${SCRIPTS_DIR} --httpdocs-dir ${HTTPDOCS_DIR} --callbacks-dir ${CALLBACKS_DIR} --shutdown-when-done --disable-login 1"
 
-${EXECUTABLE} -i ./tests/pcap/ping_req_reply.pcap --test-script test_ping_req_reply.lua
+${EXECUTABLE} -i ./tests/pcap/ping_req_reply.pcap --test-script test_ping_req_reply.lua -F "nindex"
 
