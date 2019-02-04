db5053899d416564969eb7f288f7a8614be94af9
Luca Deri <deri@ntop.org>
2019-02-04 13:42:50 +0100

    Added filter for mostly idle hosts

--- scripts/lua/local_hosts_anomaly_checker.lua
+++ scripts/lua/local_hosts_anomaly_checker.lua
@@ -12,34 +12,44 @@ sendHTTPContentTypeHeader('text/html')
 
 local hosts = interface.getLocalHostsInfo(true, "column_traffic")
 
+function isanomaly(value)   
+   if(value > 0) then
+      if((value < 25) or (value > 75)) then
+	 return true
+      end
+   end
+   
+   return false
+end
 
 for k,h in pairs(hosts.hosts) do
-   tprint(h)
-   
-   print(k.." ")
-   
-   if(h["active_flows.as_client.anomaly_index"] ~= nil) then
-      if(h["active_flows.as_client.anomaly_index"] > 0) then
-	 print("[client: "..h["active_flows.as_client.anomaly_index"].."]")
+   -- TODO: add counter for discarding mostly idle hosts
+   if((h.last_throughput_pps > 0) or (h.throughput_pps > 0)) then
+      print("<A HREF='/lua/host_details.lua?host="..k.."' target=_blank>"..k.."</A> ")
+      
+      if(h["active_flows.as_client.anomaly_index"] ~= nil) then
+	 if(isanomaly(h["active_flows.as_client.anomaly_index"]) == true) then
+	    print("[client: "..h["active_flows.as_client.anomaly_index"].."]")
+	 end
       end
-   end
-   
-   if(h["active_flows.as_client.anomaly_index"] ~= nil) then
-      if(h["active_flows.as_client.anomaly_index"] > 0) then
-	 print("[client: "..h["active_flows.as_client.anomaly_index"].."]")
+      
+      if(h["active_flows.as_client.anomaly_index"] ~= nil) then
+	 if(isanomaly(h["active_flows.as_client.anomaly_index"]) == true) then
+	    print("[client: "..h["active_flows.as_client.anomaly_index"].."]")
+	 end
       end
-   end
 
-   if(h["tcp.bytes.sent.anomaly_index"] > 0) then
-      if(h["tcp.bytes.sent.anomaly_index"] > 0) then print("[tcp.bytes.sent: "..h["tcp.bytes.sent.anomaly_index"].."]") end
-      if(h["tcp.bytes.rcvd.anomaly_index"] > 0) then print("[tcp.bytes.rcvd: "..h["tcp.bytes.rcvd.anomaly_index"].."]") end
-      if(h["udp.bytes.sent.anomaly_index"] > 0) then print("[udp.bytes.sent: "..h["udp.bytes.sent.anomaly_index"].."]") end
-      if(h["udp.bytes.rcvd.anomaly_index"] > 0) then print("[udp.bytes.rcvd: "..h["udp.bytes.rcvd.anomaly_index"].."]") end
-      if(h["icmp.bytes.sent.anomaly_index"] > 0) then print("[icmp.bytes.sent: "..h["icmp.bytes.sent.anomaly_index"].."]") end
-      if(h["icmp.bytes.rcvd.anomaly_index"] > 0) then print("[icmp.bytes.rcvd: "..h["icmp.bytes.rcvd.anomaly_index"].."]") end
-      if(h["other_ip.bytes.sent.anomaly_index"] > 0) then print("[other_ip.bytes.sent: "..h["other_ip.bytes.sent.anomaly_index"].."]") end
-      if(h["other_ip.bytes.rcvd.anomaly_index"] > 0) then print("[other_ip.bytes.rcvd: "..h["other_ip.bytes.rcvd.anomaly_index"].."]") end
+      if(h["tcp.bytes.sent.anomaly_index"] ~= nil) then
+	 if(isanomaly(h["tcp.bytes.sent.anomaly_index"]) == true) then print("[tcp.bytes.sent: "..h["tcp.bytes.sent.anomaly_index"].."]") end
+	 if(isanomaly(h["tcp.bytes.rcvd.anomaly_index"]) == true) then print("[tcp.bytes.rcvd: "..h["tcp.bytes.rcvd.anomaly_index"].."]") end
+	 if(isanomaly(h["udp.bytes.sent.anomaly_index"]) == true) then print("[udp.bytes.sent: "..h["udp.bytes.sent.anomaly_index"].."]") end
+	 if(isanomaly(h["udp.bytes.rcvd.anomaly_index"]) == true) then print("[udp.bytes.rcvd: "..h["udp.bytes.rcvd.anomaly_index"].."]") end
+	 if(isanomaly(h["icmp.bytes.sent.anomaly_index"]) == true) then print("[icmp.bytes.sent: "..h["icmp.bytes.sent.anomaly_index"].."]") end
+	 if(isanomaly(h["icmp.bytes.rcvd.anomaly_index"]) == true) then print("[icmp.bytes.rcvd: "..h["icmp.bytes.rcvd.anomaly_index"].."]") end
+	 if(isanomaly(h["other_ip.bytes.sent.anomaly_index"]) == true) then print("[other_ip.bytes.sent: "..h["other_ip.bytes.sent.anomaly_index"].."]") end
+	 if(isanomaly(h["other_ip.bytes.rcvd.anomaly_index"]) == true) then print("[other_ip.bytes.rcvd: "..h["other_ip.bytes.rcvd.anomaly_index"].."]") end
+      end
+      
+      print("<br>\n")
    end
-   
-   print("<br>\n")
 end
