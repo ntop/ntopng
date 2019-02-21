dee19c218f2ebe39b8e17519d42dfc82b24a4abf
Alfredo Cardigliano <cardigliano@ntop.org>
2019-02-21 19:26:48 +0100

    Fixed n2disk-ntopng service check

--- scripts/lua/modules/recording_utils.lua
+++ scripts/lua/modules/recording_utils.lua
@@ -120,7 +120,7 @@ function recording_utils.checkAvailable()
 
   if(not ntop.isWindows())
     and (not ntop.isnEdge())
-    and os_utils.hasService("n2disk-ntopng", getInterfaceName(getFirstInterfaceId())) then
+    and os_utils.hasService("n2disk-ntopng", "dummy") then
     is_available = true
   end
 
