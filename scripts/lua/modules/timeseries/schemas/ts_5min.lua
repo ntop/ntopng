cdcac0e6a2284853f6866744084c5c448dd9769b
emanuele-f <faranda@ntop.org>
2019-04-29 14:15:32 +0200

    Add event exporter traffic timeseries

--- scripts/lua/modules/timeseries/schemas/ts_5min.lua
+++ scripts/lua/modules/timeseries/schemas/ts_5min.lua
@@ -153,6 +153,17 @@ schema:addMetric("bytes_sent")
 schema:addMetric("bytes_rcvd")
 
 -------------------------------------------------------
+-- EVENT EXPORTER SCHEMAS
+-------------------------------------------------------
+
+schema = ts_utils.newSchema("evexporter_iface:traffic", {step=300, rrd_fname="bytes"})
+schema:addTag("ifid")
+schema:addTag("exporter")
+schema:addTag("ifname")
+schema:addMetric("bytes_sent")
+schema:addMetric("bytes_rcvd")
+
+-------------------------------------------------------
 -- SNMP SCHEMAS
 -------------------------------------------------------
 
