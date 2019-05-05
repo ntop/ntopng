dd3830cfb0eb348eb66f0655b4c4e3d515866fa4
Simone Mainardi <mainardi@ntop.org>
2019-05-05 17:27:05 +0200

    Updates eBPF kubernetes json

--- src/ZMQParserInterface.cpp
+++ src/ZMQParserInterface.cpp
@@ -795,7 +795,7 @@ u_int8_t ZMQParserInterface::parseFlow(const char * const payload, int payload_s
 bool ZMQParserInterface::parseContainerInfo(json_object *jo, ContainerInfo * const container_info) {
   json_object *obj, *obj2;
 
-  if(json_object_object_get_ex(jo, "KUBE", &obj)) {
+  if(json_object_object_get_ex(jo, "K8S", &obj)) {
     if(json_object_object_get_ex(obj, "POD", &obj2))  container_info->data.k8s.pod  = (char*)json_object_get_string(obj2);
     if(json_object_object_get_ex(obj, "NS", &obj2))   container_info->data.k8s.ns   = (char*)json_object_get_string(obj2);
     container_info->data_type = container_info_data_type_k8s;
