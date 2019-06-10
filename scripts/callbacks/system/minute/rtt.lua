14cbf4be372575a8def6d4a68ef7db4d47fa1e8f
Luca Deri <deri@ntop.org>
2019-06-10 12:54:11 +0200

    RTT improvements

--- scripts/callbacks/system/minute/rtt.lua
+++ scripts/callbacks/system/minute/rtt.lua
@@ -9,6 +9,8 @@ local probe = {
   description = "Monitors the round trip time of an host",
 }
 
+local debug = false
+
 -- ##############################################
 
 function probe.isEnabled()
@@ -22,33 +24,80 @@ function probe.loadSchemas(ts_utils)
 
   schema = ts_utils.newSchema("host:rtt", {label = i18n("graphs.num_ms_rtt"), metrics_type = ts_utils.metrics.gauge})
   schema:addTag("host")
+  schema:addTag("label")
   schema:addMetric("millis_rtt")
+end
+
+-- ##############################################
+
+function probe.stateful_alert_handler(numeric_ip, ip_label, trigger_alert, current_value, upper_threshold)
+   if(trigger_alert) then
+      if(current_value == 0) then
+	 io.write("[TRIGGER] Host "..ip_label.."/"..numeric_ip.." in unreacheable\n")
+      else
+	 io.write("[TRIGGER] Host "..ip_label.."/"..numeric_ip.." [value: "..current_value.."][threshold: "..upper_threshold.."]\n")
+      end
+   else
+      io.write("[OK] Host "..ip_label.."/"..numeric_ip.." [value: "..current_value.."][threshold: "..upper_threshold.."]\n")
+   end
 end
 
 -- ##############################################
 
 function probe.runTask(when, ts_utils)
   local hosts = ntop.getHashAllCache("ntopng.prefs.rtt_hosts")
+  local pinged_hosts = {}
+  local max_latency = {}
 
+  if(debug) then
+     io.write("[RTT] Script started\n")
+  end
+  
   if table.empty(hosts) then
     return
   end
 
-  for host, max_rtt in pairs(hosts) do
-    ntop.pingHost(host)
+  for ip_name, max_rtt in pairs(hosts) do
+     -- Host format is <IP>@<label>
+     local e = string.split(ip_name, "%@")
+     local ip_address = e[1]
+     local host_label = e[2]
+
+     if(debug) then
+	io.write("[RTT] Pinging "..ip_address.."/"..host_label.."\n")
+     end
+     
+     ntop.pingHost(ip_address)
+     pinged_hosts[ip_address] = host_label
+     max_latency[ip_address]  = tonumber(max_rtt)
   end
 
-  ntop.msleep(1000) -- wait results for 1 second
-
+  ntop.msleep(2000) -- wait results
+  
   local res = ntop.collectPingResults()
 
-  for host, rtt in pairs(res) do
-    local max_rtt = hosts[address]
-    ts_utils.append("host:rtt", {host = host, millis_rtt = rtt}, when)
+  if(res ~= nil) then
+     for host, rtt in pairs(res) do
+	local max_rtt = max_latency[host]
+	local label   = pinged_hosts[host]
+	ts_utils.append("host:rtt", {host = host, label = label, millis_rtt = rtt}, when)
 
-    if(max_rtt and (rtt > max_rtt)) then
-      -- TODO alert
-    end
+	if(max_rtt and (rtt > max_rtt)) then
+	   probe.stateful_alert_handler(host, label, 1, rtt, max_rtt)
+	else
+	   probe.stateful_alert_handler(host, label, 0, rtt, max_rtt)
+	end
+	
+	pinged_hosts[host] = nil -- Remove key
+     end
+  end
+  
+  for ip,label in pairs(pinged_hosts) do
+     probe.stateful_alert_handler(ip, label, 1, 0, 0)
+  end
+
+  if(debug) then
+     io.write("[RTT] Script is over\n")
   end
 end
 
