846c24c379106a3ac298f524253860c83254efe5
emanuele-f <faranda@ntop.org>
2019-07-02 17:36:21 +0200

    prefs fix

--- scripts/lua/modules/prefs_menu.lua
+++ scripts/lua/modules/prefs_menu.lua
@@ -11,7 +11,7 @@ local hasRadius = ntop.hasRadiusSupport()
 local hasNindex = hasNindexSupport()
 local hasLdap = ntop.hasLdapSupport()
 local max_nindex_retention = 0
-local nindex_utils = require("nindex_utils")
+local nindex_utils = nil
 
 if ntop.isPro() then
   package.path = dirs.installdir .. "/scripts/lua/pro/modules/?.lua;" .. package.path
