959b60705d57900af86633afdab198d9e57a47c8
emanuele-f <faranda@ntop.org>
2019-07-31 12:36:08 +0200

    Increase alerts database version after the latest changes which break compatibility

--- src/AlertsManager.cpp
+++ src/AlertsManager.cpp
@@ -53,6 +53,8 @@ AlertsManager::AlertsManager(int interface_id, const char *filename) : StoreMana
   unlink(filePath);
   sprintf(&filePath[base_offset], "%s", "alerts_v11.db");
   unlink(filePath);
+  sprintf(&filePath[base_offset], "%s", "alerts_v12.db");
+  unlink(filePath);
   filePath[base_offset] = 0;
 
   /* open the newest */
