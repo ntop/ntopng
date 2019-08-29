5cc319a787899fd76ad7532770f92744fd4c83d1
Luca Deri <deri@ntop.org>
2019-08-29 07:13:24 +0200

    Compilation fix

--- include/Utils.h
+++ include/Utils.h
@@ -105,7 +105,7 @@ class Utils {
   static u_int32_t timeval2usec(const struct timeval *tv);
   static void xor_encdec(u_char *data, int data_len, u_char *key);
   static bool isPrintableChar(u_char c);
-  static const char* flowStatus2str(FlowStatus s, AlertType *aType, AlertLevel *aLevel);
+  static const char* flowStatus2str(FlowStatus s, u_int8_t ext_severity, AlertType *aType, AlertLevel *aLevel);
   static char* formatMac(const u_int8_t * const mac, char *buf, u_int buf_len);
   static void  parseMac(u_int8_t *mac, const char *symMac);
   static u_int32_t macHash(const u_int8_t * const mac);
