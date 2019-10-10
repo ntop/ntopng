4c715c7d731f93b18cb88a1d29edce1486496260
emanuele-f <faranda@ntop.org>
2019-10-10 13:36:56 +0200

    Implement L4 protocol filter in flow user scripts

--- scripts/callbacks/interface/flow.lua
+++ scripts/callbacks/interface/flow.lua
@@ -31,6 +31,43 @@ function setup()
    if do_trace then print("flow.lua:setup() called\n") end
 
    available_modules = user_scripts.load(user_scripts.script_types.flow, interface.getId(), "flow", nil, nil, do_benchmark)
+
+   -- Reorganize the modules to optimize lookup by L4 protocol
+   -- E.g. l4_hooks = {tcp -> {periodicUpdate -> {check_tcp_retr}}, other -> {protocolDetected -> {mud, score}}}
+   available_modules.l4_hooks = {}
+
+   for hook_name, hooks in pairs(available_modules.hooks) do
+      -- available_modules.l4_hooks
+      for script_key, callback in pairs(hooks) do
+         local script = available_modules.modules[script_key]
+         local l4_proto
+         local l4_scripts
+
+         if(script.l4_proto ~= nil) then
+            l4_proto = l4_proto_to_id(script.l4_proto)
+         else
+            -- No l4 filter is active for the specified module
+            l4_proto = 255 -- "any" L4 proto
+         end
+
+         if(l4_proto == nil) then
+            traceError(TRACE_ERROR, TRACE_CONSOLE, string.format("Unknown l4_proto '%s' in module '%s', skipping", script.l4_proto, script_key))
+            goto continue
+         end
+
+         l4_scripts = available_modules.l4_hooks[l4_proto]
+
+         if(l4_scripts == nil) then
+            l4_scripts = {}
+            available_modules.l4_hooks[l4_proto] = l4_scripts
+         end
+
+         l4_scripts[hook_name] = l4_scripts[hook_name] or {}
+         l4_scripts[hook_name][script_key] = callback
+
+         ::continue::
+      end
+   end
 end
 
 -- #################################################################
@@ -51,44 +88,58 @@ end
 
 -- Function for the actual module execution. Iterates over available (and enabled)
 -- modules, calling them one after one.
-local function call_modules(mod_fn)
-   if table.empty(available_modules.hooks[mod_fn]) then
-      if do_trace then print(string.format("No flow.lua modules, skipping %s() for %s\n", mod_fn, shortFlowLabel(flow.getInfo()))) end
+-- @param l4_filter the L4 protocol filter
+-- @param mod_fn the callback to call
+local function call_modules(l4_filter, mod_fn)
+   local hooks = available_modules.l4_hooks[l4_filter]
+
+   if(hooks ~= nil) then
+      hooks = hooks[mod_fn]
+   end
+
+   if(hooks == nil) then
+      if do_trace then print(string.format("No flow.lua modules, skipping %s(%d) for %s\n", mod_fn, l4_filter, shortFlowLabel(flow.getInfo()))) end
       return
    end
 
+   -- TODO too expensive, remove
    local info = flow.getInfo()
 
-   for mod_key, hook_fn in pairs(available_modules.hooks[mod_fn]) do
+   for mod_key, hook_fn in pairs(hooks) do
       if do_trace then print(string.format("%s() [check: %s]: %s\n", mod_fn, mod_key, shortFlowLabel(info))) end
 
       hook_fn({
         flow_info = info
       })
    end
-
 end
 
 -- #################################################################
 
-function protocolDetected()
-   return call_modules("protocolDetected")
+-- Given an L4 protocol, we must call both the hooks registered for that protocol and
+-- the hooks registered for any L4 protocol (id 255)
+function protocolDetected(l4_proto)
+   call_modules(l4_proto, "protocolDetected")
+   call_modules(255, "protocolDetected")
 end
 
 -- #################################################################
 
-function statusChanged()
-   return call_modules("statusChanged")
+function statusChanged(l4_proto)
+   call_modules(l4_proto, "statusChanged")
+   call_modules(255, "protocolDetected")
 end
 
 -- #################################################################
 
-function flowEnd()
-   return call_modules("flowEnd")
+function flowEnd(l4_proto)
+   call_modules(l4_proto, "flowEnd")
+   call_modules(255, "protocolDetected")
 end
 
 -- #################################################################
 
-function periodicUpdate()
-   return call_modules("periodicUpdate")
+function periodicUpdate(l4_proto)
+   call_modules(l4_proto, "periodicUpdate")
+   call_modules(255, "protocolDetected")
 end
