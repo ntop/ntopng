b44900276bbb5cbc5798c316e13fbdab4e7e3eab
Simone Mainardi <mainardi@ntop.org>
2019-10-22 13:00:19 +0200

    Fixes ports pie charts with VLANs and reworks their appearence
    Fixes #2971

--- scripts/lua/iface_ports_list.lua
+++ scripts/lua/iface_ports_list.lua
@@ -1,109 +1,97 @@
 --
--- (C) 2013-18 - ntop.org
+-- (C) 2013-19 - ntop.org
 --
 
-dirs = ntop.getDirs()
+local dirs = ntop.getDirs()
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 
 require "lua_utils"
+local format_utils = require "format_utils"
 
 sendHTTPContentTypeHeader('text/html')
 
-interface.select(ifname)
+local host = _GET["host"]
+local host_info = url2hostinfo(_GET)
+local host_key = hostinfo2hostkey(host_info)
 
-host = _GET["host"]
+local function fill_ports_array(field_key, flows_stats)
+   local ports_array = {}
 
-client_ports = nil
-server_ports = nil
+   for _, value in ipairs(flows_stats) do
+      local p = value[field_key..".port"]
+      if(ports_array[p] == nil) then ports_array[p] = 0 end
+      ports_array[p] = ports_array[p] + value["bytes"]
+   end
 
-function fill_ports_array(field_key, flows_stats, host)
-    local ports_array = {}
-    for key, value in ipairs(flows_stats) do
-      if ((host == nil) or (flows_stats[key][field_key..".ip"] == host)) then
-        p = flows_stats[key][field_key..".port"]
-        if(ports_array[p] == nil) then ports_array[p] = 0 end
-        ports_array[p] = ports_array[p] + flows_stats[key]["bytes"]
-      end
-    end
-    return ports_array
+   return ports_array
 end
 
-if (host == nil) then
-  flows_stats = interface.getFlowsInfo()
-else
- flows_stats = interface.getFlowsInfo(host)
-end
-
-if(flows_stats == nil) then
-   flows_stats = {}
-else
+local flows_stats = interface.getFlowsInfo(host_key) or {}
+if flows_stats["flows"] then
    flows_stats = flows_stats["flows"]
 end
 
-client_ports = fill_ports_array("cli", flows_stats, host)
-server_ports = fill_ports_array("srv", flows_stats, host)
+local client_ports = fill_ports_array("cli", flows_stats)
+local server_ports = fill_ports_array("srv", flows_stats)
+local ports
 
 if(_GET["clisrv"] == "server") then
-  ports = server_ports
+   ports = server_ports
 else
-  ports = client_ports
+   ports = client_ports
 end
 
-_ports = { } 
-tot = 0
+local _ports = { }
+local tot = 0
 
-for k,v in pairs(ports) do
-  _ports[v] = k
-  tot = tot + v
+for k, v in pairs(ports) do
+   _ports[k] = v
+   tot = tot + v
 end
 
-threshold = (tot * 5) / 100
+local threshold_percent = 5
+local threshold = (tot * threshold_percent) / 100
 
 print "[ "
 
-num = 0
-accumulate = 0
-for key, value in pairsByKeys(_ports, rev) do
-      if key < threshold then
-	 break
-      end
+local min_num = 4
+local num = 0
+local accumulate = 0
+for key, value in pairsByValues(_ports, rev) do
+   if value < threshold then
+      break
+   end
 
-      if(num > 0) then
-	 print ",\n"
-      end
+   if(num > 0) then
+      print ",\n"
+   end
 
-      print("\t { \"label\": \"" .. value .."\", \"value\": ".. key ..", \"url\": \""..ntop.getHttpPrefix().."/lua/port_details.lua?port="..value)
+   print("\t { \"label\": \"" .. key .."\", \"value\": ".. value ..", \"url\": \""..ntop.getHttpPrefix().."/lua/port_details.lua?port="..key.."&host="..hostinfo2hostkey(host_info))
 
-      if(host ~= nil) then 
-      print("&host="..host)
-      end
-      
-      print("\" }")
+   print("\" }")
 
-       accumulate = accumulate + key
-       num = num + 1
-
-       if(num == max_num_entries) then
-	  break
-       end      
+   accumulate = accumulate + value
+   num = num + 1
 end
 
 -- In case there is some leftover do print it as "Other"
 if accumulate < tot then
-  local other_label = i18n("other")
-  if(num > 0) then
-    print (",\n")
-  else
-    if table.len(_ports) > 0 then
-      other_label = i18n("num_different_ports", { num = table.len(_ports)})
-    end
-  end
+   local other_label = i18n("other")
+   local url = ntop.getHttpPrefix().."/lua/host_details.lua?page=flows&host="..hostinfo2hostkey(host_info)
 
-  print("\t { \"label\": \""..other_label.."\", \"value\": ".. (tot-accumulate) .." }")
+   if(num > 0) then
+      print (",\n")
+   else
+      if table.len(_ports) > 0 then
+	 other_label = i18n("num_different_ports", {num = format_utils.formatValue(table.len(_ports)), threshold = threshold_percent})
+      end
+   end
+
+   print("\t { \"label\": \""..other_label.."\", \"value\": ".. (tot - accumulate) ..", \"url\": \""..url.."\"}")
 end
 
 if tot == 0 then
    print("\t { \"label\": \""..i18n("no_ports").."\", \"value\": 0 }")
 end
 
- print "\n]"
+print "\n]"
