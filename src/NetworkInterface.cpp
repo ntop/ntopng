44a9d65f242533c0f8855c2242cc2e2a4d3095dd
Simone Mainardi <mainardi@ntop.org>
2019-10-23 14:51:25 +0200

    Compilation fix

--- src/NetworkInterface.cpp
+++ src/NetworkInterface.cpp
@@ -5412,7 +5412,11 @@ void NetworkInterface::lua(lua_State *vm) {
 void NetworkInterface::lua_hash_tables_stats(lua_State *vm) {
   GenericHash *gh[] = {flows_hash, hosts_hash, macs_hash,
 		       vlans_hash, ases_hash, countries_hash,
-		       arp_hash_matrix, aggregated_flows_hash};
+		       arp_hash_matrix
+#ifdef NTOPNG_PRO
+		       ,aggregated_flows_hash
+#endif
+  };
 
   lua_newtable(vm);
 
