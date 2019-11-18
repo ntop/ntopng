bd5041ead24263c4da911a4e64eec361728c05bb
Simone Mainardi <mainardi@ntop.org>
2019-11-18 11:35:43 +0100

    Renames walkIdle to walkAllStates and documents method behavior

--- src/GenericHash.cpp
+++ src/GenericHash.cpp
@@ -122,7 +122,7 @@ bool GenericHash::add(GenericHashEntry *h, bool do_lock) {
 
 /* ************************************ */
 
-void GenericHash::walkIdle(bool (*walker)(GenericHashEntry *h, void *user_data), void *user_data) {
+void GenericHash::walkAllStates(bool (*walker)(GenericHashEntry *h, void *user_data), void *user_data) {
   vector<GenericHashEntry*> *cur_idle = NULL;
   u_int new_walk_idle_start_hash_id = 0;
   bool update_walk_idle_start_hash_id;
