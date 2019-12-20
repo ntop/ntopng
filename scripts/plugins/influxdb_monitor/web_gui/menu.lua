818a21d9eeb48d844b9e2169b9b0dcf0e9c454e0
emanuele-f <faranda@ntop.org>
2019-12-20 11:03:44 +0100

    Add ability to hide a plugin menu entry

--- scripts/plugins/influxdb_monitor/web_gui/menu.lua
+++ scripts/plugins/influxdb_monitor/web_gui/menu.lua
@@ -2,4 +2,9 @@ return {
   label = "InfluxDB",
   script = "influxdb_stats.lua",
   sort_order = 1600,
+  is_shown = function()
+    local ts_utils = require("ts_utils_core")
+
+    return(ts_utils.getDriverName() == "influxdb")
+  end
 }
