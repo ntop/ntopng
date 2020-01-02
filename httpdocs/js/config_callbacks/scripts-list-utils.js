c61f8eb3deda1ff12adcd7aeaa2eff4452605664
gabryon99 <pappalardo@ntop.org>
2020-01-02 13:17:25 +0100

    check csrf token expiration

--- httpdocs/js/config_callbacks/scripts-list-utils.js
+++ httpdocs/js/config_callbacks/scripts-list-utils.js
@@ -141,6 +141,9 @@
                           }
                        })
 
+                       // hide alert
+                       $("#alert-row-buttons").hide();
+
                        // disable all buttons to prevent more requests
                        $("#scripts-config input[name$='-check']").attr("disabled", "").parent().addClass("disabled");
 
@@ -152,12 +155,24 @@
                           confset_id: confset_id
                        })
                        .done((d, status, xhr) => {
-                          location.reload();
+
+                          if (!d.success) {
+                              $("#alert-row-buttons").text(data.error).removeClass('d-none').show();
+                              // update csrf
+                              csrf_toggle_buttons = d.csrf;
+                          }
+                          
+                          if (d.success) location.reload();
+
                        })
                        .fail(({status, statusText}) => {
                           check_status_code(status, statusText, null);
-                       })
-                       .always(() => {
+
+                          // if the csrf has expired 
+                          if (status == 200) {
+                              $("#alert-row-buttons").text(`${i18n.expired_csrf}`).removeClass('d-none').show();
+                          }
+
                           // re eanble buttons
                           $("#scripts-config input[name$='-check']").removeAttr("disabled").parent().removeClass("disabled");
                        })
@@ -272,6 +287,9 @@
 
           // destructure gui and hooks from data
           const {gui, hooks} = data;
+
+          // hide previous error
+          $("#apply-error").hide();
 
           const build_gui = (gui, hooks) => {
 
@@ -445,8 +463,16 @@
               .done((d, status, xhr) => {
 
                  if (check_status_code(xhr.status, xhr.statusText, $error_label)) return;
+
+                 if (!d.success) {
+
+                     $error_label.text(d.error).show();
+                     // update token
+                     csrf_edit_config = d.csrf;
+                 }
+
                  // if the operation was successfull then reload the page
-                 location.reload();
+                 if (d.success) location.reload();
               })
               .fail(({status, statusText}) => {
 
