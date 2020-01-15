4a1d217d3a90b475f96ceb3989ea2bccc7bd7a54
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-01-15 11:58:09 +0100

    Rest endpoint to export user scripts configuration

---
+++ scripts/lua/rest/get/scripts/config.lua
@@ -0,0 +1,29 @@
+--
+-- (C) 2019-20 - ntop.org
+--
+dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+require "lua_utils"
+
+local info = ntop.getInfo() 
+
+local json = require ("dkjson")
+local page_utils = require("page_utils")
+local format_utils = require("format_utils")
+local os_utils = require "os_utils"
+local user_scripts = require "user_scripts"
+
+if not haveAdminPrivileges() then
+   sendHTTPContentTypeHeader('text/html')
+   page_utils.print_header()
+   dofile(dirs.installdir .. "/scripts/lua/inc/menu.lua")
+   print("<div class=\"alert alert-danger\"><img src=".. ntop.getHttpPrefix() .. "/img/warning.png>"..i18n("error_not_granted").."</div>")
+  return
+end
+
+sendHTTPHeader('application/json')
+
+local conf = user_scripts.getConfigsets()
+
+print(json.encode(conf, nil))
