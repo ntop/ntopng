a97dae6cbb66418c30a0b643b0db6688a150094c
Luca Deri <deri@ntop.org>
2020-01-30 20:50:54 +0100

    Footer UI fixes

--- scripts/lua/inc/footer.lua
+++ scripts/lua/inc/footer.lua
@@ -78,11 +78,11 @@ end
       print [[
         </div>
 
-        <div class="col-4">
+        <div class="col-4 text-right">
          <small>
-           <div style="width: 45em" class="text-right">
+           <div class="text-right">
             <i class="fas fa-clock"></i> <div class="d-inline-block" id='network-clock' style="width: 8em"></div> | ]]
-           print(i18n("about.uptime")) print[[: <div class="d-inline-block" id='network-uptime' style="width: 4em"></div>
+           print(i18n("about.uptime")) print[[: <div class="d-inline-block" id='network-uptime' style="width: 2em"></div>
          </div>
         </small>
         </div>
