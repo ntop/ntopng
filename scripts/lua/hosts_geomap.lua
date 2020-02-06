69f61415aea01131c706a85bf6b7ad9a3a51b15d
gabryon99 <pappalardo@ntop.org>
2020-02-06 10:46:23 +0100

    implemented leaflet maps support

--- scripts/lua/hosts_geomap.lua
+++ scripts/lua/hosts_geomap.lua
@@ -1,5 +1,5 @@
 --
--- (C) 2013-20 - ntop.org
+-- (C) 2020 - ntop.org
 --
 
 dirs = ntop.getDirs()
@@ -8,7 +8,6 @@ package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 require "lua_utils"
 
 local page_utils = require("page_utils")
-
 sendHTTPContentTypeHeader('text/html')
 
 page_utils.set_active_menu_entry(page_utils.menu_entries.geo_map)
@@ -21,44 +20,144 @@ local num = hosts_stats["numHosts"]
 hosts_stats = hosts_stats["hosts"]
 
 if(num > 0) then
-print [[
 
-<style type="text/css">
-  #map-canvas { width: 100%; height: 480px; }
-</style>
+  print ([[
+    <div class="container-fluid">
+      <div class="row">
+        <div class='col-md-12 col-lg-12 col-xs-12'>
+          <div class='border-bottom pb-2 mb-3'> 
+            <h1 class='h2'>]].. i18n("geo_map.hosts_geomap").. [[</h1>
+          </div>
+          <div id='geomap-alert' style="display: none" role="alert" class='alert alert-danger'>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <p id='error-message'></p>
+          </div>
+          <div style="height: 720px" id="map-canvas"></div>
+          <div class='border-top mt-4'>
+            <p id='my-location'></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  ]])
 
-<h2>]] print(i18n("geo_map.hosts_geomap")) print[[</H2>
+  print([[
+    <link rel="stylesheet" href="]].. ntop.getHttpPrefix() ..[[/leaflet/leaflet.css"/>
+    <link rel="stylesheet" href="]].. ntop.getHttpPrefix() ..[[/leaflet/MarkerCluster.Default.css"/>
+    <link rel="stylesheet" href="]].. ntop.getHttpPrefix() ..[[/leaflet/MarkerCluster.css"/>
+    <script src="]].. ntop.getHttpPrefix() ..[[/leaflet/leaflet.js" type="text/javascript"></script>
+    <script src="]].. ntop.getHttpPrefix() ..[[/leaflet/leaflet.markercluster.js" type="text/javascript"></script>
 
-]]
+    <script type='text/javascript'>
 
-addGoogleMapsScript()
+    $(document).ready(function() {
 
-print[[
+      const default_coords = [41.9, 12.4833333];
+      const zoom_level = 4;
 
-    <script src="]] print(ntop.getHttpPrefix()) print [[/js/markerclusterer.js"></script>
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="span8">
-      <div id="map-canvas"></div>
-]]
+      // initialize alert api
+      $('#geomap-alert').alert();
 
-dofile(dirs.installdir .. "/scripts/lua/show_geolocation_note.lua")
+      const display_localized_error = (error_code) => {
+        $('#geomap-alert p').html(`]].. i18n("geo_map.geolocation_error") ..[[[${error_code}]: ]].. i18n("geo_map.using_default_location") ..[[`);
+        $('#geomap-alert').removeClass('alert-info').addClass('alert-danger').show();
+      }
 
-print [[
-</div>
-</div>
-</div>
+      const display_localized_position = (position) => {
+        $('#my-location').html(`
+          ]].. i18n("geo_map.browser_reported_home_map")..[[: 
+          <a href='https://www.openstreetmap.org/#map=6/${position[0]}/${position[1]}'>
+          ]]..i18n("geo_map.latitude").. [[: ${position[0]}, ]].. i18n("geo_map.longitude").. [[: ${position[1]} </a>
+        `);
+      }
 
-<script type="text/javascript">
-var zoomIP = undefined;
-var url_prefix = "]] print(ntop.getHttpPrefix()) print [[";
-</script>
+      const display_localized_no_geolocation_msg = () => {
 
-<script type="text/javascript" src="]] print(ntop.getHttpPrefix()) print [[/js/googleMapJson.js" ></script>
+        $('#geomap-alert p').html(`]].. i18n("geo_map.unavailable_geolocation") .. ' ' .. i18n("geo_map.using_default_location") ..[[`);
+        $('#geomap-alert').addClass('alert-info').removeClass('alert-danger').show();
 
-</body>
-</html>
-]]
+      }
+
+      const display_errors = (errors) => {
+
+        const error_messages = {
+          1: 'Permission denied',
+          2: 'Position unavailable',
+          3: 'Request timeout'
+        };
+
+        const error_code = errors[error.code];
+        
+        display_localized_error(error_code);
+      }
+
+      const init_map = () => {
+
+        const timeout_val = 10 * 1000 * 1000;
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(show_positions, display_errors, { enableHighAccuracy: true, timeout: timeout_val, maximumAge: 0 });
+        }
+      }
+
+      const draw_markers = (json, map_markers, map) => {
+
+        const {center, objects} = json;
+          const hosts = objects.map((h) => {
+            return {
+              lat: h.host[0].lat,
+              lng: h.host[0].lng,
+              html: h.host[0].html,
+              ip_address: h.host[0].name
+            }
+          });
+  
+          hosts.forEach(h => {
+            const marker = L.marker([h.lat, h.lng], { title: h.ip_address }).bindPopup(`
+              <div class='infowin'>
+                <a href='/lua/host_details.lua?host=${h.ip_address}'>${h.ip_address}</a>
+                <hr>
+                ${h.html}
+              </div>
+            `);
+            map_markers.addLayer(marker);
+          });
+  
+          map.addLayer(map_markers);
+      }
+
+      const show_positions = (current_user_position) => {
+
+        const user_coords = [current_user_position.coords.latitude, current_user_position.coords.longitude];
+
+        if (user_coords[0] == 0 && user_coords[1] == 0) {
+          display_localized_no_geolocation_msg();
+        }
+
+        const hosts_map = L.map('map-canvas').setView(user_coords || default_coords, zoom_level);
+        const map_markers = L.markerClusterGroup();
+
+        display_localized_position(user_coords || default_coords);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(hosts_map);
+
+        $.get(`]].. ntop.getHttpPrefix() ..[[/lua/get_geo_hosts.lua`).then((data) => {
+          draw_markers(data, map_markers, hosts_map);
+        });
+
+      }     
+
+      init_map();
+
+    });
+
+    </script>
+  ]])
+
 else 
    print("<div class=\"alert alert-danger\"><img src=".. ntop.getHttpPrefix() .. "/img/warning.png> " .. i18n("no_results_found") .. "</div>")
 end
