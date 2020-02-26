da805487d3017a2eb7435408a5c5124837dc662c
emanuele-f <faranda@ntop.org>
2020-02-26 10:53:33 +0100

    Typo fix

--- scripts/lua/modules/timeseries/ts_utils_core.lua
+++ scripts/lua/modules/timeseries/ts_utils_core.lua
@@ -856,7 +856,7 @@ function ts_utils.getPossiblyChangedSchemas()
     "redis:keys",
     "redis:memory",
     "host:score",
-    "periodic_script:rrd",
+    "periodic_script:rrd_writes",
   }
 end
 
