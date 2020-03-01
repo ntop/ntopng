3b9478b11124d31d1bf75983fb035e5a65bbb105
gabryon99 <pappalardo@ntop.org>
2020-03-01 21:11:31 +0100

    fixed sync icon

--- scripts/lua/inc/menu.lua
+++ scripts/lua/inc/menu.lua
@@ -890,7 +890,7 @@ if is_admin and ntop.isPackage() and not ntop.isWindows() then
 print([[
    <li class="dropdown-divider"></li>
    <li class="dropdown-header" id="updates-info-li">No updates available.</li>
-   <li><button class="dropdown-item" id="updates-install-li"><i class="fas fa-refresh"></i> Check for updates</button></li>
+   <li><button class="dropdown-item" id="updates-install-li"><i class="fas fa-sync"></i> Check for updates</button></li>
 ]])
 end
 
