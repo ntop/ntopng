ntop/ntopng

commit 297195abab6702e3bc1daaa53deabfccaf970600

Add dropped/stored/queries system stats for alerts

src/main.cpp
@@ -319,7 +319,6 @@ int main(int argc, char *argv[])
   ntop->loadGeolocation();
   ntop->loadMacManufacturers(prefs->get_docs_dir());
   ntop->loadTrackers();
-  ntop->registerHTTPserver(new HTTPserver(prefs->get_docs_dir(), prefs->get_scripts_dir(), ntop->get_runtime_dir()));
 
   /* initInterface writes DB data on disk, keep it after changing user
    * Note: privileges can be dropped by mongoose (creating HTTPserver) */
@@ -329,6 +328,9 @@ int main(int argc, char *argv[])
     if((iface = ntop->getInterface(i)) != NULL)
       ntop->initInterface(iface);
   }
+
+  /* Register the HTTP server after the interfaces have been initialized */
+  ntop->registerHTTPserver(new HTTPserver(prefs->get_docs_dir(), prefs->get_scripts_dir(), ntop->get_runtime_dir()));
 
   /*
     We have created the network interface and thus changed user. Let's now check

include/NetworkInterface.h
@@ -78,6 +78,7 @@ class NetworkInterface : public AlertableEntity {
   u_int64_t num_active_alerted_flows, num_idle_alerted_flows;
   u_int64_t num_active_misbehaving_flows, num_idle_misbehaving_flows;
   u_int32_t num_dropped_alerts, prev_dropped_alerts, checked_dropped_alerts, num_dropped_flow_scripts_calls;
+  u_int64_t num_written_alerts, num_alerts_queries;
   u_int64_t num_new_flows;
   bool has_stored_alerts;
   AlertsQueue *alertsQueue;
@@ -800,6 +801,11 @@ class NetworkInterface : public AlertableEntity {
   inline bool hasAlerts()                                 { return(has_stored_alerts || (getNumEngagedAlerts() > 0)); }
   inline void refreshHasAlerts()                          { has_stored_alerts = alertsManager ? alertsManager->hasAlerts() : false; }
   inline void incNumDroppedAlerts(u_int32_t num_dropped)  { num_dropped_alerts += num_dropped; }
+  inline void incNumWrittenAlerts()			  { num_written_alerts++; }
+  inline void incNumAlertsQueries()			  { num_alerts_queries++; }
+  inline u_int64_t getNumDroppedAlerts()		  { return(num_dropped_alerts); }
+  inline u_int64_t getNumWrittenAlerts()		  { return(num_written_alerts); }
+  inline u_int64_t getNumAlertsQueries()		  { return(num_alerts_queries); }
   void walkAlertables(int entity_type, const char *entity_value, std::set<int> *entity_excludes,
 	    AddressTree *allowed_nets, alertable_callback *callback, void *user_data);
   void getEngagedAlertsCount(lua_State *vm, int entity_type, const char *entity_value,

scripts/locales/en.lua
@@ -3378,6 +3378,7 @@ local lang = {
     ["alert_type"] = "Alert Type",
     ["alerts"] = "Alerts",
     ["alerts_are_disabled_message"] = "Alerts are disabled. Please check the preferences page to enable them.",
+    ["alerts_queries"] = "Alerts Queries",
     ["alerts_to_purge"] = "Alerts to Purge",
     ["confirm_delete_alert"] = "Do you really want to delete the specified alert",
     ["confirm_release_alert"] = "Do you really want to release the specified alert?",
@@ -3442,6 +3443,7 @@ local lang = {
     ["reset_alert_database_message"] = "This action will remove any alerts related data, including<ul><li>Runtime alerts status</li><li>Persistent alerts SQLite storage</li></ul>Please go to the User Scripts settings if you want to reset also the alerts configuration.<br><br>Do you really want to proceed",
     ["rtt"] = "RTT",
     ["startup"] = "Startup",
+    ["stored_alerts"] = "Stored Alerts",
     ["trigger_alert_descr"] = "Enable alerts triggering on this alarmable",
     ["trigger_host_alert_descr"] = "Trigger alerts generated by %{host}",
     ["trigger_iface_alert_descr"] = "Trigger alerts generated by %{iface}",

scripts/lua/inc/footer.lua
@@ -220,6 +220,9 @@ print[[
 		   mem_used_ratio = mem_used_ratio + "%";
 		   $('#ram-used').html('Used: ' + mem_used_ratio + ' / Available: ' + bytesToSize((mem_total - mem_used) * 1024) + ' / Total: ' + bytesToSize(mem_total * 1024));
 		   $('#ram-process-used').html('Used: ' + bytesToSize(rsp.system_host_stats.mem_ntopng_resident * 1024));
+		   $('#dropped-alerts').html(rsp.system_host_stats.dropped_alerts ? formatValue(rsp.system_host_stats.dropped_alerts) : "");
+		   $('#stored-alerts').html(rsp.system_host_stats.written_alerts ? formatValue(rsp.system_host_stats.written_alerts) : "");
+		   $('#alerts-queries').html(rsp.system_host_stats.alerts_queries ? formatValue(rsp.system_host_stats.alerts_queries) : "");
 		}
 
 		if(rsp.system_host_stats.cpu_load !== undefined)

scripts/lua/system_stats.lua
@@ -67,6 +67,7 @@ if(page == "overview") then
    print("<table class=\"table table-bordered table-striped\">\n")
 
    local system_rowspan = 1
+   local ntopng_rowspan = 20
    local system_host_stats = system_utils.systemHostStats()
 
    if system_host_stats["cpu_load"] ~= nil then  system_rowspan = system_rowspan + 1 end
@@ -83,13 +84,19 @@ if(page == "overview") then
       print("<tr><th nowrap>"..i18n("about.ram_memory").."</th><td><span id='ram-used'></span></td></tr>\n")
    end
 
-   print("<tr><th rowspan=20>"..info["product"].."</th>")
+   print("<tr><th rowspan=".. ntopng_rowspan ..">"..info["product"].."</th>")
 
    if(info.pid ~= nil) then
       print("<tr><th nowrap>PID (Process ID)</th><td>"..info.pid.."</td></tr>\n")
    end
    if system_host_stats["mem_ntopng_resident"] ~= nil then
       print("<tr><th nowrap>"..i18n("about.ram_memory").."</th><td><span id='ram-process-used'></span></td></tr>\n")
+   end
+
+   if areAlertsEnabled() then
+      print("<tr><th nowrap>"..i18n("show_alerts.dropped_alerts").."</th><td><span id='dropped-alerts'></span></td></tr>\n")
+      print("<tr><th nowrap>"..i18n("show_alerts.stored_alerts").."</th><td><span id='stored-alerts'></span></td></tr>\n")
+      print("<tr><th nowrap>"..i18n("show_alerts.alerts_queries").."</th><td><span id='alerts-queries'></span></td></tr>\n")
    end
 
    print("<tr id='storage-info-tr'><th>"..i18n("traffic_recording.storage_utilization").."</th><td>")

src/AlertsManager.cpp
@@ -279,6 +279,8 @@ bool AlertsManager::hasAlerts() {
       rc = true;
   }
 
+  iface->incNumAlertsQueries();
+
 out:
   if(stmt) sqlite3_finalize(stmt);
   m.unlock(__FILE__, __LINE__);
@@ -383,6 +385,8 @@ int AlertsManager::storeAlert(time_t tstart, time_t tend, int granularity, Alert
 	goto out;
       }
 
+      iface->incNumAlertsQueries();
+
       /* Try and read the rowid (if the record exists) */
       while((rc = sqlite3_step(stmt2)) != SQLITE_DONE) {
 	if(rc == SQLITE_ROW) {
@@ -423,6 +427,7 @@ int AlertsManager::storeAlert(time_t tstart, time_t tend, int granularity, Alert
 
 	/* Done updating... */
 	*rowid = cur_rowid;
+	iface->incNumWrittenAlerts();
 	rc = 0;
 	goto out;
       } else {
@@ -466,6 +471,7 @@ int AlertsManager::storeAlert(time_t tstart, time_t tend, int granularity, Alert
 
     /* Success */
     *rowid = sqlite3_last_insert_rowid(db);
+    iface->incNumWrittenAlerts();
     rc = 0;
 
   out:
@@ -641,6 +647,8 @@ int AlertsManager::storeFlowAlert(lua_State *L, int index, u_int64_t *rowid) {
     goto out;
   }
 
+  iface->incNumAlertsQueries();
+
   if(replace_alert) {
     /* Match the exact flow */
     if(sqlite3_bind_int(stmt,    9, first_seen)) {
@@ -796,6 +804,8 @@ int AlertsManager::storeFlowAlert(lua_State *L, int index, u_int64_t *rowid) {
     cur_rowid = sqlite3_last_insert_rowid(db);
   }
 
+  /* Success */
+  iface->incNumWrittenAlerts();
   rc = 0;
 out:
 
@@ -892,6 +902,7 @@ int AlertsManager::queryAlertsRaw(lua_State *vm, const char *selection,
 
     ar.vm = vm, ar.current_offset = 0;
     rc = sqlite3_exec(db, query, getAlertsCallback, (void*)&ar, &zErrMsg);
+    iface->incNumAlertsQueries();
 
     if( rc != SQLITE_OK ){
       rc = 1;

src/LuaEngine.cpp
@@ -7110,11 +7110,27 @@ static int ntop_get_uptime(lua_State* vm) {
 // ***API***
 static int ntop_system_host_stat(lua_State* vm) {
   float cpu_load;
+  u_int64_t dropped_alerts = 0, written_alerts = 0, alerts_queries = 0;
+
   ntop->getTrace()->traceEvent(TRACE_DEBUG, "%s() called", __FUNCTION__);
 
   lua_newtable(vm);
   if(ntop->getCpuLoad(&cpu_load)) lua_push_float_table_entry(vm, "cpu_load", cpu_load);
   Utils::luaMeminfo(vm);
+
+  for(int i=-1; i<ntop->get_num_interfaces(); i++) {
+    NetworkInterface *iface = (i == -1) ? ntop->getSystemInterface() : ntop->getInterface(i);
+
+    if(iface) {
+      dropped_alerts += iface->getNumDroppedAlerts();
+      written_alerts += iface->getNumWrittenAlerts();
+      alerts_queries += iface->getNumAlertsQueries();
+    }
+  }
+
+  lua_push_uint64_table_entry(vm, "dropped_alerts", dropped_alerts);
+  lua_push_uint64_table_entry(vm, "written_alerts", written_alerts);
+  lua_push_uint64_table_entry(vm, "alerts_queries", alerts_queries);
 
   return(CONST_LUA_OK);
 }

src/NetworkInterface.cpp
@@ -279,6 +279,7 @@ void NetworkInterface::init() {
 
   gettimeofday(&last_periodic_stats_update, NULL);
   num_live_captures = 0, num_dropped_alerts = 0, checked_dropped_alerts = 0, prev_dropped_alerts = 0;
+  num_written_alerts = num_alerts_queries = 0;
   memset(live_captures, 0, sizeof(live_captures));
   memset(&num_alerts_engaged, 0, sizeof(num_alerts_engaged));
   num_active_alerted_flows = num_idle_alerted_flows = 0;

commit bd1de19c1e9ffc702710eab0eecadf67f30db174

Fix privileges dropping logic and fix causing bad file owner
Fixes #3480
Broken since 297195abab6702e3bc1daaa53deabfccaf970600

src/main.cpp
@@ -301,17 +301,6 @@ int main(int argc, char *argv[])
   }
 #endif
 
-  /*
-    It's safe to drop privileges now if http and https ports
-    are non-privileged. Otherwise it is necessary to delay
-    the privilege drop after the web server bind()
-   */
-  if(prefs->do_change_user()
-     && (prefs->get_http_port()  >= 1024 || prefs->get_http_port()  == 0)
-     && (prefs->get_https_port() >= 1024 || prefs->get_https_port() == 0)) {
-    Utils::dropPrivileges();
-  }
-
 #ifndef HAVE_NEDGE
   ntop->createExportInterface();
 #endif
@@ -320,17 +309,23 @@ int main(int argc, char *argv[])
   ntop->loadMacManufacturers(prefs->get_docs_dir());
   ntop->loadTrackers();
 
-  /* initInterface writes DB data on disk, keep it after changing user
-   * Note: privileges can be dropped by mongoose (creating HTTPserver) */
+  /* Register the HTTP server before dropping the privileges. This is required
+   * in order to possibly bind the HTTP server to privileged ports (< 1024) */
+  ntop->registerHTTPserver(new HTTPserver(prefs->get_docs_dir(), prefs->get_scripts_dir(), ntop->get_runtime_dir()));
+
+  /* Drop the privileges before initializing the network interfaces. This
+   * is necessary as the initialization may create files/directories, which
+   * should not be created as root. */
+  if(ntop->getPrefs()->do_change_user())
+    Utils::dropPrivileges();
+
+  /* initInterface writes DB data on disk, keep it after changing user */
   for(int i = 0; i < MAX_NUM_INTERFACE_IDS; i++) {
     NetworkInterface *iface;
 
     if((iface = ntop->getInterface(i)) != NULL)
       ntop->initInterface(iface);
   }
-
-  /* Register the HTTP server after the interfaces have been initialized */
-  ntop->registerHTTPserver(new HTTPserver(prefs->get_docs_dir(), prefs->get_scripts_dir(), ntop->get_runtime_dir()));
 
   /*
     We have created the network interface and thus changed user. Let's now check

third-party/mongoose/mongoose.c
@@ -4648,20 +4648,7 @@ static int set_ports_option(struct mg_context *ctx) {
   if (!success) {
     close_all_listening_sockets(ctx);
   }
-#if defined _UTILS_H_ && defined _NTOP_CLASS_H_
-  else {
-  /*
-    Privileges drop may have been delayed until now, that is,
-    after the bind(). This happens when the web server needs to be
-    bound on privileged-ports < 1024. If this is the case, now
-    it's time to drop the privileges.
-   */
-  if(ntop->getPrefs()->do_change_user()
-     && ( (ntop->getPrefs()->get_http_port()   < 1024)
-       || (ntop->getPrefs()->get_https_port()  < 1024)))
-    Utils::dropPrivileges();
-  }
-#endif
+
   return success;
 }
 