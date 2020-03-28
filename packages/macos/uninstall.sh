79377d86f398ec06fcad5be9a69d5e3ba13f5198
Luca Deri <deri@ntop.org>
2020-03-28 19:05:18 +0100

    Uninstall script

---
+++ packages/macos/uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo /bin/rm -r /usr/local/bin/ntopng
+sudo /bin/rm -r /usr/local/share/ntopng/
+sudo /bin/rm -f /usr/local/share/man/man8/ntopng.8
+sudo /bin/rm -f /usr/local/etc/ntopng.conf
