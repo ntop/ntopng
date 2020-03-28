c39d127b79942b5cb2359e391d9d4c52265b0e60
Luca Deri <deri@ntop.org>
2020-03-28 19:54:32 +0100

    Dependency update for MacOS

--- packages/macos/uninstall.sh
+++ packages/macos/uninstall.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-echo "[1/4] Uninstall ntopng"
+NUM=1
+echo "$NUM. Uninstall ntopng"
 sudo /bin/rm -f /usr/local/bin/ntopng
 
-echo "[2/4] Uninstall companion files"
+NUM=$((NUM+1))
+echo "$NUM. Uninstall companion files"
 sudo /bin/rm -rf /usr/local/share/ntopng/
 sudo /bin/rm -f /usr/local/share/man/man8/ntopng.8
 
-echo "[3/4] Uninstall config file"
+NUM=$((NUM+1))
+echo "$NUM. Uninstall config file"
 sudo /bin/rm -f /usr/local/etc/ntopng.conf
 
-echo "[4/4] Uninstall startup file"
+NUM=$((NUM+1))
+echo "$NUM. Uninstall startup file"
 sudo /bin/rm -f /Library/LaunchDaemons/org.ntop.ntopng.plist
+
+NUM=$((NUM+1))
+echo "$NUM. Deleting package information"
+sudo /usr/sbin/pkgutil --forget org.ntop.pkg.ntopng > /dev/null
 
 echo "Done"
