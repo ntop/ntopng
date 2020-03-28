e1b04a96dd727d8537836152713dd36678754489
Luca Deri <deri@ntop.org>
2020-03-28 19:09:02 +0100

    Added progress string

--- packages/macos/uninstall.sh
+++ packages/macos/uninstall.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-sudo /bin/rm -r /usr/local/bin/ntopng
-sudo /bin/rm -r /usr/local/share/ntopng/
+echo "[1/4] Uninstall ntopng"
+sudo /bin/rm -f /usr/local/bin/ntopng
+
+echo "[2/4] Uninstall companion files"
+sudo /bin/rm -rf /usr/local/share/ntopng/
 sudo /bin/rm -f /usr/local/share/man/man8/ntopng.8
+
+echo "[3/4] Uninstall config file"
 sudo /bin/rm -f /usr/local/etc/ntopng.conf
+
+echo "[4/4] Uninstall startup file"
+sudo /bin/rm -f /Library/LaunchDaemons/org.ntop.ntopng.plist
+
+echo "Done"
