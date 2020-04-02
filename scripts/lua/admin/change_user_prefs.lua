ntop/ntopng

commit a86aaa61e6819d129a4cd211a5d2ff4e09fb8e54

Add configuration of user permission for downloading pcap (hidden for the time being)

scripts/lua/admin/change_user_prefs.lua
@@ -12,6 +12,7 @@ local username  = _POST["username"]
 local host_role = _POST["user_role"]
 local networks  = _POST["allowed_networks"]
 local allowed_interface = _POST["allowed_interface"]
+local allow_pcap_download = _POST["allow_pcap_download"]
 local language  = _POST["user_language"]
 
 -- for captive portal users
@@ -50,11 +51,13 @@ if(networks ~= nil) then
   end
 end
 
-if(allowed_interface ~= nil) then
-   if(not ntop.changeAllowedIfname(username, getInterfaceName(allowed_interface))) then
-      print ("{ \"result\" : -1, \"message\" : \"Error in changing the allowed interface\" }")
-      return
-   end
+local allow_pcap_download_enabled = false
+if allow_pcap_download and allow_pcap_download == "1" then
+  allow_pcap_download_enabled = true;
+end
+if(not ntop.changeUserPermission(username, allow_pcap_download_enabled)) then
+   print ("{ \"result\" : -1, \"message\" : \"Error in changing user permission\" }")
+   return
 end
 
 if(language ~= nil) then

include/Ntop.h
@@ -388,10 +388,11 @@ class Ntop {
   bool changeAllowedIfname(char *username, char *allowed_ifname) const;
   bool changeUserHostPool(const char * const username, const char * const host_pool_id) const;
   bool changeUserLanguage(const char * const username, const char * const language) const;
+  bool changeUserPermission(const char * const username, bool allow_pcap_download) const;
   bool existsUser(const char * const username) const;
   bool addUser(char *username, char *full_name, char *password, char *host_role,
 	       char *allowed_networks, char *allowed_ifname, char *host_pool_id,
-	       char *language);
+	       char *language, bool allow_pcap_download);
   bool addUserLifetime(const char * const username, u_int32_t lifetime_secs); /* Captive portal users may expire */
   bool clearUserLifetime(const char * const username);
   bool isCaptivePortalUser(const char * const username);

include/ntop_defines.h
@@ -249,6 +249,7 @@
 #define CONST_STR_USER_ALLOWED_IFNAME  "ntopng.user.%s.allowed_ifname"
 #define CONST_STR_USER_HOST_POOL_ID    "ntopng.user.%s.host_pool_id"
 #define CONST_STR_USER_LANGUAGE        "ntopng.user.%s.language"
+#define CONST_STR_USER_ALLOW_PCAP      "ntopng.user.%s.allow_pcap"
 #define CONST_STR_USER_EXPIRE          "ntopng.user.%s.expire"
 #define CONST_ALLOWED_NETS             "allowed_nets"
 #define CONST_ALLOWED_IFNAME           "allowed_ifname"

scripts/locales/en.lua
@@ -416,6 +416,7 @@ local lang = {
     ["too_many_hosts_description"] = "Trigger an alert when the number of active hosts is too high",
     ["total_alerts"] = "Total Alerts",
     ["trailing_msg"] = "Time Window",
+    ["trailing_msg_compact"] = "Window",
     ["unresponsive_device"] = "Unresponsive Device",
     ["user_activity"] = "User Activity",
     ["user_scripts_calls_drops"] = "User Scripts Calls Dropped",
@@ -2239,6 +2240,8 @@ local lang = {
   ["manage_users"] = {
     ["add_new_user"] = "Add New User",
     ["administrator"] = "Administrator",
+    ["allow_pcap_download"] = "Allow PCAP Download",
+    ["allow_pcap_download_descr"] = "Allow the user to download live traffic and PCAPs",
     ["allowed_interface"] = "Allowed Interface",
     ["allowed_networks"] = "Allowed Networks",
     ["allowed_networks_descr"] = "Comma separated list of networks this user can view. Example:",

scripts/lua/admin/add_user.lua
@@ -9,17 +9,18 @@ require "lua_utils"
 sendHTTPContentTypeHeader('text/html')
 
 if(haveAdminPrivileges()) then
-   username = _POST["username"]
-   full_name = _POST["full_name"]
-   password = _POST["password"]
-   confirm_password = _POST["confirm_password"]
-   host_role = _POST["user_role"]
-   networks = _POST["allowed_networks"]
-   allowed_interface = _POST["allowed_interface"]
-   language = _POST["user_language"]
-   host_pool_id = _POST["host_pool_id"]
-   limited_lifetime = _POST["lifetime_limited"]
-   lifetime_secs = tonumber((_POST["lifetime_secs"] or -1))
+   local username = _POST["username"]
+   local full_name = _POST["full_name"]
+   local password = _POST["password"]
+   local confirm_password = _POST["confirm_password"]
+   local host_role = _POST["user_role"]
+   local networks = _POST["allowed_networks"]
+   local allowed_interface = _POST["allowed_interface"]
+   local language = _POST["user_language"]
+   local allow_pcap_download = _POST["allow_pcap_download"]
+   local host_pool_id = _POST["host_pool_id"]
+   local limited_lifetime = _POST["lifetime_limited"]
+   local lifetime_secs = tonumber((_POST["lifetime_secs"] or -1))
 
    if(username == nil or full_name == nil or password == nil or confirm_password == nil or host_role == nil or networks == nil or allowed_interface == nil) then
       print ("{ \"result\" : -1, \"message\" : \"Invalid parameters\" }")
@@ -34,7 +35,12 @@ if(haveAdminPrivileges()) then
    local ret = false
    username = string.lower(username)
 
-   if(ntop.addUser(username, full_name, password, host_role, networks, getInterfaceName(allowed_interface), host_pool_id, language)) then
+   local allow_pcap_download_enabled = false
+   if _POST["allow_pcap_download"] and _POST["allow_pcap_download"] == "1" then
+     allow_pcap_download_enabled = true
+   end
+
+   if(ntop.addUser(username, full_name, password, host_role, networks, getInterfaceName(allowed_interface), host_pool_id, language, allow_pcap_download_enabled)) then
       ret = true
 
       if limited_lifetime and not ntop.addUserLifetime(username, lifetime_secs) then

scripts/lua/admin/get_user_info.lua
@@ -48,6 +48,9 @@ if(haveAdminPrivileges()) then
 	    end
 	 end
 	 print(' "language": "'..value["language"]..'",\n')
+         if value["allow_pcap_download"] then
+           print(' "allow_pcap_download": true,\n')
+         end
 
 	 print(' "username": "'..key..'",\n')
 	 print(' "password": "'..value["password"]..'",\n')

scripts/lua/inc/add_user_dialog.lua
@@ -66,7 +66,6 @@ print [[
       <input id="confirm_password_input" type="password" name="confirm_password" value="" class="form-control" pattern="]] print(getPasswordInputPattern()) print[[" required>
     </div>
 
-
     <label for="user_role">]] print(i18n("manage_users.user_role")) print[[</label>
     <div class="input-group mb-6">
 	<select id="user_role" name="user_role" class="form-control" style="width:100%;">
@@ -75,9 +74,11 @@ print [[
 	</select>
     </div>
 
-    <label for="allowed_interface">]] print(i18n("manage_users.allowed_interface")) print[[</labe>l
+    <div id="unprivileged_input">
+
+    <label for="allowed_interface_input">]] print(i18n("manage_users.allowed_interface")) print[[</labe>
     <div class="input-group mb-6">
-	<select name="allowed_interface" class="form-control">
+	<select id="allowed_interface_input" name="allowed_interface" class="form-control">
 	  <option value="">]] print(i18n("manage_users.any_interface")) print[[</option>
 ]]
 
@@ -93,6 +94,25 @@ print [[
       <input id="allowed_networks_input" type="text" name="allowed_networks" value="" class="form-control">
       <small>]] print(i18n("manage_users.allowed_networks_descr")) print[[: 192.168.1.0/24,172.16.0.0/16</small>
     </div>
+
+    <!--div class="input-group mb-6">
+      <div class="form-check">
+        <input id="allow_pcap_download_input" type="checkbox" name="allow_pcap_download" value="1" class="form-check-input">
+        <label for="allow_pcap_download_input" class="form-check-label">]] print(i18n("manage_users.allow_pcap_download_descr")) print[[</label>
+      </div>
+    </div-->
+
+    </div>
+
+    <script>
+    $("#user_role").change(function() {
+      if ($(this).val() == "unprivileged")
+        $('#unprivileged_input').show();
+      else
+        $('#unprivileged_input').hide();
+    });
+    $("#user_role").trigger("change");
+    </script>
 ]]
 
 if not ntop.isnEdge() then

scripts/lua/inc/password_dialog.lua
@@ -121,6 +121,8 @@ print [[
         </select>
   </div>
 
+  <div id="unprivileged_manage_input">
+
   <label for="allowed_interface">]] print(i18n("manage_users.allowed_interface")) print[[</label>
   <div class='input-group mb-6'>
         <select name="allowed_interface" id="allowed_interface" class="form-control">
@@ -139,6 +141,26 @@ print [[
       <input id="networks_input" type="text" name="allowed_networks" value="" class="form-control" required>
       <small>]] print(i18n("manage_users.allowed_networks_descr")) print[[ 192.168.1.0/24,172.16.0.0/16</small>
     </div>
+
+    <!--div class="input-group mb-6">
+      <div class="form-check">
+        <input id="allow_pcap_input" type="checkbox" name="allow_pcap_download" value="1" class="form-check-input">
+        <label for="allow_pcap_input" class="form-check-label">]] print(i18n("manage_users.allow_pcap_download_descr")) print[[</label>
+      </div>
+    </div-->
+
+    </div>
+
+    <script>
+    $("#host_role_select").change(function() {
+      if ($(this).val() == "unprivileged")
+        $('#unprivileged_manage_input').show();
+      else
+        $('#unprivileged_manage_input').hide();
+    });
+    $("#host_role_select").trigger("change");
+    </script>
+
 ]]
 
 if not ntop.isnEdge() then
@@ -288,9 +310,9 @@ function reset_pwd_dialog(user) {
       $('#networks_input').val(data.allowed_nets);
       $('#allowed_interface option[value="' + data.allowed_if_id + '"]').attr('selected','selected');
 
-      if(data.language !== "") {
-      $('#user_language option[value="' + data.language + '"]').attr('selected','selected');
-      }
+      if(data.language !== "")
+        $('#user_language option[value="' + data.language + '"]').attr('selected','selected');
+      $('#allow_pcap_input').prop('checked', data.allow_pcap_download === true ? true : false);
       if(data.host_pool_id) {
         $('#old_host_pool_id').val(data.host_pool_id);
         $('#host_pool_id option[value = '+data.host_pool_id+']').attr('selected','selected');

commit 8dc04eb37d203a500a886336cd9b25bff2d5f071

Fix allowed interface change not working
Broken since a86aaa61e6819d129a4cd211a5d2ff4e09fb8e54

Fixes #3690

scripts/lua/admin/change_user_prefs.lua
@@ -51,6 +51,13 @@ if(networks ~= nil) then
   end
 end
 
+if(allowed_interface ~= nil) then
+   if(not ntop.changeAllowedIfname(username, getInterfaceName(allowed_interface))) then
+     print ("{ \"result\" : -1, \"message\" : \"Error in changing the allowed interface\" }")
+     return
+   end
+end
+
 local allow_pcap_download_enabled = false
 if allow_pcap_download and allow_pcap_download == "1" then
   allow_pcap_download_enabled = true;