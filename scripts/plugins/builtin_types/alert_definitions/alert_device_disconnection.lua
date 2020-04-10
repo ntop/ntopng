97863e06afae5cc4ac30c48590a012ac7a53f023
Simone Mainardi <mainardi@ntop.org>
2020-04-10 17:00:50 +0200

    Fixes missing device in device disconnection alert
    Fixes #3738

--- scripts/plugins/builtin_types/alert_definitions/alert_device_disconnection.lua
+++ scripts/plugins/builtin_types/alert_definitions/alert_device_disconnection.lua
@@ -5,8 +5,14 @@
 local alert_keys = require "alert_keys"
 
 local function formatDeviceDisconnectionAlert(ifid, alert, info)
+  local device = info.device
+
+  if not device or device == "" then
+    device = alert.alert_entity_val
+  end
+
   return(i18n("alert_messages.device_has_disconnected", {
-    device = info.device,
+    device = device,
     url = getMacUrl(alert.alert_entity_val),
   }))
 end
