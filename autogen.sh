341a217e260415cf0bc30814f2a1221c5c86dc73
Luca Deri <deri@ntop.org>
2020-04-20 15:26:32 +0200

    Added tool checks

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
 
+
+AUTOCONF=$(command -v autoconf)
+AUTOMAKE=$(command -v automake)
+LIBTOOL=$(command -v libtool)
+LIBTOOLIZE=$(command -v libtoolize)
+AUTORECONF=$(command -v autoreconf)
+PKG_CONFIG=$(command -v pkg-config)
+
+if test -z $AUTOCONF; then
+    echo "autoconf is missing: please install it and try again"
+    exit
+fi
+
+if test -z $AUTOMAKE; then
+    echo "automake is missing: please install it and try again"
+    exit
+fi
+
+if test -z $LIBTOOL && test -z $LIBTOOLIZE ; then
+    echo "libtool and libtoolize is missing: please install it and try again"
+    exit
+fi
+
+if test -z $AUTORECONF; then
+    echo "autoreconf is missing: please install it and try again"
+    exit
+fi
+
+if test -z $PKG_CONFIG; then
+    echo "pkg-config is missing: please install it (apt-get install pkg-config) and try again"
+    exit
+fi
+
+##########################################
+
 TODAY=`date +%y%m%d`
 MAJOR_RELEASE="4"
 MINOR_RELEASE="1"
