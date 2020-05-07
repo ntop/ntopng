88a3e05afd45a8182860ff11cac17726e5bfd14d
Simone Mainardi <mainardi@ntop.org>
2020-05-07 18:58:07 +0200

    Compilation fix

--- include/NetworkInterface.h
+++ include/NetworkInterface.h
@@ -272,20 +272,6 @@ class NetworkInterface : public AlertableEntity {
 			   u_int32_t num_bytes, u_int pkt_overhead) {
     ethStats.incStats(ingressPacket, proto, num_pkts, num_bytes, pkt_overhead);
   };
-  inline void _incStats(bool ingressPacket, time_t when,
-			u_int16_t eth_proto,
-			u_int16_t ndpi_proto, ndpi_protocol_category_t ndpi_category,
-			u_int8_t l4proto,
-		       u_int pkt_len, u_int num_pkts, u_int pkt_overhead) {
-    incEthStats(ingressPacket, eth_proto, num_pkts, pkt_len, pkt_overhead);
-    ndpiStats->incStats(when, ndpi_proto, 0, 0, num_pkts, pkt_len);
-    // Note: here we are not currently interested in packet direction, so we tell it is receive
-    ndpiStats->incCategoryStats(when, ndpi_category, 0 /* see above comment */, pkt_len);
-    pktStats.incStats(1, pkt_len);
-    l4Stats.incStats(when, l4proto,
-      ingressPacket ? num_pkts : 0, ingressPacket ? pkt_len : 0,
-      !ingressPacket ? num_pkts : 0, !ingressPacket ? pkt_len : 0);
-  };
 
  public:
   /**
@@ -397,6 +383,20 @@ class NetworkInterface : public AlertableEntity {
   virtual u_int64_t getCheckPointNumDiscardedProbingBytes() const;
   inline void incFlagStats(u_int8_t flags, bool cumulative_flags) {
     pktStats.incFlagStats(flags, cumulative_flags);
+  };
+  inline void _incStats(bool ingressPacket, time_t when,
+			u_int16_t eth_proto,
+			u_int16_t ndpi_proto, ndpi_protocol_category_t ndpi_category,
+			u_int8_t l4proto,
+			u_int pkt_len, u_int num_pkts, u_int pkt_overhead) {
+    incEthStats(ingressPacket, eth_proto, num_pkts, pkt_len, pkt_overhead);
+    ndpiStats->incStats(when, ndpi_proto, 0, 0, num_pkts, pkt_len);
+    // Note: here we are not currently interested in packet direction, so we tell it is receive
+    ndpiStats->incCategoryStats(when, ndpi_category, 0 /* see above comment */, pkt_len);
+    pktStats.incStats(1, pkt_len);
+    l4Stats.incStats(when, l4proto,
+		     ingressPacket ? num_pkts : 0, ingressPacket ? pkt_len : 0,
+		     !ingressPacket ? num_pkts : 0, !ingressPacket ? pkt_len : 0);
   };
   inline void incStats(bool ingressPacket, time_t when, u_int16_t eth_proto,
 		       u_int16_t ndpi_proto, ndpi_protocol_category_t ndpi_category,
