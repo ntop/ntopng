0870ae2abc348fff5e36b6b40f9647c21645de4e
Simone Mainardi <mainardi@ntop.org>
2020-05-22 16:41:03 +0200

    Enforces strict interface names check on linux only

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -3894,7 +3894,7 @@ void Utils::ntop_freealldevs(ntop_if_t *alldevsp) {
 bool Utils::validInterfaceName(const char *name) {
 #ifdef HAVE_NEDGE
   return((name && (strncmp(name, "nf:", 3) == 0)) ? true : false);
-#else
+#elif defined(linux)
   if(!name
      || !strncmp(name, "virbr", 5) /* Ignore virtual interfaces */
      )
@@ -3919,8 +3919,10 @@ bool Utils::validInterfaceName(const char *name) {
       return false;
   }
 
-  return true;
+
 #endif
+
+  return true;
 }
 
 /* ****************************************************** */
