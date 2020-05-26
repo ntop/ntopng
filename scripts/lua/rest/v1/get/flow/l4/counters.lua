dcaadc7a3dbcf6f132b8db02ab13bfcbf0600456
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-05-26 15:13:37 +0200

    Add rest/v1/get/flow/l4/counters.lua rest/v1/get/flow/l7/counters.lua (implement #3947 #3950)

---
+++ scripts/lua/rest/v1/get/flow/l4/counters.lua
@@ -0,0 +1,43 @@
+--
+-- (C) 2013-20 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+require "lua_utils"
+local json = require ("dkjson")
+local tracker = require("tracker")
+local rest_utils = require("rest_utils")
+
+--
+-- Read number of active flows per protocol
+-- Example: curl -u admin:admin -d '{"ifid": "1"}' http://localhost:3000/lua/rest/v1/get/flow/l4/counters.lua
+--
+-- NOTE: in case of invalid login, no error is returned but redirected to login
+--
+
+sendHTTPHeader('application/json')
+
+local rc = rest_utils.consts_ok
+local res = {}
+
+local ifid = _GET["ifid"]
+
+if isEmptyString(ifid) then
+   print(rest_utils.rc(rest_utils.consts_invalid_interface))
+   return
+end
+
+interface.select(ifid)
+
+local flowstats = interface.getActiveFlowsStats()
+local l4_proto = flowstats["l4_protocols"]
+
+res.counters = {}
+for k,v in pairs(l4_proto, asc) do
+  res.counters[k] = v.count
+end
+
+print(rest_utils.rc(rc, res))
+
