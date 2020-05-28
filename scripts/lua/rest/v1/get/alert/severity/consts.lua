699ec1e2fbc7e7c26b3950cab0bb8930ea342ecf
Simone Mainardi <mainardi@ntop.org>
2020-05-28 15:51:36 +0200

    Implement getter for alert severity constants
    Implements #3946

---
+++ scripts/lua/rest/v1/get/alert/severity/consts.lua
@@ -0,0 +1,30 @@
+--
+-- (C) 2013-20 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+require "lua_utils"
+local json = require "dkjson"
+local alert_consts = require "alert_consts"
+local rest_utils = require "rest_utils"
+
+--
+-- Read all the defined alert type constants
+-- Example: curl -u admin:admin http://localhost:3000/lua/rest/v1/get/alert/type/consts.lua
+--
+-- NOTE: in case of invalid login, no error is returned but redirected to login
+--
+
+sendHTTPHeader('application/json')
+
+local rc = rest_utils.consts_ok
+local res = {}
+
+for severity, severity_descr in pairs(alert_consts.alert_severities) do
+   res[severity] = {severity_id = severity_descr.severity_id}
+end
+
+print(rest_utils.rc(rc, res))
+
