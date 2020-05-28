63628af5bdeefffa3aee60ea65c743b65b646561
Simone Mainardi <mainardi@ntop.org>
2020-05-28 15:54:07 +0200

    Simplifies getter for alert constants

--- scripts/lua/rest/v1/get/alert/type/consts.lua
+++ scripts/lua/rest/v1/get/alert/type/consts.lua
@@ -23,7 +23,7 @@ local rc = rest_utils.consts_ok
 local res = {}
 
 for alert_type, alert in pairs(alert_consts.alert_types) do
-   res[#res + 1] = {alert_type = alert_type, alert_key = alert.alert_key}
+   res[alert_type] = {alert_key = alert.alert_key}
 end
 
 print(rest_utils.rc(rc, res))
