c12ad4a3414caaaeb16b76ae7998eee2a0786ccf
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-05-29 17:55:16 +0200

    Ignore selected lines from the diff

--- tests/rest/do.sh
+++ tests/rest/do.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 RC=0
 TESTS=`cd tests; /bin/ls *.test`
 
 build_results() {
     for f in $TESTS; do 
-	#echo $f
-	# create result files if not present
-	if [ ! -f result/$f.out ]; then
-	    CMD=`bash tests/$f | jq -cS . > result/$f.out`
-	fi
+        # create result files if not present
+        if [ ! -f result/$f.out ]; then
+            CMD=`bash tests/$f | jq -cS . > result/$f.out`
+        fi
     done
 }
 
@@ -19,24 +18,36 @@ build_results() {
 #
 check_results() {
     for f in $TESTS; do 
-	if [ -f result/$f.out ]; then
-	    CMD=`bash tests/$f | jq -cS . > /tmp/test.out`
+        if [ -f result/$f.out ]; then
+            TMP_OUT=$(mktemp)
+            TMP_OUT_DIFF=$(mktemp)
 
-	    # Comparison of two JSONs in bash, see
-	    # https://stackoverflow.com/questions/31930041/using-jq-or-alternative-command-line-tools-to-compare-json-files/31933234#31933234
-	    JSON_EQUAL=`jq --argfile a result/$f.out --argfile b /tmp/test.out -n 'def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); ($a | (post_recurse | arrays) |= sort) as $a | ($b | (post_recurse | arrays) |= sort) as $b | $a == $b'`
-	    
-	    if [ "${JSON_EQUAL}" = "true" ]; then
-		printf "%-32s\tOK\n" "$f"
-	    else
-		printf "%-32s\tERROR\n" "$f"
-		echo `diff result/$f.out /tmp/test.out | wc -l`
-		diff result/$f.out /tmp/test.out
-		RC=1
-	    fi
+            CMD=`bash tests/$f | jq -cS . > ${TMP_OUT}`
 
-	    /bin/rm /tmp/test.out
-	fi
+            # Comparison of two JSONs in bash, see
+            # https://stackoverflow.com/questions/31930041/using-jq-or-alternative-command-line-tools-to-compare-json-files/31933234#31933234
+           
+            diff --side-by-side --suppress-common-lines \
+                <(jq -S 'def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); (. | (post_recurse | arrays) |= sort)' "result/$f.out") \
+                <(jq -S 'def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); (. | (post_recurse | arrays) |= sort)' "${TMP_OUT}") \
+                > "${TMP_OUT_DIFF}"
+
+            if [ -f "tests/$f.ignore" ]; then
+                TMP_TMP_OUT_DIFF=$(mktemp)
+                cat ${TMP_OUT_DIFF} | grep -v -f "tests/$f.ignore" > ${TMP_TMP_OUT_DIFF}
+                cat ${TMP_TMP_OUT_DIFF} > ${TMP_OUT_DIFF}
+            fi
+
+            if [ `cat "${TMP_OUT_DIFF}" | wc -l` -eq 0 ]; then
+                printf "%-32s\tOK\n" "$f"
+            else
+                printf "%-32s\tERROR\n" "$f"
+                cat "${TMP_OUT_DIFF}"
+                RC=1
+            fi
+
+            /bin/rm ${TMP_OUT} ${TMP_OUT_DIFF}
+        fi
     done
 }
 
