0c1eff4dfd394e34b06459bba837fdf2caafdb1b
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-05-29 10:26:08 +0200

    Use local datadir (no privileges)

--- tests/rest/run_ntopng.sh
+++ tests/rest/run_ntopng.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NTOPNG_TEST_DATADIR="/var/lib/ntopng_test"
+NTOPNG_TEST_DATADIR="${PWD}/data"
 NTOPNG_TEST_CUSTOM_PROTOS="${NTOPNG_TEST_DATADIR}/protos.txt"
 NTOPNG_TEST_REDIS="2"
 NTOPNG_TEST_LOCALNETS="192.168.1.0/24"
@@ -28,4 +28,13 @@ host:"lando"@Lando
 EOF
 
 # Start the test
-cd ../../; ./ntopng -d "${NTOPNG_TEST_DATADIR}" -r "@${NTOPNG_TEST_REDIS}" -p "${NTOPNG_TEST_CUSTOM_PROTOS}" -N "ntopng_test" -m "${NTOPNG_TEST_LOCALNETS}" -i tests/rest/pcap/test_01.pcap -i tests/rest/pcap/test_02.pcap --disable-login 1
+cd ../../;					\
+./ntopng					\
+	-d "${NTOPNG_TEST_DATADIR}"		\
+	-r "@${NTOPNG_TEST_REDIS}"		\
+	-p "${NTOPNG_TEST_CUSTOM_PROTOS}"	\
+	-N "ntopng_test"			\
+	-m "${NTOPNG_TEST_LOCALNETS}"		\
+	-i tests/rest/pcap/test_01.pcap		\
+	-i tests/rest/pcap/test_02.pcap		\
+	--disable-login 1
