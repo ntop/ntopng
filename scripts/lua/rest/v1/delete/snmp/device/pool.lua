fdb486e088753871809637ba66e25dc246cb543c
Simone Mainardi <mainardi@ntop.org>
2020-07-03 19:14:32 +0200

    Adds stubs to delete pool members
    Addresses #4108

---
+++ scripts/lua/rest/v1/delete/snmp/device/pool.lua
@@ -0,0 +1,24 @@
+--
+-- (C) 2013-20 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+require "lua_utils"
+local json = require "dkjson"
+local rest_utils = require "rest_utils"
+
+--
+-- Delete an existing pool
+--
+
+sendHTTPHeader('application/json')
+
+local rc = rest_utils.consts_ok
+local res = {
+   -- STUB
+}
+
+print(rest_utils.rc(rc, res))
+
