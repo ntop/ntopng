a9040f1057c3bdb7a68af32c660b87f1105779bd
Simone Mainardi <mainardi@ntop.org>
2020-07-05 12:15:08 +0200

    Initial implementation of lua classes to handle pools and interface pools

--- scripts/lua/modules/pools/base_pools.lua
+++ scripts/lua/modules/pools/base_pools.lua
@@ -6,7 +6,87 @@
 
 require "lua_utils"
 local user_scripts = require "user_scripts"
+
+-- ##############################################
+
 local base_pools = {}
+
+-- ##############################################
+
+local base_pools_prefix = "ntopng.pools"
+
+-- ##############################################
+
+function base_pools:create(args)
+   if args then
+      -- We're being sub-classed
+      if not args.key or not args.name or not args.members or not args.configset_id then
+	 return nil
+      end
+   end
+
+   local this = args or {key = "base", members = {}, configset_id = 0}
+
+   setmetatable(this, self)
+   self.__index = self
+
+   return this
+end
+
+-- ##############################################
+
+function base_pools:_get_pool_ids_key()
+   return string.format("%s.%s_pools.pool_ids", base_pools_prefix, self.key)
+end
+
+-- ##############################################
+
+function base_pools:_get_next_pool_id_key()
+   return string.format("%s.%s_pools.next_pool_id", base_pools_prefix, self.key)
+end
+
+-- ##############################################
+
+function base_pools:_assign_pool_id()
+   local next_pool_id_key = self:_get_next_pool_id_key()
+   -- Atomically assign a new pool id
+   local next_pool_id = ntop.incrCache(next_pool_id_key)
+
+   -- Add the atomically assigned pool id to the set of current pool ids (set wants a string)
+   ntop.setMembersCache(self:_get_pool_ids_key(), string.format("%d", next_pool_id))
+
+   -- tprint({next_pool_id = next_pool_id, pool_ids = ntop.getMembersCache(self:_get_pool_ids_key())})
+   return next_pool_id
+end
+
+-- ##############################################
+
+function base_pools:_persist()
+   io.write("persist")
+   -- self:cleanup()
+
+   -- LOCK
+
+   -- Check if duplicate names exist
+   -- Check is members are valid and do not belong to any other pool
+   -- Check if the configset_id is valid
+
+   -- Now that everything is ok, the id can be assigned
+   local pool_id = self:_assign_pool_id()
+
+   -- UNLOCK
+
+   -- Set the pool id to the current instance
+   self.pool_id = pool_id
+   return true
+end
+
+-- ##############################################
+
+function base_pools:cleanup()
+   ntop.delCache(self:_get_pool_ids_key())
+   ntop.delCache(self:_get_next_pool_id_key())
+end
 
 -- ##############################################
 
--- scripts/lua/modules/pools/interface_pools.lua
+++ scripts/lua/modules/pools/interface_pools.lua
@@ -8,6 +8,26 @@ local interface_pools = {}
 
 -- ##############################################
 
+function interface_pools:create(name, members, configset_id)
+   -- Instance of the base class
+   local _interface_pools = base_pools:create()
+   -- Subclass using the base class instance
+   local _interface_pools_instance = _interface_pools:create({key = "interface", name = name or '', members = members or {}, configset_id = configset_id or 0})
+
+   -- In the instance has been created successfully, we can persist it
+   if _interface_pools_instance then
+      if not _interface_pools_instance:_persist() then
+	 -- Failed to persist the instance, unable to create
+	 return nil
+      end
+   end
+
+   -- Return the instance
+   return _interface_pools_instance
+end
+
+-- ##############################################
+
 -- @brief Returns members which doesn't belong to any pool
 function interface_pools.list_available_members()
    -- STUB: currently returns all members
