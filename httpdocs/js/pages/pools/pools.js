ef79f00d7acaa8a4948575d846f4c7f58b930178
gabryon99 <pappalardo@ntop.org>
2020-07-27 09:41:22 +0200

    check on undefined array recipient

--- httpdocs/js/pages/pools/pools.js
+++ httpdocs/js/pages/pools/pools.js
@@ -39,6 +39,8 @@ $(document).ready(function() {
 
     const arrayToListString = (array, limit) => {
 
+        if (array == undefined) return "";
+
         if (array.length > limit) {
             const otherStr = ((length - limit) == 1) ? i18n.other : i18n.others;
             return array.slice(0, limit).join(", ") + ` ${i18n.and} ${array.length - limit} ${otherStr.toLowerCase()}`;
