b85086aa65da8fb1d49238175150ea0c32a2c1c2
gabryon99 <pappalardo@ntop.org>
2020-07-29 11:06:09 +0200

    fixed name error on pool column

--- httpdocs/js/pages/snmpdevices_stats/overview.js
+++ httpdocs/js/pages/snmpdevices_stats/overview.js
@@ -131,11 +131,7 @@ $(document).ready(function () {
                 }
             },
             {
-                data: "column_pool_id",
-                render: function(data, type, row) {
-                    if (type == "display" && data == defaultPoolID) return "";
-                    return snmpPools.find(pool => pool.pool_id == data).name;
-                }
+                data: "column_pool_name"
             },
             { data: "column_chart", className: "text-center", width: "5%" },
             { data: "column_name" },
