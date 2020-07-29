c291fe91c949020dd3182239711daf2d559073e6
Simone Mainardi <mainardi@ntop.org>
2020-07-29 14:47:11 +0200

    Adds periodic_user_scripts.lua for the execution of periodic scripts

---
+++ scripts/callbacks/interface/periodic_user_scripts.lua
@@ -0,0 +1,15 @@
+--
+-- (C) 2019-20 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+package.path = dirs.installdir .. "/scripts/lua/modules/timeseries/?.lua;" .. package.path
+local user_scripts = require "user_scripts"
+
+-- ########################################################
+
+user_scripts.runScripts()
+
+-- ########################################################
+
