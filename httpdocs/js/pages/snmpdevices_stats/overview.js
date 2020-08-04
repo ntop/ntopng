05efd87d3db567898eabc59862a09eaad561dfe4
gabryon99 <pappalardo@ntop.org>
2020-08-04 11:13:58 +0200

    edit for snmp devices

--- httpdocs/js/pages/snmpdevices_stats/overview.js
+++ httpdocs/js/pages/snmpdevices_stats/overview.js
@@ -6,14 +6,17 @@
 
 $(document).ready(function () {
 
-    let snmpDeviceRowData = null;
+    let snmpDeviceRowData = {};
 
+    // constant for filtering table
     const RESPONSIVE_COLUMN_INDEX = 0;
     const POOL_COLUMN_INDEX = 2;
-    // define a constant for the snmp version dropdown value
+
+    // define a constant for the SNMP version dropdown value
     const SNMP_VERSION_THREE = 2;
 
-    const requiredFieldsAdd = { community: [], nonCommunity: [] };
+    // required fields for SNMPv3
+    const requiredFields = {};
 
     const addResponsivenessFilter = (tableAPI) => {
         DataTableUtils.addFilterDropdown(
@@ -51,43 +54,159 @@ $(document).ready(function () {
 
     }
 
-    $(`#add-reload-pools`).click(async function(e) {
-        e.preventDefault();
-        $(this).attr("disabled", true);
+    function onRequestSuccess(response, textStatus, modalHandler, modalSelector) {
 
-        const request = await fetch(`${http_prefix}/lua/rest/v1/get/snmp/device/pools.lua`);
-        const data = await request.json();
-
-        if (data.rc < 0) {
-            console.warn("Something went wrong when reloading SNMP Pools list!");
+        if (response.rc < 0) {
+            // hide the spinner and show a localized error
+            $(`${modalSelector} button[type='submit'] span.spinner-border`).fadeOut(() => {
+                $(`${modalSelector} span.invalid-feedback`).html(i18n.rest[response.rc_str]).fadeIn()
+            });
             return;
         }
 
-        // update the select if there are new pools
-        data.rsp.forEach((pool) => {
+        // clean the form if the response was successful
+        modalHandler.cleanForm();
+        $snmpTable.ajax.reload(toggleSnmpTableButtons, false);
 
-            const {pool_id, name} = pool;
-            // if there is already the pool then return
-            if ($(`#add-select-pool option[value='${pool_id}']`).length != 0) {
-                return;
-            }
-            $(`#add-select-pool`).append(`<option value='${pool_id}'>${name}</option>`)
+        $(`${modalSelector} button[type='submit'] span.spinner-border`).hide();
+        $(modalSelector).modal('hide');
+    }
+
+    function buildDataRequest(modalSelector) {
+
+        const data = {};
+        // show the spinner and hide the errors
+        $(`${modalSelector} span.invalid-feedback`).hide();
+        $(`${modalSelector} button[type='submit'] span.spinner-border`).fadeIn();
+
+        // build the post params
+        $(`${modalSelector} form`).find('input,select,textarea').each((idx, element) => {
+            data[$(element).attr("name")] = $(element).val();
         });
 
-        $(this).removeAttr("disabled");
-    });
+        return data;
+    }
 
-    $(`.community-field input[required], .community-field select[required]`)
-    .each(function() {
-        requiredFieldsAdd.community.push($(this));
-    });
+    function bindSNMPHostKeyup(modalSelector) {
 
-    $(`.non-community-field input[required], .non-community-field select[required]`)
-    .each(function() {
-        requiredFieldsAdd.nonCommunity.push($(this));
-        $(this).removeAttr("required");
-    });
+        $(`${modalSelector} input[name='snmp_host']`).keyup(function(e) {
 
+            const value = $(this).val();
+
+            if (new RegExp(REGEXES.domainName).test(value)) {
+                $(`${modalSelector} select[name='cidr']`).attr("disabled", "disabled");
+            }
+            else if (new RegExp(REGEXES.ipv6).test(value)) {
+                $(`${modalSelector} select[name='cidr'] option[value!='128']`).attr("disabled", "disabled");
+                $(`${modalSelector} select[name='cidr']`).val(128);
+            }
+            else {
+                $(`${modalSelector} select[name='cidr'] option[value!='128']`).removeAttr("disabled");
+                $(`${modalSelector} select[name='cidr']`).removeAttr("disabled");
+            }
+
+        });
+    }
+
+    function bindSNMPLevelSelect(modalSelector) {
+
+        $(`${modalSelector} select[name='snmp_level']`).change(function(e) {
+
+            const usernameSelector = `${modalSelector} input[name='snmp_username']`;
+            const privacySelector = `${modalSelector} select[name='snmp_privacy_protocol'], ${modalSelector} input[name='snmp_privacy_passphrase']`;
+            const authSelector = `${modalSelector} input[name='snmp_auth_protocol'], ${modalSelector} select[name='snmp_auth_protocol']`;
+
+            switch ($(this).val()) {
+                case "authPriv":
+                    $(`${privacySelector},${usernameSelector},${authSelector}`).removeAttr("disabled");
+                    break;
+                case "noAuthNoPriv":
+                    $(`${authSelector},${privacySelector},${usernameSelector}`).attr("disabled", "disabled");
+                    break;
+                case "authNoPriv":
+                    $(`${authSelector},${usernameSelector}`).removeAttr("disabled");
+                    $(privacySelector).attr("disabled", "disabled");
+                    break;
+            }
+
+        });
+    }
+
+    function bindSNMPVersionSelect(modalSelector) {
+
+        $(`${modalSelector} select[name='snmp_version']`).change(function() {
+
+            if (!requiredFields[modalSelector]) requiredFields[modalSelector] = {};
+            if (!requiredFields[modalSelector]['community'])
+                requiredFields[modalSelector]['community'] = $(`.community-field [name][required]`);
+            if (!requiredFields[modalSelector]['non-community'])
+                requiredFields[modalSelector]['non-community'] = $(`.non-community-field [name][required]`);
+
+            console.log(requiredFields);
+
+            // if the selected SNMPversion is the third one
+            // then show the necessary fields (.non-community-field)
+            if ($(this).val() == SNMP_VERSION_THREE) {
+                $(`.community-field`).fadeOut(500, () => { $(`.non-community-field`).fadeIn(500); });
+                requiredFields[modalSelector]['community'].attr("disabled", "disabled");
+                requiredFields[modalSelector]['non-community'].removeAttr("disabled");
+                return;
+            }
+
+
+            // hide non community fields and show community fields
+            $(`.non-community-field`).fadeOut(500, () => { $(`.community-field`).fadeIn(500); });
+            requiredFields[modalSelector]['non-community'].attr("disabled", "disabled");
+            requiredFields[modalSelector]['community'].removeAttr("disabled");
+
+        });
+    }
+
+    function bindReloadingPoolsButton(reloadButtonSelector) {
+        $(reloadButtonSelector).click(async function(e) {
+
+            e.preventDefault();
+
+            $(this).attr("disabled", true);
+
+            const request = await fetch(`${http_prefix}/lua/rest/v1/get/snmp/device/pools.lua`);
+            const data = await request.json();
+
+            if (data.rc < 0) {
+                console.warn("Something went wrong when reloading SNMPPools list!");
+                return;
+            }
+
+            // update the select if there are new pools
+            data.rsp.forEach((pool) => {
+
+                const {pool_id, name} = pool;
+                // if there is already the pool then return
+                if ($(`#add-select-pool option[value='${pool_id}']`).length != 0) {
+                    return;
+                }
+
+                $(`#add-select-pool`).append(`<option value='${pool_id}'>${name}</option>`)
+            });
+
+            $(this).removeAttr("disabled");
+        });
+    }
+
+    /* ****************************************************************** */
+
+    bindReloadingPoolsButton(`#add-reload-pools`);
+    bindReloadingPoolsButton(`#edit-reload-pools`);
+
+    bindSNMPVersionSelect(`#add-snmp-device-modal`);
+    bindSNMPVersionSelect(`#edit-snmp-device-modal`);
+
+    // disable dropdown if the user inputs an hostname
+    bindSNMPHostKeyup(`#add-snmp-device-modal`);
+    bindSNMPHostKeyup(`#edit-snmp-device-modal`);
+    // disable passhphrase if the user selects none
+    bindSNMPLevelSelect(`#add-snmp-device-modal`);
+    bindSNMPLevelSelect(`#edit-snmp-device-modal`);
 
     let dtConfig = DataTableUtils.getStdDatatableConfig( [
         {
@@ -103,11 +222,7 @@ $(document).ready(function () {
             }
         }
     ]);
-    dtConfig = DataTableUtils.setAjaxConfig(
-        dtConfig,
-        "/lua/pro/enterprise/get_snmp_devices_list.lua",
-        'data',
-    );
+    dtConfig = DataTableUtils.setAjaxConfig(dtConfig, '/lua/pro/enterprise/get_snmp_devices_list.lua', 'data');
     dtConfig = DataTableUtils.extendConfig(dtConfig, {
         orderFixed: { post: [[1, "asc"]] },
         columns: [
@@ -172,6 +287,9 @@ $(document).ready(function () {
 
                     return (`
                         <div class="btn-group btn-group-sm" role="group">
+                            <a data-toggle="modal" class="btn btn-info" href="#edit-snmp-device-modal">
+                                <i class="fas fa-edit"></i>
+                            </a>
                             <a data-toggle="modal" class="btn btn-danger" href="#delete-snmp-device-modal">
                                 <i class="fas fa-trash"></i>
                             </a>
@@ -198,6 +316,25 @@ $(document).ready(function () {
 
     // initialize the DataTable with the created config
     const $snmpTable = $(`#table-devices`).DataTable(dtConfig);
+
+    const $editModalHandler = $(`#edit-snmp-device-modal form`).modalHandler({
+        method: 'post',
+        csrf: addCsrf,
+        resetAfterSubmit: false,
+        endpoint: `${ http_prefix }/lua/pro/rest/v1/edit/snmp/device/device.lua`,
+        beforeSumbit: () => {
+            return buildDataRequest('#edit-snmp-device-modal');
+        },
+        onModalInit: () => {
+            $(`#edit-snmp-device-modal input[name='snmp_host']`).val(snmpDeviceRowData.column_key).attr("readonly", true);
+            $(`#edit-snmp-device-modal input[name='snmp_read_community']`).val(snmpDeviceRowData.column_community);
+            $(`#edit-snmp-device-modal select[name='snmp_version']`).val(snmpDeviceRowData.column_version);
+            $(`#edit-snmp-device-modal select[name='pool']`).val(snmpDeviceRowData.column_pool_id);
+        },
+        onSubmitSuccess: (response, textStatus, modalHandler) => {
+            onRequestSuccess(response, textStatus, modalHandler, '#edit-snmp-device-modal');
+        }
+    });
 
     const $deleteModalHandler = $(`#delete-snmp-device-modal form`).modalHandler({
         method: 'post',
@@ -232,131 +369,26 @@ $(document).ready(function () {
         $editModalHandler.invokeModalInit();
     });
 
-
     $(`#add-snmp-device-modal form`).modalHandler({
         method: 'post',
         csrf: addCsrf,
         resetAfterSubmit: false,
         endpoint: `${ http_prefix }/lua/pro/rest/v1/add/snmp/device.lua`,
-        beforeSumbit: function() {
-
-            const data = {};
-            // show the spinner and hide the errors
-            $(`#add-snmp-feedback`).hide();
-            $(`#snmp-add-spinner`).fadeIn();
-
-            // build the post params
-            $(`#add-snmp-device-modal form`).find('input,select,textarea').each((idx, element) => {
-                data[$(element).attr("name")] = $(element).val();
-            });
-
-            return data;
+        beforeSumbit: () => {
+            return buildDataRequest(`#add-snmp-device-modal`);
         },
-        onModalInit: function() {
-
-            // disable dropdown if the user inputs an hostname
-            $(`input[name='snmp_host']`).keyup(function(e) {
-                const value = $(this).val();
-
-                if (new RegExp(REGEXES.domainName).test(value)) {
-                    $('#select-cidr').attr("disabled", "disabled");
-                }
-                else if (new RegExp(REGEXES.ipv6).test(value)) {
-                    $(`#select-cidr option[value!='128']`).attr("disabled", "disabled");
-                    $(`#select-cidr`).val(128);
-                }
-                else {
-                    $(`#select-cidr option[value!='128']`).removeAttr("disabled");
-                    $(`#select-cidr`).removeAttr("disabled");
-                }
-
-            });
-
-            // Disable passhphrase if the user selects none
-            $(`select#select-level-snmp`).change(function(e) {
-
-                const usernameSelector = `#input-snmp-username`;
-                const privacySelector = `#select-privacy-protocol-snmp,#input-privacy-passphrase`;
-                const authSelector = `#input-auth-passphrase,#select-auth-protocol-snmp`
-
-                switch ($(this).val()) {
-                    case "authPriv":
-                        $(`${privacySelector},${usernameSelector},${authSelector}`).removeAttr("disabled");
-                        break;
-                    case "noAuthNoPriv":
-                        $(`${authSelector},${privacySelector},${usernameSelector}`).attr("disabled", "disabled");
-                        break;
-                    case "authNoPriv":
-                        $(`${authSelector},${usernameSelector}`).removeAttr("disabled");
-                        $(privacySelector).attr("disabled", "disabled");
-                        break;
-                }
-
-            });
-        },
-        onSubmitSuccess: function (response, textStatus, modalHandler) {
-
-            if (response.rc < 0) {
-                // hide the spinner and show a localized error
-                $(`#snmp-add-spinner`).fadeOut(() => {
-                    $(`#add-snmp-feedback`).html(i18n.rest[response.rc_str]).fadeIn()
-                });
-                return;
-            }
-
-            // clean the form if the response was successful
-            modalHandler.cleanForm();
-            $snmpTable.ajax.reload(toggleSnmpTableButtons, false);
-            $(`#snmp-add-spinner`).hide();
-            $(`#add-snmp-device-modal`).modal('hide');
-
+        onModalShow: () => { $(`#add-snmp-feedback`).hide(); },
+        onSubmitSuccess: (response, textStatus, modalHandler) => {
+            onRequestSuccess(response, textStatus, modalHandler, '#add-snmp-device-modal');
         }
     }).invokeModalInit();
-
-    $(`#select-snmp-version`).change(function() {
-
-        const value = $(this).val();
-
-        // if the selected snmp version is the third one
-        // then show the necessary fields (.non-community-field)
-
-        if (value == SNMP_VERSION_THREE) {
-
-            requiredFieldsAdd.community.forEach(($input) => {
-                $input.removeAttr("required");
-            });
-            requiredFieldsAdd.nonCommunity.forEach(($input) => {
-                $input.attr("required", "");
-            });
-
-            $(`.community-field`).fadeOut(500, function() {
-                $(`.non-community-field`).fadeIn(500);
-            });
-            return;
-        }
-
-        $(`.non-community-field`).fadeOut(500, function() {
-
-            requiredFieldsAdd.nonCommunity.forEach(($input) => {
-                $input.removeAttr("required");
-            });
-            requiredFieldsAdd.community.forEach(($input) => {
-                $input.attr("required", "");
-            });
-
-            $(`.community-field`).fadeIn(500);
-        });
-
-    });
 
     // configure import config modal
     importModalHelper({
         load_config_xhr: (jsonConf) => {
-          return $.post(`${http_prefix}/lua/pro/enterprise/import_snmp_devices_config.lua`, {
-            csrf: importCsrf,
-            JSON: jsonConf,
-          });
-        }, reset_csrf: (newCsrf) => {
+          return $.post(`${http_prefix}/lua/pro/enterprise/import_snmp_devices_config.lua`, { csrf: importCsrf, JSON: jsonConf,});
+        },
+        reset_csrf: (newCsrf) => {
             importCsrf = newCsrf;
         }
     });
