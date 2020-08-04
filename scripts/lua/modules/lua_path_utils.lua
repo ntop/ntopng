13447c10c02bb42ca8ae6cc9d431e4c03dcb9e7f
Luca Deri <deri@ntop.org>
2020-08-04 11:24:57 +0200

    Fixed issue that was preventing the script to run

--- scripts/lua/modules/lua_path_utils.lua
+++ scripts/lua/modules/lua_path_utils.lua
@@ -6,11 +6,17 @@ local lua_path_utils = {}
 
 -- ########################################################
 
+function string.starts(String,Start)
+   return string.sub(String,1,string.len(Start)) == Start
+end
+
+-- ########################################################
+
 function lua_path_utils.package_path_prepend(path)
    local include_path = path.."/?.lua;"
 
    -- If the path is already inside package.path, we remove it, before prepending it
-   if not package.path:starts(include_path) and package.path:gmatch(include_path) then
+   if not string.starts(package.path, include_path) and package.path:gmatch(include_path) then
       package.path = package.path:gsub(include_path, '')
    end
 
