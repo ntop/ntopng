10b27b12c8dd783843434bbb0eb8fb64b0112dcf
Luca Deri <deri@ntop.org>
2020-08-12 10:54:57 +0200

    Added check for reducing load pressure when too many idle flow instances are present in the system

--- scripts/callbacks/interface/ht_state_update.lua
+++ scripts/callbacks/interface/ht_state_update.lua
@@ -15,6 +15,27 @@ local skip_user_scripts = false
 
 -- ########################################################
 
+local ht_stats = interface.getHashTablesStats()
+
+local idle_flows   = ht_stats.FlowHash.hash_entry_states.hash_entry_state_idle
+local active_flows = ht_stats.FlowHash.hash_entry_states.hash_entry_state_active
+local idle_ratio_threshold = 66.
+local idle_flows_threshold = 100000
+local idle_ratio
+
+if(active_flows == 0) then
+   idle_ratio = 0
+else
+   idle_ratio = (100 * idle_flows) / active_flows
+end
+
+if((idle_flows > idle_flows_threshold) or (idle_ratio > idle_ratio_threshold)) then
+   skip_user_scripts = true
+   io.write("[ht_state_update.lua] Skipping scripts on ".. interface.getName() ..": [idle ratio: "..idle_ratio.."]\n")   
+end
+
+-- ########################################################
+
 local periodic_ht_state_update_stats = interface.periodicHTStateUpdate(ntop.getDeadline(), skip_user_scripts)
 ts_dump.run_5sec_dump(interface.getId(), when, periodic_ht_state_update_stats)
 
