8a0525702eae7f79ec109fca582222425e0eb349
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-08-18 11:48:15 +0200

    Update internal rest api

--- scripts/lua/rest/v1/get/dns/resolve.lua
+++ scripts/lua/rest/v1/get/dns/resolve.lua
@@ -18,13 +18,13 @@ local rest_utils = require("rest_utils")
 
 sendHTTPHeader('application/json')
 
-local rc = rest_utils.consts_ok
+local rc = rest_utils.consts.success.ok
 local res = {}
 
 local hostname = _GET["hostname"]
 
 if isEmptyString(hostname) then
-   print(rest_utils.rc(rest_utils.consts_invalid_args))
+   print(rest_utils.rc(rest_utils.consts.err.invalid_args))
    return
 end
 
@@ -34,7 +34,7 @@ if not resolved then
 end
 
 if not resolved then
-   print(rest_utils.rc(rest_utils.consts_resolution_failed))
+   print(rest_utils.rc(rest_utils.consts.err.resolution_failed))
    return
 end
 
