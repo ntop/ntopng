9875e9b398f01aaecf53349c87f33d157af4b979
Simone Mainardi <mainardi@ntop.org>
2020-09-10 15:17:40 +0200

    Implements REST API endpoint to factory-reset notif. endpoints
    Implements #4391

---
+++ scripts/lua/rest/v1/delete/endpoints.lua
@@ -0,0 +1,16 @@
+--
+-- (C) 2013-20 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+local rest_utils = require "rest_utils"
+local notification_configs = require "notification_configs"
+local recipients = require "recipients"
+local recipients_instance = recipients:create()
+
+notification_configs.reset_configs()
+recipients_instance:cleanup()
+
+rest_utils.answer(rest_utils.consts.success.ok)
