4253d449965c062c6d21780e48d4fecdb6c09527
Luca Deri <deri@ntop.org>
2020-09-10 12:06:24 +0200

    Added check when recipient do not have a correspondign endpoint defined. Not a fix but a workaround until https://github.com/ntop/ntopng/issues/4389 is fixed

--- scripts/lua/get_recipients_endpoint.lua
+++ scripts/lua/get_recipients_endpoint.lua
@@ -25,8 +25,10 @@ local recipients = recipients_instance:get_all_recipients()
 -- Builtin recipients will be possibly included later and made uneditable from the UI
 local res = {}
 for _, recipient in pairs(recipients) do
-   if not recipient.endpoint_conf.builtin then
-      res[#res + 1] = recipient
+   if recipient.endpoint_conf then
+      if not recipient.endpoint_conf.builtin then
+	 res[#res + 1] = recipient
+      end
    end
 end
 
