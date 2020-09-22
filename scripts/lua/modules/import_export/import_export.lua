a6be8f79ac40da473d244d83ba3696b5d6e32a25
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-09-22 22:19:51 +0200

    Add import_export base module and user scripts module

---
+++ scripts/lua/modules/import_export/import_export.lua
@@ -0,0 +1,49 @@
+--
+-- (C) 2020 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+
+-- ##############################################
+
+local import_export = {}
+
+-- ##############################################
+
+function import_export:create(args)
+   if args then
+      -- We're being sub-classed
+      if not args.key then
+	 return nil
+      end
+   end
+
+   local this = args or { key = "import_export" }
+
+   setmetatable(this, self)
+   self.__index = self
+
+   return this
+end
+
+-- ##############################################
+
+-- @brief Import configuration
+-- @param conf The configuration to be imported
+-- @return A table with a key "success" set to true is returned on success. A key "err" is set in case of failure, with one of the errors defined in rest_utils.consts.err.
+function import_export:import(conf)
+   --
+end
+
+-- ##############################################
+
+-- @brief Export configuration
+-- @return The current configuration
+function import_export:export()
+   local conf = {}
+   return conf
+end
+
+-- ##############################################
+
+return import_export
---
+++ scripts/lua/modules/import_export/scripts_import_export.lua
@@ -0,0 +1,80 @@
+--
+-- (C) 2020 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/import_export/?.lua;" .. package.path
+require "lua_utils" 
+local import_export = require "import_export"
+local json = require "dkjson"
+local user_scripts = require "user_scripts"
+local rest_utils = require "rest_utils"
+
+-- ##############################################
+
+local scripts_import_export = {}
+
+-- ##############################################
+
+function scripts_import_export:create(args)
+    -- Instance of the base class
+    local _scripts_import_export = import_export:create()
+
+    -- Subclass using the base class instance
+    self.key = "scripts"
+    -- self is passed as argument so it will be set as base class metatable
+    -- and this will actually make it possible to override functions
+    local _scripts_import_export_instance = _scripts_import_export:create(self)
+
+    -- Compute
+
+    -- Return the instance
+    return _scripts_import_export_instance
+end
+
+-- ##############################################
+
+-- @brief Import configuration
+-- @param conf The configuration to be imported
+-- @return A table with a key "success" set to true is returned on success. A key "err" is set in case of failure, with one of the errors defined in rest_utils.consts.err.
+function scripts_import_export:import(conf)
+   local res = {}
+
+   if table.empty(conf) then
+      res.err = rest_utils.consts.err.bad_format
+      return res
+   end
+
+   if conf["0"] == nil then
+      res.err = rest_utils.consts.err.bad_content
+      return res
+   end
+
+   for config_id, config_set in pairs(conf) do
+      if config_set.name ~= nil then
+         local success, err = user_scripts.createOrReplaceConfigset(config_set)
+         if not success then
+            res.err = rest_utils.consts.err.internal_error
+         end
+      end
+   end
+
+   if not res.err then
+      res.success = true
+   end
+
+   return res
+end
+
+-- ##############################################
+
+-- @brief Export configuration
+-- @return The current configuration
+function scripts_import_export:export()
+   local conf = user_scripts.getConfigsets()
+   return conf
+end
+
+-- ##############################################
+
+return scripts_import_export
