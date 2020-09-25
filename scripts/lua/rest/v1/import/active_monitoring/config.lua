2a3d4b16efd7634197d342c33839a6c72175eb2c
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-09-25 10:09:31 +0200

    Add import/export endpoints for the Active Monitoring configuration (#4475)

---
+++ scripts/lua/rest/v1/import/active_monitoring/config.lua
@@ -0,0 +1,44 @@
+--
+-- (C) 2019-20 - ntop.org
+--
+dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+package.path = dirs.installdir .. "/scripts/lua/modules/import_export/?.lua;" .. package.path
+
+require "lua_utils"
+
+local plugins_utils = require("plugins_utils")
+local am_import_export = plugins_utils.loadModule("active_monitoring", "am_import_export")
+local json = require "dkjson"
+local rest_utils = require "rest_utils"
+local import_export_rest_utils = require "import_export_rest_utils"
+
+--
+-- Import Active Monitoring configuration
+--
+-- NOTE: in case of invalid login, no error is returned but redirected to login
+--
+
+if not haveAdminPrivileges() then
+   rest_utils.answer(rest_utils.consts.err.not_granted)
+   return
+end
+
+-- ################################################
+
+local modules = import_export_rest_utils.unpack(_POST["JSON"])
+
+if not modules or not modules["am"] then
+  rest_utils.answer(rest_utils.consts.err.invalid_args)
+  return
+end
+
+local items = {}
+local am_import_export = am_import_export:create()
+items["active_monitoring"] = {
+   conf = modules["active_monitoring"],
+   instance = am_import_export 
+}
+
+import_export_rest_utils.import(items)
+
