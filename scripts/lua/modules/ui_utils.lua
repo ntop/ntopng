f520a2708f4ad11d8acb199b46205204db9df6a3
gabryon99 <pappalardo@ntop.org>
2020-10-01 18:49:37 +0200

    removed iold import export related stuff

--- scripts/lua/modules/ui_utils.lua
+++ scripts/lua/modules/ui_utils.lua
@@ -9,14 +9,8 @@ local template_utils = require("template_utils")
 
 local ui_utils = {}
 
-function ui_utils.render_configuration_footer(import_export_context, reset_context)
-
-    local template = {
-        template_utils.gen('pages/components/import-export-link.template', import_export_context),
-        template_utils.gen('pages/components/reset-config-link.template', reset_context)
-    }
-
-    return table.concat(template, "\n")
+function ui_utils.render_configuration_footer(item)
+    return template_utils.gen('pages/components/manage-configuration-link.template', {item = item})
 end
 
 --- Single not element: { content = 'note description', hidden = true|false }
