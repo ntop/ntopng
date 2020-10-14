24a5b1f9085b1be8eefe8d11a060c052fa35c233
Luca Deri <deri@ntop.org>
2020-10-14 22:32:13 +0200

    Added minimum number of packets transmitted before triggering a retransmision alert

--- scripts/plugins/alerts/security/retransmissions/locales/en.lua
+++
@@ -1,8 +0,0 @@
---
--- (C) 2019-20 ntop.org
---
-
-return {
-   retransmissions_alert_description = "Alert when too many retransmitted flow packets",
-   retransmissions_alert_title = "Severe Retransmissions"
-}
