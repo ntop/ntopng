c151e1025309bb390943d1fa63ddac3b6d8a73f0
Wallace4 <mikde4@gmail.com>
2020-10-17 20:01:39 +0200

    New version of retransmission plugin, fixed and tested

---
+++ scripts/plugins/alerts/security/retransmissions/locales/en.lua
@@ -0,0 +1,8 @@
+--
+-- (C) ntop.org 2019-20
+--
+
+return {
+   retransmissions_alert_description = "Alert when too many retransmitted flow packets",
+   retransmissions_alert_title = "Severe Retransmissions"
+}
