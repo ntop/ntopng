5c6367e7bd9670178de156ecf83213913149cf3e
Matteo Biscosi <49585191+MatteoBiscosi@users.noreply.github.com>
2020-10-30 17:27:32 +0100

    Added Telegram plugin (#4269)

---
+++ scripts/plugins/endpoints/telegram_alert_endpoint/locales/en.lua
@@ -0,0 +1,19 @@
+--
+-- (C) 2020 - ntop.org
+--
+
+return {
+   telegram_token = "Token",
+   telegram_channel = "Channel Id",
+   validation = {
+      invalid_token = "Invalid Telegram Token.",
+      invalid_channel_name = "Invalid Telegram Channel Name.",
+   },
+   telegram_send_error = "Error sending message to Telegram.",
+
+   webhook_description = {
+      token_description = "Instructions:<ul><li>Start a new chat with @BotFather<li>Type and send '/newbot'<li>Give a name to your bot<li>Give a username to your bot<li>Copy here the token the @BotFather gave to you</ul>",
+      channel_id_description = "Instructions if you want to use the bot in a group:<ul><li>Add to your group the bot you created<li>Add to your group @getidsbot<li>Copy here the id the @getidsbot gave to you</ul>Instructions if you want to use the bot in a chat:<ul><li>Start a new conversation with @getidsbot<li>Copy here the id the @getidsbot gave to you</ul>",
+   }
+
+}
