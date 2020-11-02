0ae35362236c96ed28d8d412b7876a507f7d2ae8
gabryon99 <pappalardo@ntop.org>
2020-11-02 08:43:48 +0100

    add missing path #4661

--- scripts/lua/edit_notification_recipient.lua
+++ scripts/lua/edit_notification_recipient.lua
@@ -4,6 +4,7 @@
 
 local dirs = ntop.getDirs()
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+package.path = dirs.installdir .. "/scripts/lua/modules/pools/?.lua;" .. package.path
 package.path = dirs.installdir .. "/scripts/lua/modules/recipients/?.lua;" .. package.path
 
 require "lua_utils"
