7b7c1cf21f8fda1ee0e5254997b1a364fe0b1677
Luca Deri <deri@ntop.org>
2020-11-04 11:19:56 +0100

    Added tool for computing library dependencies on MacOS

---
+++ packages/macos/copylibs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+TARGET_DIR=$1
+
+if [ -z "$TARGET_DIR" ]; then
+    echo "Usage: copylibs.sh <target dir>"
+    exit
+fi
+
+TMPFILE="/tmp/findlibs"
+TMPFILEALL="/tmp/findlibsall"
+
+function findlibs {
+    otool -L $1 | tail -n +2 | cut -d '(' -f 1 | tr -d '[:blank:]' | grep -v "^/usr/lib/" | grep -v "^/System/Library/" >> $2
+}
+
+/bin/rm -f $TMPFILE $TMPFILEALL
+
+findlibs ../../ntopng $TMPFILE
+
+cp $TMPFILE $TMPFILEALL
+
+while read p; do
+    # echo "** Processing $p"
+    findlibs $p $TMPFILEALL
+done <$TMPFILE
+
+sort -u $TMPFILEALL > $TMPFILE
+
+#
+# One more try for nested dependencies
+#
+
+/bin/rm -f $TMPFILEALL
+
+while read p; do
+    # echo "** Processing $p"
+    findlibs $p $TMPFILEALL
+done <$TMPFILE
+
+sort -u $TMPFILEALL > $TMPFILE
+
+# Now copy the libraries to the target directory
+
+while read p; do
+    # echo "** cp $p $TARGET_DIR"
+    cp $p $TARGET_DIR
+done <$TMPFILE
+
+
+/bin/rm -f $TMPFILE $TMPFILEALL
