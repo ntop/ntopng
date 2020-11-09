cfac78e89eb4e9d0e3edc9ed16126b98d7809622
Matteo Biscosi <49585191+MatteoBiscosi@users.noreply.github.com>
2020-11-09 16:35:39 +0100

    Fixes #4416 Implement recipient/endpoint shell script (#4688)
    * Fixes #4416 implement shell script endpoint
    
    * removed debugging code
    
    * Removed for windows env

---
+++ scripts/plugins/endpoints/shell_alert_endpoint/locales/it.lua
@@ -0,0 +1,24 @@
+--
+-- (C) 2020 - ntop.org
+--
+
+
+return {
+    shell_script = "PERCORSO dello Script",
+    shell_options = "Opzioni",
+
+    validation = {
+        empty_path = "Il percorso di uno script shell non può essere vuoto.",
+        invalid_path = "Percorso dello script shell non valido. Lo script deve essere nella cartella \"/usr/share/ntopng/\".",
+        invalid_script = "Script non valido. Script ritenuto non sicuro.",
+     },
+
+    shell_send_error = "Errore nell'esecuzione dello script.", 
+
+    shell_description = {
+        path_description = "Istruzioni:<ul><li>Copiare qui il percorso in cui si trova lo script</ul>Note:<ul><li>Lo script deve essere all'interno di \"/usr/share/ntopng/\"<li>Lo script verrà richiamato nel seguente modo \"script -options JSON\" dove JSON è una stringa formattata JSON contenente gli allarmi</lu>",
+        option_description = "Istruzioni<ul><li>Inserire qui le opzioni che si vogliono passare allo script</ul>",
+     }
+ }
+ 
+ 
