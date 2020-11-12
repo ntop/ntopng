3d8b808cca61963948d9649daaa4f89f471754f1
matteo <biscosi@ntop.org>
2020-11-12 14:44:52 +0100

    Fixes ntop#4709 notify when a new device connects to the network

---
+++ scripts/plugins/alerts/network/new_device_connection/locales/en.lua
@@ -0,0 +1,8 @@
+--
+-- (C) 2020 - ntop.org
+--
+
+return {
+   description = "Trigger an alert when a new device connects to the network.", -- 
+   title = "New Device Connection",
+}
---
+++ scripts/plugins/alerts/network/new_device_connection/manifest.lua
@@ -0,0 +1,10 @@
+--
+-- (C) 2019-20 - ntop.org
+--
+
+return {
+  title = "New Device Connection",
+  description = "Trigger an alert upon a connection from a new device",
+  author = "ntop",
+  dependencies = {},
+}
