9889d6750fc803b2d81baca04cc89f82bf3d6d42
Simone Mainardi <mainardi@ntop.org>
2020-11-18 12:51:43 +0100

    Fixes infrastructure dashboard not hidden for non-admins

--- scripts/lua/inc/menu.lua
+++ scripts/lua/inc/menu.lua
@@ -121,7 +121,7 @@ else
        },
        {
           entry = page_utils.menu_entries.infrastructure_dashboard,
-          hidden = not ntop.isEnterpriseL() or not isAdministrator,
+          hidden = not ntop.isEnterpriseL() or not is_admin,
           url = '/lua/pro/enterprise/infrastructure_dashboard.lua'
        },
        {
