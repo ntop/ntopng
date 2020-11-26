5d7abd0b535baec9578e15a298f3047fa29380e4
Simone Mainardi <mainardi@ntop.org>
2020-11-26 18:03:17 +0100

    Alerts API class name change

--- scripts/plugins/alerts/security/new_api_demo/alert_definitions/alert_host_new_api_demo.lua
+++ scripts/plugins/alerts/security/new_api_demo/alert_definitions/alert_host_new_api_demo.lua
@@ -12,11 +12,11 @@ local alert = require "alert"
 
 -- ##############################################
 
-local NewHostAlert = classes.class(alert)
+local alert_host_new_api_demo = classes.class(alert)
 
 -- ##############################################
 
-NewHostAlert.meta = {
+alert_host_new_api_demo.meta = {
    alert_key = alert_keys.user.alert_user_04,
    i18n_title = "New Host API Demo",
    icon = "fas fa-exclamation",
@@ -28,8 +28,7 @@ NewHostAlert.meta = {
 -- @param one_param The first alert param
 -- @param another_param The second alert param
 -- @return A table with the alert built
-function NewHostAlert:init(one_param, another_param)
-   -- Optional, call to the `alert` constructor
+function alert_host_new_api_demo:init(one_param, another_param)
    self.super:init()
 
    self.alert_type_params = {
@@ -40,10 +39,10 @@ end
 
 -- #######################################################
 
-function NewHostAlert:format()
+function alert_host_new_api_demo:format()
    -- tprint("new format: "..self.def_script.alert_key)
 end
 
 -- #######################################################
 
-return NewHostAlert
+return alert_host_new_api_demo
