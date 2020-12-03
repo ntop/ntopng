01e18d44b80d0275ea819390a8427ace2b6cbd3f
gabryon99 <pappalardo@ntop.org>
2020-12-03 11:10:54 +0100

    fixed clipping dropdown inside responsive tables (#4798)

--- scripts/lua/inc/footer.lua
+++ scripts/lua/inc/footer.lua
@@ -168,14 +168,19 @@ print([[
 
 		$(document).ready(function() {
 
-		   /* fix the dropdown overflow */
-		   $('.table-responsive').on('show.bs.dropdown', function () {
-				$('.table-responsive').css( "overflow", "inherit" );
-		   });
-		   
-		   $('.table-responsive').on('hide.bs.dropdown', function () {
-				$('.table-responsive').css( "overflow", "auto" );
-		   })
+			$('.table-responsive').on('shown.bs.dropdown', function (e) {
+				var $table = $(this),
+					$menu = $(e.target).find('.dropdown-menu'),
+					tableOffsetHeight = $table.offset().top + $table.height(),
+					menuOffsetHeight = $menu.offset().top + $menu.outerHeight(true);
+			
+				if (menuOffsetHeight > tableOffsetHeight)
+				  $table.css("padding-bottom", menuOffsetHeight - tableOffsetHeight);
+			});
+			
+			$('.table-responsive').on('hide.bs.dropdown', function () {
+				$(this).css("padding-bottom", 0);
+			})
 
 			// ignore scientific numbers input
 			$(`.ignore-scientific`).keypress(function(e) {
