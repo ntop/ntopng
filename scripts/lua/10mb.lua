cf01160db42d93ae9e414b04f9017ddfae8f2b4b
Luca Deri <deri@ntop.org>
2020-12-11 18:00:35 +0100

    Test files for upload/download bandwidth test

---
+++ scripts/lua/10mb.lua
@@ -0,0 +1,16 @@
+--
+-- (C) 2013-20 - ntop.org
+--
+
+dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+require "lua_utils"
+
+sendHTTPContentTypeHeader('application/octet-stream')
+
+str = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+tot = 10 * (1024 * 1024) / string.len(str)
+
+for i=1,tot do
+   print(str)   
+end
