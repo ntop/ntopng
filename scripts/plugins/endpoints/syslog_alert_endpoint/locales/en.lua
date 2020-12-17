124156fd3e1ab899140007bca6a6683ebf0af438
Simone Mainardi <mainardi@ntop.org>
2020-12-17 18:34:41 +0100

    Adds references and explanation to syslog alerts format
    Fixes #4843

--- scripts/plugins/endpoints/syslog_alert_endpoint/locales/en.lua
+++ scripts/plugins/endpoints/syslog_alert_endpoint/locales/en.lua
@@ -6,6 +6,8 @@ return {
    alert_format = "Format",
    content = "Content",
    description = "Host, Port and Protocol should be specified for remote syslog servers only.",
+   description_ecs = "ECS (Elasticsearch Common Schema) format is documented <a href='%{url}' target='_blank'>here</a> <i class='%{icon}'></i>.",
+   description_raw_json = "Raw JSON format is self-documented in the <a href='%{url}' target='_blank'>code</a> <i class='%{icon}'></i> and is meant to be used only by programmers who intend to programmatically process notifications.",
    host = "Host",
    port = "Port",
    protocol = "Protocol",
