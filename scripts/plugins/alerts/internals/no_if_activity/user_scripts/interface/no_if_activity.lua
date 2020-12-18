04a26563819d5341b2c738981d4877a7137659fe
Alfredo Cardigliano <cardigliano@ntop.org>
2020-12-18 10:38:10 +0100

    Handle syslog interface in no_if_activity plugin

--- scripts/plugins/alerts/internals/no_if_activity/user_scripts/interface/no_if_activity.lua
+++ scripts/plugins/alerts/internals/no_if_activity/user_scripts/interface/no_if_activity.lua
@@ -20,6 +20,11 @@ local function check_interface_activity(params)
   -- Get total number of packets, flows and interface id
   local num_packets = params.entity_info.eth.packets
   local num_flows = params.entity_info.stats.flows
+  local num_logs = 0
+  if params.entity_info.syslog then
+    num_logs = params.entity_info.syslog.tot_events
+  end
+
   local ifid = tonumber(params.entity_info.id)
   local tmp_if_table = interface.getIfNames()
 
@@ -27,7 +32,7 @@ local function check_interface_activity(params)
   local ifname = tmp_if_table[tostring(ifid)]
 
   -- Creating a string, used to put into the redis cache the number of packets and flows
-  local new_counters = num_packets .. "_" .. num_flows
+  local new_counters = num_packets .. "_" .. num_flows .. "_" .. num_logs
 
   local no_if_activity_type = alert_consts.alert_types.alert_no_if_activity.create(
       alert_severities.error,
@@ -39,12 +44,14 @@ local function check_interface_activity(params)
   -- Get from the cache the previous number of total packets received
   local previous_counters = ntop.getCache(redis_key)
 
-  previous_packets, previous_flows = string.match(previous_counters, "(.*)_(.*)")
+  previous_packets, previous_flows, previous_logs = string.match(previous_counters, "(.*)_(.*)_(.*)")
 
   -- Check if the previous number it's equal to the actual number of both, packets and flows
   -- this distinction is done due to the fact that exist packet based interfaces
   -- and flow based interfaces
-  if(tonumber(previous_packets) == num_packets and tonumber(previous_flows) == num_flows) then
+  if(tonumber(previous_packets) == num_packets and
+     tonumber(previous_flows)   == num_flows and
+     tonumber(previous_logs)    == num_logs) then
     alerts_api.trigger(params.alert_entity, no_if_activity_type, nil, params.cur_alerts)
 
   else -- One of the two or both stats were different, so the interface is still active
