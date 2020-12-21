7c6c249adf8397fc97e536918b9037f2c84aaf09
Simone Mainardi <mainardi@ntop.org>
2020-12-21 10:01:42 +0100

    Fixes attempt to index a nil value (global 'dirs')

--- scripts/lua/modules/alert_definitions/alert_periodic_activity_not_executed.lua
+++ scripts/lua/modules/alert_definitions/alert_periodic_activity_not_executed.lua
@@ -2,6 +2,7 @@
 -- (C) 2019-20 - ntop.org
 --
 
+local dirs = ntop.getDirs()
 local alert_keys = require "alert_keys"
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 local format_utils = require "format_utils"
