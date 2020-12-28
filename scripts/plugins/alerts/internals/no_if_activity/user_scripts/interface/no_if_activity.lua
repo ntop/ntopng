8a7cca644a5d47eaf06c37f6554ac2ff5a707f31
Simone Mainardi <mainardi@ntop.org>
2020-12-28 16:01:38 +0100

    Reworks (simplifies) no_if_activity to avoid Redis accesses
    Partially addresses #4673

--- scripts/plugins/alerts/internals/no_if_activity/user_scripts/interface/no_if_activity.lua
+++ scripts/plugins/alerts/internals/no_if_activity/user_scripts/interface/no_if_activity.lua
@@ -11,116 +11,62 @@ local script
 
 -- #################################################################
 
--- Name of the cache key, created from a fixed name + ifid
-local NO_ACTIVITY_PLUGIN_CACHE_KEY = "ntopng.cache.user_scripts.no_activity_plugins_enabled.ifid"
-
-
 local function check_interface_activity(params)
+   -- Get total number of packets, flows and interface id
+   local num_packets = params.entity_info.eth.packets
+   local num_flows = params.entity_info.stats.new_flows -- .new_flows keep the cumulative total, .flows is just a gauge
+   local num_logs = 0
+   if params.entity_info.syslog then
+      num_logs = params.entity_info.syslog.tot_events
+   end
 
-  -- Get total number of packets, flows and interface id
-  local num_packets = params.entity_info.eth.packets
-  local num_flows = params.entity_info.stats.flows
-  local num_logs = 0
-  if params.entity_info.syslog then
-    num_logs = params.entity_info.syslog.tot_events
-  end
+   local no_if_activity_type = alert_consts.alert_types.alert_no_if_activity.new()
 
-  local ifid = tonumber(params.entity_info.id)
-  local tmp_if_table = interface.getIfNames()
+   no_if_activity_type:set_severity(alert_severities.error)
+   no_if_activity_type:set_granularity(params.granularity)
 
-  -- Getting the interface name from the list of interfaces
-  local ifname = tmp_if_table[tostring(ifid)]
+   local delta_packets = alerts_api.interface_delta_val(params.user_script.key..".pkts" --[[ metric name --]], params.granularity, num_packets or 0)
+   local delta_flows = alerts_api.interface_delta_val(params.user_script.key..".flows" --[[ metric name --]], params.granularity, num_flows or 0)
+   local delta_logs = alerts_api.interface_delta_val(params.user_script.key..".logs" --[[ metric name --]], params.granularity, num_logs or 0)
 
-  -- Creating a string, used to put into the redis cache the number of packets and flows
-  local new_counters = num_packets .. "_" .. num_flows .. "_" .. num_logs
+   -- tprint(">>> selected: "..interface.getId() .. " name: "..getInterfaceName(interface.getId()))
+   -- tprint(params.alert_entity)
+   -- tprint("delta_packets: "..delta_packets.. " delta_flows: "..delta_flows.. " delta_logs: "..delta_logs)
+   -- tprint("num_packets: "..num_packets.. " num_flows: "..num_flows.. " num_logs: "..num_logs)
+   -- tprint("<<<")
 
-  local no_if_activity_type = alert_consts.alert_types.alert_no_if_activity.new()
-
-  no_if_activity_type:set_severity(alert_severities.error)
-  no_if_activity_type:set_granularity(params.granularity)
-  
-  local redis_key = NO_ACTIVITY_PLUGIN_CACHE_KEY .. ifname
-  
-  -- Get from the cache the previous number of total packets received
-  local previous_counters = ntop.getCache(redis_key)
-
-  previous_packets, previous_flows, previous_logs = string.match(previous_counters, "(.*)_(.*)_(.*)")
-
-  -- Check if the previous number it's equal to the actual number of both, packets and flows
-  -- this distinction is done due to the fact that exist packet based interfaces
-  -- and flow based interfaces
-  if(tonumber(previous_packets) == num_packets and
-     tonumber(previous_flows)   == num_flows and
-     tonumber(previous_logs)    == num_logs) then
-     no_if_activity_type:trigger(params.alert_entity, nil, params.cur_alerts)
-
-  else -- One of the two or both stats were different, so the interface is still active
-    no_if_activity_type:release(params.alert_entity, nil, params.cur_alerts)
-  end
-
-  ntop.setCache(NO_ACTIVITY_PLUGIN_CACHE_KEY .. ifname, new_counters, 360)
+   -- Check if the previous number it's equal to the actual number of both, packets and flows
+   -- this distinction is done due to the fact that exist packet based interfaces
+   -- and flow based interfaces
+   if delta_packets == 0 and delta_flows == 0 and delta_logs == 0 then
+      no_if_activity_type:trigger(params.alert_entity, nil, params.cur_alerts)
+   else -- One of the two or both stats were different, so the interface is still active
+      no_if_activity_type:release(params.alert_entity, nil, params.cur_alerts)
+   end
 end
 
 -- #################################################################
 
 script = {
-  -- Script category
-  category = user_scripts.script_categories.internals,
+   -- Script category
+   category = user_scripts.script_categories.internals,
 
-  default_enabled = true,
-  hooks = {
-    -- Time past between one call and an other
-    --["5mins"] = check_interface_activity,
-    min = check_interface_activity,
-  },
+   default_enabled = true,
+   hooks = {
+      -- Time past between one call and an other
+      --["5mins"] = check_interface_activity,
+      min = check_interface_activity,
+   },
 
-  -- This script is only for alerts generation
-  is_alert = true,
+   -- This script is only for alerts generation
+   is_alert = true,
 
-  gui = {
-    i18n_title        = "no_if_activity.no_if_activity_title",
-    i18n_description  = "no_if_activity.no_if_activity_description",
-  }
+   gui = {
+      i18n_title        = "no_if_activity.no_if_activity_title",
+      i18n_description  = "no_if_activity.no_if_activity_description",
+   }
 }
 
 -- #################################################################
-
-function script.onEnable(hook, hook_config)
-  ntop.setPref(NO_ACTIVITY_PLUGIN_CACHE_KEY, "1")
-end
-
--- #################################################################
-
-function script.onUnload(hook, hook_config)
-  local tmp_table = interface.getIfNames()
-
-  -- Removing the entries from the redis cache table
-  for k in pairs(tmp_table) do 
-    ntop.delCache(NO_ACTIVITY_PLUGIN_CACHE_KEY .. tmp_table[k]) 
-  end
-end
-
--- #################################################################
-
-function script.onDisable(hook, hook_config)
-  local tmp_table = interface.getIfNames()
-
-  -- Removing the entries from the redis cache table
-  for k in pairs(tmp_table) do 
-    ntop.delCache(NO_ACTIVITY_PLUGIN_CACHE_KEY .. tmp_table[k]) 
-  end
-end
-
--- #################################################################
-
-function script.onLoad(hook, hook_config)
-  if hook_config and hook_config.enabled then
-     ntop.setPref(NO_ACTIVITY_PLUGIN_CACHE_KEY, "1")
-  end
-
-end
-
--- #################################################################
-
 
 return script
