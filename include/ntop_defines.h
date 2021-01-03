1e9b52be06e4e13ca478a1ac1d8405fd85e2e7b5
Luca Deri <deri@ntop.org>
2021-01-03 11:41:36 +0100

    FreeBSD path changes

--- include/ntop_defines.h
+++ include/ntop_defines.h
@@ -477,7 +477,6 @@
 #define NTOPNG_NDPI_OS_PROTO_ID      (NDPI_LAST_IMPLEMENTED_PROTOCOL+NDPI_MAX_NUM_CUSTOM_PROTOCOLS-2)
 #define CONST_DEFAULT_HOME_NET       "192.168.1.0/24"
 #define CONST_OLD_DEFAULT_DATA_DIR   "/var/tmp/ntopng"
-#define CONST_DEFAULT_DATA_DIR       "/var/lib/ntopng"
 #define CONST_DEFAULT_MAX_UI_STRLEN  24
 #define CONST_DEFAULT_IS_AUTOLOGOUT_ENABLED               1
 #define CONST_DEFAULT_IS_IDLE_LOCAL_HOSTS_CACHE_ENABLED   1
@@ -492,9 +491,13 @@
 #if defined(__FreeBSD__)
 #define CONST_ALT_INSTALL_DIR        "/usr/local/share/ntopng"
 #define CONST_ALT2_INSTALL_DIR       "/usr/share/ntopng"
+#define CONST_ETC_DIR                "/usr/local/etc"
+#define CONST_DEFAULT_DATA_DIR       "/var/db/ntopng"
 #else
 #define CONST_ALT_INSTALL_DIR        "/usr/share/ntopng"
 #define CONST_ALT2_INSTALL_DIR       "/usr/local/share/ntopng"
+#define CONST_ETC_DIR                "/etc"
+#define CONST_DEFAULT_DATA_DIR       "/var/lib/ntopng"
 #endif
 #define CONST_HTTP_PREFIX_STRING     "@HTTP_PREFIX@"
 #define CONST_NTOP_STARTUP_EPOCH     "@NTOP_STARTUP_EPOCH@"
