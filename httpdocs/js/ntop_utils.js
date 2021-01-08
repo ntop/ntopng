59d1c440c18b8c8512fa15e15d65cb943d16b3a2
gabryon99 <pappalardo@ntop.org>
2021-01-08 12:12:37 +0100

    fixed comparion check inside typeahead afterSelect

--- httpdocs/js/ntop_utils.js
+++ httpdocs/js/ntop_utils.js
@@ -661,7 +661,8 @@ class NtopUtils {
 				form.attr("action", http_prefix + "/lua/hosts_stats.lua");
 				NtopUtils._add_find_host_link(form, "asn", data.asn);
 			} else if (data.type == "interface") {
-				// TODO: toggleSystemInterface(false, $('#switch_interface_form_' + data.ifid));
+				toggleSystemInterface(false, $('#switch_interface_form_' + data.ifid));
+				return false;
 			} else {
 				form.attr("action", http_prefix + "/lua/host_details.lua");
 				NtopUtils._add_find_host_link(form, "mode", "restore");
