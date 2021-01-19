d89516539f8a0fc1022b06ca3234e016e210d2ac
Simone Mainardi <mainardi@ntop.org>
2021-01-19 12:05:23 +0100

    Fixes Elasticsearch version initialization
    Possibly fixes #4833

--- include/ElasticSearch.h
+++ include/ElasticSearch.h
@@ -34,6 +34,7 @@ class ElasticSearch : public DB {
 
   char *es_template_push_url, *es_version_query_url;
   char es_version[2];
+  bool es_version_inited;
   const char * const get_es_version();
   const char * const get_es_template();
 
