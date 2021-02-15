5c6c7b173ea771c273a2850fa28b22d028218555
Simone Mainardi <mainardi@ntop.org>
2021-02-15 18:19:06 +0100

    Ensures purgeIdle is executed before executing tests

--- src/PcapInterface.cpp
+++ src/PcapInterface.cpp
@@ -375,6 +375,9 @@ static void* packetPollLoop(void* ptr) {
     } /* while */
   } while(pcap_list != NULL);
 
+  /* Sleep for a purge frequency to ensure purgeIdle will be called against all entries */
+  sleep(OTHER_PURGE_FREQUENCY);
+
   iface->purgeIdle(time(NULL), false, true /* Full scan */);
 
   if(iface->read_from_pcap_dump() && !iface->reproducePcapOriginalSpeed()) {
