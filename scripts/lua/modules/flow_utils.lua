02aa0af75b0a12b306ed2cd915a270bfc71d4af1
Matteo Biscosi <biscosi@ntop.org>
2021-02-19 16:51:27 +0100

    Added description to hosts badges

--- scripts/lua/modules/flow_utils.lua
+++ scripts/lua/modules/flow_utils.lua
@@ -552,9 +552,9 @@ local function formatFlowHost(flow, cli_or_srv, historical_bounds, hyperlink_suf
      host_name = host_name.." <i class='fas fa-ban' aria-hidden='true' title='Blacklisted'></i>"
   end
   if(flow[cli_or_srv .. ".localhost"] == true) then
-     host_name = host_name .. ' <span class="badge badge-success">'..i18n("details.label_short_local_host")..'</span> '
+     host_name = host_name .. ' <abbr title=\"'.. i18n("details.label_local_host") ..'\"><span class="badge badge-success">'..i18n("details.label_short_local_host")..'</span></abbr>'
   else 
-     host_name = host_name .. ' <span class="badge badge-secondary">'..i18n("details.label_short_remote_host")..'</span> '
+     host_name = host_name .. ' <abbr title=\"'.. i18n("details.label_remote") ..'\"><span class="badge badge-secondary">'..i18n("details.label_short_remote")..'</span></abbr>'
   end
 
   return hostinfo2detailshref(flow2hostinfo(flow, cli_or_srv), hyperlink_params, host_name, nil, true --[[ perform link existance checks --]])
--- scripts/lua/modules/format_utils.lua
+++ scripts/lua/modules/format_utils.lua
@@ -333,17 +333,17 @@ function format_utils.formatAddressCategory(host)
       end
 
       if(host["localhost"] == true) then
-         addr_category = addr_category .. ' <span class="badge badge-success">'..i18n("details.label_local_host")..'</span>'
+         addr_category = addr_category .. ' <abbr title=\"'.. i18n("details.label_local_host") ..'\"><span class="badge badge-success">'..i18n("details.label_short_local_host")..'</span></abbr>'
       else 
-         addr_category = addr_category .. ' <span class="badge badge-secondary">'..i18n("details.label_remote")..'</span>'
+         addr_category = addr_category .. ' <abbr title=\"'.. i18n("details.label_remote") ..'\"><span class="badge badge-secondary">'..i18n("details.label_short_remote")..'</span></abbr>'
       end
 
       if(host["is_multicast"] == true) then 
-         addr_category = addr_category .. " <span class='badge badge-primary'>" ..i18n("multicast").. "</span> "
+         addr_category = addr_category .. " <abbr title=\"".. i18n("multicast") .."\"><span class='badge badge-primary'>" ..i18n("short_multicast").. "</span></abbr>"
       end
       
       if(host["is_broadcast"] == true) then 
-         addr_category = addr_category .. " <span class='badge badge-dark'>" ..i18n("broadcast").. "</span> "
+         addr_category = addr_category .. " <abbr title=\"".. i18n("broadcast") .."\"><span class='badge badge-dark'>" ..i18n("short_broadcast").. "</span></abbr>"
       end
       
       if(host["broadcast_domain_host"] == true) then
@@ -351,7 +351,7 @@ function format_utils.formatAddressCategory(host)
       end
       
       if(host["privatehost"] == true) then 
-         addr_category = addr_category .. ' <span class="badge badge-warning">'..i18n("details.label_private_ip")..'</span>'
+         addr_category = addr_category .. ' <abbr title=\"'.. i18n("details.label_private_ip") ..'\"><span class="badge badge-warning">'..i18n("details.label_short_private_ip")..'</span></abbr>'
       end
 
       if(host["dhcpHost"] == true) then
