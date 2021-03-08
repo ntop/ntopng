e90597f3e121f0a66a2364b64c16f1993a0c83f1
Alfredo Cardigliano <cardigliano@ntop.org>
2021-03-08 09:43:25 +0100

    Fix export of entire configuration (#5083)

--- scripts/lua/rest/v1/export/all/config.lua
+++ scripts/lua/rest/v1/export/all/config.lua
@@ -7,7 +7,16 @@ package.path = dirs.installdir .. "/scripts/lua/modules/import_export/?.lua;" ..
 
 require "lua_utils"
 
+local plugins_utils = require("plugins_utils")
+
 local all_import_export = require "all_import_export"
+local snmp_import_export = require "snmp_import_export"
+local am_import_export = plugins_utils.loadModule("active_monitoring", "am_import_export")
+local notifications_import_export = require "notifications_import_export"
+local scripts_import_export = require "scripts_import_export"
+local pool_import_export = require "pool_import_export"
+local infrastructure_import_export = require("infrastructure_import_export")
+
 local rest_utils = require "rest_utils"
 local import_export_rest_utils = require "import_export_rest_utils"
 
@@ -27,5 +36,11 @@ end
 
 local instances = {}
 instances["all"] = all_import_export:create()
+instances["snmp"] = snmp_import_export:create()
+instances["active_monitoring"] = am_import_export:create()
+instances["notifications"] = notifications_import_export:create()
+instances["scripts"] = scripts_import_export:create()
+instances["pool"] = pool_import_export:create()
+instances["infrastructure"] = infrastructure_import_export:create()
 import_export_rest_utils.export(instances, not isEmptyString(download))
 
