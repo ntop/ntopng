d42bfc5072bf233e6c9a535d822adc0d7fcb7ae3
gabryon99 <pappalardo@ntop.org>
2021-03-12 11:08:10 +0100

    improvments on test_picker page

--- tests/lua/test_picker.lua
+++ tests/lua/test_picker.lua
@@ -7,8 +7,10 @@ package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 
 require "lua_utils"
 local page_utils = require("page_utils")
+local ui_utils = require "ui_utils"
+local json = require "dkjson"
+local template_utils = require "template_utils"
 local widget_gui_utils = require "widget_gui_utils"
-
 local Datasource = widget_gui_utils.datasource
 
 sendHTTPContentTypeHeader('text/html')
@@ -23,13 +25,16 @@ local begin_epoch = _GET["begin_epoch"] or (os.time() - 3600)
 local end_epoch = _GET["end_epoch"] or (os.time())
 local totalRows = _GET["totalRows"] or 10
 
-widget_gui_utils.render_table_picker('my-table', {
+template_utils.render("pages/table-picker.template", {
+    ui_utils = ui_utils,
+    json = json,
+    template_utils = template_utils,
+    modals = {},
     datasource = Datasource("/lua/rest/v1/get/time/data.lua", {begin_epoch = begin_epoch, end_epoch = end_epoch, totalRows = totalRows}),
-    table = {
-        columns = {'Index', 'Date'},
-        js_columns = ([[
-            [{data: 'index', width: '100px'}, {data: 'date'}]
-        ]])
+    datatable = {
+        name = 'my-table', -- the table name
+        columns = {'Index', 'Date'}, -- the columns to print inside the table
+        js_columns = ([[ [{data: 'index', width: '100px'}, {data: 'date'}] ]]), -- a custom javascript code to format the columns
     }
 })
 
