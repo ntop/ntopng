99b9b43cb532d04ed1074e51f07f956e8d86aaa0
Matteo Biscosi <biscosi@ntop.org>
2021-03-30 16:02:32 +0200

    Reviewed no data exchanged alert description

--- scripts/plugins/alerts/network/tcp_no_data_exchanged/locales/it.lua
+++ scripts/plugins/alerts/network/tcp_no_data_exchanged/locales/it.lua
@@ -3,17 +3,11 @@
 --
 
 return {
-   zero_tcp_window_description = "Emette un allarme quando la TCP window di un flusso ha dimensione zero",
-   zero_tcp_window_title = "TCP Window Zero",
-
--- ####################### Status strings
-
-   status_zero_tcp_window_description     = "La TCP window è zero",
-   status_zero_tcp_window_description_c2s = "La TCP window del client è zero",
-   status_zero_tcp_window_description_s2c = "La TCP window del server è zero",
+   tcp_no_data_exchanged_description = "Lancia un Alert quando un flusso TCP termina e nessun dato è stato scambiato",
+   tcp_no_data_exchanged_title = "Nessun Dato Scambiato",
 
 -- ####################### Alert strings
 
-   alert_zero_tcp_window_title = "TCP Window Zero",
-   alert_zero_tcp_window_description = "La TCP Window è Zero",
+   alert_tcp_no_data_exchanged_title = "Nessun Dato Scambiato",
+   alert_tcp_no_data_exchanged_description = "Flusso TCP terminato con nessun dato scambiato",
 }
