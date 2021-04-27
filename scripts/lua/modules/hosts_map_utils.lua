9b85af028062bfaf9c4c5abc741d90895851edab
Matteo Biscosi <biscosi@ntop.org>
2021-04-27 11:46:40 +0200

    Fixes missing value format Implements #5222

--- scripts/lua/modules/hosts_map_utils.lua
+++ scripts/lua/modules/hosts_map_utils.lua
@@ -28,22 +28,30 @@ hosts_map_utils.MODES = {
         mode = hosts_map_utils.HostsMapMode.ALL_FLOWS,
         label   = i18n("hosts_map_page.all_flows"),
         x_label = i18n("hosts_map_page.labels.f_s"),
-        y_label = i18n("hosts_map_page.labels.f_c")
+        y_label = i18n("hosts_map_page.labels.f_c"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.UNREACHABLE_FLOWS,
         label   = i18n("hosts_map_page.unreach_flows"),
         x_label = i18n("hosts_map_page.labels.uf_s"),
-        y_label = i18n("hosts_map_page.labels.uf_c")
+        y_label = i18n("hosts_map_page.labels.uf_c"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.ALERTED_FLOWS,
         label   = i18n("hosts_map_page.alerted_flows"),
         x_label = i18n("hosts_map_page.labels.af_s"),
-        y_label = i18n("hosts_map_page.labels.af_c")
+        y_label = i18n("hosts_map_page.labels.af_c"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.DNS_QUERIES,
         label   = i18n("hosts_map_page.dns_queries"),
         x_label = i18n("hosts_map_page.labels.dns_p_r"),
-        y_label = i18n("hosts_map_page.labels.dns_s")
+        y_label = i18n("hosts_map_page.labels.dns_s"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.DNS_BYTES,
         label   = i18n("hosts_map_page.dns_bytes"),
@@ -64,17 +72,23 @@ hosts_map_utils.MODES = {
         mode = hosts_map_utils.HostsMapMode.SYN_DISTRIBUTION,
         label   = i18n("hosts_map_page.syn_distribution"),
         x_label = i18n("hosts_map_page.labels.syn_s"),
-        y_label = i18n("hosts_map_page.labels.syn_r")
+        y_label = i18n("hosts_map_page.labels.syn_r"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.SYN_VS_RST,
         label   = i18n("hosts_map_page.syn_vs_rst"),
         x_label = i18n("hosts_map_page.labels.syn_s"),
-        y_label = i18n("hosts_map_page.labels.rst_r")
+        y_label = i18n("hosts_map_page.labels.rst_r"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.SYN_VS_SYNACK,
         label   = i18n("hosts_map_page.syn_vs_synack"),
         x_label = i18n("hosts_map_page.labels.syn_s"),
-        y_label = i18n("hosts_map_page.labels.sa_r")
+        y_label = i18n("hosts_map_page.labels.sa_r"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.TCP_PKTS_SENT_VS_RCVD,
         label   = i18n("hosts_map_page.tcp_pkts_sent_vs_rcvd"),
@@ -89,27 +103,34 @@ hosts_map_utils.MODES = {
         y_label = i18n("hosts_map_page.labels.tcp_b_r"),
         x_formatter = "bytesToSize",
         y_formatter = "bytesToSize",
-    }, {
+       }, {
         mode = hosts_map_utils.HostsMapMode.ACTIVE_ALERT_FLOWS,
         label   = i18n("hosts_map_page.active_alert_flows"),
         x_label = i18n("hosts_map_page.labels.f_a_s"),
-        y_label = i18n("hosts_map_page.labels.f_a_c")
+        y_label = i18n("hosts_map_page.labels.f_a_c"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.TRAFFIC_RATIO,
         label   = i18n("hosts_map_page.traffic_ratio"),
         x_label = i18n("hosts_map_page.labels.b_ratio"),
         y_label = i18n("hosts_map_page.labels.p_ratio"),
-        y_formatter = "toFixed2"
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.SCORE,
         label   = i18n("hosts_map_page.score"),
         x_label = i18n("hosts_map_page.labels.client_score"),
-        y_label = i18n("hosts_map_page.labels.server_score")
+        y_label = i18n("hosts_map_page.labels.server_score"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }, {
         mode = hosts_map_utils.HostsMapMode.BLACKLISTED_FLOWS_HOSTS,
         label   = i18n("hosts_map_page.blacklisted_flows_hosts"),
         x_label = i18n("hosts_map_page.labels.blacklisted_as_client"),
-        y_label = i18n("hosts_map_page.labels.blacklisted_as_server")
+        y_label = i18n("hosts_map_page.labels.blacklisted_as_server"),
+        x_formatter = "fint",
+        y_formatter = "fint",
     }
 }
 
