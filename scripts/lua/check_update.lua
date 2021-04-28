28fd3b3516dfd0dba8e2be1a693327d410183ee0
Alfredo Cardigliano <cardigliano@ntop.org>
2021-04-28 09:09:35 +0000

    Handle manual update after failure

--- scripts/lua/check_update.lua
+++ scripts/lua/check_update.lua
@@ -51,7 +51,20 @@ if _POST["search"] ~= nil then
   checking_updates = "1"
 end
 
+-- Checking if there is a new version and current version is < available version
 local new_version = ntop.getCache(new_version_available_key)
+if not isEmptyString(new_version) then
+  local curr_version = version2number(info["version"], info["revision"])
+  local new_version_spl = string.split(new_version, "-");
+  if new_version_spl ~= nil then
+    local avail_version = version2number(new_version_spl[1], new_version_spl[2])
+    if avail_version <= curr_version then
+      new_version = nil
+    end
+  else
+    new_version = nil
+  end
+end
 
 -- Check if an upgrade has been already requested
 local installing = ntop.getCache(upgrade_request_key)
@@ -63,7 +76,9 @@ else
   if checking_updates == nil then
     checking_updates = ntop.getCache(check_for_updates_key)
   end
+
   if not isEmptyString(checking_updates) then
+    -- Checking updates
     status = "checking"
   else
 
@@ -71,26 +86,20 @@ else
     local update_failure = ntop.getCache(update_failure_key)
 
     -- Allow updates with no license in forced Community mode
-    if not isEmptyString(update_failure) and update_failure == "no-license" and 
-      ntop.isForcedCommunity() then
-      update_failure = nil
+    if not isEmptyString(update_failure) then
+      if update_failure == "no-license" and ntop.isForcedCommunity() then
+        update_failure = nil
+      elseif update_failure == "upgrade-failure" and isEmptyString(new_version) then
+        update_failure = nil -- Manual update after a failure?
+      end
     end
 
     if not isEmptyString(update_failure) then
       status = update_failure
     else
-
       -- Check if the availability of a new update has been detected
       if not isEmptyString(new_version) then
-        -- Checking if current version is < available version (to handle manual updates)
-        local curr_version = version2number(info["version"], info["revision"])
-        local new_version_spl = string.split(new_version, "-");
-        if new_version_spl ~= nil then
-          local avail_version = version2number(new_version_spl[1], new_version_spl[2])
-          if avail_version > curr_version then
-            status = "update-avail"
-          end
-        end
+        status = "update-avail"
       end
     end
   end
