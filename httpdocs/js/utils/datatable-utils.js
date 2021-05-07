6171d8cd29bb6eaa7630fe5d9307dfcc7d375c50
Matteo Biscosi <biscosi@ntop.org>
2021-05-07 10:58:15 +0200

    Fixes flow filters not working

--- httpdocs/js/utils/datatable-utils.js
+++ httpdocs/js/utils/datatable-utils.js
@@ -642,8 +642,8 @@ class DataTableRenders {
         return seconds;
     }
 
-    static filterize(value, label) {
-        return `<a class='tag-filter' data-tag-value='${value}' href='#'>${label || value}</a>`;
+    static filterize(key, value, label) {
+        return `<a class='tag-filter' data-tag-key='${key}' data-tag-value='${value}' data-tag-label='${label || value}' href='#'>${label || value}</a>`;
     }
 
     static formatValueLabel(obj, type, row) {
