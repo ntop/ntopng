7f489d2dfc362fd89b76a43c36f0054981e4d758
Luca Pruneti <luca.pruneti@gmail.com>
2021-06-04 16:53:04 +0200

    Added export msg.description for alert

--- scripts/lua/modules/alert_store/flow_alert_store.lua
+++ scripts/lua/modules/alert_store/flow_alert_store.lua
@@ -387,7 +387,7 @@ local RNAME = {
    ADDITIONAL_ALERTS = { name = "additional_alerts", export = true},
    ALERT_NAME = { name = "alert_name", export = true},
    DESCRIPTION = { name = "description", export = true},
-   MSG = { name = "msg", export = true, elements = {"name", "value"}},
+   MSG = { name = "msg", export = true, elements = {"name", "value", "description"}},
    FLOW = { name = "flow", export = true, elements = {"srv_ip.label", "srv_ip.value", "srv_port", "cli_ip.label", "cli_ip.value", "cli_port"}},
    VLAN_ID = { name = "vlan_id", export = true},
    PROTO = { name = "proto", export = true},
@@ -479,6 +479,10 @@ function flow_alert_store:format_record(value, no_html)
 
    if string.lower(noHtml(msg)) == string.lower(noHtml(alert_name)) then
       msg = ""
+   end
+
+   if no_html then
+      msg = noHtml(msg)
    end
 
    record[RNAME.MSG.name] = {
--- scripts/lua/modules/alert_store/mac_alert_store.lua
+++ scripts/lua/modules/alert_store/mac_alert_store.lua
@@ -93,7 +93,7 @@ local RNAME = {
    ADDRESS = { name = "address", export = true},
    DEVICE_TYPE = { name = "device_type", export = true},
    NAME = { name = "name", export = true},
-   MSG = { name = "msg", export = true, elements = {"name", "value"}}
+   MSG = { name = "msg", export = true, elements = {"name", "value", "description"}}
 }
 
 function mac_alert_store:get_rnames()
@@ -118,6 +118,10 @@ function mac_alert_store:format_record(value, no_html)
 
    if string.lower(noHtml(msg)) == string.lower(noHtml(alert_name)) then
       msg = ""
+   end
+
+   if no_html then
+      msg = noHtml(msg)
    end
 
    record[RNAME.MSG.name] = {
