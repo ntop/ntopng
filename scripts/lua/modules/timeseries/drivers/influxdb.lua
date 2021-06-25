7d264efe7b76dfeda7b95bb76ba24039d4ed7f8e
MatteoBiscosi <biscosi@ntop.org>
2021-06-25 10:59:13 +0200

    Removed wrong assignement

--- scripts/lua/modules/timeseries/drivers/influxdb.lua
+++ scripts/lua/modules/timeseries/drivers/influxdb.lua
@@ -782,7 +782,6 @@ function driver:query(schema, tstart, tend, tags, options)
         local s = ts_common.calculateStatistics(v.data, time_step, tend - tstart, data_type)
         -- Adding per timeseries min-max stats
         stats.by_serie[k] = table.merge(s, ts_common.calculateMinMax(v.data))
-        stats.by_serie[k] = s
 
         -- Remove the total for now as it requires a complex computation (see below)
         s.total = nil
