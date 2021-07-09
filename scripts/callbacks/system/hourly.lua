f02986dff54b48f60d9d155207a440a2a0d0a898
Alfredo Cardigliano <cardigliano@ntop.org>
2021-07-09 16:01:15 +0200

    Add missing require

--- scripts/callbacks/system/hourly.lua
+++ scripts/callbacks/system/hourly.lua
@@ -11,6 +11,7 @@ local lists_utils = require "lists_utils"
 
 -- If ntopng is in offline mode, retry checking connectivity
 if ntop.isOffline() then
+   local connectivity_utils = require "connectivity_utils"
    local online = connectivity_utils.checkConnectivity()
    if online then
       traceError(TRACE_NORMAL, TRACE_CONSOLE, "Connectivity restored, ntopng will now run in online mode")
