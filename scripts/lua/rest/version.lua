920e86f26106bb983a240ac1648924943d6c40bc
Simone Mainardi <mainardi@ntop.org>
2021-07-30 10:18:19 +0200

    Implements REST API endpoint to read the API version

---
+++ scripts/lua/rest/version.lua
@@ -0,0 +1,30 @@
+--
+-- (C) 2013-21 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+local json = require "dkjson"
+local rest_utils = require "rest_utils"
+
+--
+-- Return all the supported REST API versions and the current REST API version used by ntopng
+-- Example: curl -u admin:admin -H "Content-Type: application/json" http://localhost:3000/lua/rest/version.lua
+--
+-- NOTE: in case of invalid login, no error is returned but redirected to login
+--
+
+local rc = rest_utils.consts.success.ok
+local res = {
+   -- An array of all the supported REST API versions
+   supported_versions = {
+      { version = "1", root = string.format("%s/rest/v1", ntop.getHttpPrefix())},
+      { version = "2", root = string.format("%s/rest/v2", ntop.getHttpPrefix())},
+   },
+   -- Current REST API version used by ntopng
+   current_version = "2"
+}
+
+rest_utils.answer(rc, res)
+
