ntop/ntopng

commit 13d804e19927fc1d5774a8f34035a17ec596bf91

Set proto icon to lock only for encrypted flows (#5733)

scripts/lua/get_flows_data.lua
@@ -276,7 +276,7 @@ for _key, value in ipairs(flows_stats) do -- pairsByValues(vals, funct) do
 
    record["column_ndpi"] = app -- can't set the hosts_stats hyperlink for viewed interfaces
    if((not ifstats.isViewed) and (value["proto.ndpi_id"] ~= -1)) then
-      record["column_ndpi"] = "<A HREF='".. ntop.getHttpPrefix().."/lua/hosts_stats.lua?protocol=" .. value["proto.ndpi_id"] .."'>"..app.." " .. formatBreed(value["proto.ndpi_breed"]) .."</A>"
+      record["column_ndpi"] = "<A HREF='".. ntop.getHttpPrefix().."/lua/hosts_stats.lua?protocol=" .. value["proto.ndpi_id"] .."'>"..app.." " .. formatBreed(value["proto.ndpi_breed"], value["proto.is_tls"]) .."</A>"
    end
    record["column_duration"] = secondsToTime(value["duration"])
    record["column_bytes"] = value["bytes"]

scripts/lua/modules/lua_utils.lua
@@ -2730,9 +2730,11 @@ end
 
  -- ##############################################
 
-function formatBreed(breed)
+function formatBreed(breed, is_tls)
    if(breed == "Safe") then
-      return("<i class='fas fa-lock' alt='"..i18n("breed.safe").."'></i>")
+      local icon = 'lock'
+      if is_tls ~= nil and not is_tls then icon = 'thumbs-up' end
+      return("<i class='fas fa-"..icon.."' alt='"..i18n("breed.safe").."'></i>")
    elseif(breed == "Acceptable") then
       return("<i class='fas fa-thumbs-up' alt='"..i18n("breed.acceptable").."'></i>")
    elseif(breed == "Fun") then

src/Flow.cpp
@@ -4872,6 +4872,7 @@ void Flow::lua_get_protocols(lua_State* vm) const {
   }
 
   lua_push_str_table_entry(vm, "proto.ndpi_breed", get_protocol_breed_name());
+  lua_push_bool_table_entry(vm, "proto.is_tls", isTLS());
 
   lua_push_uint64_table_entry(vm, "proto.ndpi_cat_id", get_protocol_category());
   lua_push_str_table_entry(vm, "proto.ndpi_cat", get_protocol_category_name());
@@ -5077,6 +5078,7 @@ void Flow::lua_get_min_info(lua_State *vm) {
   lua_push_str_table_entry(vm, "proto.ndpi_cat", get_protocol_category_name());
   lua_push_uint64_table_entry(vm, "proto.ndpi_cat_id", get_protocol_category());
   lua_push_str_table_entry(vm, "proto.ndpi_breed", get_protocol_breed_name());
+  lua_push_bool_table_entry(vm, "proto.is_tls", isTLS());
   lua_push_uint64_table_entry(vm, "cli2srv.bytes", get_bytes_cli2srv());
   lua_push_uint64_table_entry(vm, "srv2cli.bytes", get_bytes_srv2cli());
   lua_push_uint64_table_entry(vm, "cli2srv.packets", get_packets_cli2srv());

commit aca352452e1e5d909327bf1bfc89ce0e03ca23f4

Fixes #5733 and the regressions introduced by https://github.com/ntop/ntopng/commit/13d804e19927fc1d5774a8f34035a17ec596bf91 Updated code for new nDPI API

include/Flow.h
@@ -333,6 +333,7 @@ class Flow : public GenericHashEntry {
 						   || (ndpiDetectedProtocol.app_protocol == p))
 						  ? true : false); }
   bool isTLS() const;
+  inline bool isEncryptedProto() const { return(ndpi_is_encrypted_proto(iface->get_ndpi_struct(), ndpiDetectedProtocol)); }
   inline bool isSSH()  const { return(isProto(NDPI_PROTOCOL_SSH));  }
   inline bool isDNS()  const { return(isProto(NDPI_PROTOCOL_DNS));  }
   inline bool isIEC60870()  const { return(isProto(NDPI_PROTOCOL_IEC60870));  }

scripts/lua/flow_details.lua
@@ -745,7 +745,7 @@ else
       print("(<A HREF=\""..ntop.getHttpPrefix().."/lua/")
       print("flows_stats.lua?category=" .. flow["proto.ndpi_cat"] .. "\">")
       print(getCategoryLabel(flow["proto.ndpi_cat"]))
-      print("</A>) ".. formatBreed(flow["proto.ndpi_breed"]))
+      print("</A>) ".. formatBreed(flow["proto.ndpi_breed"], flow["proto.is_encrypted"]))
    end
    
    if(flow["verdict.pass"] == false) then print("</strike>") end

scripts/lua/get_flows_data.lua
@@ -276,7 +276,7 @@ for _key, value in ipairs(flows_stats) do -- pairsByValues(vals, funct) do
 
    record["column_ndpi"] = app -- can't set the hosts_stats hyperlink for viewed interfaces
    if((not ifstats.isViewed) and (value["proto.ndpi_id"] ~= -1)) then
-      record["column_ndpi"] = "<A HREF='".. ntop.getHttpPrefix().."/lua/hosts_stats.lua?protocol=" .. value["proto.ndpi_id"] .."'>"..app.." " .. formatBreed(value["proto.ndpi_breed"], value["proto.is_tls"]) .."</A>"
+      record["column_ndpi"] = "<A HREF='".. ntop.getHttpPrefix().."/lua/hosts_stats.lua?protocol=" .. value["proto.ndpi_id"] .."'>"..app.." " .. formatBreed(value["proto.ndpi_breed"], value["proto.is_encrypted"]) .."</A>"
    end
    record["column_duration"] = secondsToTime(value["duration"])
    record["column_bytes"] = value["bytes"]

scripts/lua/modules/lua_utils.lua
@@ -2730,22 +2730,26 @@ end
 
  -- ##############################################
 
-function formatBreed(breed, is_tls)
+function formatBreed(breed, is_encrypted)
+   local ret = ""
+   
    if(breed == "Safe") then
-      local icon = 'lock'
-      if is_tls ~= nil and not is_tls then icon = 'thumbs-up' end
-      return("<i class='fas fa-"..icon.."' alt='"..i18n("breed.safe").."'></i>")
+      if(is_encrypted == false) then ret = "<i class='fas fa-thumbs-up' alt='"..i18n("breed.safe").."'></i>" end
    elseif(breed == "Acceptable") then
-      return("<i class='fas fa-thumbs-up' alt='"..i18n("breed.acceptable").."'></i>")
+      if(is_encrypted == false) then ret = "<i class='fas fa-thumbs-up' alt='"..i18n("breed.acceptable").."'></i>" end
    elseif(breed == "Fun") then
-      return("<i class='fas fa-smile' alt='"..i18n("breed.fun").."'></i>")
+      ret = "<i class='fas fa-smile' alt='"..i18n("breed.fun").."'></i>"
    elseif(breed == "Unsafe") then
-      return("<i class='fas fa-thumbs-down' style='color: red' alt='"..i18n("breed.unsafe").."'></i>")
+      ret = "<i class='fas fa-thumbs-down' style='color: red' alt='"..i18n("breed.unsafe").."'></i>"
    elseif(breed == "Dangerous") then
-      return("<i class='fas fa-exclamation-triangle' alt='"..i18n("breed.dangerous").."'></i>")
-   else
-      return("")
+      ret = "<i class='fas fa-exclamation-triangle' alt='"..i18n("breed.dangerous").."'></i>"
    end
+
+   if(is_encrypted == true) then
+      ret = ret .. " <i class='fas fa-lock'></i>"
+   end
+
+   return(ret)
 end
 
 function getFlag(country)

src/Flow.cpp
@@ -4872,7 +4872,7 @@ void Flow::lua_get_protocols(lua_State* vm) const {
   }
 
   lua_push_str_table_entry(vm, "proto.ndpi_breed", get_protocol_breed_name());
-  lua_push_bool_table_entry(vm, "proto.is_tls", isTLS());
+  lua_push_bool_table_entry(vm, "proto.is_encrypted", isEncryptedProto());
 
   lua_push_uint64_table_entry(vm, "proto.ndpi_cat_id", get_protocol_category());
   lua_push_str_table_entry(vm, "proto.ndpi_cat", get_protocol_category_name());
@@ -5078,7 +5078,7 @@ void Flow::lua_get_min_info(lua_State *vm) {
   lua_push_str_table_entry(vm, "proto.ndpi_cat", get_protocol_category_name());
   lua_push_uint64_table_entry(vm, "proto.ndpi_cat_id", get_protocol_category());
   lua_push_str_table_entry(vm, "proto.ndpi_breed", get_protocol_breed_name());
-  lua_push_bool_table_entry(vm, "proto.is_tls", isTLS());
+  lua_push_bool_table_entry(vm, "proto.is_encrypted", isEncryptedProto());
   lua_push_uint64_table_entry(vm, "cli2srv.bytes", get_bytes_cli2srv());
   lua_push_uint64_table_entry(vm, "srv2cli.bytes", get_bytes_srv2cli());
   lua_push_uint64_table_entry(vm, "cli2srv.packets", get_packets_cli2srv());

src/Ntop.cpp
@@ -3145,7 +3145,7 @@ struct ndpi_detection_module_struct* Ntop::initnDPIStruct() {
     ndpi_load_protocols_file(ndpi_s, getCustomnDPIProtos());
 
   memset(d_port, 0, sizeof(d_port));
-  ndpi_set_proto_defaults(ndpi_s, NDPI_PROTOCOL_UNRATED, NTOPNG_NDPI_OS_PROTO_ID,
+  ndpi_set_proto_defaults(ndpi_s, 0, NDPI_PROTOCOL_UNRATED, NTOPNG_NDPI_OS_PROTO_ID,
 			  (char*)"Operating System",
 			  NDPI_PROTOCOL_CATEGORY_SYSTEM_OS, d_port, d_port);
 