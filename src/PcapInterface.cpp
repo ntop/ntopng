efab3aaec3dc11753d78a8fbd2f89667875d0bce
Simone Mainardi <mainardi@ntop.org>
2021-08-18 14:47:05 +0200

    Reduces the number of hash table walks after processing pcap file

--- src/PcapInterface.cpp
+++ src/PcapInterface.cpp
@@ -349,16 +349,14 @@ static void* packetPollLoop(void* ptr) {
     } /* while */
   } while(pcap_list != NULL);
 
-  /* Do two full scans to make sure all stats are updated */
-  for(int i = 0; i < 2; i++)
-    iface->purgeIdle(time(NULL), false, true /* Full scan */);
- 
   if(iface->read_from_pcap_dump() && !iface->reproducePcapOriginalSpeed()) {
     iface->set_read_from_pcap_dump_done();
   }
 
-  iface->purgeIdle(time(NULL), false, true /* Full scan */);
-
+  /* Do two full scans to make sure all stats are updated */
+  for(int i = 0; i < 2; i++)
+    iface->purgeIdle(time(NULL), false, true /* Full scan */);
+ 
   ntop->getTrace()->traceEvent(TRACE_NORMAL, "Terminated packet polling for %s",
 			       iface->get_description());
 
