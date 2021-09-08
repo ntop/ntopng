a8f5055d35d0017882f683a3720887584adb2696
Simone Mainardi <mainardi@ntop.org>
2021-09-08 17:54:09 +0200

    Fixes regression causing missing host ARP chart
    Fixes regression introduced with 60f309ae52a09aa6117df273e243252bd8682aa4

---
+++ scripts/lua/get_arp_data.lua
@@ -0,0 +1,35 @@
+--
+-- (C) 2013-21 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+require "lua_utils"
+local json = require("dkjson")
+
+--interface.select(ifname)
+local ifid = _GET["ifid"]
+local host_info = url2hostinfo(_GET)
+
+-- #####################################################################
+
+interface.select(ifid)
+
+local breakdown = {}
+
+if not isEmptyString(ifid) and not isEmptyString(host_info["host"]) then
+   local stats = interface.getMacInfo(host_info["host"])
+
+   if stats then
+      -- Show ARP sent/recv breakdown
+      local arp_sent = stats["arp_requests.sent"] + stats["arp_replies.sent"]
+      local arp_rcvd = stats["arp_requests.rcvd"] + stats["arp_replies.rcvd"]
+
+      breakdown[#breakdown + 1] = {label=i18n("sent"), value=arp_sent}
+      breakdown[#breakdown + 1] = {label=i18n("received"), value=arp_rcvd}
+   end
+end
+
+sendHTTPContentTypeHeader('text/html')
+print(json.encode(breakdown, nil))
