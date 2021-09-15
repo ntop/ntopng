b5640f2b7baf9505ae8e1d6edd83ae3098022bbd
Simone Mainardi <mainardi@ntop.org>
2021-09-15 16:08:44 +0200

    Implements lua {check,alert}_definitions for all unhandled risks
    Addresses #5743

---
+++ scripts/lua/modules/flow_risk_simple_alert_definition.lua
@@ -0,0 +1,46 @@
+--
+-- (C) 2019-21 - ntop.org
+--
+
+-- ##############################################
+
+-- Import the classes library.
+local classes = require "classes"
+-- Make sure to import the Superclass!
+local alert = require "alert"
+
+-- ##############################################
+
+local flow_risk_simple_alert_definition = classes.class(alert)
+
+-- ##############################################
+
+flow_risk_simple_alert_definition.meta = {
+   -- alert_key = <added at runtime>
+   -- i18n_title = <added at runtime>
+   icon = "fas fa-fw fa-exclamation",
+}
+
+-- ##############################################
+
+-- @brief Prepare an alert table used to generate the alert
+-- @return A table with the alert built
+function flow_risk_simple_alert_definition:init()
+   -- Call the parent constructor
+   self.super:init()
+end
+
+-- #######################################################
+
+-- @brief Format an alert into a human-readable string
+-- @param ifid The integer interface id of the generated alert
+-- @param alert The alert description table, including alert data such as the generating entity, timestamp, granularity, type
+-- @param alert_type_params Table `alert_type_params` as built in the `:init` method
+-- @return A human-readable string
+function flow_risk_simple_alert_definition.format(ifid, alert, alert_type_params)
+   return flow_risk_simple_alert_definition.meta.i18n_title
+end
+
+-- #######################################################
+
+return flow_risk_simple_alert_definition
---
+++ scripts/lua/modules/flow_risk_simple_check_definition.lua
@@ -0,0 +1,26 @@
+--
+-- (C) 2019-21 - ntop.org
+--
+
+-- #################################################################
+
+local script = {
+   -- Script category
+   -- category = <added at runtime>
+   -- alert_id = <added at runtime>
+
+   default_enabled = true,
+
+   default_value = {
+   },
+
+
+   gui = {
+      -- i18n_title = <added at runtime>
+      -- i18n_description = <added at runtime>
+   }
+}
+
+-- #################################################################
+
+return script
