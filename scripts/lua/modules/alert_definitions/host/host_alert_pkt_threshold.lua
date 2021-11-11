0d802735be85cbd5eddc431c97232ddc442d79f9
Marco Tranchida <94124788+Nexon13@users.noreply.github.com>
2021-11-11 16:10:24 +0100

    Add files via upload

---
+++ scripts/lua/modules/alert_definitions/host/host_alert_pkt_threshold.lua
@@ -0,0 +1,81 @@
+--
+-- (C) 2019-21 - ntop.org
+--
+
+-- ##############################################
+
+local host_alert_keys = require "host_alert_keys"
+
+local json = require("dkjson")
+local alert_creators = require "alert_creators"
+-- Import the classes library.
+local classes = require "classes"
+-- Make sure to import the Superclass!
+local alert = require "alert"
+
+-- ##############################################
+
+local host_alert_pkt_threshold = classes.class(alert)
+
+-- ##############################################
+
+host_alert_pkt_threshold.meta = {
+  alert_key = host_alert_keys.host_alert_pkt_threshold,
+  i18n_title = "alerts_dashboard.host_alert_pkt_threshold",
+  icon = "fas fa-fw fa-arrow-circle-up",
+}
+
+-- ##############################################
+
+-- @brief Prepare an alert table used to generate the alert
+-- @param metric Same as `alert_subtype`
+-- @param value A number indicating the measure which crossed the threshold
+-- @param operator A string indicating the operator used when evaluating the threshold, one of "gt", ">", "<"
+-- @param threshold A number indicating the threshold compared with `value`  using operator
+-- @return A table with the alert built
+function host_alert_pkt_threshold:init(metric, value, operator, threshold)
+   -- Call the parent constructor
+   self.super:init()
+
+   self.alert_type_params = alert_creators.createThresholdCross(metric, value, operator, threshold)
+end
+
+-- #######################################################
+
+-- @brief Format an alert into a human-readable string
+-- @param ifid The integer interface id of the generated alert
+-- @param alert The alert description table, including alert data such as the generating entity, timestamp, granularity, type
+-- @param alert_type_params Table `alert_type_params` as built in the `:init` method
+-- @return A human-readable string
+function host_alert_pkt_threshold.format(ifid, alert, alert_type_params)
+  local alert_consts = require("alert_consts")
+  local entity = alert_consts.formatHostAlert(ifid, alert["ip"], alert["vlan_id"])
+
+  return i18n("alert_messages.host_alert_pkt_threshold", {
+    entity = entity,
+    value = string.format("%u", math.ceil(alert_type_params.value)),
+    op = "&".. (alert_type_params.operator or "gt") ..";",
+    threshold = alert_type_params.threshold,
+  })
+end
+
+-- #######################################################
+
+-- @brief Prepare a table containing a set of filters useful to query historical flows that contributed to the generation of this alert
+-- @param ifid The integer interface id of the generated alert
+-- @param alert The alert description table, including alert data such as the generating entity, timestamp, granularity, type
+-- @param alert_type_params Table `alert_type_params` as built in the `:init` method
+-- @return A human-readable string
+function host_alert_pkt_threshold.filter_to_past_flows(ifid, alert, alert_type_params)
+   local res = {}
+   local host_key = hostinfo2hostkey({ip = alert["ip"], vlan = alert["vlan_id"]})
+
+   -- Look for the IP both as client and as server as the alert does not differentiate
+   res["ip"] = host_key
+   
+   return res
+end
+
+-- #######################################################
+
+return host_alert_pkt_threshold
