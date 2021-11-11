54254c9880ee55e33fbbb9d94dd605e4a02741ca
Marco Tranchida <94124788+Nexon13@users.noreply.github.com>
2021-11-11 16:11:12 +0100

    Add files via upload

---
+++ scripts/lua/modules/check_definitions/host/pkt_threshold.lua
@@ -0,0 +1,33 @@
+--
+-- (C) 2019-21 - ntop.org
+--
+
+local checks = require("checks")
+local host_alert_keys = require "host_alert_keys"
+
+-- #################################################################
+
+local pkt_threshold = {
+   -- Script category
+   category = checks.check_categories.network,
+
+   default_enabled = false,
+   alert_id = host_alert_keys.host_alert_pkt_threshold,
+
+   default_value = {
+      operator = "gt",
+      threshold = 10000,
+   },
+
+   gui = {
+      i18n_title = "alerts_thresholds_config.pkt_threshold",
+      i18n_description = "alerts_thresholds_config.alert_pkt_threshold_description",
+      i18n_field_unit = checks.field_units.packets,
+      input_builder = "threshold_cross",
+      field_operator = "gt";
+   },
+}
+
+-- #################################################################
+
+return pkt_threshold
