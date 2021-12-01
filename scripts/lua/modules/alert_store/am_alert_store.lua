a727ba4418c2d7c99ba4bee4005815ad3d296e55
MatteoBiscosi <biscosi@ntop.org>
2021-12-01 11:22:27 +0100

    Fixes am alert double interface_id insert error

--- scripts/lua/modules/alert_store/am_alert_store.lua
+++ scripts/lua/modules/alert_store/am_alert_store.lua
@@ -54,9 +54,9 @@ function am_alert_store:insert(alert)
    end
 
    local insert_stmt = string.format("INSERT INTO %s "..
-      "(alert_id, tstamp, tstamp_end, severity, score, interface_id, resolved_ip, resolved_name, "..
+      "(alert_id, interface_id, tstamp, tstamp_end, severity, score, resolved_ip, resolved_name, "..
       "measurement, measure_threshold, measure_value, json) "..
-      "VALUES (%u, %u, %u, %u, %u, %d, '%s', '%s', '%s', %u, %f, '%s'); ",
+      "VALUES (%u, %u, %u, %u, %u, %u, '%s', '%s', '%s', %u, %f, '%s'); ",
       self._table_name, 
       alert.alert_id,
       alert.tstamp,
