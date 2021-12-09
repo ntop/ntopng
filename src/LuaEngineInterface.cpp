5d1182b21528a7d27f7df0f6bc92a06746edf1ab
Luca Deri <deri@ntop.org>
2021-12-09 09:25:47 +0100

    Added check

--- src/LuaEngineInterface.cpp
+++ src/LuaEngineInterface.cpp
@@ -4330,14 +4330,14 @@ static int ntop_clickhouse_exec_csv_query(lua_State* vm) {
   bool use_json = false;
   
   ntop->getTrace()->traceEvent(TRACE_DEBUG, "%s() called", __FUNCTION__);
+
+  if((!ntop_interface) || (!conn))
+    return(ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_ERROR));
   
   if(ntop_lua_check(vm, __FUNCTION__, 1, LUA_TSTRING) != CONST_LUA_OK)
     return(ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_PARAM_ERROR));
 
   sql = lua_tostring(vm, 1);
-
-  if(!ntop_interface)
-    return(ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_ERROR));
 
   if(lua_type(vm, 2) == LUA_TBOOLEAN) /* optional */
     use_json = lua_toboolean(vm, 2) ? true : false;
@@ -4349,7 +4349,6 @@ static int ntop_clickhouse_exec_csv_query(lua_State* vm) {
   lua_pushnil(vm); /* Data is pushed via the HTTP server */
   
   return(ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_OK));
-
 }
 
 /* ****************************************** */
