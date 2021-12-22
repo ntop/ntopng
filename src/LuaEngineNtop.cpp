0d119244865f3844c6070d9600fedada009b646f
Luca Deri <deri@ntop.org>
2021-12-22 20:00:50 +0100

    Compilation fix

--- src/LuaEngineNtop.cpp
+++ src/LuaEngineNtop.cpp
@@ -653,13 +653,14 @@ static int ntop_match_custom_category(lua_State* vm) {
 static int ntop_get_tls_version_name(lua_State* vm) {
   u_int16_t tls_version;
   u_int8_t unknown_version = 0;
-
+  char buf[32];
+  
   ntop->getTrace()->traceEvent(TRACE_DEBUG, "%s() called", __FUNCTION__);
 
   if(ntop_lua_check(vm, __FUNCTION__, 1, LUA_TNUMBER) != CONST_LUA_OK) return(ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_ERROR));
   tls_version = (u_int16_t)lua_tonumber(vm, 1);
 
-  lua_pushstring(vm, ndpi_ssl_version2str(NULL, tls_version, &unknown_version));
+  lua_pushstring(vm, ndpi_ssl_version2str(buf, sizeof(buf), tls_version, &unknown_version));
 
   return(ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_OK));
 }
