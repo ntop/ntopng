c4cd13532343af6983a733239c4b6f68eca27603
Sam James <sam@gentoo.org>
2022-01-07 09:16:59 +0000

    build: respect environment options more (#6177)
    * build: move away from configure.seed -> configure.ac
    
    Analogous to the change in nDPI [0].
    
    [0] https://github.com/ntop/nDPI/commit/cf931fda6bfb3925555c7bd11d950a886676bcb3
    
    Signed-off-by: Sam James <sam@gentoo.org>
    
    * build: use $(MAKE)
    
    This ensures that parallel make works correctly, as otherwise, a fresh
    make job will be started without the jobserver fd, and hence
    not know about its parent, forcing -j1.
    
    Signed-off-by: Sam James <sam@gentoo.org>
    
    * build: respect CXX, CXXFLAGS, LDFLAGS/LIBS
    
    - Use standard CXX variable for the C++ compiler
    - Respect CXXFLAGS from the environment
    - LDFLAGS needs to be before objects in order for some flags to work
      like -Wl,--as-needed
    
    Signed-off-by: Sam James <sam@gentoo.org>
    
    * build: add comment for dynamic linking nDPI
    
    It seems to work for me but add a commented
    in line for now for convenience, not actually
    changing behaviour right now.
    
    (May want to make it a proper configure option
    in future.)
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- autogen.sh
+++ autogen.sh
@@ -33,45 +33,6 @@ if test -z $PKG_CONFIG; then
     exit
 fi
 
-##########################################
-
-TODAY=`date +%y%m%d`
-MAJOR_RELEASE="5"
-MINOR_RELEASE="1"
-SHORT_VERSION="$MAJOR_RELEASE.$MINOR_RELEASE"
-VERSION="$SHORT_VERSION.$TODAY"
-
-if test -d ".git"; then
-GIT_TAG=`git rev-parse HEAD`
-GIT_DATE=`date +%Y%m%d`
-GIT_RELEASE="$GIT_TAG:$GIT_DATE"
-GIT_BRANCH=`git rev-parse --abbrev-ref HEAD | sed "s/heads\///g"`
-else
-GIT_RELEASE="$VERSION"
-GIT_DATE=`date`
-GIT_BRANCH=""
-fi
-
-if test -d "pro"; then
-PRO_GIT_RELEASE=`cd pro; git log --pretty=oneline | wc -l`
-PRO_GIT_RELEASE=${PRO_GIT_RELEASE//[[:blank:]]/}
-PRO_GIT_DATE=`cd pro; git log --pretty=medium -1 | grep "^Date:" | cut -d " " -f 4-`
-else
-PRO_GIT_RELEASE=""
-PRO_GIT_DATE=""
-fi
-
-cat configure.seed | sed \
-    -e "s/@VERSION@/$VERSION/g" \
-    -e "s/@SHORT_VERSION@/$SHORT_VERSION/g" \
-    -e "s/@GIT_TAG@/$GIT_TAG/g" \
-    -e "s/@GIT_DATE@/$GIT_DATE/g" \
-    -e "s/@GIT_RELEASE@/$GIT_RELEASE/g" \
-    -e "s/@GIT_BRANCH@/$GIT_BRANCH/g" \
-    -e "s/@PRO_GIT_RELEASE@/$PRO_GIT_RELEASE/g" \
-    -e "s/@PRO_GIT_DATE@/$PRO_GIT_DATE/g" \
-    > configure.ac
-
 rm -f config.h config.h.in *~ #*
 
 echo "Wait please..."
