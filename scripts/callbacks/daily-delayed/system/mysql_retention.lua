8828a9ca90ef8edef3c1a69dcb63b1dec4b1ab92
Luca Deri <deri@ntop.org>
2022-01-26 13:01:47 +0100

    MySQL retention is implemented serially in order to avoid that slow MySQL DBs create issues

--- scripts/callbacks/daily-delayed/system/mysql_retention.lua
+++ scripts/callbacks/daily-delayed/system/mysql_retention.lua
@@ -11,17 +11,17 @@ local scripts_triggers = require "scripts_triggers"
 -- ########################################################
 
 local verbose = ntop.verboseTrace()
-local ifstats = interface.getStats()
 
 -- ###########################################
 
 if scripts_triggers.isDumpFlowToSQLEnabled(ifstats) then
    local db_utils = require "db_utils"
    local data_retention_utils = require "data_retention_utils"
-
+   local iface_names = interface.getIfNames()
    local data_retention = data_retention_utils.getDataRetentionDays()
-   local _ifname = ifstats.name
    local mysql_retention = os.time() - 86400 * data_retention
-
-   db_utils.harverstExpiredMySQLFlows(_ifname, mysql_retention, verbose)
+   
+   for _,ifname in pairs(iface_names) do
+      db_utils.harverstExpiredMySQLFlows(ifname, mysql_retention, verbose)
+   end
 end
