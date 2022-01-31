b93d2a8feb6c3f198c1629c0c62ea4f8880c6d42
Luca Deri <deri@ntop.org>
2022-01-31 12:40:46 +0100

    Delete old discard and tmp files

--- scripts/callbacks/daily-delayed/system/clickhouse_retention.lua
+++ scripts/callbacks/daily-delayed/system/clickhouse_retention.lua
@@ -10,9 +10,28 @@ local data_retention_utils = require "data_retention_utils"
 
 -- ########################################################
 
-if ntop.isClickHouseEnabled() then
-   local data_retention = data_retention_utils.getDataRetentionDays()
-   local mysql_retention = os.time() - 86400 * data_retention
+function cleanup_clickhouse_logs()
+   local ch_dir = dirs.workingdir.."/tmp/clickhouse"
+   local os_utils = require "os_utils"
+   
+   -- Delete discards older than 3 days
+   os_utils.execWithOutput("find "..ch_dir.." -name \"*.dsc\" -type f -mtime +3 -exec rm -f {} +")
 
-   db_utils.clickhouseDeleteOldPartitions(mysql_retention)
+   -- Delete tmp older than 1 day
+   os_utils.execWithOutput("find "..ch_dir.." -name \"*.tmp\" -type f -mtime +1 -exec rm -f {} +")
 end
+
+-- ########################################################
+
+if(not ntop.isWindows()) then
+   if ntop.isClickHouseEnabled() then
+      local data_retention = data_retention_utils.getDataRetentionDays()
+      local mysql_retention = os.time() - 86400 * data_retention
+      
+      db_utils.clickhouseDeleteOldPartitions(mysql_retention)
+      
+      -- Delete old discard files
+      cleanup_clickhouse_logs()
+   end
+end
+
