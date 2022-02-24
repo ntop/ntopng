234bce3e99dde1b6946b7fee855336ef116058ec
uccidibuti <vannucci@ntop.org>
2022-02-24 06:55:34 +0100

    fix log errors in ntopng_vue_loader

--- httpdocs/vue/ntopng_vue_loader.js
+++ httpdocs/vue/ntopng_vue_loader.js
@@ -14,15 +14,18 @@ const ntopng_vue_loader = function() {
             return fetch(url).then((response) => {
                 if (response.ok) { return response.text(); }
                 else {
-                    console.error("ntopng_vue_loader fail loading: ${url}");
-                    return Promise.reject(response);
+                    console.error(`ntopng_vue_loader fail loading: ${url}`);
+		    //throw Object.assign(new Error(url+' '+res.statusText), { res });
+                    //return Promise.reject(response);
                 }
             }).catch((err) => {
-                console.error("ntopng_vue_loader fail loading: ${url}");
+                console.error(`ntopng_vue_loader fail loading: ${url}`);
                 throw err;
             });
 	},
-	
+	log(type, ...args) {
+            console[type](...args);
+	},	
 	addStyle(styleStr) {
             const style = document.createElement('style');
             style.textContent = styleStr;
