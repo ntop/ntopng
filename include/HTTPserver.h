7f05a0142871d236327c875348185bfee0c070a6
Luca Deri <deri@ntop.org>
2022-03-04 00:17:39 +0100

    Extended ACL buffer to accommodate more entries

--- include/HTTPserver.h
+++ include/HTTPserver.h
@@ -36,7 +36,7 @@ class HTTPserver {
   char *captive_redirect_addr;
   char *wispr_captive_data;
   bool check_ssl_cert(char *ssl_cert_path, size_t ssl_cert_path_len);
-  char ports[256], acl_management[64], ssl_cert_path[2*MAX_PATH], access_log_path[2*MAX_PATH];
+  char ports[256], acl_management[256], ssl_cert_path[2*MAX_PATH], access_log_path[2*MAX_PATH];
   const char *http_binding_addr1, *http_binding_addr2;
   const char *https_binding_addr1, *https_binding_addr2;
   const char *http_options[32];
