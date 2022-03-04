467db1fbb5c1248081f4013ba828223490c42887
MatteoBiscosi <biscosi@ntop.org>
2022-03-04 11:56:35 +0100

    Fixes alert datatable bugs

--- http_src/utilities/datatable/sprymedia-datatable-utils.js
+++ http_src/utilities/datatable/sprymedia-datatable-utils.js
@@ -5,45 +5,6 @@
 'use strict';
 /*
 
-jQuery.fn.dataTableExt.sErrMode = 'console';
-jQuery.fn.dataTableExt.formatSecondsToHHMMSS = (data, type, row) => {
-    if (isNaN(data)) return data;
-    if (type == "display" && data <= 0) return ' ';
-    if (type == "display") return NtopUtils.secondsToTime(data);
-    return data;
-};
-jQuery.fn.dataTableExt.absoluteFormatSecondsToHHMMSS = (data, type, row) => {
-
-    if (isNaN(data)) return data;
-    if (type == "display" && (data <= 0)) return ' ';
-
-    const delta = Math.floor(Date.now() / 1000) - data;
-    if (type == "display") return NtopUtils.secondsToTime(delta);
-    return data;
-};
-jQuery.fn.dataTableExt.sortBytes = (byte, type, row) => {
-    if (type == "display") return NtopUtils.bytesToSize(byte);
-    return byte;
-};
-jQuery.fn.dataTableExt.hideIfZero = (value, type, row) => {
-    if (type === "display" && parseInt(value) === 0) return "";
-    return value;
-};
-jQuery.fn.dataTableExt.showProgress = (percentage, type, row) => {
-    if (type === "display") {
-        const fixed = percentage.toFixed(1)
-        return `
-        <div class="d-flex align-items-center">
-        <span class="progress w-100">
-          <span class="progress-bar bg-warning" role="progressbar" style="width: ${fixed}%" aria-valuenow="${fixed}" aria-valuemin="0" aria-valuemax="100"></span>
-        </span>
-        <span>${fixed}%</span>
-        </div>
-        `
-    }
-    return percentage;
-};
-
 //https://datatables.net/forums/discussion/44885
 $.fn.dataTable.Api.registerPlural( 'columns().names()', 'column().name()', function ( setter ) {
     return this.iterator( 'column', function ( settings, column ) {
@@ -58,8 +19,8 @@ $.fn.dataTable.Api.registerPlural( 'columns().names()', 'column().name()', funct
         }
     }, 1 );
 } );
-
 */
+
 export class DataTableFiltersMenu {
 
     /**
