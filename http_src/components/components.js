4cc715266d764331989d5562e32761a08f95b2ba
MatteoBiscosi <biscosi@ntop.org>
2022-03-07 17:18:46 +0100

    Added npm README

--- http_src/components/components.js
+++ http_src/components/components.js
@@ -3,9 +3,7 @@ import { do_pie } from './charts/pie-chart'
 global.do_pie = do_pie
 
 import './components.scss'
-//import './charts/cal-heatmap'
 
-//import './charts/heatmap'
 import { ChartWidget, WidgetUtils } from './widget/widgets'
 import './sidebar/sidebar'
 
