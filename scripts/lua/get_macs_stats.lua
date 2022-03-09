678b66c896c5ba4195ab68d576945730e594ed3e
Alfredo Cardigliano <cardigliano@ntop.org>
2022-03-09 18:27:06 +0100

    Optimize stats update in MACs page (#6372)

--- scripts/lua/get_macs_stats.lua
+++ scripts/lua/get_macs_stats.lua
@@ -9,19 +9,30 @@ require "mac_utils" -- needed for the function mac2record
 local json = require("dkjson")
 
 sendHTTPContentTypeHeader('text/html')
-
 -- sendHTTPHeader('application/json')
-interface.select(ifname)
-
-local host_info = url2hostinfo(_GET)
 
 interface.select(ifname)
 
-local host = interface.getMacInfo(host_info["host"])
+local hosts = _GET["hosts"]
 
 local res = {}
-if host ~= nil then
-   res = mac2record(host)
+
+if not isEmptyString(hosts) then
+
+   local items = split(hosts, ',')
+
+   for _, item in pairs(items) do
+      local host_info = hostkey2hostinfo(item)
+
+      if host_info["host"] ~= nil then
+         local host = interface.getMacInfo(host_info["host"])
+
+         if host then
+            res[item] = mac2record(host)
+         end
+      end
+   end
+
 end
 
 print(json.encode(res, nil))
