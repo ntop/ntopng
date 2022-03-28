365c04c36a03e9c6d728cb515f773c0de6a76f5d
uccidibuti <lorenzo@vannucci.io>
2022-03-28 20:57:35 +0200

    Menu Badges Overlap with Small Windows #6415

--- scripts/lua/inc/menu.lua
+++ scripts/lua/inc/menu.lua
@@ -762,7 +762,7 @@ page_utils.print_menubar()
 -- Interface
 
 print([[
-   <nav class="navbar navbar-expand-lg navbar-light fixed-top px-2 navbar-main-top" id='n-navbar'>
+   <nav style="margin-top:-4.5rem;margin-right:5rem;" class="navbar navbar-expand-lg navbar-light px-2 navbar-main-top" id='n-navbar'>
       <ul class='navbar-nav flex-row flex-wrap navbar-main-top'>
          <li class='nav-item'>
             <button class='btn btn-outline-dark border-0 btn-sidebar' data-bs-toggle='sidebar'>
