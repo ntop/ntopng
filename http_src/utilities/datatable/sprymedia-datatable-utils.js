978ba0fff0d65fe742ea2ab3d299e7acc250b0b2
MatteoBiscosi <biscosi@ntop.org>
2022-03-28 11:04:48 +0200

    Fixes entries message not working (#6474)

--- http_src/utilities/datatable/sprymedia-datatable-utils.js
+++ http_src/utilities/datatable/sprymedia-datatable-utils.js
@@ -267,11 +267,9 @@ export class DataTableUtils {
         return {
             dom: dom,
             pagingType: 'full_numbers',
-            lengthMenu: [[10, 25, 50, -1], [10, 25, 50, `${i18n_ext.all}`]],
+            lengthMenu: [[10, 25, 50, -1], [10, 25, 50, `${i18n.all}`]],
             language: {
-                info: i18n_ext.showing_x_to_y_rows,
-                search: i18n_ext.script_search,
-                infoFiltered: "",
+                search: i18n.script_search,
                 paginate: {
                     previous: '&lt;',
                     next: '&gt;',
