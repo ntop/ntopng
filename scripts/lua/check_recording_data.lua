7613382929a86ef9a3041994be7a81e67468adb3
Alfredo Cardigliano <cardigliano@ntop.org>
2022-03-31 17:40:00 +0200

    Fix recording data check on sub interfaces

--- scripts/lua/check_recording_data.lua
+++ scripts/lua/check_recording_data.lua
@@ -16,14 +16,13 @@ if not recording_utils.isExtractionAvailable() then
   return
 end
 
+local ifid = tonumber(_GET["ifid"] or interface.getId())
 local epoch_begin = tonumber(_GET["epoch_begin"])
 local epoch_end   = tonumber(_GET["epoch_end"])
 
-interface.select(ifname)
+local master_ifid = interface.getMasterInterfaceId(ifid)
 
-local ifstats = interface.getStats()
-
-local window_info = recording_utils.isDataAvailable(ifstats.id, epoch_begin, epoch_end)
+local window_info = recording_utils.isDataAvailable(master_ifid, epoch_begin, epoch_end)
 
 if window_info.epoch_begin and window_info.epoch_end then
   window_info.epoch_begin_formatted = format_utils.formatPastEpochShort(window_info.epoch_begin)
