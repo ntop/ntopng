c5823a24961d4cf899eeffc5bb1bd779d315f1bc
Luca Deri <deri@ntop.org>
2022-06-07 19:20:15 +0200

    Fixed cookie

--- src/HTTPserver.cpp
+++ src/HTTPserver.cpp
@@ -634,9 +634,7 @@ static void redirect_to_login(struct mg_connection *conn,
                               const struct mg_request_info *request_info,
 			      const char * referer, const char * reason) {
   char session_id[NTOP_SESSION_ID_LENGTH], session_key[32];
-#if 0
   char buf[128];
-#endif
   char *referer_enc = NULL, *reason_enc = NULL;
 
   if(isCaptiveConnection(conn)) {
@@ -681,11 +679,11 @@ static void redirect_to_login(struct mg_connection *conn,
 	      PACKAGE_VERSION, PACKAGE_MACHINE,
 	      session_id,
 	      get_secure_cookie_attributes(request_info),
-	      get_secure_cookie_attributes(request_info),
-	      get_secure_cookie_attributes(request_info),
 #if 0
-	      ntop->getPrefs()->get_http_prefix(), Utils::getURL((char*)LOGIN_URL, buf, sizeof(buf)),
+	      get_secure_cookie_attributes(request_info),
+	      get_secure_cookie_attributes(request_info),
 #endif
+	      ntop->getPrefs()->get_http_prefix(), Utils::getURL((char*)LOGIN_URL, buf, sizeof(buf)),
 	      (referer || reason) ? "?" : "",
 	      referer ? (char*)"referer=" : "", referer ? (referer_enc = Utils::urlEncode(referer)) : (char*)"",
 	      (referer && reason) ? "&" : "",
