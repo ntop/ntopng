f3c802e4a4f5b36a3812972a56f63e9bd72ea679
MatteoBiscosi <biscosi@ntop.org>
2022-06-21 19:16:56 +0200

    Removed debug print

--- scripts/lua/flow_details.lua
+++ scripts/lua/flow_details.lua
@@ -1388,7 +1388,7 @@ else
       print("</tr>\n")
    end
 
-   if interface.isPacketInterface() and flow["flow_verdict"] then
+   if not interface.isPacketInterface() and flow["flow_verdict"] then
       local flow_verdict_badge = addFlowVerdictBadge(flow["flow_verdict"], true)
       print("<tr><th width=30%>" .. i18n("details.flow_verdict") .. "</th><td colspan=2>" .. flow_verdict_badge .. "</td></tr>\n")
    end
