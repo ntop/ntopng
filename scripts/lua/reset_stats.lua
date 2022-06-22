72622f9cdfe417b53148ab158f7eb2cbed944c56
MatteoBiscosi <biscosi@ntop.org>
2022-06-22 12:38:54 +0200

    Reset all counters not correctly working (#6655)

--- scripts/lua/reset_stats.lua
+++ scripts/lua/reset_stats.lua
@@ -19,38 +19,38 @@ local res = { ["status"] = "ok" }
 
 -- Function used to reset the stats
 local function reset_stats(ifids)
-   interface.select(ifids)
-      
-   if isAdministrator() then
-      if action == "reset_drops" then
-	 interface.resetCounters(true --[[ reset only drops --]])
-      elseif action == "reset_all" then
-	 interface.resetCounters(false --[[ reset all counters --]])
-      end
-   else
-      res["status"] = "unauthorized to reset interface: " .. ifids
-   end
+  interface.select(ifids)
+    
+  if isAdministrator() then
+    if action == "reset_drops" then
+  interface.resetCounters(true --[[ reset only drops --]])
+    elseif action == "reset_all" then
+  interface.resetCounters(false --[[ reset all counters --]])
+    end
+  else
+    res["status"] = "unauthorized to reset interface: " .. ifids
+  end
 end
 
 -- ##################################
 
 if action ~= nil then
-   -- Reset counters for all interfaces
-   if not ifid then
-      local ifs = interface.getIfNames()
+  -- Reset counters for all interfaces
+  if not ifid or ifid == "all" then
+    local ifs = interface.getIfNames()
 
-      for ifids, name in pairs(ifs) do
-	 if ifids == -1 then
-	    goto continue
-	 end
-
-	 reset_stats(ifids)
-	 ::continue::
+    for ifids, name in pairs(ifs) do
+      if ifids == -1 then
+        goto continue
       end
-   else
-      -- Reset counters for a specific interface
-      reset_stats(ifid)
-   end
+
+      reset_stats(ifids)
+      ::continue::
+    end
+  else
+    -- Reset counters for a specific interface
+    reset_stats(ifid)
+  end
 end
 
 print(json.encode(res, nil))
