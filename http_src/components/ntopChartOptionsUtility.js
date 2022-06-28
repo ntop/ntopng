c5acc4cf11bc27c690974455ae2c97488d7e603c
uccidibuti <lorenzo@vannucci.io>
2022-06-28 11:48:52 +0200

    add support interface data in chart

--- http_src/components/ntopChartOptionsUtility.js
+++ http_src/components/ntopChartOptionsUtility.js
@@ -3,10 +3,12 @@
 */
 //import { ntopng_utility } from '../services/context/ntopng_globals_services';
 
-function tsInterfaceToApexOptions(tsInterface) {
-    let startTime = tsInterface.start;
-    let step = tsInterface.step * 1000;
-    tsInterface.series.forEach((s) => {
+function tsInterfaceToApexOptions(tsInterfaceOptions) {
+    let startTime = tsInterfaceOptions.start;
+    let step = tsInterfaceOptions.step * 1000;
+    tsInterfaceOptions.series.forEach((s) => {
+	s.name = s.label;
+	delete s.type;
 	let time = startTime * 1000;;
 	s.data = s.data.map((d) => {
 	    let d2 = { x: time, y: d };
@@ -14,6 +16,34 @@ function tsInterfaceToApexOptions(tsInterface) {
 	    return d2;
 	});
     });
+    tsInterfaceOptions.xaxis = {
+	labels: {
+	    show: true,
+	},
+	axisTicks: {
+	    show: true,
+	},
+    };
+    tsInterfaceOptions.yaxis = {
+	labels: {
+	    formatter: function(value, index) {
+		if (value < 1 << 10) {
+		    return `${parseInt(value)} bit/s`;
+		}
+		value = value >> 10;
+		if (value < 1 << 10) {
+		    return `${parseInt(value)} Kbit/s`;
+		}
+		value = value >> 10;
+		if (value < 1 << 10) {
+		    return `${parseInt(value)} Mbit/s`;
+		}
+		value = value >> 10;
+		return `${parseInt(value)} Gbit/s`;
+	    },
+	}
+    }
+    //tsInterface.colors = ["#ff3231", "#ffc007"];
 }
 
 const ntopChartOptionsUtility = function() {
