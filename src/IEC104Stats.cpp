dd8f1a18e8c9e7f47e1142f634dc4b2d65e5ce85
Luca Deri <deri@ntop.org>
2022-07-21 23:33:01 +0200

    Indent

--- src/IEC104Stats.cpp
+++ src/IEC104Stats.cpp
@@ -23,8 +23,8 @@
 #include "flow_alerts_includes.h"
 
 /*
-#define DEBUG_IEC60870
-#define IEC60870_TRACE
+  #define DEBUG_IEC60870
+  #define IEC60870_TRACE
 */
 
 #define CLIENT_ALERT_SCORE  90
@@ -255,21 +255,21 @@ void IEC104Stats::processPacket(Flow *f, bool tx_direction,
 	    if(it == type_i_transitions.end()) {
 	      if(f->get_duration() > ntop->getPrefs()->getIEC60870LearingPeriod()) {
 	        FlowAlert *alert = NULL;
-		      u_int16_t c_score = 50, s_score = 10;
+		u_int16_t c_score = 50, s_score = 10;
 
 #ifdef IEC60870_TRACE
                 ntop->getTrace()->traceEvent(TRACE_NORMAL, "Found new transition %u -> %u", last_type_i, type_id);
 #endif
-          char key[128], rsp[64];
-          snprintf(key, sizeof(key), CHECKS_IEC_INVALID_TRANSITION);
+		char key[128], rsp[64];
+		snprintf(key, sizeof(key), CHECKS_IEC_INVALID_TRANSITION);
             
-          if((!ntop->getRedis()->get(key, rsp, sizeof(rsp))) && ((rsp[0] != '\0') && (!strcmp(rsp, "1"))))
-            alert = new IECInvalidTransitionAlert(NULL, f, packet_time, last_type_i, type_id); 
+		if((!ntop->getRedis()->get(key, rsp, sizeof(rsp))) && ((rsp[0] != '\0') && (!strcmp(rsp, "1"))))
+		  alert = new IECInvalidTransitionAlert(NULL, f, packet_time, last_type_i, type_id); 
           
-          if(alert) {
-            f->setPredominantAlertInfo(alert);
-            f->triggerAlertSync(alert, c_score, s_score);
-          }
+		if(alert) {
+		  f->setPredominantAlertInfo(alert);
+		  f->triggerAlertSync(alert, c_score, s_score);
+		}
 
 		type_i_transitions[transition] = 2; /* Post Learning */
 	      } else
@@ -296,19 +296,19 @@ void IEC104Stats::processPacket(Flow *f, bool tx_direction,
 	      FlowAlert *alert = NULL;
 	      u_int16_t c_score = CLIENT_ALERT_SCORE, s_score = SERVER_ALERT_SCORE;
 	      
-        char key[128], rsp[64];
-        snprintf(key, sizeof(key), CHECKS_IEC_INVALID_COMMAND_TRANSITION);
+	      char key[128], rsp[64];
+	      snprintf(key, sizeof(key), CHECKS_IEC_INVALID_COMMAND_TRANSITION);
           
-        if((!ntop->getRedis()->get(key, rsp, sizeof(rsp))) && ((rsp[0] != '\0') && (!strcmp(rsp, "1"))))
+	      if((!ntop->getRedis()->get(key, rsp, sizeof(rsp))) && ((rsp[0] != '\0') && (!strcmp(rsp, "1"))))
 	        alert = new IECInvalidCommandTransitionAlert(NULL, f, packet_time,
-							   transitions.m_to_c,
-							   transitions.c_to_m,
-							   transitions.c_to_c);
+							     transitions.m_to_c,
+							     transitions.c_to_m,
+							     transitions.c_to_c);
 	      
 	      if(alert) {
-          f->setPredominantAlertInfo(alert);
-          f->triggerAlertSync(alert, c_score, s_score);
-        }
+		f->setPredominantAlertInfo(alert);
+		f->triggerAlertSync(alert, c_score, s_score);
+	      }
 	      
 	      // ntop->getTrace()->traceEvent(TRACE_WARNING, "*** INVALID TRANSITION %u -> %u", last_type_i, type_id);
 		
@@ -336,18 +336,18 @@ void IEC104Stats::processPacket(Flow *f, bool tx_direction,
 
 	  if(unexpected_typeid_alerted) {
 	    FlowAlert *alert = NULL;
-      u_int16_t c_score = CLIENT_ALERT_SCORE, s_score = SERVER_ALERT_SCORE;
+	    u_int16_t c_score = CLIENT_ALERT_SCORE, s_score = SERVER_ALERT_SCORE;
 
 	    char key[128], rsp[64];
-      snprintf(key, sizeof(key), CHECKS_IEC_UNEXPECTED_TYPE_ID);
+	    snprintf(key, sizeof(key), CHECKS_IEC_UNEXPECTED_TYPE_ID);
         
-      if((!ntop->getRedis()->get(key, rsp, sizeof(rsp))) && ((rsp[0] != '\0') && (!strcmp(rsp, "1"))))
+	    if((!ntop->getRedis()->get(key, rsp, sizeof(rsp))) && ((rsp[0] != '\0') && (!strcmp(rsp, "1"))))
 	      alert = new IECUnexpectedTypeIdAlert(NULL, f, type_id, asdu, cause_tx, negative);
 	
-      if(alert) {
-        f->setPredominantAlertInfo(alert);
+	    if(alert) {
+	      f->setPredominantAlertInfo(alert);
 	      f->triggerAlertSync(alert, c_score, s_score);
-      } 
+	    } 
 	  } /* unexpected_typeid_alerted */
 	  /* Discard typeIds 127..255 */
 	} else /* payload_len < len */
