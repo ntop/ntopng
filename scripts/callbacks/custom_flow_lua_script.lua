6ea5cfbba1d96403b81cc75a71cd1b3a5c414144
MatteoBiscosi <biscosi@ntop.org>
2022-11-21 10:15:57 +0100

    Removed debug print

--- scripts/callbacks/custom_flow_lua_script.lua
+++ scripts/callbacks/custom_flow_lua_script.lua
@@ -3,5 +3,5 @@ function flow_key()
    return "[ ".. flow.protocol() .. " ]" .. flow.cli() .. ":" .. flow.cli_port() .. " <-> ".. flow.srv() .. ":" .. flow.srv_port()
 end
 
-io.write(flow_key() .. " [bytes: ".. flow.bytes().."]\n")
+-- io.write(flow_key() .. " [bytes: ".. flow.bytes().."]\n")
 return(0)
