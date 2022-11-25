c534c628f8a2d7ff71d3bd9e189221ecd45999fd
MatteoBiscosi <biscosi@ntop.org>
2022-11-25 16:13:56 +0100

    Added Note component

---
+++ http_src/vue/note-list.vue
@@ -0,0 +1,22 @@
+<!-- (C) 2022 - ntop.org     -->
+
+<template>
+  <div id="script-description" class="notes bg-light border" role="alert">
+    <b>{{ _i18n('notes') }}</b>
+    <ul>
+      <template v-for="note in props.note_list">
+        <li>{{ note }}</li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const _i18n = (t) => i18n(t);
+
+const props = defineProps({
+  note_list: Array,
+});
+</script>
