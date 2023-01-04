2ffac0b5ef3e3be396265ffd85e646ae02d89b0c
uccidibuti <vannucci@ntop.org>
2023-01-04 20:56:39 +0100

    restyle sankey in progress

--- http_src/vue/page-sankey.vue
+++ http_src/vue/page-sankey.vue
@@ -1,147 +1,359 @@
 <!-- (C) 2022 - ntop.org     -->
 <template>
-  <div id="my_dataviz">
-  </div>
+<div>
+  <svg
+    ref="sankey_chart_ref"
+    :width="sankey_size.width"
+    :height="sankey_size.height"
+    style="margin:10px;">
+    <defs />
+    <g class="nodes" style="stroke: #000;strokeOpacity: 0.5;"/>
+    <g class="links"
+       style="stroke: #000;strokeOpacity: 0.3; fill: none;"/>
+    <g class="texts" />
+  </svg>
+</div>
 </template>
 
 <script setup>
 import { ref, onMounted, onBeforeMount, computed, watch } from "vue";
 import { ntopng_utility, ntopng_url_manager, ntopng_status_manager } from "../services/context/ntopng_globals_services.js";
 
+const d3 = d3v7;
+
 const props = defineProps({
+    width: Number,
+    height: Number,
 });
 
-onBeforeMount(async() => {
-    
-    
+const sankey_chart_ref = ref(null);
+const sankey_size = ref({});
+
+onBeforeMount(async() => {});
+
+onMounted(async () => {    
+    draw_sankey();
 });
 
-let d3 = d3v7;
-onMounted(async () => {
-    // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 10, bottom: 10, left: 10},
-	width = 450 - margin.left - margin.right,
-	height = 480 - margin.top - margin.bottom;
+async function draw_sankey() {
+    const colors = d3.scaleOrdinal(d3.schemeCategory10);
+    let data = await get_sankey_data();
+    const size = get_size();
+    sankey_size.value = size;
+    const { links, nodes } = calculate_sankey(data, size.width - 10, size.height - 5);
     
-    // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+    d3.select(sankey_chart_ref.value)
+	.select("g.nodes")
+	.selectAll("rect")
+	.data(nodes)
+	.join(
+            (enter) => {
+		const e = enter.append("rect");
+		
+		e.attr("x", (d) => d.x0).attr("y", (d) => d.y0);
+		
+		e.transition(d3.easeLinear)
+		    .delay(1000)
+		    .duration(500)
+		    .attr("height", (d) => d.y1 - d.y0)
+		    .attr("width", (d) => d.x1 - d.x0)
+		    .attr("dataIndex", (d) => d.index)
+		    .attr("fill", (d) => colors(d.index / nodes.length));
+		e.append("title").text((d) => `${d.name}\n${d.hours}`);
+            },
+            (update) =>
+		update
+		.transition(d3.easeLinear)
+		.delay(500)
+		.duration(500)
+		.attr("x", (d) => d.x0)
+		.attr("y", (d) => d.y0)
+		.attr("height", (d) => d.y1 - d.y0)
+		.attr("width", (d) => d.x1 - d.x0)
+		.attr("dataIndex", (d) => d.index)
+		.attr("fill", (d) => colors(d.index / nodes.length))
+		.select("title")
+		.text((d) => `${d.name}\n${d.hours}`),
+            (exit) =>
+		exit.transition(d3.easeLinear).duration(500).style("opacity", 0).remove()
+	).on("dblclick", function(data) {
+	    //todo portare fuori
+	    // data = data.currentTarget.__data__
+	    // const sourceLink = data.sourceLinks;
+	    // const targetLink = data.targetLinks;
+	    // const link = (sourceLink && sourceLink[0]) ? sourceLink[0] : targetLink[0];
+	    
+	    // if(link) {
+	    //   /* Get the node link from the rest */
+	    //   if(link.source.id === data.id) {
+	    //     if(link.source_link && link.source_link !== '')
+	    //       window.open(link.source_link, '_blank');
+	    //   } else if(link.target.id === data.id) {
+	    //     if(link.target_link && link.target_link !== '')
+	    //       window.open(link.target_link, '_blank');
+	    //   } 
+	    // } 
+	});
     
-    let graph = {
-	"nodes":[
-	    {"node":0,"name":"node0"},
-	    {"node":1,"name":"node1"},
-	    {"node":2,"name":"node2"},
-	    {"node":3,"name":"node3"},
-	    {"node":4,"name":"node4"}
+    d3.select(sankey_chart_ref.value)
+	.select("g.texts")
+	.selectAll("text")
+	.data(nodes)
+	.join(
+            (enter) => {
+		const e = enter.append("text");
+		
+		e.transition(d3.easeLinear)
+		    .delay(1000)
+		    .duration(500)
+		    .attr("x", (d) => (d.x0 < size.width / 2 ? d.x1 + 6 : d.x0 - 6))
+		    .attr("y", (d) => (d.y1 + d.y0) / 2)
+		    .attr("fill", (d) => d3.rgb(colors(d.index / nodes.length)).darker())
+		    .attr("alignment-baseline", "middle")
+		    .attr("text-anchor", (d) =>
+			  d.x0 < size.width / 2 ? "start" : "end"
+			 )
+		    .attr("font-size", 9)
+		    .text((d) => d.name);
+            },
+            (update) =>
+		update
+		.transition(d3.easeLinear)
+		.delay(500)
+		.duration(500)
+		.attr("x", (d) => (d.x0 < size.width / 2 ? d.x1 + 6 : d.x0 - 6))
+		.attr("y", (d) => (d.y1 + d.y0) / 2)
+		.attr("fill", (d) => d3.rgb(colors(d.index / nodes.length)).darker())
+		.attr("text-anchor", (d) =>
+		      d.x0 < size.width / 2 ? "start" : "end"
+		     )
+		.attr("font-size", 9)
+		.text((d) => d.name),
+            (exit) =>
+		exit
+		.transition(d3.easeLinear)
+            /* .delay(500) */
+		.duration(500)
+		.style("opacity", 0)
+		.remove()
+	);
+    
+    d3.select(sankey_chart_ref.value)
+	.select("defs")
+	.selectAll("linearGradient")
+	.data(links)
+	.join(
+            (enter) => {
+		const lg = enter.append("linearGradient");
+		
+		lg.attr("id", (d) => `gradient-${d.index}`)
+		    .attr("gradientUnits", "userSpaceOnUse")
+		    .attr("x1", (d) => d.source.x1)
+		    .attr("x2", (d) => d.target.x0);
+		
+		lg.append("stop")
+		    .attr("offset", "0")
+		    .attr("stop-color", (d) => colors(d.source.index / nodes.length));
+		
+		lg.append("stop")
+		    .attr("offset", "100%")
+		    .attr("stop-color", (d) => colors(d.target.index / nodes.length));
+            },
+            (update) => {
+		update
+		    .attr("id", (d) => `gradient-${d.index}`)
+		    .attr("gradientUnits", "userSpaceOnUse")
+		    .attr("x1", (d) => d.source.x1)
+		    .attr("x2", (d) => d.target.x0);
+		update.selectAll("stop").remove();
+		update
+		    .append("stop")
+		    .attr("offset", "0")
+		    .attr("stop-color", (d) => colors(d.source.index / nodes.length));
+		
+		update
+		    .append("stop")
+		    .attr("offset", "100%")
+		    .attr("stop-color", (d) => colors(d.target.index / nodes.length));
+            },
+            (exit) => exit.remove()
+	);
+    
+    d3.select(sankey_chart_ref.value)
+	.select("g.links")
+	.selectAll("path")
+	.data(links)
+	.join(
+            (enter) => {
+		const e = enter.append("path");
+		e.transition(d3.easeLinear)
+		    .delay(1000)
+		    .duration(500)
+		    .attr("d", d3.sankeyLinkHorizontal())
+		    .attr("stroke", (d) => `url(#gradient-${d.index}`)
+		    .attr("stroke-width", (d) => d.width);
+		e.append("title").text((d) => `${d.hours}`);
+            },
+            (update) =>
+		update
+		.transition(d3.easeLinear)
+		.delay(500)
+		.duration(500)
+		.attr("d", d3.sankeyLinkHorizontal())
+		.attr("stroke", (d) => `url(#gradient-${d.index}`)
+		.attr("stroke-width", (d) => d.width)
+		.select("title")
+		.text((d) => `${d.hours}`),
+            (exit) =>
+		exit
+		.transition(d3.easeLinear)
+            /* .delay(1000) */
+		.duration(500)
+		.style("opacity", 0)
+		.remove()
+	);
+}
+
+async function get_sankey_data() {
+    const rsp = [
+	{
+	    "link_color": "#e377c2",
+	    "source_color": "#e377c2",
+	    "source_link": "/lua/host_details.lua?page=flows&host=192.168.1.7&vlan=0&application=IGMP",
+	    "target": "224.0.0.251",
+	    "source": "IGMP",
+	    "link": "IGMP",
+	    "target_link": "/lua/host_details.lua?host=224.0.0.251&vlan=0",
+	    "target_node": "224.0.0.251",
+	    "source_node": "192.168.1.7",
+	    "value": 60
+	},
+	{
+	    "link_color": "#e377c2",
+	    "source_link": "/lua/host_details.lua?host=192.168.1.7&vlan=0",
+	    "target": "IGMP",
+	    "source": "192.168.1.7",
+	    "link": "IGMP",
+	    "target_link": "/lua/host_details.lua?page=flows&host=192.168.1.7&vlan=0&application=IGMP",
+	    "target_node": "224.0.0.2",
+	    "target_color": "#e377c2",
+	    "source_node": "192.168.1.7",
+	    "value": 120
+	},
+	{
+	    "link_color": "#e377c2",
+	    "source_color": "#e377c2",
+	    "source_link": "/lua/host_details.lua?page=flows&host=192.168.1.7&vlan=0&application=IGMP",
+	    "target": "224.0.0.2",
+	    "source": "IGMP",
+	    "link": "IGMP",
+	    "target_link": "/lua/host_details.lua?host=224.0.0.2&vlan=0",
+	    "target_node": "224.0.0.2",
+	    "source_node": "192.168.1.7",
+	    "value": 60
+	},
+	{
+	    "link_color": "#bcbd22",
+	    "source_link": "/lua/host_details.lua?host=192.168.1.7&vlan=0",
+	    "target": "MDNS",
+	    "source": "192.168.1.7",
+	    "link": "MDNS",
+	    "target_link": "/lua/host_details.lua?page=flows&host=192.168.1.7&vlan=0&application=MDNS",
+	    "target_node": "224.0.0.251",
+	    "target_color": "#bcbd22",
+	    "source_node": "192.168.1.7",
+	    "value": 396
+	},
+	{
+	    "link_color": "#bcbd22",
+	    "source_color": "#bcbd22",
+	    "source_link": "/lua/host_details.lua?page=flows&host=192.168.1.7&vlan=0&application=MDNS",
+	    "target": "224.0.0.251",
+	    "source": "MDNS",
+	    "link": "MDNS",
+	    "target_link": "/lua/host_details.lua?host=224.0.0.251&vlan=0",
+	    "target_node": "224.0.0.251",
+	    "source_node": "192.168.1.7",
+	    "value": 396
+	}
+    ];
+    
+        
+    const data = {
+	// nodes: [
+	//     { index: 0, name: "Liikevaihto", value: 100, hours: "100%" },
+	//     { index: 1, name: "Kiinteät kulut", value: 75, hours: "85%" },
+	//     { index: 2, name: "Muuttuvat kulut", value: 10, hours: "3:00" },
+	//     { index: 3, name: "Palkkakulut", value: 69, hours: "1:20" },
+	//     { index: 4, name: "Muut kiinte", value: 6, hours: "1:40" },
+	//     { index: 5, name: "Kate", value: 15, hours: "1:40" }
+	// ],
+	nodes: [
+	    { index: 0, name: "Liikevaihto", hours: "100%" },
+	    { index: 1, name: "Kiinteät kulut", hours: "85%" },
+	    { index: 2, name: "Muuttuvat kulut", hours: "3:00" },
+	    { index: 3, name: "Palkkakulut", hours: "1:20" },
+	    { index: 4, name: "Muut kiinte", hours: "1:40" },
+	    { index: 5, name: "Kate", hours: "1:40" }
 	],
-	"links":[
-	    {"source":0,"target":2,"value":2},
-	    {"source":1,"target":2,"value":2},
-	    {"source":1,"target":3,"value":2},
-	    {"source":0,"target":4,"value":2},
-	    {"source":2,"target":3,"value":2},
-	    {"source":2,"target":4,"value":2},
-	    {"source":3,"target":4,"value":4}
+	links: [
+	    { source: 0, target: 1, value: 75, hours: "+1:00" },
+	    { source: 0, target: 2, value: 10, hours: "+2:00" },
+	    { source: 1, target: 3, value: 69, hours: "+1:20" },
+	    { source: 1, target: 4, value: 6, hours: "+1:40" },
+	    { source: 0, target: 5, value: 15, hours: "+1:40" }
 	]
     };
-    console.log(graph);    
-    
-    // svg.append("g")
-    // 	.attr("fill", "none")
-    // 	.attr("stroke", "#000")
-    // 	.attr("stroke-opacity", 0.2)
-    // 	.selectAll("path")
-    // 	.data(graph.links)
-    // 	.join("path")
-    // 	.attr("d", d3.sankeyLinkHorizontal())
-    // 	.attr("stroke-width", function(d) { return d.width; });
-    // return;
+    return data;
+}
 
+function wrap_graph_rsp(rsp) {
+    let nodes = [];
+    let links = [];
 
-    // Color scale used
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    let nodes_added_dict = {};
+    let links_added_dict = {};
+    const f_add_node = (node_id, href, color) => {
+	if (nodes_added_dict[node_id] == null) { return; }
+	let index = nodes.length;
+	nodes_added_dict[node_id] = index;
+	let new_node = { index, name: node_id, href, color };
+	nodes.push(new_node);
+    };
+    const f_add_link = (source, target, value, label) => {
+	const source_index = nodes_added_dict[source];
+	const target_index = nodes_added_dict[target];
+	let new_link = { source: source_index, target: target_index, value, label };
+	links.push(new_link);
+    };
+    rsp.forEach((el) => {
+	f_add_node(el.source, el.source_link, el.source_color);
+	f_add_node(el.target, el.target_link, el.target_color);
+	f_add_link(el.source, el.target, el.value, el.link);
+    });
+}
+
+function get_size() {
+    let width = props.width;
+    if (width == null) { width = window.innerWidth - 200; }
+    let height = props.height;
+    if (height == null) { height = window.innerHeight - 50; }
+
+    return { width, height };
+}
+
+function calculate_sankey(data, width, height) {
+    const sankeyimpl = d3.sankey()
+	  .nodeAlign(d3.sankeyCenter)
+	  .nodeWidth(10)
+	  .nodePadding(10)
+	  .extent([
+	      [0, 5],
+	      [width, height]
+	  ]);
     
-    // Set the sankey diagram properties
-    var sankey = d3.sankey()
-	.nodeWidth(36)
-	.nodePadding(290)
-	.size([width, height]);
-    
-    // load the data
-    // let graph = await ntopng_utility.http_request("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_sankey.json");
-    // return;
-    // Constructs a new Sankey generator with the default settings.
-    sankey
-	.nodes(graph.nodes)
-	.links(graph.links)
-    // .extent([0,0], [1000, 1000]);
-    
-    
-    // add in the links
-    var link = svg.append("g")
-	.selectAll(".link")
-	.data(graph.links)
-	.enter()
-	.append("path")
-	.attr("class", "link")
-	// .attr("d", sankey.link() )
-	.style("stroke-width", function(d) { return Math.max(1, d.dy); })
-	.sort(function(a, b) { return b.dy - a.dy; });
-    
-    // add in the nodes
-    var node = svg.append("g")
-	.selectAll(".node")
-	.data(graph.nodes)
-	.enter().append("g")
-	.attr("class", "node")
-	.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-	.call(d3.drag()
-	      .subject(function(d) { return d; })
-	      .on("start", function() { this.parentNode.appendChild(this); })
-	      .on("drag", dragmove));
-    
-    // add the rectangles for the nodes
-    node
-	.append("rect")
-	.attr("height", function(d) { return d.dy; })
-	.attr("width", sankey.nodeWidth())
-	.style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
-	.style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
-    // Add hover text
-	.append("title")
-	.text(function(d) { return d.name + "\n" + "There is " + d.value + " stuff in this node"; });
-    
-    // add in the title for the nodes
-    node
-	.append("text")
-        .attr("x", -6)
-        .attr("y", function(d) { return d.dy / 2; })
-        .attr("dy", ".35em")
-        .attr("text-anchor", "end")
-        .attr("transform", null)
-        .text(function(d) { return d.name; })
-	.filter(function(d) { return d.x < width / 2; })
-        .attr("x", 6 + sankey.nodeWidth())
-        .attr("text-anchor", "start");
-    
-    // the function for moving the nodes
-    function dragmove(d) {
-	d3.select(this)
-	    .attr("transform",
-		  "translate("
-		  + d.x + ","
-		  + (d.y = Math.max(
-		      0, Math.min(height - d.dy, d3.event.y))
-		    ) + ")");
-	sankey.relayout();
-	link.attr("d", sankey.link() );
-    }	
-});
+    return sankeyimpl(data);
+}
 
 const _i18n = (t) => i18n(t);
 
