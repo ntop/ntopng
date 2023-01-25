1ac75506786be713b8db6d72d34ec0b3d18591b3
MatteoBiscosi <biscosi@ntop.org>
2023-01-25 15:37:33 +0100

    Fixes name and size of charts

--- scripts/lua/flows_stats.lua
+++ scripts/lua/flows_stats.lua
@@ -191,10 +191,17 @@ if (page == "flows" or page == nil) then
    local is_chart_printed = false
 
    print[[<div class="d-flex m-1">]]
+   if is_chart_printed then
+    print[[<h3 class="m-auto">]]
+   else
+    print[[<h3 class="me-auto mt-auto mb-auto">]]
+   end
+   print(active_msg)
+   print[[</h3>]]
    if (table.len(page_params) > 0) and (not isEmptyString(page_params["application"])) then
     is_chart_printed = true
          print [[
-         <div class="col-4">
+         <div class="col-5">
             <div class="info-stats">
                <ul class="nav-side m-0 ps-5 ms-1" style="list-style-type: none;">
                   <li class="nav-item">
@@ -224,13 +231,6 @@ if (page == "flows" or page == nil) then
          </div>
    ]]
    end
-   if is_chart_printed then
-    print[[<h3 class="m-auto">]]
-   else
-    print[[<h3 class="me-auto mt-auto mb-auto">]]
-   end
-   print(active_msg)
-   print[[</h3>]]
    print[[<h6 class="ms-auto mt-auto mb-auto">]]
    print(mini_title)
    print[[</h6>]]
