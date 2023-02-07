78d6acb867fccf20328a723a9392824d14379f71
MatteoBiscosi <biscosi@ntop.org>
2023-02-07 11:06:36 +0100

    Removed no needed checks

--- scripts/lua/modules/alert_definitions/host/host_alert_score_threshold.lua
+++ scripts/lua/modules/alert_definitions/host/host_alert_score_threshold.lua
@@ -47,84 +47,77 @@ end
 -- @param alert_type_params Table `alert_type_params` as built in the `:init` method
 -- @return A human-readable string
 function host_alert_score_threshold.format(ifid, alert, alert_type_params)
-   local alert_consts = require("alert_consts")
-   local host = alert_consts.formatHostAlert(ifid, alert["ip"], alert["vlan_id"])
-   local threshold = alert_type_params["threshold"] or 0
-   local as_cli_or_srv = i18n("client")
-   local as_cli = true
-   local vlan_id = tonumber(alert["vlan_id"] or 0)
+  local alert_consts = require("alert_consts")
+  local host = alert_consts.formatHostAlert(ifid, alert["ip"], alert["vlan_id"])
+  local threshold = alert_type_params["threshold"] or 0
+  local as_cli_or_srv = i18n("client")
+  local as_cli = true
+  local vlan_id = tonumber(alert["vlan_id"] or 0)
 
-   -- Checks if alerted host is a client or a server.
-   if alert_type_params["is_client_alert"] == false then
-      as_cli_or_srv = i18n("server")
-      as_cli = false
-   end
+  -- Checks if alerted host is a client or a server.
+  if alert_type_params["is_client_alert"] == false then
+    as_cli_or_srv = i18n("server")
+    as_cli = false
+  end
 
-   local flows_info_href = ""
+  local flows_info_href = ""
 
-   -- Checks if host is still in memory, in case add link to live flows on the alert description.
-   interface.select(ifid)
-   if interface.getHostInfo(alert["ip"],vlan_id) then
-      flows_info_href = '[ '..i18n("check_live")..':  <a href="' .. ntop.getHttpPrefix().."/lua/flows_stats.lua?host="..host..'" data-placement="bottom" title="Live Flow Explorer"><i class="fas fa-search-plus"></i></a> ] '
-   end
-   
-   -- Checks if ClickHouse is enabled, in case add link to historical flows on the alert description.
-   if ntop.isClickHouseEnabled() then
-      local extra_params = {
-         ifid = {
-            value = ifid,
-            operator = "eq"
-         },
-         epoch_begin = {
-            value = tonumber(alert.tstamp) - 600,
-            operator = "eq"
-         },
-         epoch_end = {
-            value = tonumber(alert.tstamp_end) + 600,
-            operator = "eq"
-         },
-         score = {
-            value = 0,
-            operator = "gt"
-         }
+  -- Checks if host is still in memory, in case add link to live flows on the alert description.
+  if interface.getHostInfo(hostinfo2hostkey(alert)) then
+    flows_info_href = '[ '..i18n("check_live")..':  <a href="' .. ntop.getHttpPrefix().."/lua/flows_stats.lua?host="..host..'&alert_type=alerted" data-placement="bottom" title="Live Flow Explorer"><i class="fas fa-search-plus"></i></a> ] '
+  end
+  
+  -- Checks if ClickHouse is enabled, in case add link to historical flows on the alert description.
+  if ntop.isClickHouseEnabled() then
+    local extra_params = {
+      ifid = {
+        value = ifid,
+        operator = "eq"
+      },
+      epoch_begin = {
+        value = tonumber(alert.tstamp) - 600, -- 10 minutes before the trigger of the alert
+        operator = "eq"
+      },
+      epoch_end = {
+        value = alert.tstamp_end, -- If the alert ended, means that the score decreased, no need of 10 minutes
+        operator = "eq"
+      },
+      score = {
+        value = 0,
+        operator = "gt"
       }
-      
-      if(alert["vlan_id"] ~= nil) then
-	 local alert_id = tonumber(alert["vlan_id"])
+    }
+    
+    if(vlan_id > 0) then
+      extra_params.vlan_id = {
+        value = vlan_id,
+        operator = "eq"
+      }
+    end      
 
-	 if(alert_id > 0) then
-	    extra_params.vlan_id = {
-	       value = alert["vlan_id"],
-	       operator = "eq"
-	    }
-	 end
-      end      
+    if(as_cli) then 
+        extra_params.cli_ip = {
+          value = alert["ip"],
+          operator = "eq"
+        }
+    else 
+        extra_params.srv_ip = {
+          value = alert["ip"],
+          operator = "eq"
+        }
+    end
 
-      if(as_cli) then 
-         extra_params.cli_ip = {
-            value = alert["ip"],
-            operator = "eq"
-         }
-      else 
-         extra_params.srv_ip = {
-            value = alert["ip"],
-            operator = "eq"
-         }
-      end
+    flows_info_href = flows_info_href..'[ '..i18n("check_historical")..': <a href="' .. add_historical_flow_explorer_button_ref(extra_params,true) ..'" data-placement="bottom" title="Historical Flow Explorer"><i class="fas fa-search-plus"></i></a> ]' 
+  end
 
-      flows_info_href = flows_info_href..'[ '..i18n("check_historical")..': <a href="' .. add_historical_flow_explorer_button_ref(extra_params,true) ..'" data-placement="bottom" title="Historical Flow Explorer"><i class="fas fa-search-plus"></i></a> ]' 
-   end
-
-   if(tonumber(alert_type_params["value"]) > tonumber(threshold)) and (threshold > 0) then
-      -- threshold due to threshold crossed
-      return i18n("alert_messages.score_threshold", {
-         entity = host,
-         cli_or_srv = as_cli_or_srv,
-         value = alert_type_params["value"],
-         threshold = threshold,
-         flows_info = flows_info_href
-      })
-   end
+  -- threshold due to threshold crossed
+  return i18n("alert_messages.score_threshold", {
+      entity = host,
+      cli_or_srv = as_cli_or_srv,
+      value = alert_type_params["value"],
+      threshold = threshold,
+      flows_info = flows_info_href
+  })
 end
 
 -- #######################################################
