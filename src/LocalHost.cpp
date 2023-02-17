31c8d7fbf0e10b1cd83aa5d79786424342d6af93
Luca Deri <deri@ntop.org>
2023-02-17 16:43:04 +0100

    Method signature change to be called it also from a lua host script

--- src/LocalHost.cpp
+++ src/LocalHost.cpp
@@ -389,7 +389,7 @@ void LocalHost::lua_get_timeseries(lua_State* vm) {
   }
   
   Host::lua_blacklisted_flows(vm);
-  lua_unidirectional_tcp_flows(vm);
+  lua_unidirectional_tcp_flows(vm, true);
   
   /* NOTE: the following data is *not* exported for the initial_point */
   lua_push_uint64_table_entry(vm, "active_flows.as_client", getNumOutgoingFlows());
