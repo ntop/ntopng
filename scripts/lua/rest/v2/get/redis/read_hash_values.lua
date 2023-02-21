52331d4dfa9d00cf2d19857d69f1b0ccf65781ed
Luca Deri <deri@ntop.org>
2023-02-21 11:58:24 +0100

    Simple REST call for reading redis hash values

---
+++ scripts/lua/rest/v2/get/redis/read_hash_values.lua
@@ -0,0 +1,32 @@
+--
+-- (C) 2013-23 - ntop.org
+--
+
+local dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+require "lua_utils"
+local rest_utils = require("rest_utils")
+
+local rc = rest_utils.consts.success.ok
+local res = {}
+
+local hashname = _GET["hashname"] or "ntop.host.blackhole" 
+
+if isEmptyString(hashname) then
+  rc = rest_utils.consts.err.invalid_interface
+  rest_utils.answer(rc)
+  return
+end
+
+local h_values = ntop.getHashKeysCache(hashname)
+
+for key, val in pairs(h_values) do
+    res[#res + 1] = {
+       ip = key,
+       time = ntop.getHashCache(hashname, key)
+    }
+    
+end
+
+rest_utils.answer(rc, res)
