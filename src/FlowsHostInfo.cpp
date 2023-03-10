9fa97304db7da6fd8a39d2abb186ea5ecdfa6f4f
Nicolo Maio <maio@ntop.org>
2023-03-10 14:26:36 +0000

    Fix host name resolution in aggregated live flows. (#7302  #7233)

--- src/FlowsHostInfo.cpp
+++ src/FlowsHostInfo.cpp
@@ -31,11 +31,30 @@ char* FlowsHostInfo::getIP(char* buf, u_int len) {
 /* ************************************************ */
 
 char* FlowsHostInfo::getHostName(char* buf, u_int len) {
+  
   if(host) {
-    return host->get_name(buf, len, false);
-  } else {
-    return((char*)"");
+    char* name = host->get_name(buf, len, false);
+    if(name && strcmp(name,"") != 0 && strcmp(name," ") != 0)
+      return(name);
   }
+  char symIP[64];
+
+  memset(symIP, 0 ,sizeof(symIP));
+  memset(buf, 0, len);
+  char *ip_addr = ip->print(buf, len);
+
+  if(ip_addr) {
+    ntop->resolveHostName(ip_addr, symIP, sizeof(symIP));
+    if(symIP && strcmp(symIP, ip_addr) != 0) {
+      
+      memset(buf, 0, len);
+      snprintf(buf, len, "%s", symIP);
+      return(buf ? buf : (char*)"");
+    } else 
+      return((char*) "");
+  }
+  
+  return((char*) "");
 }
 
 /* ************************************************ */
