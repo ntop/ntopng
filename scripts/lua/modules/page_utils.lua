ad30e602e21b6da36ab6503235b11c1c1348f90e
Alfredo Cardigliano <cardigliano@ntop.org>
2023-03-29 18:41:47 +0200

    Fix style

--- scripts/lua/modules/page_utils.lua
+++ scripts/lua/modules/page_utils.lua
@@ -424,6 +424,7 @@ function page_utils.print_header_minimal(title, addLoginJS)
             margin-top: -5px;
             background:url(]] print(http_prefix) print[[/dist/images/flags.png) no-repeat
           }
+          </style>
           <script type="application/javascript" src="]] print(http_prefix) print("/lua/locale.lua?"..locale_when .. "&user_language=" ..language); print[["> </script>
           <script type="application/javascript" src="]] print(http_prefix) print[[/dist/third-party.js?]] print(static_file_epoch) print[["></script>
           <script type="application/javascript" src="]] print(http_prefix) print[[/dist/ntopng.js?]] print(static_file_epoch) print[["></script>
