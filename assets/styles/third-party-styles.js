804a9a5c681a5a5c5acf5d6cd0e56740fcbbd6b5
Matteo Biscosi <biscosi@ntop.org>
2023-04-05 08:16:58 +0000

    Removed graph-utils dependency

--- assets/styles/third-party-styles.js
+++ assets/styles/third-party-styles.js
@@ -3,7 +3,3 @@ import './third-party-styles.scss'
 /* TODO: Remove bs3 import! */
 import './vendors/bootstrap3/tempusdominus/js/tempusdominus-bootstrap-4'
 
-import * as GraphUtils from '../../http_src/utilities/graph/graph-utils'
-
-window.GraphUtils = GraphUtils
-
