d5d7c9151a757205b16a8f0f7630b1c0cde33f90
Luca Deri <deri@ntop.org>
2023-04-27 17:41:10 +0200

    Added table.is_equal() to compare tables regardless of the entries order

--- scripts/lua/modules/lua_utils_generic.lua
+++ scripts/lua/modules/lua_utils_generic.lua
@@ -433,6 +433,60 @@ function convertDate(vardate)
 
 end
 
+-- ##############################################
+
+-- compare two tables for equality
+
+function table.is_equal(t1, t2, ignore_keys)
+   local ty1 = type(t1)
+   local ty2 = type(t2)
+
+   if ty1 ~= ty2 then
+      return false
+   end
+
+   -- non-table types can be directly compared
+   if ty1 ~= 'table' and ty2 ~= 'table' then
+      return t1 == t2
+   end
+
+   -- as well as tables which have the metamethod __eq
+   local mt = getmetatable(t1)
+   if mt and mt.__eq then return t1 == t2 end
+
+   for k1,v1 in pairs(t1) do
+      if((ignore_keys == nil) or (ignore_keys[k1] == nil)) then
+	 local v2 = t2[k1]
+
+	 if v2 == nil or not table.is_equal(v1,v2) then
+	    --tprint("Change in key (1) "..k1)
+
+	    if(k1 == "ntopng.prefs.snmp_configuration") then
+	       -- tprint(v1)
+	       -- tprint(v2)
+	    end
+
+	    return false
+	 end
+      end
+   end
+
+   for k2,v2 in pairs(t2) do
+      if((ignore_keys == nil) or (ignore_keys[k2] == nil)) then
+	 local v1 = t1[k2]
+
+	 if v1 == nil or not table.is_equal(v1,v2) then
+	    -- tprint("Change in key (2) "..k2)
+	    return false
+	 end
+      end
+   end
+
+   return true
+end
+
+-- ##############################################
+
 if(trace_script_duration ~= nil) then
    io.write(debug.getinfo(1,'S').source .." executed in ".. (os.clock()-clock_start)*1000 .. " ms\n")
 end
