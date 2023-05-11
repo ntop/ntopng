3fca3005dc83784be6e639b44513c5e281a04c8e
Lorenzo Vannucci <vannucci@ntop.org>
2023-05-11 15:30:20 +0200

    Add spinner component

---
+++ http_src/vue/spinner.vue
@@ -0,0 +1,17 @@
+<template>
+<div v-if="show" class="spinner-border text-primary" :style="{'width': size, 'height': size}" role="status">
+</div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeMount } from "vue";
+
+const props = defineProps({
+    size: String,
+    show: Boolean,
+});
+
+onMounted(async () => {
+});
+
+</script>
