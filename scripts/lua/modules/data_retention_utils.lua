a4f731d61e08c8fd736397b405670ed5b50d1a4a
Luca Deri <deri@ntop.org>
2023-05-31 11:39:05 +0200

    Reworked clickhouse retention

--- scripts/lua/modules/data_retention_utils.lua
+++ scripts/lua/modules/data_retention_utils.lua
@@ -10,6 +10,7 @@ local DEFAULT_DATA_RETENTION_DAYS = 30
 local DEFAULT_AGGREGATED_FLOWS_DATA_RETENTION_DAYS = 60
 local DEFAULT_DATA_RETENTION_DAYS_KEY = "ntopng.prefs.data_retention_days"
 local FLOWS_AND_ALERTS_DATA_RETENTION_DAYS_KEY = "ntopng.prefs.flows_and_alerts_data_retention_days"
+local AGGREGATED_FLOWS_DATA_RETENTION_DAYS_KEY = "ntopng.prefs.aggregated_flows_data_retention_days"
 local TS_AND_STATS_DATA_RETENTION_DAYS_KEY = "ntopng.prefs.ts_and_stats_data_retention_days"
 
 local data_retention_utils = {}
@@ -32,6 +33,14 @@ function data_retention_utils.getFlowsAndAlertsDataRetentionDays()
   local data_retention = ntop.getCache(FLOWS_AND_ALERTS_DATA_RETENTION_DAYS_KEY) or ntop.getCache(DEFAULT_DATA_RETENTION_DAYS_KEY) 
 
   return tonumber(data_retention) or data_retention_utils.getDefaultRetention()
+end
+
+-- ########################################################
+
+function data_retention_utils.getAggregatedFlowsRetentionDays()
+  local data_retention = ntop.getCache(AGGREGATED_FLOWS_DATA_RETENTION_DAYS_KEY)
+  -- aggregated data is at least one day more that non-aggregated data
+  return tonumber(data_retention) or (data_retention_utils.getDefaultRetention() + 1)
 end
 
 -- ########################################################
