f1df8ef40af862dfa60fc3179079009578380f68
Lorenzo Vannucci <vannucci@ntop.org>
2023-06-05 16:45:35 +0200

    Fix new table component

--- http_src/vue/table.vue
+++ http_src/vue/table.vue
@@ -37,7 +37,7 @@
       </template>
       <template v-slot:menu>
 	<div v-for="col in columns_wrap" class="form-check form-switch ms-1">
-	  <input class="form-check-input" style="cursor:pointer;" v-model="col.visible == true" @click="change_columns_visibility(col)"  checked="" type="checkbox" id="toggle-Begin">
+	  <input class="form-check-input" style="cursor:pointer;" :checked="col.visible == true" @click="change_columns_visibility(col)"  checked="" type="checkbox" id="toggle-Begin">
           <label class="form-check-label" for="toggle-Begin" v-html="print_column_name(col.data)">
           </label>
 	</div>
@@ -168,6 +168,7 @@ async function load_table() {
 const changing_column_visibility = ref(false);
 async function change_columns_visibility(col) {
     changing_column_visibility.value = true;
+    col.visible = !col.visible;
     if (props.paging) {
 	await set_rows();
     }
