119db1f0709bb1bcb3f2c76e0aab9435801aa170
Matteo Biscosi <biscosi@ntop.org>
2023-07-18 08:41:06 +0000

    Removed not useful imports

--- scripts/callbacks/daily-delayed/interface/stats_retention.lua
+++ scripts/callbacks/daily-delayed/interface/stats_retention.lua
@@ -4,7 +4,6 @@
 
 local dirs = ntop.getDirs()
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
-package.path = dirs.installdir .. "/scripts/lua/modules/timeseries/?.lua;" .. package.path
 
 local data_retention_utils = require "data_retention_utils"
 
