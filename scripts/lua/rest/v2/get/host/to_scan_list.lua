c632e13f093c17416c750923a1df46ca623c24a0
Nicolo Maio <maio@ntop.org>
2023-08-09 14:28:33 +0200

    Add search map. (#7749)

--- scripts/lua/rest/v2/get/host/to_scan_list.lua
+++ scripts/lua/rest/v2/get/host/to_scan_list.lua
@@ -8,14 +8,23 @@ package.path = dirs.installdir .. "/scripts/lua/modules/vulnerability_scan/?.lua
 require "lua_utils"
 local rest_utils = require "rest_utils"
 local vs_utils = require "vs_utils"
+local search_map = _GET["map_search"]
 
 local function format_result(result) 
     local rsp = {}
     if result then
         for _,value in ipairs(result) do
-            rsp[#rsp+1] = value
-            rsp[#rsp].num_vulnerabilities_found = format_high_num_value_for_tables(value, "num_vulnerabilities_found")
-            rsp[#rsp].num_open_ports = format_high_num_value_for_tables(value, "num_open_ports")
+            if (isEmptyString(search_map)) then
+                rsp[#rsp+1] = value
+                rsp[#rsp].num_vulnerabilities_found = format_high_num_value_for_tables(value, "num_vulnerabilities_found")
+                rsp[#rsp].num_open_ports = format_high_num_value_for_tables(value, "num_open_ports")
+            else 
+                if (value.host == search_map) then
+                    rsp[#rsp+1] = value
+                    rsp[#rsp].num_vulnerabilities_found = format_high_num_value_for_tables(value, "num_vulnerabilities_found")
+                    rsp[#rsp].num_open_ports = format_high_num_value_for_tables(value, "num_open_ports")
+                end
+            end
         end
     end
     return rsp 
