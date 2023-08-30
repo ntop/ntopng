921286bec2b46caccb61f1c7dd382d87c9ef9ac2
Luca Deri <deri@ntop.org>
2023-08-30 15:41:07 +0200

    Rename

--- scripts/callbacks/hourly/system/vulnsca_timeseries.lua
+++ scripts/callbacks/hourly/system/vulnsca_timeseries.lua
@@ -17,10 +17,12 @@ local ts_utils = require "ts_utils"
 local when = os.time()
 
 -- TODO
-local num_hosts      = 1
-local num_open_ports = 1
-local num_cve        = 1
+local num_scans      = 1 -- counter
+local num_hosts      = 1 -- gauge
+local num_open_ports = 1 -- gauge
+local num_cve        = 1 -- gauge
 
+ts_utils.append("am_vuln_scan:num_scans",            { ifid = -1, num_hosts = num_scans }, when)
 ts_utils.append("am_vuln_scan:num_configured_hosts", { ifid = -1, num_hosts = num_hosts }, when)
 ts_utils.append("am_vuln_scan:num_open_ports",       { ifid = -1, num_open_ports = num_open_ports }, when)
 ts_utils.append("am_vuln_scan:num_cve",              { ifid = -1, num_cve = num_cve }, when)
