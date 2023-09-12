6fa18515d0d4f06b0afcf268cf39c7277792d0dd
Nicolo Maio <maio@ntop.org>
2023-09-12 12:48:01 +0200

    Fix resolve hostname

--- scripts/lua/rest/v2/get/dns/resolve.lua
+++ scripts/lua/rest/v2/get/dns/resolve.lua
@@ -33,6 +33,13 @@ if string.find(hostname, '://') then
    end
 end
 
+if string.find(hostname, '/') then
+   -- Looks like an URI, strip the scheme
+   local uri_info = string.split(hostname, "/")
+   hostname = uri_info[1]
+   
+end
+
 if string.find(hostname, ':') then
    local host_port_info = string.split(hostname, ":")
    if #host_port_info == 2 then
