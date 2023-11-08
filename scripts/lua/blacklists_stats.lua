3237c145dc1045e929bafa99b7646cb7ce109a68
Matteo Biscosi <biscosi@ntop.org>
2023-11-08 17:54:41 +0000

    Included footer.lua

--- scripts/lua/blacklists_stats.lua
+++ scripts/lua/blacklists_stats.lua
@@ -3,14 +3,9 @@
 --
 local dirs = ntop.getDirs()
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
-if((dirs.scriptdir ~= nil) and (dirs.scriptdir ~= "")) then package.path = dirs.scriptdir .. "/lua/modules/?.lua;" .. package.path end
 
 require "lua_utils"
 local page_utils = require("page_utils")
-local script_manager = require("script_manager")
-local internals_utils = require "internals_utils"
-local cpu_utils = require("cpu_utils")
-local ts_utils = require "ts_utils"
 local graph_utils = require("graph_utils")
 
 sendHTTPContentTypeHeader('text/html')
@@ -19,3 +14,5 @@ page_utils.print_header_and_set_active_menu_entry(page_utils.menu_entries.system
 dofile(dirs.installdir .. "/scripts/lua/inc/menu.lua")
 
 graph_utils.drawNewGraphs({ ifid = interface.getId()})
+
+dofile(dirs.installdir .. "/scripts/lua/inc/footer.lua")
