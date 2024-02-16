aced644b5ee2e9933f3c08a0bcf092e85c62169e
Nicolo Maio <maio@ntop.org>
2024-02-16 16:05:09 +0100

    Fix cpu states percentages missing.

--- scripts/callbacks/5second/system/timeseries.lua
+++ scripts/callbacks/5second/system/timeseries.lua
@@ -10,6 +10,7 @@ package.path = dirs.installdir .. "/scripts/lua/modules/timeseries/schemas/?.lua
 
 local ts_utils = require "ts_utils_core"
 local ts_dump = require "ts_5sec_dump_utils"
+local cpu_utils = require "cpu_utils"
 
 -- ########################################################
 
@@ -26,6 +27,7 @@ for i = 1, num_runs do
     end
 
     local when = os.time()
+    cpu_utils.compute_cpu_states()
 
     -- Update CPU load timeseries
     if (ntop.getPref("ntopng.prefs.system_probes_timeseries") ~= "0") then
