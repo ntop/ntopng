ntop/ntopng

commit 9165d05e40fc2749d07051fd6af8ce2bda5aa883

Add JA4C

include/Host.h
@@ -37,6 +37,7 @@ class Host : public GenericHashEntry,
 
   struct {
     Fingerprint ja3;
+    Fingerprint ja4;
     Fingerprint hassh;
   } fingerprints;
 
@@ -799,6 +800,8 @@ class Host : public GenericHashEntry,
   void setServerName(const char *n);
   void setResolvedName(const char *resolved_name);
   inline Fingerprint *getJA3Fingerprint() { return (&fingerprints.ja3); }
+  inline Fingerprint *getJA4Fingerprint() { return (&fingerprints.ja4); }
+
   inline Fingerprint *getHASSHFingerprint() { return (&fingerprints.hassh); }
 
   void setPrefsChanged() { prefs_loaded = 0; }

src/Flow.cpp
@@ -473,6 +473,7 @@ Flow::~Flow() {
       free(protos.tls.client_requested_server_name);
     if (protos.tls.server_names) free(protos.tls.server_names);
     if (protos.tls.ja3.client_hash) free(protos.tls.ja3.client_hash);
+    if (protos.tls.ja4.client_hash) free(protos.tls.ja4.client_hash);
     if (protos.tls.ja3.server_hash) free(protos.tls.ja3.server_hash);
     if (protos.tls.client_alpn) free(protos.tls.client_alpn);
     if (protos.tls.client_tls_supported_versions)
@@ -747,6 +748,13 @@ void Flow::processExtraDissectedInformation() {
           protos.tls.ja3.client_hash =
               strdup(ndpiFlow->protos.tls_quic.ja3_client);
           updateCliJA3();
+        }
+
+        if ((protos.tls.ja4.client_hash == NULL) &&
+            (ndpiFlow->protos.tls_quic.ja4_client[0] != '\0')) {
+          protos.tls.ja4.client_hash =
+              strdup(ndpiFlow->protos.tls_quic.ja4_client);
+          updateCliJA4();
         }
 
         if ((protos.tls.ja3.server_hash == NULL) &&
@@ -3778,6 +3786,12 @@ void Flow::formatSyslogFlow(json_object *my_object) {
         Utils::jsonLabel(JA3S_HASH, "JA3S_HASH", jsonbuf, sizeof(jsonbuf)),
         json_object_new_string(protos.tls.ja3.server_hash));
 
+  if (isTLS() && protos.tls.ja4.client_hash)
+    json_object_object_add(
+        my_object,
+        Utils::jsonLabel(JA4C_HASH, "JA4C_HASH", jsonbuf, sizeof(jsonbuf)),
+        json_object_new_string(protos.tls.ja4.client_hash));
+
   if (isSSH() && protos.ssh.hassh.client_hash)
     json_object_object_add(
         my_object,
@@ -4407,6 +4421,10 @@ void Flow::alert2JSON(FlowAlert *alert, ndpi_serializer *s) {
   if (protos.tls.ja3.server_hash)
     ndpi_serialize_string_string(s, "ja3_server_hash",
                                  protos.tls.ja3.server_hash);
+
+  if (protos.tls.ja4.client_hash)
+    ndpi_serialize_string_string(s, "ja4_client_hash",
+                                 protos.tls.ja4.client_hash);
 
   if (getErrorCode() != 0)
     ndpi_serialize_string_uint32(s, "l7_error_code", getErrorCode());
@@ -6683,6 +6701,7 @@ void Flow::setParsedeBPFInfo(const ParsedeBPF *const _ebpf,
 
   updateCliJA3();
   updateSrvJA3();
+  updateCliJA4();
   updateHASSH(true /* AS client */);
   updateHASSH(false /* AS server */);
 }
@@ -6693,6 +6712,15 @@ void Flow::updateCliJA3() {
   if (cli_host && isTLS() && protos.tls.ja3.client_hash) {
     cli_host->getJA3Fingerprint()->update(
         protos.tls.ja3.client_hash,
+        ebpf ? ebpf->src_process_info.process_name : NULL,
+        has_malicious_cli_signature);
+  }
+}
+
+void Flow::updateCliJA4() {
+  if (cli_host && isTLS() && protos.tls.ja4.client_hash) {
+    cli_host->getJA4Fingerprint()->update(
+        protos.tls.ja4.client_hash,
         ebpf ? ebpf->src_process_info.process_name : NULL,
         has_malicious_cli_signature);
   }
@@ -7340,6 +7368,14 @@ void Flow::lua_get_tls_info(lua_State *vm) const {
       if (has_malicious_srv_signature)
         lua_push_bool_table_entry(vm, "protos.tls.ja3.server_malicious", true);
     }
+
+    if (protos.tls.ja4.client_hash) {
+      lua_push_str_table_entry(vm, "protos.tls.ja4.client_hash",
+                               protos.tls.ja4.client_hash);
+
+      if (has_malicious_cli_signature)
+        lua_push_bool_table_entry(vm, "protos.tls.ja4.client_malicious", true);
+    }
   }
 }
 
@@ -7399,7 +7435,18 @@ void Flow::getTLSInfo(ndpi_serializer *serializer) const {
       if (has_malicious_srv_signature)
         ndpi_serialize_string_boolean(serializer, "ja3.server_malicious", true);
     }
+
+    if (protos.tls.ja4.client_hash) {
+      ndpi_serialize_string_string(serializer, "ja4.client_hash",
+                                   protos.tls.ja4.client_hash);
+
+      if (has_malicious_cli_signature)
+        ndpi_serialize_string_boolean(serializer, "ja4.client_malicious", true);
+    }
   }
+
+  
+
 }
 
 /* ***************************************************** */

include/Flow.h
@@ -200,6 +200,10 @@ class Flow : public GenericHashEntry {
         u_int16_t server_cipher;
         ndpi_cipher_weakness server_unsafe_cipher;
       } ja3;
+
+      struct {
+        char *client_hash;
+      } ja4;
     } tls;
 
     struct {
@@ -317,6 +321,7 @@ class Flow : public GenericHashEntry {
    */
   void dumpCheck(time_t t, bool last_dump_before_free);
   void updateCliJA3();
+  void updateCliJA4();
   void updateSrvJA3();
   void updateHASSH(bool as_client);
   void processExtraDissectedInformation();
@@ -436,6 +441,7 @@ class Flow : public GenericHashEntry {
   void getProtocolJSONInfo(ndpi_serializer *serializer);
 
   inline char *getJa3CliHash() { return (protos.tls.ja3.client_hash); }
+  inline char *getJa4CliHash() { return (protos.tls.ja4.client_hash); }
 
   bool isBlacklistedFlow() const;
   bool isBlacklistedClient() const;
@@ -562,6 +568,12 @@ class Flow : public GenericHashEntry {
     if (j && (j[0] != '\0') && (protos.tls.ja3.server_hash == NULL))
       protos.tls.ja3.server_hash = strdup(j);
     updateSrvJA3();
+  }
+
+  inline void updateJA4C(char *j) {
+    if (j && (j[0] != '\0') && (protos.tls.ja4.client_hash == NULL))
+      protos.tls.ja4.client_hash = strdup(j);
+    updateCliJA4();
   }
 
   inline u_int8_t getTcpFlags() const {

include/ParsedFlow.h
@@ -35,7 +35,8 @@ class ParsedFlow : public ParsedFlowCore, public ParsedeBPF {
   char *dns_query;
   char *end_reason;
   char *tls_server_name, *bittorrent_hash;
-  char *ja3c_hash, *ja3s_hash, *flow_risk_info;
+  char *ja3c_hash, *ja3s_hash, *ja4c_hash;
+  char *flow_risk_info;
   char *external_alert;
   char *smtp_rcp_to, *smtp_mail_from;
   u_int8_t tls_unsafe_cipher, flow_verdict;
@@ -101,6 +102,7 @@ class ParsedFlow : public ParsedFlowCore, public ParsedeBPF {
   inline void setBittorrentHash(const char *str) { if(bittorrent_hash != NULL) free(bittorrent_hash);  if(str) { bittorrent_hash = strdup(str);} else bittorrent_hash = NULL; }
   inline void setJA3cHash(const char *str) { if(ja3c_hash != NULL) free(ja3c_hash);  if(str) { ja3c_hash = strdup(str); } else ja3c_hash = NULL; }
   inline void setJA3sHash(const char *str) { if(ja3s_hash != NULL) free(ja3s_hash);  if(str) { ja3s_hash = strdup(str); } else ja3s_hash = NULL; }
+  inline void setJA4cHash(const char *str) { if(ja4c_hash != NULL) free(ja4c_hash);  if(str) { ja4c_hash = strdup(str); } else ja4c_hash = NULL; }
   inline void setRiskInfo(const char *str) { if(flow_risk_info != NULL) free(flow_risk_info);  if(str) { flow_risk_info = strdup(str); } else flow_risk_info = NULL; }
   inline void setExternalAlert(const char *str) { if(external_alert != NULL) free(external_alert);  if(str) { external_alert = strdup(str);} else external_alert = NULL; }
   inline void setTLSUnsafeCipher(u_int8_t v) { tls_unsafe_cipher = v; }
@@ -129,6 +131,7 @@ class ParsedFlow : public ParsedFlowCore, public ParsedeBPF {
   inline char* getBittorrentHash(bool setToNULL = false) { char *r = bittorrent_hash; if(setToNULL) bittorrent_hash = NULL; return(r); }
   inline char* getJA3cHash(bool setToNULL = false) { char *r = ja3c_hash; if(setToNULL) ja3c_hash = NULL; return(r); }
   inline char* getJA3sHash(bool setToNULL = false) { char *r = ja3s_hash; if(setToNULL) ja3s_hash = NULL; return(r); }
+  inline char* getJA4cHash(bool setToNULL = false) { char *r = ja4c_hash; if(setToNULL) ja4c_hash = NULL; return(r); }
   inline char* getRiskInfo(bool setToNULL = false) { char *r = flow_risk_info; if(setToNULL) flow_risk_info  = NULL; return(r); }
   inline char* getExternalAlert(bool setToNULL = false) { char *r = external_alert; if(setToNULL) external_alert = NULL; return(r); }
   inline char* getEndReason(bool setToNull = false) { char *r = end_reason; if(setToNull) end_reason = NULL; return(r); }

include/ntop_flow.h
@@ -248,6 +248,7 @@
 #define FLOW_VERDICT NTOP_BASE_ID + 522
 #define L7_ERROR_CODE NTOP_BASE_ID + 554
 #define L7_RISK_INFO NTOP_BASE_ID + 555
+#define JA4C_HASH NTOP_BASE_ID + 576
 #define FLOW_SOURCE  NTOP_BASE_ID+579 /* 0 = packets, 1 = collected NetFlow/IPFIX, 2 = collected sFlow/nfLite */
 
 /* eBPF / Process */

scripts/locales/en.lua
@@ -232,7 +232,7 @@ local lang = {
   ["expired_csrf"] = "Could not perform the requested action because the page expired. Please reload the page and perform the action again.",
   ["export"] = "Export",
   ["external_link"] = "External Link",
-  ["external_link_url"] = "<span><button data-to-copy='%{proto}://%{url}' class='copy-http-url btn btn-light btn-sm border ms-1' style='cursor: pointer;'><i class='fas fa-copy'></i></button>  <a class='ntopng-external-link' href='%{proto}://%{url}' target='_blank'><i class='fas fa-external-link-alt'></i> %{url_name}</a></span>",
+  ["external_link_url"] = "<span><button data-to-copy='%{proto}://%{url}' class='copy-http-url btn btn-light btn-sm border ms-1' style='cursor: pointer;'><i class='fas fa-copy'></i></button>  <a class='ntopng-external-lin' style='width:260px' href='%{proto}://%{url}' target='_blank'><i class='fas fa-external-link-alt'></i> %{url_name}</a></span>",
   ["factory_reset"] = "Factory Reset",
   ["file"] = "File",
   ["filter"] = "Exclude",

scripts/lua/flow_details.lua
@@ -213,6 +213,20 @@ local function ja3url(what, safety, label)
     end
 end
 
+local function ja4url(what, safety, label)
+    if (what == nil) then
+        print("&nbsp;")
+    else
+        print(format_external_link("sslbl.abuse.ch/ja3-fingerprints/" .. what .. "/", what, false, "https"))
+
+        if ((safety ~= nil) and (safety ~= "safe")) then
+            print(
+                ' [ <i class="fas fa-exclamation-triangle" aria-hidden=true style="color: orange;"></i> <A HREF=https://en.wikipedia.org/wiki/Cipher_suite>' ..
+                    capitalize(safety) .. ' Cipher</A> ]')
+        end
+    end
+end
+
 sendHTTPContentTypeHeader('text/html')
 
 warn_shown = 0
@@ -1162,6 +1176,17 @@ else
         -- print(tls_consts.cipher2str(flow["protos.tls.ja3.server_cipher"]))
         print("</td></tr>")
     end
+
+    if ((flow["protos.tls.ja4.client_hash"] ~= nil)) then
+        print('<tr><th width=30%><A HREF="https://github.com/salesforce/ja3">JA4C</A></th><td>')
+        if (flow["protos.tls.ja4.client_malicious"]) then
+            print('<font color=red><i class="fas fa-ban" title="' ..
+                      i18n("alerts_dashboard.malicious_signature_detected") .. '"></i></font> ')
+        end
+
+        ja4url(flow["protos.tls.ja4.client_hash"], nil, 'ja4c')
+        print("</td></tr>")
+    end 
 
     if (flow["protos.tls.client_alpn"] ~= nil) then
         print(

src/ParserInterface.cpp
@@ -581,6 +581,7 @@ bool ParserInterface::processFlow(ParsedFlow *zflow) {
 
     if (zflow->getJA3cHash()) flow->updateJA3C(zflow->getJA3cHash());
     if (zflow->getJA3sHash()) flow->updateJA3S(zflow->getJA3sHash());
+    if (zflow->getJA4cHash()) flow->updateJA4C(zflow->getJA4cHash());
 
     if (zflow->getRiskInfo()) {
       json_object *o, *obj;

commit c0dc66e2a349ff0ec09da200560962365bdac2a2

Fixed crash and memory leak introduced by https://github.com/ntop/ntopng/commit/9165d05e40fc2749d07051fd6af8ce2bda5aa883#diff-4a9770ab104dff163033f079dc1863dc13faa9630353c9efe071510cf5a4e39e

include/Host.h
@@ -799,9 +799,8 @@ class Host : public GenericHashEntry,
   void offlineSetDHCPName(const char *n);
   void setServerName(const char *n);
   void setResolvedName(const char *resolved_name);
-  inline Fingerprint *getJA3Fingerprint() { return (&fingerprints.ja3); }
-  inline Fingerprint *getJA4Fingerprint() { return (&fingerprints.ja4); }
-
+  inline Fingerprint *getJA3Fingerprint()   { return (&fingerprints.ja3);   }
+  inline Fingerprint *getJA4Fingerprint()   { return (&fingerprints.ja4);   }
   inline Fingerprint *getHASSHFingerprint() { return (&fingerprints.hassh); }
 
   void setPrefsChanged() { prefs_loaded = 0; }

src/Flow.cpp
@@ -752,19 +752,15 @@ void Flow::processExtraDissectedInformation() {
 
         if ((protos.tls.ja4.client_hash == NULL) &&
             (ndpiFlow->protos.tls_quic.ja4_client[0] != '\0')) {
-          protos.tls.ja4.client_hash =
-              strdup(ndpiFlow->protos.tls_quic.ja4_client);
+          protos.tls.ja4.client_hash = strdup(ndpiFlow->protos.tls_quic.ja4_client);
           updateCliJA4();
         }
 
         if ((protos.tls.ja3.server_hash == NULL) &&
             (ndpiFlow->protos.tls_quic.ja3_server[0] != '\0')) {
-          protos.tls.ja3.server_hash =
-              strdup(ndpiFlow->protos.tls_quic.ja3_server);
-          protos.tls.ja3.server_unsafe_cipher =
-              ndpiFlow->protos.tls_quic.server_unsafe_cipher;
-          protos.tls.ja3.server_cipher =
-              ndpiFlow->protos.tls_quic.server_cipher;
+          protos.tls.ja3.server_hash = strdup(ndpiFlow->protos.tls_quic.ja3_server);
+          protos.tls.ja3.server_unsafe_cipher = ndpiFlow->protos.tls_quic.server_unsafe_cipher;
+          protos.tls.ja3.server_cipher = ndpiFlow->protos.tls_quic.server_cipher;
           updateSrvJA3();
         }
         break;
@@ -6710,19 +6706,19 @@ void Flow::setParsedeBPFInfo(const ParsedeBPF *const _ebpf,
 
 void Flow::updateCliJA3() {
   if (cli_host && isTLS() && protos.tls.ja3.client_hash) {
-    cli_host->getJA3Fingerprint()->update(
-        protos.tls.ja3.client_hash,
-        ebpf ? ebpf->src_process_info.process_name : NULL,
-        has_malicious_cli_signature);
+    cli_host->getJA3Fingerprint()->update(protos.tls.ja3.client_hash,
+					  ebpf ? ebpf->src_process_info.process_name : NULL,
+					  has_malicious_cli_signature);
   }
 }
 
+/* ***************************************************** */
+
 void Flow::updateCliJA4() {
   if (cli_host && isTLS() && protos.tls.ja4.client_hash) {
-    cli_host->getJA4Fingerprint()->update(
-        protos.tls.ja4.client_hash,
-        ebpf ? ebpf->src_process_info.process_name : NULL,
-        has_malicious_cli_signature);
+    cli_host->getJA4Fingerprint()->update(protos.tls.ja4.client_hash,
+					  ebpf ? ebpf->src_process_info.process_name : NULL,
+					  has_malicious_cli_signature);
   }
 }
 

src/ParsedFlow.cpp
@@ -34,7 +34,7 @@ ParsedFlow::ParsedFlow() : ParsedFlowCore(), ParsedeBPF() {
   http_method = NDPI_HTTP_METHOD_UNKNOWN;
   dns_query = tls_server_name = end_reason = NULL;
   smtp_mail_from = smtp_rcp_to = NULL;
-  ja3c_hash = ja3s_hash = NULL;
+  ja3c_hash = ja3s_hash = ja4c_hash = NULL;
   external_alert = NULL;
   flow_risk_info = NULL;
   tls_cipher = tls_unsafe_cipher = http_ret_code = 0;
@@ -67,65 +67,84 @@ ParsedFlow::ParsedFlow(const ParsedFlow &pf)
     l7_info = strdup(pf.l7_info);
   else
     l7_info = NULL;
+
   if (pf.http_url)
     http_url = strdup(pf.http_url);
   else
     http_url = NULL;
+  
   if (pf.http_site)
     http_site = strdup(pf.http_site);
   else
     http_site = NULL;
+
   if (pf.http_user_agent)
     http_user_agent = strdup(pf.http_user_agent);
   else
     http_user_agent = NULL;
+
   http_method = pf.http_method;
+
   if (pf.dns_query)
     dns_query = strdup(pf.dns_query);
   else
     dns_query = NULL;
+
   if(pf.end_reason)
     end_reason = strdup(pf.end_reason);
   else
     end_reason = NULL;
+  
   if (pf.tls_server_name)
     tls_server_name = strdup(pf.tls_server_name);
   else
     tls_server_name = NULL;
+  
   if (pf.bittorrent_hash)
     bittorrent_hash = strdup(pf.bittorrent_hash);
   else
     bittorrent_hash = NULL;
+  
   if (pf.ja3c_hash)
     ja3c_hash = strdup(pf.ja3c_hash);
   else
     ja3c_hash = NULL;
+  
   if (pf.ja3s_hash)
     ja3s_hash = strdup(pf.ja3s_hash);
   else
     ja3s_hash = NULL;
+  
+  if (pf.ja4c_hash)
+    ja4c_hash = strdup(pf.ja4c_hash);
+  else
+    ja4c_hash = NULL;
+  
   if (pf.external_alert)
     external_alert = strdup(pf.external_alert);
   else
     external_alert = NULL;
+
   if (pf.flow_risk_info)
     flow_risk_info = strdup(pf.flow_risk_info);
   else
     flow_risk_info = NULL;
+
   if (pf.ndpi_flow_risk_name)
     ndpi_flow_risk_name = strdup(pf.ndpi_flow_risk_name);
   else
     ndpi_flow_risk_name = NULL;
+
   if (pf.smtp_mail_from)
     smtp_mail_from = strdup(pf.smtp_mail_from);
   else
     smtp_mail_from = NULL;
+
   if (pf.smtp_rcp_to)
     smtp_rcp_to = strdup(pf.smtp_rcp_to);
   else
     smtp_rcp_to = NULL;
   
-
   tls_cipher = pf.tls_cipher;
   tls_unsafe_cipher = pf.tls_unsafe_cipher;
   ndpi_flow_risk_bitmap = pf.ndpi_flow_risk_bitmap;
@@ -179,6 +198,9 @@ void ParsedFlow::fromLua(lua_State *L, int index) {
         } else if (!strcmp(key, "ja3s_hash")) {
           if (ja3s_hash) free(ja3s_hash);
           ja3s_hash = strdup(lua_tostring(L, -1));
+        } else if (!strcmp(key, "ja4c_hash")) {
+          if (ja4c_hash) free(ja4c_hash);
+          ja4c_hash = strdup(lua_tostring(L, -1));
         } else if (!strcmp(key, "external_alert")) {
           if (external_alert) free(external_alert);
           external_alert = strdup(lua_tostring(L, -1));
@@ -283,6 +305,7 @@ void ParsedFlow::freeMemory() {
   if (bittorrent_hash)      { free(bittorrent_hash); bittorrent_hash = NULL; }
   if (ja3c_hash)            { free(ja3c_hash); ja3c_hash = NULL; }
   if (ja3s_hash)            { free(ja3s_hash); ja3s_hash = NULL; }
+  if (ja4c_hash)            { free(ja4c_hash); ja4c_hash = NULL; }
   if (external_alert)       { free(external_alert); external_alert = NULL; }
   if (flow_risk_info)       { free(flow_risk_info); flow_risk_info = NULL; }
   if (ndpi_flow_risk_name)  { free(ndpi_flow_risk_name); ndpi_flow_risk_name = NULL; }