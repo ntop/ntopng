57eb0c7207e570d3c8355b8930fbfe11f4f09532
Matteo Biscosi <biscosi@ntop.org>
2024-05-03 09:29:30 -0400

    Started fixing issue #8325

--- scripts/lua/modules/alert_definitions/flow/alert_ndpi_suspicious_dga_domain.lua
+++ scripts/lua/modules/alert_definitions/flow/alert_ndpi_suspicious_dga_domain.lua
@@ -1,9 +1,7 @@
 --
 -- (C) 2019-24 - ntop.org
 --
-
 -- ##############################################
-
 local flow_alert_keys = require "flow_alert_keys"
 -- Import the classes library.
 local classes = require "classes"
@@ -17,16 +15,16 @@ local alert_ndpi_suspicious_dga_domain = classes.class(alert)
 -- ##############################################
 
 alert_ndpi_suspicious_dga_domain.meta = {
-   alert_key  = flow_alert_keys.flow_alert_ndpi_suspicious_dga_domain,
-   i18n_title = "alerts_dashboard.ndpi_suspicious_dga_domain_title",
-   icon = "fas fa-fw fa-exclamation",
+    alert_key = flow_alert_keys.flow_alert_ndpi_suspicious_dga_domain,
+    i18n_title = "alerts_dashboard.ndpi_suspicious_dga_domain_title",
+    icon = "fas fa-fw fa-exclamation",
 
-   -- A compromised host can do DGA domain requests. A compromised host can be:
-   --  1. 'victim' as it is compromised
-   --  2. 'attacker' as it can do malicious activities due to the fact that it has been compromised
-   -- Since 'attacker' implies 'victim' in this case, the alert is assumed to have the 'attacker'.
-   -- The DNS server is not assumed to be the victim as it justs serves the request.
-   has_attacker = true,
+    -- A compromised host can do DGA domain requests. A compromised host can be:
+    --  1. 'victim' as it is compromised
+    --  2. 'attacker' as it can do malicious activities due to the fact that it has been compromised
+    -- Since 'attacker' implies 'victim' in this case, the alert is assumed to have the 'attacker'.
+    -- The DNS server is not assumed to be the victim as it justs serves the request.
+    has_attacker = true
 }
 
 -- ##############################################
@@ -34,35 +32,44 @@ alert_ndpi_suspicious_dga_domain.meta = {
 -- @brief Prepare an alert table used to generate the alert
 -- @return A table with the alert built
 function alert_ndpi_suspicious_dga_domain:init()
-   -- Call the parent constructor
-   self.super:init()
+    -- Call the parent constructor
+    self.super:init()
 end
 
 -- #######################################################
 
 function alert_ndpi_suspicious_dga_domain.format(ifid, alert, alert_type_params)
-   local domain = alert_type_params.dga_domain
-   local href = ''
-   local info = ''
+    local domain = alert_type_params.dga_domain
+    local href = ''
+    local info = ''
 
-   if not isEmptyString(domain) then
-    -- URL check
-    local url = alert_type_params.dga_domain
-    if string.find(url, 'https') then
-      url = url:gsub('://', '')
-      url = url:gsub('https', '')
+    if not isEmptyString(domain) then
+        -- URL check
+        local url = alert_type_params.dga_domain
+        if string.find(url, 'https') then
+            url = url:gsub('://', '')
+            url = url:gsub('https', '')
+        end
+
+        local proto = string.lower(interface.getnDPIProtoName(tonumber(alert["l7_master_proto"])))
+        proto = ternary(((proto) and (proto == 'http')), 'http', 'https')
+        href = url
     end
 
-    local proto = string.lower(interface.getnDPIProtoName(tonumber(alert["l7_master_proto"])))
-    proto = ternary(((proto) and (proto == 'http')), 'http', 'https')
-    href = format_external_link(url, url, false, proto)
-   end
+    if alert_type_params.proto and alert_type_params.proto.http then
+        return i18n("alert_messages.suspicious_dga_domain_http", {
+            domain = domain,
+            href = href,
+            info = info
+        })
+    else
+        return i18n("alert_messages.suspicious_dga_domain_other", {
+            domain = domain,
+            href = href,
+            info = info
+        })
+    end
 
-   return i18n("alert_messages.suspicious_dga_domain", {
-	   domain = domain,
-      href = href,
-      info = info,
-   })
 end
 
 -- #######################################################
