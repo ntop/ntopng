48b9637522ad3a3f3654c0be35077860469f494a
Matteo Biscosi <biscosi@ntop.org>
2024-05-06 06:34:24 -0400

    Formatted table-with-config

--- http_src/vue/table-with-config.vue
+++ http_src/vue/table-with-config.vue
@@ -1,36 +1,22 @@
 <!-- (C) 2022 - ntop.org     -->
 <template>
-  <Table v-if="mount_table" ref="table"
-	 :id="table_id_2"
-	 :columns="table_config.columns"
-         :get_rows="table_config.get_rows"
-         :get_column_id="table_config.get_column_id"
-         :print_column_name="table_config.print_column_name"
-	 :print_html_row="table_config.print_html_row"
-	 :print_vue_node_row="table_config.print_vue_node_row"
-	 :f_is_column_sortable="table_config.f_is_column_sortable"
-	 :f_column_min_width="table_config.f_column_min_width"
-	 :f_get_column_classes="table_config.f_get_column_classes"
-	 :f_get_column_style="table_config.f_get_column_style"
-	 :display_empty_rows="table_config.display_empty_rows"
-	 :f_sort_rows="f_sort_rows"
-	 :enable_search="table_config.enable_search"
-	 :default_sort="table_config.default_sort"
-	 :show_autorefresh="table_config.show_autorefresh"
-	 :paging="table_config.paging"
-	 :csrf="csrf"
-     :display_message="display_message"
-     :message_to_display="message_to_display"
-	 @loaded="on_loaded"
-     @rows_loaded="rows_loaded"
-	 @custom_event="on_custom_event">
-    <template v-slot:custom_header>
-      <slot name="custom_header"></slot>
-    </template>
-    <template v-slot:custom_buttons>
-      <slot name="custom_buttons"></slot>
-    </template>
-  </Table>
+    <Table v-if="mount_table" ref="table" :id="table_id_2" :columns="table_config.columns"
+        :get_rows="table_config.get_rows" :get_column_id="table_config.get_column_id"
+        :print_column_name="table_config.print_column_name" :print_html_row="table_config.print_html_row"
+        :print_vue_node_row="table_config.print_vue_node_row" :f_is_column_sortable="table_config.f_is_column_sortable"
+        :f_column_min_width="table_config.f_column_min_width" :f_get_column_classes="table_config.f_get_column_classes"
+        :f_get_column_style="table_config.f_get_column_style" :display_empty_rows="table_config.display_empty_rows"
+        :f_sort_rows="f_sort_rows" :enable_search="table_config.enable_search" :default_sort="table_config.default_sort"
+        :show_autorefresh="table_config.show_autorefresh" :paging="table_config.paging" :csrf="csrf"
+        :display_message="display_message" :message_to_display="message_to_display" @loaded="on_loaded"
+        @rows_loaded="rows_loaded" @custom_event="on_custom_event">
+        <template v-slot:custom_header>
+            <slot name="custom_header"></slot>
+        </template>
+        <template v-slot:custom_buttons>
+            <slot name="custom_buttons"></slot>
+        </template>
+    </Table>
 </template>
 
 <script setup>
@@ -57,13 +43,13 @@ const mount_table = ref(false);
 
 onMounted(async () => {
     if (props.table_id != null || props.table_config_id != null) {
-	load_table();
+        load_table();
     }
 });
 
 watch(() => [props.table_id, props.table_config_id], (cur_value, old_value) => {
     load_table();
-}, { flush: 'pre'});
+}, { flush: 'pre' });
 
 const table_id_2 = computed(() => {
     if (props.table_id != null) { return props.table_id; }
@@ -75,11 +61,11 @@ async function load_table() {
     await nextTick();
     let table_config_id_2 = props.table_config_id;
     if (table_config_id_2 == null) {
-	table_config_id_2 = props.table_id;
+        table_config_id_2 = props.table_id;
     }
     table_config.value = await TableUtils.build_table(http_prefix, table_config_id_2, props.f_map_columns, props.get_extra_params_obj);
     if (props.f_map_config != null) {
-	table_config.value = props.f_map_config(table_config.value);
+        table_config.value = props.f_map_config(table_config.value);
     }
     mount_table.value = true;
     await nextTick();
@@ -98,7 +84,7 @@ function rows_loaded(res) {
 }
 
 const refresh_table = (disable_loading) => {
-    if(table.value) {
+    if (table.value) {
         table.value.refresh_table(disable_loading);
     }
 }
@@ -120,5 +106,4 @@ defineExpose({ refresh_table, get_columns_defs, get_rows_num, search_value });
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
