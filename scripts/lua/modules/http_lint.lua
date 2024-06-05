26c1823750669378db81443dc4910a482c7b4271
Matteo Biscosi <biscosi@ntop.org>
2024-06-05 11:34:45 -0400

    Silenced useless lint message

--- scripts/lua/modules/http_lint.lua
+++ scripts/lua/modules/http_lint.lua
@@ -2803,7 +2803,7 @@ local function validateParameter(k, v)
 	    -- Stop, if any of the table value fails the validation against
 	    -- the expected table key
 	    if not success then
-	       if (trace_failures) then
+	       if (trace_failures and table_key ~= 'ifid') then
 		  io.write("[LINT] validateParameter failed for table entry [" .. table_key .. "][".. table_value .."]\n")
 	       end
 
