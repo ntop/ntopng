89556f2d3b59f6acf42770964d28aa4aa34c4ae6
Luca Deri <deri@ntop.org>
2024-06-08 08:30:59 +0200

    Backport of https://github.com/ntop/ntopng/pull/8442

--- src/LuaEngineNtop.cpp
+++ src/LuaEngineNtop.cpp
@@ -6605,7 +6605,7 @@ static int ntop_rrd_tune(lua_State *vm) {
   filename = argv[1];
 
   reset_rrd_state();
-  status = rrd_tune(argc, (char **)argv);
+  status = rrd_tune(argc, (std::conditional<std::is_same<decltype(rrd_tune), int(int, const char **)>::value, const char **, char **>::type)argv);
 
   if (status != 0) {
     char *err = rrd_get_error();
