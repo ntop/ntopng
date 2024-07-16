4ecd7e8bf623e1ecdbe9229e1239ed88c66e7d07
Luca Deri <deri@ntop.org>
2024-07-16 21:30:01 +0200

    Removed trace

--- scripts/lua/modules/check_definitions/system/exporters_limit_exceeded.lua
+++ scripts/lua/modules/check_definitions/system/exporters_limit_exceeded.lua
@@ -26,7 +26,9 @@ local EXPORTERS_LIMITS_EXCEEDED_KEY = "ntopng.limits.exporters"
 local function dropped_flows_check(params)
     -- Fetch if an interface dropped some flows due to limits exceeded regarding flow exporters
     local exporters_limit_exceeded = ntop.getCache(EXPORTERS_LIMITS_EXCEEDED_KEY) or ""
-    tprint(exporters_limit_exceeded)
+
+    --tprint(exporters_limit_exceeded)
+    
     if not isEmptyString(exporters_limit_exceeded) and exporters_limit_exceeded ~= "0" then
         local alert = alert_consts.alert_types.alert_exporters_limit_exceeded.new()
         -- Remove the value to not spam the message
