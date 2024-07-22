7870f176c5f69f8213d2ced87566692f812f60ac
Alfredo Cardigliano <cardigliano@ntop.org>
2024-07-22 14:24:11 +0000

    Add example

--- src/SNMPTrap.cpp
+++ src/SNMPTrap.cpp
@@ -21,6 +21,11 @@
 
 #include "ntop_includes.h"
 
+/*
+ * Example for triggering traps for testing:
+ * snmptrap -v 2c -c public 127.0.0.1 '' NET-SNMP-EXAMPLES-MIB::netSnmpExampleHeartbeatNotification netSnmpExampleHeartbeatRate i 60
+ */
+
 #ifdef HAVE_LIBSNMP
 
 SNMPTrap::SNMPTrap() {
