a308d6c623fd84c9523f1e739df7005a473c0365
Alfredo Cardigliano <cardigliano@ntop.org>
2024-08-06 11:40:34 +0200

    Fix host custom queries

--- scripts/lua/modules/alert_store/host_alert_store.lua
+++ scripts/lua/modules/alert_store/host_alert_store.lua
@@ -373,8 +373,8 @@ function host_alert_store:format_record(value, no_html)
         shown_label = host,
         reference = reference_html,
         country = interface.getHostCountry(host),
-        is_local = alert_info.alert_generation.host_info.localhost,
-        is_multicast = alert_info.alert_generation.host_info.is_multicast
+        is_local = alert_info.alert_generation and alert_info.alert_generation.host_info.localhost,
+        is_multicast = alert_info.alert_generation and alert_info.alert_generation.host_info.is_multicast
     }
 
     -- Long, unshortened label
