53d6c7193c06c1d8f69887e3f55283f7020d4ebd
Alfredo Cardigliano <cardigliano@ntop.org>
2024-08-20 10:32:39 +0200

    Do not check for 0  in isEmptyOrNull as it may be a valid value

--- http_src/utilities/data-utils.js
+++ http_src/utilities/data-utils.js
@@ -7,9 +7,10 @@
   e.g. functions that check if a string is null or empty
  */
 
-/* This function check if value is null, empty or 0 */
+/* This function check if value is not set (null or empty).
+ * Do not check for 0 as it may be a valid value. */
 const isEmptyOrNull = (value) => {
-  return !!(value == null || value == "" || value == 0);
+  return !!(value == null || value == "");
 }
 
 /* This function check if value is null, or an empty array */
