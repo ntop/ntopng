4d3829248b358e8d90c0a4547002938ec81bde0d
GabrieleDeri <33870399+DGabri@users.noreply.github.com>
2024-08-22 16:04:38 +0200

    Added ASN name in ASN timeseries page (#8659)
    * Fixed telemetry response format, no impact on base ntopng, different endpoint
    
    * Fixed error
    
    * Added toboolean instead of string compare
    
    * Added ASN name in ASN timeseries page

--- scripts/lua/as_stats.lua
+++ scripts/lua/as_stats.lua
@@ -13,13 +13,45 @@ require "lua_utils"
 local page = _GET["page"]
 local asn = _GET["asn"]
 
+interface.select(ifname)
+
 sendHTTPContentTypeHeader('text/html')
 
 page_utils.print_header_and_set_active_menu_entry(page_utils.menu_entries.autonomous_systems)
 
 dofile(dirs.installdir .. "/scripts/lua/inc/menu.lua")
 
-page_utils.print_navbar(i18n("as_stats.autonomous_systems"), ntop.getHttpPrefix() .. "/lua/as_stats.lua", {{
+-- Get Asname
+local as_info;
+local as_name = ""
+
+asn = tonumber(asn)
+if (asn ~= nil) then
+    as_info = interface.getASesInfo({detailsLevel = "high"})
+    as_info = as_info["ASes"]
+    end
+    
+if as_info ~= nil then
+    for key, value in pairs(as_info) do
+
+        if (value["asn"] == asn) then
+        as_name = value["asname"]
+        end
+    end
+end
+
+local breadcrumb
+
+if page == "overview" or not page then
+    breadcrumb = i18n("as_stats.autonomous_systems")
+else
+    
+    local asn_string = ternary(asn == 0, tostring(asn),tostring(asn) .. " (" .. as_name .. ")")
+    
+    breadcrumb =  " ASN: " .. "<a href=".. ntop.getHttpPrefix().. "/lua/hosts_stats.lua?asn=" .. tostring(asn) .. "> " .. asn_string .. " </a>" 
+end
+
+page_utils.print_navbar(breadcrumb, ntop.getHttpPrefix() .. "/lua/as_stats.lua", {{
     active = page == "overview" or not page,
     page_name = "overview",
     label = "<i class=\"fas fa-lg fa-home\"  data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" title=\"" ..
@@ -36,6 +68,7 @@ local context = {
 }
 
 local json_context = json.encode(context)
+
 if page == "overview" or not page then
     template_utils.render("pages/vue_page.template", {
         vue_page_name = "PageAsStats",
@@ -43,7 +76,7 @@ if page == "overview" or not page then
     })
 else
     local source_value_object = {
-        asn = tonumber(asn),
+        asn = asn,
         ifid = interface.getId()
     }
     graph_utils.drawNewGraphs(source_value_object)
