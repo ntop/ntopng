0561c150d86902c743ffc40ba692edb53c0d58c2
Alfredo Cardigliano <cardigliano@ntop.org>
2024-08-22 16:58:22 +0200

    Add support for XXL product id

--- src/LuaEngineNtop.cpp
+++ src/LuaEngineNtop.cpp
@@ -3540,6 +3540,14 @@ static int ntop_is_enterprise_xl(lua_State *vm) {
 
 /* ****************************************** */
 
+static int ntop_is_enterprise_xxl(lua_State *vm) {
+  ntop->getTrace()->traceEvent(TRACE_DEBUG, "%s() called", __FUNCTION__);
+  lua_pushboolean(vm, ntop->getPrefs()->is_enterprise_xxl_edition());
+  return(ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_OK));
+}
+
+/* ****************************************** */
+
 static int ntop_is_nedge(lua_State *vm) {
   ntop->getTrace()->traceEvent(TRACE_DEBUG, "%s() called", __FUNCTION__);
   lua_pushboolean(vm, ntop->getPrefs()->is_nedge_pro_edition());
@@ -4075,6 +4083,8 @@ static int ntop_get_info(lua_State *vm) {
                               ntop->getPrefs()->is_enterprise_l_edition());
     lua_push_bool_table_entry(vm, "version.enterprise_xl_edition",
                               ntop->getPrefs()->is_enterprise_xl_edition());
+    lua_push_bool_table_entry(vm, "version.enterprise_xxl_edition",
+                              ntop->getPrefs()->is_enterprise_xxl_edition());
 
     lua_push_bool_table_entry(vm, "version.nedge_edition",
                               ntop->getPrefs()->is_nedge_pro_edition());
@@ -8000,6 +8010,7 @@ static luaL_Reg _ntop_reg[] = {
     { "isEnterpriseM", ntop_is_enterprise_m},
     { "isEnterpriseL", ntop_is_enterprise_l},
     { "isEnterpriseXL", ntop_is_enterprise_xl},
+    { "isEnterpriseXXL", ntop_is_enterprise_xxl},
     { "isnEdge", ntop_is_nedge},
     { "isnEdgeEnterprise", ntop_is_nedge_enterprise},
     { "isPackage", ntop_is_package},
