72b4eb0c75cb5c8b61a074d85044723c72f0f7d8
Matteo Biscosi <biscosi@ntop.org>
2024-09-23 16:03:24 +0200

    Fixes ack/delete alerts not working on snmp

--- http_src/vue/modal-delete-alerts.vue
+++ http_src/vue/modal-delete-alerts.vue
@@ -1,44 +1,44 @@
 <!-- (C) 2022 - ntop.org     -->
 <template>
-<modal ref="modal_id">
-  <template v-slot:title>
-    {{_i18n("delete_alerts")}}
-  </template>
-  <template v-slot:body>
-    <div class="form-group row">
-      <div class="col-sm-6">
-        <label class="col-form-label"><b>{{_i18n("date_from")}}</b></label>
+  <modal ref="modal_id">
+    <template v-slot:title>
+      {{ _i18n("delete_alerts") }}
+    </template>
+    <template v-slot:body>
+      <div class="form-group row">
+        <div class="col-sm-6">
+          <label class="col-form-label"><b>{{ _i18n("date_from") }}</b></label>
+        </div>
+        <div class="col-sm-6">
+          <input class="form-control" type="text" :value="begin_time" disabled="">
+        </div>
       </div>
-      <div class="col-sm-6">
-        <input class="form-control" type="text" :value="begin_time" disabled="">
+      <div class="form-group row">
+        <div class="col-sm-6">
+          <label class="col-form-label"><b>{{ _i18n("date_from") }}</b></label>
+        </div>
+        <div class="col-sm-6 mt-1">
+          <input class="form-control" type="text" :value="end_time" disabled="">
+        </div>
       </div>
-    </div>
-    <div class="form-group row">
-      <div class="col-sm-6">
-        <label class="col-form-label"><b>{{_i18n("date_from")}}</b></label>
-      </div>
-      <div class="col-sm-6 mt-1">
-        <input class="form-control" type="text" :value="end_time" disabled="">
-      </div>
-    </div>
-    
-    <div class="form-group filters-list mb-2">
-      <div v-for="item in filters" class="form-group row">
-	<div class="col-sm-6">
-          <label class="col-form-label"><b>{{item.label}}</b></label>
-	</div>
-	<div class="col-sm-6 mt-1">
-          <input class="form-control" type="text" :value="item.value" disabled="">
-	</div>
-      </div>
-    </div>
-    <AlertInfo :no_close_button="true" ref="alert_info"></AlertInfo>    
-  </template><!-- modal-body -->
 
-  <template v-slot:footer>
-    <button type="button" @click="delete_alerts" class="btn btn-primary">{{_i18n("delete")}}</button>
-  </template>
-</modal>
+      <div class="form-group filters-list mb-2">
+        <div v-for="item in filters" class="form-group row">
+          <div class="col-sm-6">
+            <label class="col-form-label"><b>{{ item.label }}</b></label>
+          </div>
+          <div class="col-sm-6 mt-1">
+            <input class="form-control" type="text" :value="item.value" disabled="">
+          </div>
+        </div>
+      </div>
+      <AlertInfo :no_close_button="true" ref="alert_info"></AlertInfo>
+    </template><!-- modal-body -->
+
+    <template v-slot:footer>
+      <button type="button" @click="delete_alerts" class="btn btn-primary">{{ _i18n("delete") }}</button>
+    </template>
+  </modal>
 </template>
 
 <script setup>
@@ -49,8 +49,8 @@ import { ntopng_utility, ntopng_url_manager, ntopng_events_manager, ntopng_event
 import filtersManager from "../utilities/filters-manager.js";
 
 const props = defineProps({
-    page: String,
-    context: Object,    
+  page: String,
+  context: Object,
 });
 const emit = defineEmits(["delete_alerts"]);
 
@@ -66,34 +66,38 @@ onMounted(() => {
 });
 
 const show = (status) => {
-    begin_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_begin * 1000);
-    end_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_end * 1000);
-    const tag_operator_label_dict = filtersManager.tag_operator_label_dict;
-    filters.value = status.filters.map((f) => {
-	return {
-	    label: f.label,
-	    value: `${tag_operator_label_dict[f.operator]} ${f.value_label}`,
-	};
-    });
-    let message_body = _i18n("show_alerts.confirm_delete_alerts");
-    alert_info.value.show(message_body, "alert-danger");
-    modal_id.value.show();
+  begin_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_begin * 1000);
+  end_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_end * 1000);
+  const tag_operator_label_dict = filtersManager.tag_operator_label_dict;
+  filters.value = status.filters.map((f) => {
+    return {
+      label: f.label,
+      value: `${tag_operator_label_dict[f.operator]} ${f.value_label}`,
+    };
+  });
+  let message_body = _i18n("show_alerts.confirm_delete_alerts");
+  alert_info.value.show(message_body, "alert-danger");
+  modal_id.value.show();
 };
 
 async function delete_alerts() {
-    const url = `${http_prefix}/lua/rest/v2/delete/${props.page}/alerts.lua`;
-    const url_params_obj = ntopng_url_manager.get_url_object();
-    const params = {
-	csrf: props.context.csrf,
-	...url_params_obj,
-    };
-    await ntopng_utility.http_post_request(url, params);
-    emit("delete_alerts");
-    close();
+  let url = `${http_prefix}/lua/rest/v2/delete/${props.page}/alerts.lua`;
+  /* The SNMP page is the only exception where the url totally changes */
+  if (props.page == 'snmp_device') {
+    url = `${http_prefix}/lua/pro/rest/v2/delete/snmp/device/alerts.lua`
+  }
+  const url_params_obj = ntopng_url_manager.get_url_object();
+  const params = {
+    csrf: props.context.csrf,
+    ...url_params_obj,
+  };
+  await ntopng_utility.http_post_request(url, params);
+  emit("delete_alerts");
+  close();
 }
 
 const close = () => {
-    modal_id.value.close();
+  modal_id.value.close();
 };
 
 defineExpose({ show, close });
