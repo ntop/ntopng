afff1979fa108c4ec3b6feb0a4b8d700c5f64c66
Matteo Biscosi <biscosi@ntop.org>
2024-09-23 16:01:38 +0200

    Fixes missing files to snmp delete (#8724)

--- http_src/vue/modal-acknowledge-alerts.vue
+++ http_src/vue/modal-acknowledge-alerts.vue
@@ -1,34 +1,34 @@
 <!-- (C) 2022 - ntop.org     -->
 <template>
-<modal ref="modal_id">
-  <template v-slot:title>
-    {{_i18n("acknowledge_alerts")}}
-  </template>
-  <template v-slot:body>
-    <div class="form-group row">
-      <div class="col-sm-6">
-        <label class="col-form-label"><b>{{_i18n("date_from")}}</b></label>
+  <modal ref="modal_id">
+    <template v-slot:title>
+      {{ _i18n("acknowledge_alerts") }}
+    </template>
+    <template v-slot:body>
+      <div class="form-group row">
+        <div class="col-sm-6">
+          <label class="col-form-label"><b>{{ _i18n("date_from") }}</b></label>
+        </div>
+        <div class="col-sm-6">
+          <input class="form-control" type="text" :value="begin_time" disabled="">
+        </div>
       </div>
-      <div class="col-sm-6">
-        <input class="form-control" type="text" :value="begin_time" disabled="">
+      <div class="form-group row mb-2">
+        <div class="col-sm-6">
+          <label class="col-form-label"><b>{{ _i18n("date_from") }}</b></label>
+        </div>
+        <div class="col-sm-6 mt-1">
+          <input class="form-control" type="text" :value="end_time" disabled="">
+        </div>
       </div>
-    </div>
-    <div class="form-group row mb-2">
-      <div class="col-sm-6">
-        <label class="col-form-label"><b>{{_i18n("date_from")}}</b></label>
-      </div>
-      <div class="col-sm-6 mt-1">
-        <input class="form-control" type="text" :value="end_time" disabled="">
-      </div>
-    </div>
-    
-    <AlertInfo :no_close_button="true" ref="alert_info"></AlertInfo>    
-  </template><!-- modal-body -->
 
-  <template v-slot:footer>
-    <button type="button" @click="acknowledge_alerts" class="btn btn-primary">{{_i18n("acknowledge")}}</button>
-  </template>
-</modal>
+      <AlertInfo :no_close_button="true" ref="alert_info"></AlertInfo>
+    </template><!-- modal-body -->
+
+    <template v-slot:footer>
+      <button type="button" @click="acknowledge_alerts" class="btn btn-primary">{{ _i18n("acknowledge") }}</button>
+    </template>
+  </modal>
 </template>
 
 <script setup>
@@ -39,8 +39,8 @@ import { ntopng_utility, ntopng_url_manager, ntopng_events_manager, ntopng_event
 import filtersManager from "../utilities/filters-manager.js";
 
 const props = defineProps({
-    page: String,
-    context: Object,    
+  page: String,
+  context: Object,
 });
 const emit = defineEmits(["acknowledge_alerts"]);
 
@@ -56,27 +56,31 @@ onMounted(() => {
 });
 
 const show = (status) => {
-    begin_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_begin * 1000);
-    end_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_end * 1000);
-    let message_body = _i18n("show_alerts.confirm_acknowledge_alerts");
-    alert_info.value.show(message_body, "alert-danger");
-    modal_id.value.show();
+  begin_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_begin * 1000);
+  end_time.value = ntopng_utility.from_utc_to_server_date_format(status.epoch_end * 1000);
+  let message_body = _i18n("show_alerts.confirm_acknowledge_alerts");
+  alert_info.value.show(message_body, "alert-danger");
+  modal_id.value.show();
 };
 
 async function acknowledge_alerts() {
-    const url = `${http_prefix}/lua/rest/v2/acknowledge/${props.page}/alerts.lua`;
-    const url_params_obj = ntopng_url_manager.get_url_object();
-    const params = {
-	csrf: props.context.csrf,
-	...url_params_obj,
-    };
-    await ntopng_utility.http_post_request(url, params);
-    emit("acknowledge_alerts");
-    close();
+  let url = `${http_prefix}/lua/rest/v2/acknowledge/${props.page}/alerts.lua`;
+  /* The SNMP page is the only exception where the url totally changes */
+  if (props.page == 'snmp_device') {
+    url = `${http_prefix}/lua/pro/rest/v2/acknowledge/snmp/device/alerts.lua`
+  }
+  const url_params_obj = ntopng_url_manager.get_url_object();
+  const params = {
+    csrf: props.context.csrf,
+    ...url_params_obj,
+  };
+  await ntopng_utility.http_post_request(url, params);
+  emit("acknowledge_alerts");
+  close();
 }
 
 const close = () => {
-    modal_id.value.close();
+  modal_id.value.close();
 };
 
 defineExpose({ show, close });
