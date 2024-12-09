96b989371fcffbc57c825b3929793c574ce9663b
Matteo Biscosi <biscosi@ntop.org>
2024-12-09 09:57:18 +0100

    Hourly periodic activities not correctly executed

--- src/PeriodicActivities.cpp
+++ src/PeriodicActivities.cpp
@@ -99,11 +99,11 @@ void PeriodicActivities::run() {
     for (u_int16_t i = 0; i < num_sec_min_activities; i++)
       sec_min_activities[i]->schedule(this, now, false);
 
-    if(num_running_hour_day_activities == 0) {
+    //if(num_running_hour_day_activities == 0) {
       /* At most only one concurrent activity */
-      for (u_int16_t i = 0; i < num_hour_day_activities; i++)
-	hour_day_activities[i]->schedule(this, now, true);
-    }
+    for (u_int16_t i = 0; i < num_hour_day_activities; i++)
+	    hour_day_activities[i]->schedule(this, now, true);
+    //}
     
     sleep(1);
   }
