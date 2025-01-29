b4311233a70809d7e91abeba7f4d508f3da35196
Matteo Biscosi <biscosi@ntop.org>
2025-01-29 16:05:41 +0100

    Fixes modbus unexpected function code not working as expected

--- include/ntop_defines.h
+++ include/ntop_defines.h
@@ -1588,6 +1588,8 @@ extern NtopngLuaContext* getUserdata(struct lua_State *vm);
   "ntopng.checks.modbus_invalid_transition_enabled"
 #define CHECKS_MODBUS_TOO_MANY_EXCEPTIONS \
   "ntopng.checks.modbus_too_many_exceptions"
+#define CHECKS_MODBUS_UNEXPECTED_FUNCTION_CODE \
+  "ntopng.checks.modbus_unexpected_function_code_enabled"
 
 #define CUSTOM_FLOW_NDPI_SCRIPT \
   "scripts/callbacks/checks/flows/custom_flow_protocol_detected_script.lua"
