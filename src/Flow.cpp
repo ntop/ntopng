4d0353d09e85ea143e0caa42b0ff6d5c4f98203f
Matteo Biscosi <biscosi@ntop.org>
2025-02-07 11:08:06 +0100

    Added server/client tcp flags to syslog

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -3958,6 +3958,12 @@ void Flow::formatGenericFlow(json_object *my_object) {
     json_object_object_add(my_object,
 			   Utils::jsonLabel(TCP_FLAGS, "TCP_FLAGS", jsonbuf, sizeof(jsonbuf)),
 			   json_object_new_int(src2dst_tcp_flags | dst2src_tcp_flags));
+    json_object_object_add(my_object,
+                Utils::jsonLabel(TCP_FLAGS, "CLIENT_TCP_FLAGS", jsonbuf, sizeof(jsonbuf)),
+                json_object_new_int(tcp->src2dst_tcp_flags));
+    json_object_object_add(my_object,
+                Utils::jsonLabel(TCP_FLAGS, "SERVER_TCP_FLAGS", jsonbuf, sizeof(jsonbuf)),
+                json_object_new_int(tcp->dst2src_tcp_flags));
 
     json_object_object_add(my_object,
                            Utils::jsonLabel(TCP_FLAGS, "IN_RETRANSMISSIONS",
