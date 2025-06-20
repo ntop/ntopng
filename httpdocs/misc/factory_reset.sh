b2571c679a9daace4aa20f4677e28752f31c4627
manuelceroni <manuel.ceroni@protonmail.com>
2025-06-20 11:37:28 +0200

    Added options for CH to the factory reset script and fixed Redis reset

--- httpdocs/misc/factory_reset.sh
+++ httpdocs/misc/factory_reset.sh
@@ -9,20 +9,75 @@ echo "Removing contents of /var/lib/ntopng/..."
 rm -rf /var/lib/ntopng/*
 echo "ntopng data removed."
 
-echo "Flushing Redis..."
-redis-cli flushall
-echo "Redis flushed."
+NTOPNG_KEYS=$(redis-cli --raw keys "ntopng.*" | wc -l)
+if [ "$NTOPNG_KEYS" -gt 1 ]; then
+  echo "Flushing Redis ($NTOPNG_KEYS keys)..."
+  redis-cli --raw keys "ntopng.*" | xargs -n 100 redis-cli del >/dev/nul
+  echo "Redis flushed."
+else
+  echo "No ntopng-related keys found in Redis. Skipping flush."
+fi
 
 check_clickhouse_installed() {
     command -v clickhouse-client >/dev/null 2>&1
 }
 
-if check_clickhouse_installed; then
-    echo "Dropping ntopng database from ClickHouse..."
-    clickhouse-client --query="DROP DATABASE IF EXISTS ntopng"
-    echo "ClickHouse database ntopng dropped."
-else
-    echo "ClickHouse is not installed."
-fi
+CLICKHOUSE=0
+HOST=""
+USER=""
+PASS=""
 
-echo "All operations completed."
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -c|--clickhouse)
+      CLICKHOUSE=1
+      shift
+      ;;
+    -h|--host)
+      HOST="$2"
+      shift 2
+      ;;
+    -u|--user)
+      USER="$2"
+      shift 2
+      ;;
+    -p|--password)
+      PASS="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$CLICKHOUSE" -eq 1 ]; then
+    if check_clickhouse_installed; then
+
+        echo "Dropping ntopng database from ClickHouse..."
+        CMD=("clickhouse-client")
+        if [ -n "$HOST" ]; then
+            CMD+=(--host="$HOST")
+        fi
+        if [ -n "$USER" ]; then
+            CMD+=(--user="$USER")
+        fi
+        if [ -n "$PASS" ]; then
+            CMD+=(--password="$PASS")
+        fi
+        CMD+=(--query="DROP DATABASE IF EXISTS ntopng")
+
+        "${CMD[@]}"
+
+        if [ $? -eq 0 ]; then
+            echo "ClickHouse database ntopng dropped."
+        else
+            echo "Failed to drop ClickHouse database."
+        fi
+    else
+        echo "ClickHouse is not installed."
+    fi
+else
+    echo "Skipping ClickHouse operations."
+fi
