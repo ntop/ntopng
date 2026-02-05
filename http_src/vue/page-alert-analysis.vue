594a0e1aea00c4ad45fe1efe8252969705ad1d6f
Matteo Biscosi <biscosi@ntop.org>
2026-02-05 12:24:35 +0100

    Fixes incorrectly used ifid

--- http_src/vue/page-alert-analysis.vue
+++ http_src/vue/page-alert-analysis.vue
@@ -1,50 +1,43 @@
 <!-- (C) 2022 - ntop.org     -->
 
 <template>
-<div class="row">
-  <div class="col-12">
-    <div class="card">
-      <div class="card-body">
-        <div class='align-items-center justify-content-end mb-3' style='height: 70vh;'>
-          <div class="d-flex ms-auto flex-row-reverse">
-            <div>
-              <label class="my-auto me-1"></label>
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-body">
+          <div class='align-items-center justify-content-end mb-3' style='height: 70vh;'>
+            <div class="d-flex ms-auto flex-row-reverse">
               <div>
-                <button class="btn btn-link m-1" tabindex="0" type="button" @click="reload">
-                  <span><i class="fas fa-sync"></i></span>
-                </button>
-              </div>
-            </div>
-            <template v-for="(value, key, index) in available_filters">
-              <div class="m-1" v-if="value.length > 0">
-                <div style="min-width: 14rem;">
-                  <label class="my-auto me-1">{{ _i18n('bubble_map.' + key) }}: </label>
-                  <SelectSearch
-                    v-model:selected_option="active_filter_list[key]"
-                    :options="value"
-                    @select_option="click_item">
-                  </SelectSearch>
+                <label class="my-auto me-1"></label>
+                <div>
+                  <button class="btn btn-link m-1" tabindex="0" type="button" @click="reload">
+                    <span><i class="fas fa-sync"></i></span>
+                  </button>
                 </div>
               </div>
-            </template>
-          </div>
-          <Loading :isLoading="loading"></Loading>
-          <div :id="widget_name" style="height: 90%;" :class="[ loading ? 'ntopng-gray-out' : '' ]">
-            <Chart
-              ref="bubble_chart"
-              :id="widget_name"
-              :chart_type="chart_type"
-              :base_url_request="rest_url"
-              :get_params_url_request="format_request"
-              :get_custom_chart_options="get_f_get_custom_chart_options()"
-              :register_on_status_change="false">
-            </Chart>
+              <template v-for="(value, key, index) in props.context.available_filters">
+                <div class="m-1" v-if="value.length > 0">
+                  <div style="min-width: 14rem;">
+                    <label class="my-auto me-1">{{ _i18n('bubble_map.' + key) }}: </label>
+                    <SelectSearch v-model:selected_option="active_filter_list[key]" :options="value"
+                      @select_option="click_item">
+                    </SelectSearch>
+                  </div>
+                </div>
+              </template>
+            </div>
+            <Loading :isLoading="loading"></Loading>
+            <div :id="widget_name" style="height: 90%;" :class="[loading ? 'ntopng-gray-out' : '']">
+              <Chart ref="bubble_chart" :id="widget_name" :chart_type="chart_type" :base_url_request="rest_url"
+                :get_params_url_request="format_request" :get_custom_chart_options="get_f_get_custom_chart_options()"
+                :register_on_status_change="false">
+              </Chart>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script setup>
@@ -57,10 +50,7 @@ import NtopUtils from "../utilities/ntop-utils";
 
 const _i18n = (t) => i18n(t);
 const props = defineProps({
-  ifid: String,
-  page_csrf: String,
-  charts_options: Array,
-  available_filters: Object,
+  context: Object
 })
 
 const loading = ref(true);
@@ -70,42 +60,40 @@ const widget_name = 'alerts-map';
 const active_filter_list = {}
 const bubble_chart = ref(null)
 
-const format_request = function() {
-  let params_string = ''
-  const params = {
-    bubble_mode: ntopng_url_manager.get_url_entry('bubble_mode'),
-    timeframe: ntopng_url_manager.get_url_entry('timeframe'),
-    vlan: ntopng_url_manager.get_url_entry('vlan'),
-    ifid: ntopng_url_manager.get_url_entry('ifid'),
-  }
+/* ***************************************************** */
 
-  for (const [key, value] of Object.entries(params)) {
-    params_string = `${params_string}&${key}=${value}`
-  }
+const get_extra_params_obj = () => {
+  const extra_params = ntopng_url_manager.get_url_object();
+  return extra_params;
+};
 
-  /* Return a custom string, containing custom options */
-  return params_string
+/* ***************************************************** */
+
+const format_request = function () {
+  return ntopng_url_manager.obj_to_url_params(get_extra_params_obj());
 }
 
-const reload = function() {
+/* ***************************************************** */
+
+const reload = function () {
   loading.value = true;
   bubble_chart.value.update_chart(`${rest_url}?${format_request()}`)
   loading.value = false;
 }
 
-const format_options = function(mode_id) {
+const format_options = function (mode_id) {
   let options = {}
 
-  props.charts_options.forEach((option_list) => {
-    if(option_list.mode_id == mode_id)
+  props.context.charts_options.forEach((option_list) => {
+    if (option_list.mode_id == mode_id)
       options = option_list;
   })
 
   /* Add the correct event functions */
-  if(options && options.chart && options.chart.ntop_events) {
+  if (options && options.chart && options.chart.ntop_events) {
     options.chart.events = options.chart.events || {}
-    for(const [event, fun] of Object.entries(options.chart.ntop_events)) {
-      if(fun == undefined) 
+    for (const [event, fun] of Object.entries(options.chart.ntop_events)) {
+      if (fun == undefined)
         continue
 
       options.chart.events[event] = NtopUtils[fun] || NtopUtils.fnone
@@ -113,24 +101,24 @@ const format_options = function(mode_id) {
   }
 
   /* Add the correct formatting function, given from the backend */
-  if(options && options.xaxis && options.xaxis.labels && options.xaxis.labels.ntop_utils_formatter) {
+  if (options && options.xaxis && options.xaxis.labels && options.xaxis.labels.ntop_utils_formatter) {
     options.xaxis.labels.formatter = NtopUtils[options.xaxis.labels.ntop_utils_formatter] || NtopUtils.fnone
   }
 
   /* Add the correct formatting function, given from the backend */
-  if(options && options.yaxis && options.yaxis.labels && options.yaxis.labels.ntop_utils_formatter) {
+  if (options && options.yaxis && options.yaxis.labels && options.yaxis.labels.ntop_utils_formatter) {
     options.yaxis.labels.formatter = NtopUtils[options.yaxis.labels.ntop_utils_formatter] || NtopUtils.fnone
   }
 
   /* Add the correct formatting function, given from the backend */
-  if(options && options.tooltip && options.tooltip.ntop_utils_formatter)
+  if (options && options.tooltip && options.tooltip.ntop_utils_formatter)
     options.tooltip.custom = NtopUtils[options.tooltip.ntop_utils_formatter]
 
   return options
 }
 
-const get_f_get_custom_chart_options = function() {
-  
+const get_f_get_custom_chart_options = function () {
+
   /* Return the list of formatted options of the chart */
   return async (url) => {
     let options = format_options(Number(active_filter_list['bubble_mode'].id))
@@ -140,7 +128,7 @@ const get_f_get_custom_chart_options = function() {
   }
 }
 
-const click_item = function(item) {
+const click_item = function (item) {
   loading.value = true;
   ntopng_url_manager.set_key_to_url(item.filter_name, item.id)
   bubble_chart.value.update_chart(`${rest_url}?${format_request()}`)
@@ -152,17 +140,17 @@ onBeforeMount(() => {
   const timeframe = ntopng_url_manager.get_url_entry('timeframe');
   const vlan = ntopng_url_manager.get_url_entry('vlan');
   const bubble_mode = ntopng_url_manager.get_url_entry('bubble_mode');
-  
-  if(!bubble_mode) ntopng_url_manager.set_key_to_url('bubble_mode', 0) /* First Entry */
-  if(!timeframe) ntopng_url_manager.set_key_to_url('timeframe', 300) /* Default 5 min */
-  if(!vlan) ntopng_url_manager.set_key_to_url('vlan', '') /* Default no vlan */
-  
-  ntopng_url_manager.set_key_to_url('ifid', props.ifid) /* Current interface */
 
-  for(const [name, filters] of Object.entries(props.available_filters)) {
+  if (!bubble_mode) ntopng_url_manager.set_key_to_url('bubble_mode', 0) /* First Entry */
+  if (!timeframe) ntopng_url_manager.set_key_to_url('timeframe', 3600) /* Default 5 min */
+  if (!vlan) ntopng_url_manager.set_key_to_url('vlan', '') /* Default no vlan */
+
+  ntopng_url_manager.set_key_to_url('ifid', props.context.ifid) /* Current interface */
+
+  for (const [name, filters] of Object.entries(props.context.available_filters)) {
     filters.forEach((filter) => {
       filter.filter_name = name
-      if(filter.currently_active)
+      if (filter.currently_active)
         active_filter_list[name] = filter;
     })
   }
