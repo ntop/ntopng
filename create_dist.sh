7f8af98ddac611f636640733d24555a0bbd4b78a
DGabri <gabriele.deri@gmail.com>
2026-03-31 16:12:00 +0000

    Fixes create_dist

--- create_dist.sh
+++ create_dist.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
-#
-# In case you have never used npm do
-#
-# npm install
-#
-
 CURR_DIR=$(pwd)
 
-branch_name=`git branch | head | cut -d ' ' -f 2 | tail -n 1`
-
-echo "-- Cleaning up dist -- "
+branch_name=$(git branch --show-current)
+echo "Dist Branch: $branch_name"
+echo "-- Cleaning up dist --"
 cd httpdocs/dist
 git fetch
-git checkout $branch_name
-git reset --hard @{u}
+git checkout -B "$branch_name" "origin/$branch_name"
 
-echo "-- Compiling dist -- "
-cd $CURR_DIR
+echo "-- Compiling dist --"
+cd "$CURR_DIR"
 npm run build || exit 1
 
 echo "-- Pushing dist --"
 cd httpdocs/dist
-git add *
+git add -A
 git commit -m 'Update dist' || exit 1
-git push || exit 1
+git push origin "$branch_name" || exit 1
 
 echo "-- Pushing ref --"
-cd $CURR_DIR
+cd "$CURR_DIR"
 git add httpdocs/dist
 git commit -m 'Update dist' || exit 1
 git push || exit 1
-
-echo "Dist up to date"
