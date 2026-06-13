3e0ab5db739da013c51551cc4a0e87063616868e
Luca Deri <deri@ntop.org>
2026-06-13 23:18:16 +0200

    Compilation fix

--- src/LuaEngineNtop.cpp
+++ src/LuaEngineNtop.cpp
@@ -8256,7 +8256,7 @@ static int ntop_alloc_mem(lua_State* vm) {
   void *p;
   
   if (ntop_lua_check(vm, __FUNCTION__, 1, LUA_TNUMBER) != CONST_LUA_OK)
-    return (ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_NO_RETURN_VALUE));
+    return (ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_ERROR)); 
   new_size = lua_tonumber(vm, 1);
 
   if(new_size > size) size = new_size;
@@ -8266,7 +8266,7 @@ static int ntop_alloc_mem(lua_State* vm) {
     ntop->getTrace()->traceEvent(TRACE_INFO, "WARNING: allocatin failure");
 
   lua_pushnil(vm);
-  return (ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_ONE_RETURN_VALUE));
+  return (ntop_lua_return_value(vm, __FUNCTION__, CONST_LUA_OK));
 }
 
 /* **************************************************************** */
