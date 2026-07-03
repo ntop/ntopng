8ce10f00940c23838c7ec8506d08f234cce5fc7a
manuelceroni <manuel.ceroni@protonmail.com>
2026-07-03 10:36:05 +0200

    Implemented duplicated flow dump preference

--- scripts/lua/modules/http_lint.lua
+++ scripts/lua/modules/http_lint.lua
@@ -2594,6 +2594,7 @@ local known_parameters = {
    ["toggle_fingerprint_stats"] = validateBool,
    ["toggle_starttls"] = validateBool,
    ["toggle_dump_pcap_to_clickhouse"] = validateBool,
+   ["toggle_dump_duplicated_flows_to_clickhouse"] = validateBool,
    ["toggle_query_performance_log"] = validateBool,
    ["behaviour_analysis_learning_period"] = validateNumber,
    ["behaviour_analysis_learning_status_during_learning"] = validateNumber,
