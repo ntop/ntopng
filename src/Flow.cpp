c66e8b153632b4e42c843771dfc28c403335ed9e
Luca Deri <deri@ntop.org>
2026-07-30 07:39:56 +0200

    Fix for view interfaces

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -2376,13 +2376,13 @@ void Flow::hosts_periodic_stats_update(NetworkInterface* iface, Host* cli_host,
                        *srv_as = srv_host ? srv_host->get_as() : NULL;
 
       if (cli_as)
-        cli_as->incStats(get_cli_host()->isIPv4(),
+        cli_as->incStats(get_cli_ip_addr()->isIPv4(),
 			 tv->tv_sec, stats_protocol, diff_sent_packets,
                          diff_sent_bytes, diff_rcvd_packets, diff_rcvd_bytes,
                          getFlowDeviceIP(), getFlowDeviceInIndex(),
                          getFlowDeviceOutIndex());
       if (srv_as)
-        srv_as->incStats(get_cli_host()->isIPv4(),
+        srv_as->incStats(get_cli_ip_addr()->isIPv4(),
 			 tv->tv_sec, stats_protocol, diff_rcvd_packets,
                          diff_rcvd_bytes, diff_sent_packets, diff_sent_bytes,
                          getFlowDeviceIP(), getFlowDeviceInIndex(),
