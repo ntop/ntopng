c3eec094ae5626069914d072e533fac44718edb4
Matteo Biscosi <biscosi@ntop.org>
2026-08-07 08:49:46 +0000

    Reverted logout to non-view

--- scripts/lua/ntopng_logout.lua
+++ scripts/lua/ntopng_logout.lua
@@ -1,7 +1,33 @@
 --
+-- (C) 2013-26 - ntop.org
+--
+
+dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+
+require "lua_utils"
+local debug = false
+
+sendHTTPHeaderLogout('text/html')
+
+ntop.delCache("ntopng.cache.sessions.".._SESSION["session"])
+if (debug) then io.write("Deleting ".."ntopng.cache.sessions.".._SESSION["session"].."\n") end
+
+print [[
+<meta http-equiv="refresh" content="1; URL=]] print(ntop.getHttpPrefix()) print [[/">
+<html>
+<body>
+ ]] print(i18n("login.logging_out")) print[[
+</body>
+</html>
+
+]]
+
+--
 -- (C) 2026 - ntop.org
 --
 
+--[[
 dirs = ntop.getDirs()
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 
@@ -13,8 +39,6 @@ local template_utils = require "template_utils"
 sendHTTPHeaderLogout('text/html')
 
 ntop.delCache("ntopng.cache.sessions.".._SESSION["session"])
-
-page_utils.print_header_minimal(i18n("login.logging_out"))
 
 local context = {
   redirect_url = ntop.getHttpPrefix() .. "/",
