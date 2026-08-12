8e07cd2bcd084ed1ecb02029e0f2434afef5439e
Alfredo Cardigliano <cardigliano@ntop.org>
2026-08-12 15:10:11 +0200

    Script improvements

--- create_dist.sh
+++ create_dist.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 CURR_DIR=$(pwd)
+CURR_BRANCH=$(git branch --show-current)
 
-branch_name=$(git branch --show-current)
-echo "Dist Branch: $branch_name"
+echo "Current Branch: $CURR_BRANCH"
+
 echo "-- Cleaning up dist --"
 cd httpdocs/dist
-git fetch
-git checkout -B "$branch_name" "origin/$branch_name"
+git reset --hard HEAD
+git pull
+git checkout -B "$CURR_BRANCH" "origin/$CURR_BRANCH"
 
 echo "-- Compiling dist --"
 cd "$CURR_DIR/pro"
@@ -17,7 +19,7 @@ echo "-- Pushing dist --"
 cd "$CURR_DIR/httpdocs/dist"
 git add -A
 git commit -m 'Update dist' || exit 1
-git push origin "$branch_name" || exit 1
+git push origin "$CURR_BRANCH" || exit 1
 
 echo "-- Pushing ref --"
 cd "$CURR_DIR"
