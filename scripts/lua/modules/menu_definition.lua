66e0a37bcbc42da1dd048d8b3b7c7b098e8f2875
Matteo Biscosi <biscosi@ntop.org>
2026-08-14 13:53:34 +0000

    Moved license page to pro

--- scripts/lua/modules/menu_definition.lua
+++ scripts/lua/modules/menu_definition.lua
@@ -403,12 +403,6 @@ return function(f)
             hidden = f.no_admin,
             reason = { reason("perm", "menu.reason.no_admin", "menu.suggestion.no_admin") }
         }, {
-            key = "handle_licenses",
-            i18n = "about.handle_licenses.retrieve_licenses",
-            icon = "fa-solid fa-receipt",
-            url = "/lua/handle_licenses.lua",
-            hidden = f.no_admin
-        }, {
             key = "divider",
             i18n = "menu_group.community",
             is_divider = true
