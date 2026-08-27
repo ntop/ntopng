43a824e8293808be504bd3e84bb77971b30c970e
DGabri <gabriele.deri@gmail.com>
2026-08-27 10:18:39 +0200

    Added ndpi missing flow checks mapping

---
+++ scripts/lua/modules/check_definitions/flow/ndpi_tcp_issues.lua
@@ -0,0 +1,28 @@
+--
+-- (C) 2019-26 - ntop.org
+--
+
+local checks = require("checks")
+local flow_alert_keys = require "flow_alert_keys"
+
+-- #################################################################
+
+local script = {
+   -- Script category
+   category = checks.check_categories.network,
+
+   -- This script is only for alerts generation
+   alert_id = flow_alert_keys.flow_alert_ndpi_tcp_issues,
+
+   default_value = {
+   },
+
+   gui = {
+      i18n_title = "flow_risk.ndpi_tcp_issues",
+      i18n_description = "flow_risk.ndpi_tcp_issues_descr",
+   }
+}
+
+-- #################################################################
+
+return script
