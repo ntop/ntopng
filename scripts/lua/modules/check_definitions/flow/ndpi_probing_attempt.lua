8bea9c80fa828c350c0169fc526505c867594173
GabrieleDeri <33870399+DGabri@users.noreply.github.com>
2026-08-27 10:42:45 +0200

    Added ndpi missing flow checks mapping (#10841)
    Fixes href to checks from live flows
    Added ndpi missing flow checks mapping
    Update dist

---
+++ scripts/lua/modules/check_definitions/flow/ndpi_probing_attempt.lua
@@ -0,0 +1,28 @@
+--
+-- (C) 2019-26 - ntop.org
+--
+
+local checks = require("checks")
+local flow_alert_keys = require "flow_alert_keys"
+
+-- #################################################################
+
+local script = {
+   -- Script category
+   category = checks.check_categories.security,
+
+   -- This script is only for alerts generation
+   alert_id = flow_alert_keys.flow_alert_ndpi_probing_attempt,
+
+   default_value = {
+   },
+
+   gui = {
+      i18n_title = "flow_risk.ndpi_probing_attempt",
+      i18n_description = "flow_risk.ndpi_probing_attempt_descr",
+   }
+}
+
+-- #################################################################
+
+return script
