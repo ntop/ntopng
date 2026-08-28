4821a9be97afb04fe2ea24e3b7ec71e530e2d8a9
manuelceroni <manuel.ceroni@protonmail.com>
2026-08-28 12:52:06 +0200

    Fixed recipient editing

--- scripts/lua/edit_notification_recipient.lua
+++ scripts/lua/edit_notification_recipient.lua
@@ -39,7 +39,7 @@ local categories = recipients_rest_utils.parse_check_categories(_POST["recipient
 local entities = recipients_rest_utils.parse_check_entities(_POST["recipient_check_entities"])
 local host_pools_ids = recipients_rest_utils.parse_host_pools(_POST["recipient_host_pools"])
 local am_hosts_ids = recipients_rest_utils.parse_am_hosts(_POST["recipient_am_hosts"])
-local tags_ids = recipients_rest_utils.parse_tags(_POST["recipient_tags"])
+local tags_ids = recipients_rest_utils.parse_tags(_POST["recipient_labels"])
 local minimum_severity = recipients_rest_utils.parse_minimum_severity(_POST["recipient_minimum_severity"])
 
 if (action == "add") then
