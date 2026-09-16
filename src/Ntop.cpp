ad143a0215c5da77f0690b026c8a59c98cc8dc0e
Alfredo Cardigliano <cardigliano@ntop.org>
2026-09-16 10:37:03 +0200

    Move call to purgeQueuedIdleEntries from (global) Ntop::runPeriodicHousekeepingTasks to NetworkInterface::purgeIdle to avoid single-thread bottleneck

--- src/Ntop.cpp
+++ src/Ntop.cpp
@@ -4371,7 +4371,6 @@ void Ntop::runPeriodicHousekeepingTasks() {
   for (int i = 0; i < get_num_interfaces(); i++) {
     if (!iface[i]->isStartingUp()) {
       iface[i]->runPeriodicHousekeepingTasks();
-      iface[i]->purgeQueuedIdleEntries();
     }
   }
 
